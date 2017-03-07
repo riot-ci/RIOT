88f20e5a62350b742244f299d5c165cb7e903f2f
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-03-07 11:32:14 +0100

    sys/auto_init: add saul adaptation for vcnl40x0

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -392,6 +392,10 @@ auto_init_mpu9150();
     extern void auto_init_tcs37727(void);
     auto_init_tcs37727();
 #endif
+#ifdef MODULE_VCNL40X0
+    extern void auto_init_vcnl40x0(void);
+    auto_init_vcnl40x0();
+#endif
 #ifdef MODULE_VEML6070
     extern void auto_init_veml6070(void);
     auto_init_veml6070();
