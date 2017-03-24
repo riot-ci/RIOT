7c9f8df158a8be7cab4adf223c56805c1d89991a
Kaspar Schleiser <kaspar@schleiser.de>
2017-03-24 14:40:38 +0100

    murdock: add support for running tests on hardware

--- makefiles/murdock.inc.mk
+++ makefiles/murdock.inc.mk
@@ -1,6 +1,16 @@
-test-murdock: link
+FLASHFILE:=$(filter $(HEXFILE) $(BINFILE) $(ELFFILE),$(FFLAGS))
+test-murdock:
 	cd $(RIOTBASE) && \
-		./.murdock make_test_job \
+		./.murdock test_job \
 		$$(realpath --relative-to $(RIOTBASE) $(APPDIR)) \
 		$(BOARD) \
-		$(HEXFILE)
+		$(FLASHFILE)
+
+# create $(BINDIR)/.test file only if BOARD is in $(TEST_WHITELIST)
+link: $(BINDIR)/.test
+$(BINDIR)/.test: $(filter clean, $(MAKECMDGOALS))
+ifneq (,$(filter $(BOARD), $(TEST_WHITELIST)))
+	$(Q)touch $@
+else
+	$(Q)true
+endif
