05d16431a59b3940f2c5acf74916387c203dce0a
Kaspar Schleiser <kaspar@schleiser.de>
2017-03-24 14:40:38 +0100

    murdock: add support for running tests on hardware

--- makefiles/murdock.inc.mk
+++ makefiles/murdock.inc.mk
@@ -1,6 +1,29 @@
-test-murdock: link
+#
+# This file contains helper targets used by the CI.
+#
+
+# (HACK) get actual flash binary from FFLAGS.
+FLASHFILE:=$(filter $(HEXFILE) $(ELFFILE:.elf=.bin) $(ELFFILE),$(FFLAGS))
+
+#
+# This target will run "make test" on the CI cluster.
+#
+# In order to work, these requirements must be fulfilled:
+# - DWQ_REPO and DWQ_COMMIT are set correctly
+# - the user has set up autossh & proper authentication for connecting to the CI
+# (intended to be used by CI only for now)
+test-murdock:
 	cd $(RIOTBASE) && \
-		./.murdock make_test_job \
+		./.murdock test_job \
 		$$(realpath --relative-to $(RIOTBASE) $(APPDIR)) \
 		$(BOARD) \
-		$(HEXFILE)
+		$(FLASHFILE)
+
+# create $(BINDIR)/.test file only if BOARD is in $(TEST_WHITELIST)
+link: $(BINDIR)/.test
+$(BINDIR)/.test: $(filter clean, $(MAKECMDGOALS))
+ifneq (,$(filter $(BOARD) all, $(TEST_WHITELIST)))
+	$(Q)touch $@
+else
+	$(Q)true
+endif
