d448eff80ba9025db2b60ef6aeed588cc9a153b4
Aurelien Fillau <aurelien.fillau@gmail.com>
2017-05-30 06:36:11 +0200

    cpu/stm32l0: add i2c driver
    The driver for I2C has been implemented on STM32L0 family.
    For now, only I2C in 7 bits address polling mode is handled.
    NACK error and timeout are not yet handled. These features
    will be done in a next patch.
    
    Signed-off-by: Aurelien Fillau <aurelien.fillau@gmail.com>

--- boards/nucleo-l073/include/periph_conf.h
+++ boards/nucleo-l073/include/periph_conf.h
@@ -180,7 +180,7 @@ static const spi_conf_t spi_config[] = {
 
 /**
  * @name I2C configuration
-  * @{
+ * @{
  */
 #define I2C_0_EN            1
 #define I2C_1_EN            1
@@ -189,20 +189,38 @@ static const spi_conf_t spi_config[] = {
 #define I2C_APBCLK          (CLOCK_APB1)
 
 /* I2C 0 device configuration */
+#define I2C_0_DEV           I2C1
+#define I2C_0_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_I2C1EN))
+#define I2C_0_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_I2C1EN))
+#define I2C_0_EVT_IRQ       I2C1_IRQn
 #define I2C_0_EVT_ISR       isr_i2c1
-//AFU #define I2C_0_ERR_ISR       isr_i2c1_er
+/* I2C 0 pin configuration */
+#define I2C_0_SCL_PORT      PORT_B
+#define I2C_0_SCL_PIN       8
+#define I2C_0_SCL_AF        4
+#define I2C_0_SCL_CLKEN()   (periph_clk_en(AHB, RCC_IOPENR_GPIOBEN))
+#define I2C_0_SDA_PORT      PORT_B
+#define I2C_0_SDA_PIN       9
+#define I2C_0_SDA_AF        4
+#define I2C_0_SDA_CLKEN()   (periph_clk_en(AHB, RCC_IOPENR_GPIOBEN))
 
 /* I2C 1 device configuration */
-#define I2C_1_EVT_ISR       isr_i2c2
-//AFU #define I2C_1_ERR_ISR       isr_i2c2_er
+#define I2C_1_DEV           I2C3
+#define I2C_1_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_I2C3EN))
+#define I2C_1_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_I2C3EN))
+#define I2C_1_EVT_IRQ       I2C3_IRQn
+#define I2C_1_EVT_ISR       isr_i2c3
+/* I2C 1 pin configuration */
+#define I2C_1_SCL_PORT      PORT_A
+#define I2C_1_SCL_PIN       8
+#define I2C_1_SCL_AF        3
+#define I2C_1_SCL_CLKEN()   (periph_clk_en(AHB, RCC_IOPENR_GPIOAEN))
+#define I2C_1_SDA_PORT      PORT_B
+#define I2C_1_SDA_PIN       5
+#define I2C_1_SDA_AF        8
+#define I2C_1_SDA_CLKEN()   (periph_clk_en(AHB, RCC_IOPENR_GPIOBEN))
+/** @} */
 
-static const i2c_conf_t i2c_config[] = {
-    /* device, port, scl-, sda-pin-number, I2C-AF, ER-IRQn, EV-IRQn */
-    {I2C1, GPIO_PIN(PORT_B,  8), GPIO_PIN(PORT_B,  9), GPIO_OD_PU,
-     GPIO_AF4, I2C1_IRQn},
-    {I2C2, GPIO_PIN(PORT_B, 10), GPIO_PIN(PORT_B, 11), GPIO_OD_PU,
-     GPIO_AF4, I2C2_IRQn},
-};
 /** @} */
 
 /**
