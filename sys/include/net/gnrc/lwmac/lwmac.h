3b962b090e57cc366689d45e9eea3c599c4dafd0
zhuo shuguo <shuguo.zhuo@inria.fr>
2017-06-11 22:23:14 +0200

    move lwmac_init func to lwmac.h

--- sys/include/net/gnrc/lwmac/lwmac.h
+++ sys/include/net/gnrc/lwmac/lwmac.h
@@ -30,6 +30,9 @@
 extern "C" {
 #endif
 
+/* Foward declaration */
+typedef struct gnrc_netdev gnrc_netdev_t;
+
 /**
  * @brief Time between consecutive wake-ups.
  *
@@ -271,6 +274,25 @@ extern "C" {
 #ifndef LWMAC_TIMEOUT_COUNT
 #define LWMAC_TIMEOUT_COUNT             (3U)
 #endif
+
+/**
+ * @brief Initialize an instance of the LWMAC layer
+ *
+ * The initialization starts a new thread that connects to the given netdev
+ * device and starts a link layer event loop.
+ *
+ * @param[in] stack         stack for the control thread
+ * @param[in] stacksize     size of *stack*
+ * @param[in] priority      priority for the thread housing the LWMAC instance
+ * @param[in] name          name of the thread housing the LWMAC instance
+ * @param[in] dev           netdev device, needs to be already initialized
+ *
+ * @return                  PID of LWMAC thread on success
+ * @return                  -EINVAL if creation of thread fails
+ * @return                  -ENODEV if *dev* is invalid
+ */
+kernel_pid_t gnrc_lwmac_init(char *stack, int stacksize, char priority,
+                             const char *name, gnrc_netdev_t *dev);
 
 #ifdef __cplusplus
 }
