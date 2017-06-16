c96fbe36668501d622518585b82eace0f3ad2786
smlng <s@mlng.net>
2017-06-16 22:28:04 +0200

    fixup

--- sys/include/trickle.h
+++ sys/include/trickle.h
@@ -59,7 +59,7 @@ typedef struct {
 /**
  * @brief resets the trickle timer
  *
- * see https://tools.ietf.org/html/rfc6206#section-4.2, number 6
+ * @see https://tools.ietf.org/html/rfc6206#section-4.2, number 6
  *
  * @param[in] trickle   the trickle timer
  */
