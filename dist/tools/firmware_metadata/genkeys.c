4134d1a5ba96ee31cb4af1d535a5b4910c28a7fd
Kaspar Schleiser <kaspar@schleiser.de>
2017-06-16 15:16:36 +0200

    dist/tools: add firmware metadata tools

---
+++ dist/tools/firmware_metadata/genkeys.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "tweetnacl.h"
+
+const char genkeys_usage[] = "firmware genkeys <seckey-file> <pubkey-file>";
+int genkeys(int argc, const char *argv[])
+{
+    unsigned char pk[crypto_sign_PUBLICKEYBYTES];
+    unsigned char sk[crypto_sign_SECRETKEYBYTES];
+
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s\n", genkeys_usage);
+        return 1;
+    }
+
+    crypto_sign_keypair(pk,sk);
+
+    const char err[] = "error writing to %s\n";
+    if (!to_file(argv[2], sk, sizeof(sk))) {
+        fprintf(stderr, err, argv[2]);
+        return -1;
+    }
+
+    if (!to_file(argv[3], pk, sizeof(pk))) {
+        fprintf(stderr, err, argv[3]);
+        return -1;
+    }
+
+    return 0;
+}
