c6f395441cab4ba87ce14ba163a173c140575561
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-06-28 18:20:06 +0200

    fixup! drivers: initial implementation of sx1276 and sx1272 (LoRa modulation only)

--- drivers/sx127x/include/sx127x_netdev.h
+++ drivers/sx127x/include/sx127x_netdev.h
@@ -35,19 +35,11 @@ extern const netdev_driver_t sx127x_driver;
  * @brief   Received LoRa packet status information
  */
 typedef struct netdev_radio_lora_packet_info {
-    uint8_t rssi;       /**< RSSI of a received packet */
-    uint8_t lqi;        /**< LQI of a received packet */
-    int8_t snr;         /**< S/N ratio */
+    uint8_t rssi;           /**< RSSI of a received packet */
+    uint8_t lqi;            /**< LQI of a received packet */
+    int8_t snr;             /**< S/N ratio */
+    uint32_t time_on_air;   /**< Time on air of a received packet (us) */
 } netdev_sx127x_lora_packet_info_t;
-
-/**
- * @brief Received FSK packet status information
- */
-typedef struct netdev_radio_fsk_packet_info {
-    int8_t rssi;       /**< RSSI of a received packet */
-    int32_t afc;       /**< Automatic frequency control */
-    uint8_t gain;      /**< Gain */
-} netdev_sx127x_fsk_packet_info_t;
 
 #ifdef __cplusplus
 }
