c423c44c5e14eb925ebd2950cbedcb81b9f83e7d
kYc0o <fco.ja.ac@gmail.com>
2017-07-03 14:31:30 +0200

    fixup! makefiles/multislot.mk: drop else

--- makefiles/multislot.mk
+++ makefiles/multislot.mk
@@ -1,13 +1,12 @@
-ifneq (,$(filter mcuboot, $(MAKECMDGOALS)))
+ifneq (,$(filter mcuboot flash-mcuboot, $(MAKECMDGOALS)))
 IMGTOOL ?= $(RIOTBASE)/dist/tools/mcuboot/imgtool.py
 override IMGTOOL := $(abspath $(IMGTOOL))
 
 ifndef SLOT0_SIZE
 $(error Board $(BOARD) does not define multislot parameters!)
-else
-export FLASH_OFFSET := $(SLOT0_SIZE)
 endif
 
+export FLASH_OFFSET := $(SLOT0_SIZE)
 export BINFILE ?= $(BINDIR)/$(APPLICATION).bin
 export SIGN_BINFILE = $(BINDIR)/signed-$(APPLICATION).bin
 export OFLAGS = -O binary
@@ -23,13 +22,13 @@ mcuboot: create-key all
 	@$(COLOR_ECHO)
 	$(Q)$(_LINK) $(LINKFLAGPREFIX)--defsym=offset="$(SLOT0_SIZE)" \
 	$(LINKFLAGPREFIX)--defsym=length="$(SLOT1_SIZE)" \
-	$(LINKFLAGPREFIX)--defsym=image_header="$(IMAGE_HDR)" -o $(ELFFILE) && \
+	$(LINKFLAGPREFIX)--defsym=image_header="$(IMAGE_HDR_SIZE)" -o $(ELFFILE) && \
 	$(OBJCOPY) $(OFLAGS) $(ELFFILE) $(BINFILE) && \
 	$(IMGTOOL) sign --key key.pem --version $(IMAGE_VERSION) --align \
-	$(IMAGE_ALIGN) -H $(IMAGE_HDR) $(BINFILE) $(SIGN_BINFILE)
+	$(IMAGE_ALIGN) -H $(IMAGE_HDR_SIZE) $(BINFILE) $(SIGN_BINFILE)
 	@$(COLOR_ECHO)
 	@$(COLOR_ECHO) '${COLOR_PURPLE}Signed with key.pem for version $(IMAGE_VERSION)\
-	.${COLOR_RESET}'
+	${COLOR_RESET}'
 	@$(COLOR_ECHO)
 
 .PHONY: flash-mcuboot
