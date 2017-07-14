7927a2eb09c984ba63ca7fb56a427951157e6d22
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2017-07-14 09:06:01 +0200

    squash remove lpuart

--- boards/frdm-k22f/include/periph_conf.h
+++ boards/frdm-k22f/include/periph_conf.h
@@ -93,7 +93,6 @@ static const uart_conf_t uart_config[] = {
         .scgc_addr = &SIM_SCGC4,
         .scgc_bit = SIM_SCGC4_UART1_SHIFT,
         .mode   = UART_MODE_8N1,
-        .type   = KINETIS_UART,
     },
 };
 
