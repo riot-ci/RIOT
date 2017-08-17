0fc7ae50e31d54bdef8dddb46094a42df0aa8ef8
Koen Zandberg <koen@bergzand.net>
2017-08-17 16:07:58 +0200

    rpl: remove obsolete preferred parent reordering

--- sys/net/gnrc/routing/rpl/gnrc_rpl_dodag.c
+++ sys/net/gnrc/routing/rpl/gnrc_rpl_dodag.c
@@ -317,8 +317,6 @@ static gnrc_rpl_parent_t *_gnrc_rpl_find_preferred_parent(gnrc_rpl_dodag_t *doda
     }
 
     if (new_best != old_best) {
-        LL_DELETE(dodag->parents, new_best);
-        LL_PREPEND(dodag->parents, new_best);
         /* no-path DAOs only for the storing mode */
         if ((dodag->instance->mop == GNRC_RPL_MOP_STORING_MODE_NO_MC) ||
             (dodag->instance->mop == GNRC_RPL_MOP_STORING_MODE_MC)) {
