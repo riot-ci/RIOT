65f2f478ad4c42e1a4307d30117eabc66bfc9e5b
Vincent Dupont <vincent@otakeys.com>
2017-08-21 19:44:20 +0200

    wip: adapt nucleo144-f413 and nucleo-f401

--- boards/nucleo-f401/include/periph_conf.h
+++ boards/nucleo-f401/include/periph_conf.h
@@ -26,26 +26,35 @@ extern "C" {
 #endif
 
 /**
- * @name    Clock system configuration
+ * @name Clock settings
+ *
+ * @note This is auto-generated from
+ *       `cpu/stm32_common/dist/clk_conf/clk_conf.c`
  * @{
  */
-/* 0: no external high speed crystal available
- * else: actual crystal frequency [in Hz] */
-#define CLOCK_HSE           (8000000U)
-/* 0: no external low speed crystal available,
- * 1: external crystal available (always 32.768kHz) */
-#define CLOCK_LSE           (1)
 /* give the target core clock (HCLK) frequency [in Hz],
  * maximum: 84MHz */
-#define CLOCK_CORECLOCK     (84000000U)
+#define CLOCK_CORECLOCK      (84000000U)
+/* 0: no external high speed crystal available
+ * 1: use external high speed crystal */
+#define CLOCK_HSE            (1)
 /* peripheral clock setup */
-#define CLOCK_AHB_DIV       RCC_CFGR_HPRE_DIV1      /* min 25MHz */
-#define CLOCK_AHB           (CLOCK_CORECLOCK / 1)
-#define CLOCK_APB1_DIV      RCC_CFGR_PPRE1_DIV2     /* max 42MHz */
-#define CLOCK_APB1          (CLOCK_CORECLOCK / 2)
-#define CLOCK_APB2_DIV      RCC_CFGR_PPRE2_DIV1     /* max 84MHz */
-#define CLOCK_APB2          (CLOCK_CORECLOCK / 1)
+#define CLOCK_AHB_DIV        RCC_CFGR_HPRE_DIV1      /* min 25MHz */
+#define CLOCK_AHB            (CLOCK_CORECLOCK / 1)
+#define CLOCK_APB1_DIV       RCC_CFGR_PPRE1_DIV2     /* max 42MHz */
+#define CLOCK_APB1           (CLOCK_CORECLOCK / 2)
+#define CLOCK_APB2_DIV       RCC_CFGR_PPRE2_DIV1     /* max 84MHz */
+#define CLOCK_APB2           (CLOCK_CORECLOCK / 1)
+
+/* Main PLL factors */
+#define CLOCK_PLL_M          (4)
+#define CLOCK_PLL_N          (168)
+#define CLOCK_PLL_P          (4)
+#define CLOCK_PLL_Q          (7)
+
+#define CLOCK_LSE            (1)
 /** @} */
+
 
 /**
  * @name   Timer configuration
