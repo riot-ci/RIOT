4588c9e172700031966e4807a0dddd414356e33d
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 13:24:39 +0200

    make: defaultmodules: refactor, add periph_uart

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -2,14 +2,19 @@ DEFAULT_MODULE += board cpu core core_msg sys
 
 DEFAULT_MODULE += auto_init
 
+define use_periph_module_if_available =
+  ifneq (,$$(filter ${1},$(FEATURES_PROVIDED)))
+    DEFAULT_MODULE += ${1}
+  endif
+endef
+
 # all boards use gpio for leds
-ifneq (,$(filter periph_gpio,$(FEATURES_PROVIDED)))
-  DEFAULT_MODULE += periph_gpio
-endif
+$(eval $(call use_periph_module_if_available,periph_gpio))
 
 # include power management
-ifneq (,$(filter periph_pm,$(FEATURES_PROVIDED)))
-  DEFAULT_MODULE += periph_pm
-endif
+$(eval $(call use_periph_module_if_available,periph_pm))
+
+# include uart support
+$(eval $(call use_periph_module_if_available,periph_uart))
 
 USEMODULE += $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
