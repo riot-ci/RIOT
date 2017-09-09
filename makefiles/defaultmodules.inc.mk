734a7c11a49fd13dd20b7ca5ec763ff41412e872
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 13:03:52 +0200

    make: refactor feature processing, remove DEFAULT_/DISABLE_FEATURES

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -1,3 +1,15 @@
 DEFAULT_MODULE += board cpu core core_msg sys
 
 DEFAULT_MODULE += auto_init
+
+# all boards use gpio for leds
+ifneq (,$(filter periph_gpio,$(FEATURES_PROVIDED)))
+  DEFAULT_MODULE += periph_gpio
+endif
+
+# include power management
+ifneq (,$(filter periph_pm,$(FEATURES_PROVIDED)))
+  DEFAULT_MODULE += periph_pm
+endif
+
+USEMODULE += $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
