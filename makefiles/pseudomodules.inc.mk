3d17183db58bd737fcba784a54b0fd42ef9dad85
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 15:09:23 +0200

    fixup! fixup! make: add Makefile.periph

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -84,6 +84,6 @@ PSEUDOMODULES += sx1276
 # add all pseudo random number generator variants as pseudomodules
 PSEUDOMODULES += prng_%
 
-#
+# declare periph submodules as pseudomodules, but exclude periph_common
 PSEUDOMODULES += periph_%
 NO_PSEUDOMODULES += periph_common
