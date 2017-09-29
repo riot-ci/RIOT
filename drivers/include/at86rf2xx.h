16f50f35136a0abb23393aba802e950afe77ab5d
Koen Zandberg <koen@bergzand.net>
2017-09-29 11:27:07 +0200

    fixup! FIXUP! at86rf2xx: future proof frame retry operations

--- drivers/include/at86rf2xx.h
+++ drivers/include/at86rf2xx.h
@@ -167,8 +167,8 @@ typedef struct {
     uint8_t pending_tx;                 /**< keep track of pending TX calls
                                              this is required to know when to
                                              return to @ref at86rf2xx_t::idle_state */
-#if defined(MODULE_AT86RF232) || defined(MODULE_AT86RF233)
-    /* Only AT86RF232 and RF233 supports retransmission reporting */
+/* Only radios with the XAH_CTRL_2 register support frame retry reporting */
+#ifdef AT86RF2XX_XAH_CTRL_2__ARET_FRAME_RETRIES_OFFSET
     uint8_t tx_retries;                 /**< Number of NOACK retransmissions */
 #endif
     /** @} */
