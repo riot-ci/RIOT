ce8ed1bd1edc83c2169d08c364431133839e1801
Kaspar Schleiser <kaspar@schleiser.de>
2017-10-10 15:43:39 +0200

    cpu/sam0_common: add flashpage_write_raw()

--- cpu/sam0_common/periph/flashpage.c
+++ cpu/sam0_common/periph/flashpage.c
@@ -17,7 +17,7 @@
  * The sam0 has its flash memory organized in pages and rows, where each row
  * consists of 4 pages. While pages are writable one at a time, it is only
  * possible to delete a complete row. This implementation abstracts this
- * behavior by only writing complete rows at a time, so the FLASH_PAGE_SIZE we
+ * behavior by only writing complete rows at a time, so the FLASHPAGE_SIZE we
  * use in RIOT is actually the row size as specified in the datasheet.
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
@@ -25,18 +25,15 @@
  * @}
  */
 
+#include <assert.h>
+
 #include "cpu.h"
-#include "assert.h"
 #include "periph/flashpage.h"
 
 #define NVMCTRL_PAC_BIT     (0x00000002)
 
-void flashpage_write(int page, void *data)
+static void _unlock(void)
 {
-    assert(page < FLASHPAGE_NUMOF);
-
-    uint32_t *page_addr = (uint32_t *)flashpage_addr(page);
-
     /* remove peripheral access lock for the NVMCTRL peripheral */
 #ifdef CPU_FAM_SAML21
     PAC->WRCTRL.reg = (PAC_WRCTRL_KEY_CLR | ID_NVMCTRL);
@@ -45,6 +42,41 @@ void flashpage_write(int page, void *data)
         PAC1->WPCLR.reg = NVMCTRL_PAC_BIT;
     }
 #endif
+}
+
+void flashpage_write_raw(void *target_addr, void *data, size_t len)
+{
+    /* sam[dlr]21 can only write whole flash pages.
+     * Mind the use of the ATMEL FLASH_PAGE_SIZE (64B) define vs.
+     * FLASHPAGE_SIZE (256B) as defined by RIOT.
+     * The actual minimal block size for writing is 16B, thus we
+     * assert we don't write less than that.
+     */
+    assert(!(len % (FLASH_PAGE_SIZE / 4)));
+
+    /* ensure 4 byte aligned writes */
+    assert(!(((unsigned)target_addr & 0x3) || ((unsigned)data & 0x3)));
+
+    uint32_t *page_addr = (uint32_t *)target_addr;
+    uint32_t *data_addr = (uint32_t *)data;
+
+    /* write 4 bytes in one go */
+    len /= 4;
+
+    NVMCTRL->CTRLA.reg = (NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_PBC);
+    for (unsigned i = 0; i < len; i++) {
+        *page_addr++ = *data_addr++;
+    }
+    NVMCTRL->CTRLA.reg = (NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP);
+}
+
+void flashpage_write(int page, void *data)
+{
+    assert(page < FLASHPAGE_NUMOF);
+
+    uint32_t *page_addr = (uint32_t *)flashpage_addr(page);
+
+    _unlock();
 
     /* erase given page (the ADDR register uses 16-bit addresses) */
     NVMCTRL->ADDR.reg = (((uint32_t)page_addr) >> 1);
@@ -53,11 +85,6 @@ void flashpage_write(int page, void *data)
 
     /* write data to page */
     if (data != NULL) {
-        uint32_t *data_addr = (uint32_t *)data;
-        NVMCTRL->CTRLA.reg = (NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_PBC);
-        for (unsigned i = 0; i < (FLASHPAGE_SIZE / 4); i++) {
-            *page_addr++ = data_addr[i];
-        }
-        NVMCTRL->CTRLA.reg = (NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP);
+        flashpage_write_raw(page_addr, data, FLASHPAGE_SIZE);
     }
 }
