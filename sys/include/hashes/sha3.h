8f5b001d88bb3e3451e883ec575a5a154db0d30d
Mathias Tausig <mathias.tausig@fh-campuswien.ac.at>
2017-10-27 00:12:02 +0200

    Renamed state variable to ctx

--- sys/include/hashes/sha3.h
+++ sys/include/hashes/sha3.h
@@ -17,9 +17,13 @@ typedef struct {
   unsigned int blockSize;
 } keccak_state_t;
 
-void Keccak_init(keccak_state_t *state, unsigned int rate, unsigned int capacity, unsigned char delimitedSuffix);
-void Keccak_update(keccak_state_t *state, const unsigned char* input, unsigned long long int inputByteLen);
-void Keccak_final(keccak_state_t *state, unsigned char *output, unsigned long long int outputByteLen);
+void Keccak_init(keccak_state_t *ctx, unsigned int rate, unsigned int capacity, unsigned char delimitedSuffix);
+void Keccak_update(keccak_state_t *ctx, const unsigned char* input, unsigned long long int inputByteLen);
+void Keccak_final(keccak_state_t *ctx, unsigned char *output, unsigned long long int outputByteLen);
+
+void sha3_256_init(keccak_state_t *ctx);
+void sha3_update(keccak_state_t *ctx, const void *data, size_t len);
+void sha3_256_final(keccak_state_t *ctx, void *digest);
 
 void sha3_256(void *digest, const void *data, size_t len);
 void sha3_384(void *digest, const void *data, size_t len);
