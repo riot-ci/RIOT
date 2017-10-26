d694b856a4d836ee0a1a58090a118e5f59d51a61
Kaspar Schleiser <kaspar@schleiser.de>
2017-10-26 17:34:10 +0200

    fixup! tests/event: initial commit of sys/event test application

--- tests/events/main.c
+++ tests/events/main.c
@@ -25,35 +25,58 @@
 #include "event/timeout.h"
 #include "event/callback.h"
 
+static unsigned order;
+static uint32_t before;
+
+static void callback(event_t *arg);
+static void custom_callback(event_t *event);
+static void timed_callback(void *arg);
+
+
+static event_t event = { .handler=callback };
+static event_t event2 = { .handler=callback };
+
+static void callback(event_t *arg)
+{
+    order++;
+    assert(order == 1);
+    assert(arg == &event);
+    printf("triggered 0x%08x\n", (unsigned)arg);
+}
+
 typedef struct {
     event_t super;
     const char *text;
 } custom_event_t;
 
+static custom_event_t custom_event = { .super.handler=custom_callback, .text="CUSTOM CALLBACK" };
+static event_callback_t event_callback = EVENT_CALLBACK_INIT(timed_callback, 0x12345678);
+
 static void custom_callback(event_t *event)
 {
+    order++;
+    assert(order == 2);
+    assert(event == (event_t*)&custom_event);
     custom_event_t *custom_event = (custom_event_t *)event;
     printf("triggered custom event with text: \"%s\"\n", custom_event->text);
 }
 
-static void callback(event_t *event)
-{
-    printf("triggered 0x%08x\n", (unsigned)event);
-}
-
 static void timed_callback(void *arg)
 {
-    printf("triggered timed callback with arg 0x%08x\n", (unsigned)arg);
+    order++;
+    assert(order == 3);
+    assert(arg == event_callback.arg);
+    uint32_t now = xtimer_now_usec();
+    assert((now - before >= 1000000LU));
+    printf("triggered timed callback with arg 0x%08x after %"PRIu32"us\n", (unsigned)arg, now - before);
+    printf("[SUCCESS]\n");
 }
 
 int main(void)
 {
-    puts("event test application.\n");
+    puts("[START] event test application.\n");
 
     event_queue_t queue = { .waiter=(thread_t*)sched_active_thread };
-    event_t event = { .handler=callback };
-    event_t event2 = { .handler=callback };
-
     printf("posting 0x%08x\n", (unsigned)&event);
     event_post(&queue, &event);
 
@@ -63,14 +86,13 @@ int main(void)
     event_cancel(&queue, &event2);
 
     printf("posting custom event\n");
-    custom_event_t custom_event = { .super.handler=custom_callback, .text="CUSTOM CALLBACK" };
     event_post(&queue, (event_t *)&custom_event);
 
     event_timeout_t event_timeout;
-    event_callback_t event_callback = EVENT_CALLBACK_INIT(timed_callback, 0x12345678);
 
     printf("posting timed callback with timeout 1sec\n");
     event_timeout_init(&event_timeout, &queue, (event_t*)&event_callback);
+    before = xtimer_now_usec();
     event_timeout_set(&event_timeout, 1000000);
 
     printf("launching event queue\n");
