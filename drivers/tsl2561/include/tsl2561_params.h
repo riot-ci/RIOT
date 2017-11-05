514b23d98d16945e5fe1ca7f6edad516d76b2377
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-05 21:23:29 +0100

    drivers: boards: update and unify drivers params definition scheme

--- drivers/tsl2561/include/tsl2561_params.h
+++ drivers/tsl2561/include/tsl2561_params.h
@@ -44,10 +44,15 @@ extern "C" {
 #define TSL2561_PARAM_INTEGRATION     TSL2561_INTEGRATIONTIME_402MS
 #endif
 
-#define TSL2561_PARAMS_DEFAULT        { .i2c_dev     = TSL2561_PARAM_I2C_DEV, \
+#ifndef TSL2561_PARAMS
+#define TSL2561_PARAMS                { .i2c_dev     = TSL2561_PARAM_I2C_DEV, \
                                         .addr        = TSL2561_PARAM_ADDR,    \
                                         .gain        = TSL2561_PARAM_GAIN,    \
                                         .integration = TSL2561_PARAM_INTEGRATION }
+#endif
+#ifndef TSL2561_SAUL_INFO
+#define TSL2561_SAUL_INFO             { .name= "tsl2561" }
+#endif
 /**@}*/
 
 /**
@@ -55,11 +60,7 @@ extern "C" {
  */
 static const tsl2561_params_t tsl2561_params[] =
 {
-#ifdef TSL2561_PARAMS_CUSTOM
-    TSL2561_PARAMS_CUSTOM,
-#else
-    TSL2561_PARAMS_DEFAULT,
-#endif
+    TSL2561_PARAMS
 };
 
 /**
@@ -67,7 +68,7 @@ static const tsl2561_params_t tsl2561_params[] =
  */
 saul_reg_info_t tsl2561_saul_reg_info[] =
 {
-    { .name= "tsl2561" }
+    TSL2561_SAUL_INFO
 };
 
 #ifdef __cplusplus
