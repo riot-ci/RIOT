7fcaa72fb2154e64edad8f8d1b292947eeeb7742
Kaspar Schleiser <kaspar@schleiser.de>
2017-11-06 11:22:56 +0100

    fixup! make: add Makefile.periph

--- makefiles/periph.mk
+++ makefiles/periph.mk
@@ -9,7 +9,4 @@ SUBMODULES_NOFORCE := 1
 # in case of e.g., <cpu>_periph_common, use periph_* as submodule prefix
 BASE_MODULE := periph
 
-# periph_common matches the submodule selection, but shouldn't. So filter out here.
-USEMODULE := $(filter-out periph_common, $(USEMODULE))
-
 include $(RIOTBASE)/Makefile.base
