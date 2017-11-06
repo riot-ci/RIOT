538023bfbec54c669688d302283e5784bd7c1fbe
Kaspar Schleiser <kaspar@schleiser.de>
2017-11-06 13:58:11 +0100

    fixup! sys/event: initial commit of handler-based event system

--- sys/event/event.c
+++ sys/event/event.c
@@ -48,6 +48,7 @@ event_t *event_get(event_queue_t *queue)
 {
     unsigned state = irq_disable();
     event_t *result = (event_t *) clist_lpop(&queue->event_list);
+
     irq_restore(state);
     if (result) {
         result->list_node.next = NULL;
@@ -71,7 +72,8 @@ event_t *event_wait(event_queue_t *queue)
 void event_loop(event_queue_t *queue)
 {
     event_t *event;
-    while((event = event_wait(queue))) {
+
+    while ((event = event_wait(queue))) {
         event->handler(event);
     }
 }
