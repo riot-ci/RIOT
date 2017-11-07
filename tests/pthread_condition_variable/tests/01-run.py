fe34894cd2fa48e32ab1464c44a0da1b15d819c3
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-07 21:24:22 +0100

    fixup! tests/pthread_condition_variable: migrate to testrunner script

--- tests/pthread_condition_variable/tests/01-run.py
+++ tests/pthread_condition_variable/tests/01-run.py
@@ -14,4 +14,6 @@ def testfunc(child):
 
 
 if __name__ == "__main__":
+    # This test can take some time to complete when testing on hardware (e.g
+    # on samr21-xpro) and the default timeout (10s) is not enough.
     sys.exit(testrunner.run(testfunc, timeout=60))
