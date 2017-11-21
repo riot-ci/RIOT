7cb6127481d8ff646ee5dc9e3a190514c8f0df46
steffen <steffen.robertz@online.de>
2017-11-21 11:39:01 +0100

    tests/driver_lc709203f: fixed typos, adopted to changed driver

--- tests/driver_lc709203f/main.c
+++ tests/driver_lc709203f/main.c
@@ -9,12 +9,11 @@
 /**
  * @ingroup     tests
  * @{
- *
  * @file
  * @brief       test aplication for the LC709203F Battery fuel gauge by on semiconductors
  *
- * @author	Steffen Robertz <steffen.robertz@rwth-aachen.de>
- * @author	Josua Arndt <jarndt@ias.rwth-aachen.de>
+ * @author      Steffen Robertz <steffen.robertz@rwth-aachen.de>
+ * @author      Josua Arndt <jarndt@ias.rwth-aachen.de>
  * @}
  */
 
@@ -22,10 +21,7 @@
 #include "periph_conf.h"
 #include "lc709203f.h"
 #include "xtimer.h"
-
-#ifndef LC709203F_INT_PIN
-    #define LC709203F_INT_PIN GPIO_PIN(PORT_E, 6)
-#endif
+#include "lc709203f_params.h"
 
 static void _gauge_cb(void *arg)
 {
@@ -35,22 +31,27 @@ static void _gauge_cb(void *arg)
 int main(void)
 {
     puts("LC709203F Fuel Gauge test application");
-    lc709203f_init(I2C_0, LC709203F_INT_PIN, _gauge_cb, NULL);
-
-    puts("The application will trigger an low_rsoc interrupt in 2s");
-    xtimer_sleep(2);
-    lc709203f_set_alarm_low_rsoc(I2C_0, 0);
-    lc709203f_set_alarm_low_rsoc(I2C_0, 100);
-    puts("Read low rsoc threshold level. Should be 100");
-    printf("Alarm Low RSOC level: %u \n", lc709203f_get_alarm_low_rsoc(I2C_0));
-    puts("Voltage, RSOC, ITE, ID and Cell temperature will be printed every 5s");
-    while (1) {
-        xtimer_sleep(5);
-        printf("Voltage in mV: %u \n", lc709203f_get_voltage(I2C_0));
-        printf("RSOC: %u \n", lc709203f_get_rsoc(I2C_0));
-        printf("Ite in 0.1: %u \n", lc709203f_get_ite(I2C_0));
-        printf("ID: %u \n", lc709203f_get_id(I2C_0));
-        printf("Cell Temp in 0.1C: %u \n", lc709203f_get_cell_temp(I2C_0));
+    lc709203f_t dev;
+    dev.cb = _gauge_cb;
+    dev.arg = NULL;
+    if(lc709203f_init(&dev, params_default)==LC709203F_OK){
+        puts("The application will trigger an low_rsoc interrupt in 2s");
+        xtimer_sleep(2);
+        lc709203f_set_alarm_low_rsoc(&dev, 0);
+        lc709203f_set_alarm_low_rsoc(&dev, 100);
+        puts("Read low rsoc threshold level. Should be 100");
+        printf("Alarm Low RSOC level: %u \n", lc709203f_get_alarm_low_rsoc(&dev));
+        puts("Voltage, RSOC, ITE, ID and Cell temperature will be printed every 5s");
+        while (1) {
+            xtimer_sleep(5);
+            printf("Voltage in mV: %u \n", lc709203f_get_voltage(&dev));
+            printf("RSOC: %u \n", lc709203f_get_rsoc(&dev));
+            printf("Ite in 0.1: %u \n", lc709203f_get_ite(&dev));
+            printf("ID: %u \n", lc709203f_get_id(&dev));
+            printf("Cell Temp in 0.1C: %u \n", lc709203f_get_cell_temp(&dev));
+        }
+    }else{
+        printf("Could not connect to fuel gauge\n");
     }
     return 0;
 }
