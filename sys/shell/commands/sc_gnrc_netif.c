5f982664533f78cdb2ccb74a49642a2d420ec878
smlng <s@mlng.net>
2017-11-28 15:30:43 +0100

    shell: fix output in netif command

--- sys/shell/commands/sc_gnrc_netif.c
+++ sys/shell/commands/sc_gnrc_netif.c
@@ -556,10 +556,10 @@ static int _netif_set_u16(kernel_pid_t iface, netopt_t opt, uint16_t context,
     printf(" on interface %" PRIkernel_pid " to ", iface);
 
     if (hex) {
-        printf("0x%04x\n", res);
+        printf("0x%04lx\n", res);
     }
     else {
-        printf("%u\n", res);
+        printf("%lu\n", res);
     }
 
     return 0;
