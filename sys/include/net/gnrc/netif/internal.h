ee623bbce9caa0a5fd19c63f800cfa9dd0f52a4c
Martine Lenders <m.lenders@fu-berlin.de>
2017-11-29 20:15:48 +0100

    fixup! gnrc_netif: assume flag settings on compile settings

--- sys/include/net/gnrc/netif/internal.h
+++ sys/include/net/gnrc/netif/internal.h
@@ -343,7 +343,8 @@ static inline bool gnrc_netif_is_rtr_adv(const gnrc_netif_t *netif)
  *
  * @attention   Requires prior locking
  * @note        Assumed to be true, when @ref GNRC_NETIF_NUMOF == 1 and
- *              @ref net_gnrc_sixlowpan module is included.
+ *              @ref net_gnrc_sixlowpan module is included (and
+ *              @ref GNRC_IPV6_NIB_6LN is not 0, otherwise assumed to be false).
  *
  * @param[in] netif the network interface
  *
@@ -354,8 +355,10 @@ static inline bool gnrc_netif_is_rtr_adv(const gnrc_netif_t *netif)
  */
 #if (GNRC_NETIF_NUMOF > 1) || !defined(MODULE_GNRC_SIXLOWPAN) || defined(DOXYGEN)
 bool gnrc_netif_is_6ln(const gnrc_netif_t *netif);
-#else
+#elif GNRC_IPV6_NIB_6LN
 #define gnrc_netif_is_6ln(netif)                (true)
+#else
+#define gnrc_netif_is_6ln(netif)                (false)
 #endif
 
 /**
@@ -363,7 +366,8 @@ bool gnrc_netif_is_6ln(const gnrc_netif_t *netif);
  *          RFC 6775
  *
  * @attention   Requires prior locking
- * @note        Assumed to be false, when @ref GNRC_IPV6_NIB_CONF_6LR == 0.
+ * @note        Assumed to be false, when @ref GNRC_IPV6_NIB_CONF_6LR == 0 and
+ *              .
  *
  * @param[in] netif the network interface
  *
@@ -372,7 +376,11 @@ bool gnrc_netif_is_6ln(const gnrc_netif_t *netif);
  * @return  true, if the interface represents a 6LR
  * @return  false, if the interface does not represent a 6LR
  */
-#if GNRC_IPV6_NIB_CONF_6LR
+#if GNRC_IPV6_NIB_6LR && \
+    /* if flag checkers even evaluate, otherwise just assume their result */ \
+    (defined(MODULE_IPV6_ROUTER) || \
+     (GNRC_NETIF_NUMOF > 1) || !defined(MODULE_GNRC_SIXLOWPAN)) || \
+    defined(DOXYGEN)
 static inline bool gnrc_netif_is_6lr(const gnrc_netif_t *netif)
 {
     return gnrc_netif_is_rtr(netif) && gnrc_netif_is_6ln(netif);
