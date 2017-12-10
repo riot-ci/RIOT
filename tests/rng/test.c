7423dd1a402cbbfb2ff18c104a9926a481462c45
Bas Stottelaar <basstottelaar@gmail.com>
2017-12-10 23:48:40 +0100

    tests: rng: add xorshift to list of names.

--- tests/rng/test.c
+++ tests/rng/test.c
@@ -47,6 +47,8 @@ static void test_init(char *name)
         puts("Musl C PRNG.\n");
 #elif MODULE_PRNG_TINYMT32
         puts("Tiny Mersenne Twister PRNG.\n");
+#elif MODULE_PRNG_XORSHIFT
+        puts("XOR Shift PRNG.\n");
 #else
         puts("unknown PRNG.\n");
 #endif
