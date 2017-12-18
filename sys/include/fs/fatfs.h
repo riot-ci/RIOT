9394f3e598a16ed51b504251e3e5d237606f138c
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2017-12-18 16:02:31 +0100

    fix doxygen errors

--- sys/include/fs/fatfs.h
+++ sys/include/fs/fatfs.h
@@ -28,18 +28,20 @@ extern "C" {
 #include "vfs.h"
 
 #ifndef FATFS_YEAR_OFFSET
+/** The year in FatFs timestamps is relative to this offset */
 #define FATFS_YEAR_OFFSET (1980)
 #endif
 
+/** The epoch offset is used to convert between FatFs and time_t timestamps */
 #define EPOCH_YEAR_OFFSET (1970)
 
-/** Size of the buffer needed for directory -> should be: sizeof(DIR)*/
+/** Size of the buffer needed for a directory entry -> @attention this should be: sizeof(DIR).
+    sizeof(DIR) currently isn't used directly because it's not possible to use that within
+    preprocessor-if (see below) */
 #define FATFS_DIR_SIZE (44)
-/** the problem with the above is: it's not possible to use sizeof(DIR) as
-    this is later used in #if (see below) */
 
 /** Size of the buffer needed for directory
-    -> should be: sizeof(fatfs_file_desc_t)*/
+    -> should be: sizeof(fatfs_file_desc_t) */
 #define FATFS_FILE_SIZE (72)
 
 /** size needed for volume strings like "n:/" where n is the volume id */
@@ -48,6 +50,8 @@ extern "C" {
 /** 0:mount on first file access, 1 mount in f_mount() call */
 #define FATFS_MOUNT_OPT       (1)
 
+/** most FatFs file operations need an absolute path. This defines the size of the
+    needed buffer to circumvent stack allocation within vfs-wrappers */
 #define FATFS_MAX_ABS_PATH_SIZE (FATFS_MAX_VOL_STR_LEN + VFS_NAME_MAX + 1)
 
 #if (VFS_DIR_BUFFER_SIZE < FATFS_DIR_SIZE)
