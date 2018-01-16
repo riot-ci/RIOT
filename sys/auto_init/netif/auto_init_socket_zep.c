4704c815298080b9b4b09de70b90fb82979833d5
Martine Lenders <m.lenders@fu-berlin.de>
2018-01-16 15:56:28 +0100

    fixup! native: provide socket-based ZEP device

--- sys/auto_init/netif/auto_init_socket_zep.c
+++ sys/auto_init/netif/auto_init_socket_zep.c
@@ -24,8 +24,7 @@
 
 #include "socket_zep.h"
 #include "socket_zep_params.h"
-#include "net/gnrc/netdev.h"
-#include "net/gnrc/netdev/ieee802154.h"
+#include "net/gnrc/netif/ieee802154.h"
 
 
 /**
@@ -34,7 +33,7 @@
  */
 #define SOCKET_ZEP_MAC_STACKSIZE    (THREAD_STACKSIZE_DEFAULT + DEBUG_EXTRA_STACKSIZE)
 #ifndef SOCKET_ZEP_MAC_PRIO
-#define SOCKET_ZEP_MAC_PRIO         (GNRC_NETDEV_MAC_PRIO)
+#define SOCKET_ZEP_MAC_PRIO         (GNRC_NETIF_PRIO)
 #endif
 
 /**
@@ -42,23 +41,17 @@
  */
 static char _netdev_ieee802154_stacks[SOCKET_ZEP_MAX][SOCKET_ZEP_MAC_STACKSIZE];
 static socket_zep_t _socket_zeps[SOCKET_ZEP_MAX];
-static gnrc_netdev_t _gnrc_socket_zeps[SOCKET_ZEP_MAX];
 
 void auto_init_socket_zep(void)
 {
-    DEBUG("auto_init_socket_zep(): initializing device...\n");
     for (int i = 0; i < SOCKET_ZEP_MAX; i++) {
+        LOG_DEBUG("[auto_init_netif: initializing socket ZEP device #%u\n", i);
         /* setup netdev device */
         socket_zep_setup(&_socket_zeps[i], &socket_zep_params[i]);
-
-        /* initialize netdev<->gnrc adapter state */
-        gnrc_netdev_ieee802154_init(&_gnrc_socket_zeps[i],
-                                     (netdev_ieee802154_t *)&_socket_zeps[i]);
-
-        /* start gnrc netdev thread */
-        gnrc_netdev_init(_netdev_ieee802154_stacks[i], SOCKET_ZEP_MAC_STACKSIZE,
-                          SOCKET_ZEP_MAC_PRIO, "gnrc_socket_zep",
-                          &_gnrc_socket_zeps[i]);
+        gnrc_netif_ieee802154_create(_netdev_ieee802154_stacks[i],
+                                     SOCKET_ZEP_MAC_STACKSIZE,
+                                     SOCKET_ZEP_MAC_PRIO,
+                                     "socket_zep", &_socket_zeps[i]);
     }
 }
 
