39a75759e7b7f3410eb051c8f07865e848bfff6c
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-01-16 11:16:37 +0100

    fixup! pkg: add Semtech LoRaMAC package

--- pkg/semtech-loramac/contrib/semtech_loramac_arch.c
+++ pkg/semtech-loramac/contrib/semtech_loramac_arch.c
@@ -52,16 +52,3 @@ void memset1(uint8_t *dst, uint8_t value, uint16_t size)
         *dst++ = value;
     }
 }
-
-int8_t Nibble2HexChar(uint8_t a)
-{
-    if (a < 10) {
-        return '0' + a;
-    }
-    else if (a < 16) {
-        return 'A' + (a - 10);
-    }
-    else {
-        return '?';
-    }
-}
