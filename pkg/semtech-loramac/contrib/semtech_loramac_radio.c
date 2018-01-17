b901f404910da81d0d1b11658e3bfee7f94dffa9
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-01-17 13:33:52 +0100

    fixup! pkg: add Semtech LoRaMAC package

--- pkg/semtech-loramac/contrib/semtech_loramac_radio.c
+++ pkg/semtech-loramac/contrib/semtech_loramac_radio.c
@@ -13,10 +13,6 @@
  * @file
  * @brief       Compatibility functions for controlling the radio driver
  *
- * @note        Even if the function names are related to SX1276, they use
- *              the RIOT SX127x driver and can be used with SX1272 and SX1276
- *              devices.
- *
  * @author      Jose Ignacio Alamos <jialamos@uc.cl>
  * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
  * @}
@@ -44,28 +40,28 @@ extern sx127x_t sx127x;
  * Radio driver functions implementation wrappers, the netdev2 object
  * is known within the scope of the function
  */
-void SX1276Init(RadioEvents_t *events)
+void SX127XInit(RadioEvents_t *events)
 {
     (void) events;
     sx127x_init(&sx127x);
 }
 
-RadioState_t SX1276GetStatus(void)
+RadioState_t SX127XGetStatus(void)
 {
     return (RadioState_t)sx127x_get_state(&sx127x);
 }
 
-void SX1276SetModem(RadioModems_t modem)
+void SX127XSetModem(RadioModems_t modem)
 {
     sx127x_set_modem(&sx127x, (uint8_t)modem);
 }
 
-void SX1276SetChannel(uint32_t freq)
+void SX127XSetChannel(uint32_t freq)
 {
     sx127x_set_channel(&sx127x, freq);
 }
 
-bool SX1276IsChannelFree(RadioModems_t modem, uint32_t freq,
+bool SX127XIsChannelFree(RadioModems_t modem, uint32_t freq,
                          int16_t rssiThresh, uint32_t maxCarrierSenseTime )
 {
     (void) modem;
@@ -73,12 +69,12 @@ bool SX1276IsChannelFree(RadioModems_t modem, uint32_t freq,
     return sx127x_is_channel_free(&sx127x, freq, rssiThresh);
 }
 
-uint32_t SX1276Random(void)
+uint32_t SX127XRandom(void)
 {
     return sx127x_random(&sx127x);
 }
 
-void SX1276SetRxConfig(RadioModems_t modem, uint32_t bandwidth,
+void SX127XSetRxConfig(RadioModems_t modem, uint32_t bandwidth,
                        uint32_t spreading_factor, uint8_t coding_rate,
                        uint32_t bandwidthAfc, uint16_t preambleLen,
                        uint16_t symbTimeout, bool fixLen,
@@ -105,7 +101,7 @@ void SX1276SetRxConfig(RadioModems_t modem, uint32_t bandwidth,
     sx127x_set_rx(&sx127x);
 }
 
-void SX1276SetTxConfig(RadioModems_t modem, int8_t power, uint32_t fdev,
+void SX127XSetTxConfig(RadioModems_t modem, int8_t power, uint32_t fdev,
                        uint32_t bandwidth, uint32_t spreading_factor,
                        uint8_t coding_rate, uint16_t preambleLen,
                        bool fixLen, bool crcOn, bool freqHopOn,
@@ -130,13 +126,13 @@ void SX1276SetTxConfig(RadioModems_t modem, int8_t power, uint32_t fdev,
     sx127x_set_tx_timeout(&sx127x, timeout * US_PER_MS); /* base unit us, LoRaMAC ms */
 }
 
-uint32_t SX1276GetTimeOnAir(RadioModems_t modem, uint8_t pktLen)
+uint32_t SX127XGetTimeOnAir(RadioModems_t modem, uint8_t pktLen)
 {
     (void) modem;
     return sx127x_get_time_on_air(&sx127x, pktLen);
 }
 
-void SX1276Send(uint8_t *buffer, uint8_t size)
+void SX127XSend(uint8_t *buffer, uint8_t size)
 {
     netdev_t *dev = (netdev_t *)&sx127x;
     struct iovec vec[1];
@@ -145,67 +141,67 @@ void SX1276Send(uint8_t *buffer, uint8_t size)
     dev->driver->send(dev, vec, 1);
 }
 
-void SX1276SetSleep(void)
+void SX127XSetSleep(void)
 {
     sx127x_set_sleep(&sx127x);
 }
 
-void SX1276SetStby(void)
+void SX127XSetStby(void)
 {
     sx127x_set_standby(&sx127x);
 }
 
-void SX1276SetRx(uint32_t timeout)
+void SX127XSetRx(uint32_t timeout)
 {
     (void) timeout;
     sx127x_set_rx(&sx127x);
 }
 
-void SX1276StartCad(void)
+void SX127XStartCad(void)
 {
     sx127x_start_cad(&sx127x);
 }
 
-int16_t SX1276ReadRssi(RadioModems_t modem)
+int16_t SX127XReadRssi(RadioModems_t modem)
 {
     sx127x_set_modem(&sx127x, (uint8_t)modem);
     return sx127x_read_rssi(&sx127x);
 }
 
-void SX1276Write(uint8_t addr, uint8_t data)
+void SX127XWrite(uint8_t addr, uint8_t data)
 {
     sx127x_reg_write(&sx127x, addr, data);
 }
 
-uint8_t SX1276Read(uint8_t addr)
+uint8_t SX127XRead(uint8_t addr)
 {
     return sx127x_reg_read(&sx127x, addr);
 }
 
-void SX1276WriteBuffer(uint8_t addr, uint8_t *buffer, uint8_t size)
+void SX127XWriteBuffer(uint8_t addr, uint8_t *buffer, uint8_t size)
 {
     sx127x_reg_write_burst(&sx127x, addr, buffer, size);
 }
 
-void SX1276ReadBuffer(uint8_t addr, uint8_t *buffer, uint8_t size)
+void SX127XReadBuffer(uint8_t addr, uint8_t *buffer, uint8_t size)
 {
     sx127x_reg_read_burst(&sx127x, addr, buffer, size);
 }
 
-void SX1276SetMaxPayloadLength(RadioModems_t modem, uint8_t max)
+void SX127XSetMaxPayloadLength(RadioModems_t modem, uint8_t max)
 {
     (void) modem;
     sx127x_set_max_payload_len(&sx127x, max);
 }
 
-bool SX1276CheckRfFrequency(uint32_t frequency)
+bool SX127XCheckRfFrequency(uint32_t frequency)
 {
     (void) frequency;
     /* Implement check. Currently all frequencies are supported */
     return true;
 }
 
-void SX1276SetTxContinuousWave(uint32_t freq, int8_t power, uint16_t time)
+void SX127XSetTxContinuousWave(uint32_t freq, int8_t power, uint16_t time)
 {
     (void) freq;
     (void) power;
@@ -213,7 +209,7 @@ void SX1276SetTxContinuousWave(uint32_t freq, int8_t power, uint16_t time)
     /* TODO */
 }
 
-void SX1276SetPublicNetwork(bool enable)
+void SX127XSetPublicNetwork(bool enable)
 {
     if (enable) {
         /* Use public network syncword */
@@ -230,27 +226,27 @@ void SX1276SetPublicNetwork(bool enable)
  */
 const struct Radio_s Radio =
 {
-    SX1276Init,
-    SX1276GetStatus,
-    SX1276SetModem,
-    SX1276SetChannel,
-    SX1276IsChannelFree,
-    SX1276Random,
-    SX1276SetRxConfig,
-    SX1276SetTxConfig,
-    SX1276CheckRfFrequency,
-    SX1276GetTimeOnAir,
-    SX1276Send,
-    SX1276SetSleep,
-    SX1276SetStby,
-    SX1276SetRx,
-    SX1276StartCad,
-    SX1276SetTxContinuousWave,
-    SX1276ReadRssi,
-    SX1276Write,
-    SX1276Read,
-    SX1276WriteBuffer,
-    SX1276ReadBuffer,
-    SX1276SetMaxPayloadLength,
-    SX1276SetPublicNetwork
+    SX127XInit,
+    SX127XGetStatus,
+    SX127XSetModem,
+    SX127XSetChannel,
+    SX127XIsChannelFree,
+    SX127XRandom,
+    SX127XSetRxConfig,
+    SX127XSetTxConfig,
+    SX127XCheckRfFrequency,
+    SX127XGetTimeOnAir,
+    SX127XSend,
+    SX127XSetSleep,
+    SX127XSetStby,
+    SX127XSetRx,
+    SX127XStartCad,
+    SX127XSetTxContinuousWave,
+    SX127XReadRssi,
+    SX127XWrite,
+    SX127XRead,
+    SX127XWriteBuffer,
+    SX127XReadBuffer,
+    SX127XSetMaxPayloadLength,
+    SX127XSetPublicNetwork
 };
