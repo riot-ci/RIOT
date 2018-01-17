e84947e68b31900be272c963835433d59ce9065f
danpetry <daniel.petry@fu-berlin.de>
2018-01-17 17:44:48 +0100

    Fixup: Made changes to openmote config after review
    - added space in the indices for pin number
    - removed config entries for IRQ, RxD, TxD
    - changed .dev entry to use UART0_BASEADDR macro
    - removed unused UART_0_ISR macro

--- boards/openmote-cc2538/include/periph_conf.h
+++ boards/openmote-cc2538/include/periph_conf.h
@@ -87,13 +87,10 @@ static const adc_conf_t adc_config[] = {
  * @{
  */
 static const uart_conf_t uart_config[] = {
-    {    /* Virtual COM Port */
-        .dev      = (cc2538_uart_t *)0x4000c000,
-        .rx_pin   = GPIO_PIN(0,0),
-        .tx_pin   = GPIO_PIN(0,1),
-        .irq      = UART0_IRQn,
-        .rxd      = UART0_RXD,
-        .txd      = UART0_TXD,
+    {    
+        .dev      = UART0_BASEADDR,
+        .rx_pin   = GPIO_PIN(0, 0),
+        .tx_pin   = GPIO_PIN(0, 1),
         .cts_pin  = GPIO_UNDEF,
         .rts_pin  = GPIO_UNDEF
     }
@@ -103,7 +100,6 @@ static const uart_conf_t uart_config[] = {
 #define UART_0_ISR          isr_uart0
 
 /* macros common across all UARTs */
-#define UART_IRQ_PRIO       1
 #define UART_NUMOF          (sizeof(uart_config) / sizeof(uart_config[0]))
 
 /** @} */
