2b024a18c172b774c578c73a3771df924bbcd1b4
Martine Lenders <m.lenders@fu-berlin.de>
2018-01-17 16:47:33 +0100

    fixup! native: provide socket-based ZEP device

--- sys/auto_init/netif/auto_init_socket_zep.c
+++ sys/auto_init/netif/auto_init_socket_zep.c
@@ -19,13 +19,13 @@
 
 #ifdef MODULE_SOCKET_ZEP
 
-#define ENABLE_DEBUG (0)
-#include "debug.h"
-
+#include "log.h"
 #include "socket_zep.h"
 #include "socket_zep_params.h"
 #include "net/gnrc/netif/ieee802154.h"
 
+#define ENABLE_DEBUG (0)
+#include "debug.h"
 
 /**
  * @brief   Define stack parameters for the MAC layer thread
@@ -39,7 +39,7 @@
 /**
  * @brief   Stacks for the MAC layer threads
  */
-static char _netdev_ieee802154_stacks[SOCKET_ZEP_MAX][SOCKET_ZEP_MAC_STACKSIZE];
+static char _socket_zep_stacks[SOCKET_ZEP_MAX][SOCKET_ZEP_MAC_STACKSIZE];
 static socket_zep_t _socket_zeps[SOCKET_ZEP_MAX];
 
 void auto_init_socket_zep(void)
@@ -48,10 +48,10 @@ void auto_init_socket_zep(void)
         LOG_DEBUG("[auto_init_netif: initializing socket ZEP device #%u\n", i);
         /* setup netdev device */
         socket_zep_setup(&_socket_zeps[i], &socket_zep_params[i]);
-        gnrc_netif_ieee802154_create(_netdev_ieee802154_stacks[i],
+        gnrc_netif_ieee802154_create(_socket_zep_stacks[i],
                                      SOCKET_ZEP_MAC_STACKSIZE,
-                                     SOCKET_ZEP_MAC_PRIO,
-                                     "socket_zep", &_socket_zeps[i]);
+                                     SOCKET_ZEP_MAC_PRIO, "socket_zep",
+                                     (netdev_t *)&_socket_zeps[i]);
     }
 }
 
