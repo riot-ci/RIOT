340ce4f0046dcae7dbe18c8da5344152b3a9c315
Kaspar Schleiser <kaspar@schleiser.de>
2018-01-18 17:54:41 +0100

    fixup! fixup! sys: introduce iolist

--- sys/iolist/iolist.c
+++ sys/iolist/iolist.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_util
+ * @{
+ *
+ * @file
+ * @brief       iolist scatter / gather IO
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @}
+ */
+
 #include <sys/uio.h>
 
 #include "iolist.h"
