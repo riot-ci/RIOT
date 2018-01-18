8818c96f94fa11a1d49d4ecda22b041804a8087c
danpetry <daniel.petry@fu-berlin.de>
2018-01-18 14:10:12 +0100

    Fixup: cpu/cc2538: Changed UART config interface
    Integrates changes from review to the files involved
    in the cpu cc 2538 definition. To summarize:
    
    - combined uart_base and uart_init in uart.c
    - used bitmask for the interrupt setup
    - took the uart Rx, Tx, and IRQ numbers out of the config
    (as this has to match the .dev field). Replaced with
    macros from the uart number
    - took out some unused code
    - added clock on/off commands

--- cpu/cc2538/periph/uart.c
+++ cpu/cc2538/periph/uart.c
@@ -27,8 +27,27 @@
 #include "periph/uart.h"
 #include "periph_conf.h"
 
+/* Pin functions and interrupt definitions for the two UARTs */
+#define UART_RXD(X) (cc2538_ioc_pin_t)(2 * (X)) /**< UART Rx pin function */
+#define UART_TXD(X) (cc2538_ioc_sel_t)(2 * (X)) /**< UART Tx pin function */
+#define UART_IRQ(X) (IRQn_Type)(5 + (X))        /**< UART interrupt */
+
 #undef BIT
 #define BIT(n) ( 1 << (n) )
+
+/* Bit field definitions for the UART Line Control Register: */
+#define FEN   BIT( 4) /**< Enable FIFOs */
+
+/* Bit masks for the UART Masked Interrupt Status (MIS) Register: */
+#define OEMIS BIT(10) /**< UART overrun error masked status */
+#define BEMIS BIT( 9) /**< UART break error masked status */
+#define FEMIS BIT( 7) /**< UART framing error masked status */
+#define RTMIS BIT( 6) /**< UART RX time-out masked status */
+#define RXMIS BIT( 4) /**< UART RX masked interrupt status */
+
+#define UART_CTL_HSE_VALUE    0
+#define DIVFRAC_NUM_BITS      6
+#define DIVFRAC_MASK          ( (1 << DIVFRAC_NUM_BITS) - 1 )
 
 enum {
     FIFO_LEVEL_1_8TH = 0,
@@ -46,40 +65,11 @@ enum {
     WLEN_8_BITS = 3,
 };
 
-/* Bit field definitions for the UART Line Control Register: */
-#define FEN   BIT( 4) /**< Enable FIFOs */
-
-/* Bit masks for the UART Masked Interrupt Status (MIS) Register: */
-#define OEMIS BIT(10) /**< UART overrun error masked status */
-#define BEMIS BIT( 9) /**< UART break error masked status */
-#define FEMIS BIT( 7) /**< UART framing error masked status */
-#define RTMIS BIT( 6) /**< UART RX time-out masked status */
-#define RXMIS BIT( 4) /**< UART RX masked interrupt status */
-
-#define UART_CTL_HSE_VALUE    0
-#define DIVFRAC_NUM_BITS      6
-#define DIVFRAC_MASK          ( (1 << DIVFRAC_NUM_BITS) - 1 )
-
-/** @brief Indicates if there are bytes available in the UART0 receive FIFO */
-#define uart0_rx_avail() ( uart_config[0].dev->cc2538_uart_fr.FRbits.RXFE == 0 )
-
-/** @brief Indicates if there are bytes available in the UART1 receive FIFO */
-#define uart1_rx_avail() ( uart_config[1].dev->cc2538_uart_fr.FRbits.RXFE == 0 )
-
-/** @brief Read one byte from the UART0 receive FIFO */
-#define uart0_read()     ( uart_config[0].dev->DR )
-
-/** @brief Read one byte from the UART1 receive FIFO */
-#define uart1_read()     ( uart_config[1].dev->DR )
-
-/*---------------------------------------------------------------------------*/
-
 /**
  * @brief Allocate memory to store the callback functions.
  */
 static uart_isr_ctx_t uart_ctx[UART_NUMOF];
 
-/*---------------------------------------------------------------------------*/
 static void reset(cc2538_uart_t *u)
 {
     /* Make sure the UART is disabled before trying to configure it */
@@ -101,40 +91,25 @@ static void reset(cc2538_uart_t *u)
     /* UART Enable */
     u->cc2538_uart_ctl.CTLbits.UARTEN = 1;
 }
-/*---------------------------------------------------------------------------*/
-
-
-static int init_base(uart_t uart, uint32_t baudrate);
 
 int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
 {
-    /* initialize basic functionality */
-    int res = init_base(uart, baudrate);
-    if (res != UART_OK) {
-        return res;
-    }
 
-    /* register callbacks */
-    uart_ctx[uart].rx_cb = rx_cb;
-    uart_ctx[uart].arg = arg;
+    assert(uart < UART_NUMOF);
 
-    /* configure interrupts and enable RX interrupt */
-    NVIC_SetPriority(uart_config[uart].irq, UART_IRQ_PRIO);
-    NVIC_EnableIRQ(uart_config[uart].irq);
+    cc2538_uart_t *u = uart_config[uart].dev;
 
-    return UART_OK;
-}
+    /* uart_num refers to the CPU UART peripheral number, which may be
+     * different from the value of the uart variable, depending on the board
+     * configuration.
+     */
+    unsigned int uart_num = ( (uintptr_t)u - (uintptr_t)UART0_BASEADDR ) / 0x1000;
 
-static int init_base(uart_t uart, uint32_t baudrate)
-{
-    cc2538_uart_t *u = NULL;
-
-    u = uart_config[uart].dev;
-    gpio_init_af(uart_config[uart].rx_pin, uart_config[uart].rxd, GPIO_IN);
-    gpio_init_af(uart_config[uart].tx_pin, uart_config[uart].txd, GPIO_OUT);
+    /* Configure the Rx and Tx pins */
+    gpio_init_af(uart_config[uart].rx_pin, UART_RXD(uart_num), GPIO_IN);
+    gpio_init_af(uart_config[uart].tx_pin, UART_TXD(uart_num), GPIO_OUT);
 
     /* Enable clock for the UART while Running, in Sleep and Deep Sleep */
-    unsigned int uart_num = ( (uintptr_t)u - (uintptr_t)uart_config[uart].dev ) / 0x1000;
     SYS_CTRL_RCGCUART |= (1 << uart_num);
     SYS_CTRL_SCGCUART |= (1 << uart_num);
     SYS_CTRL_DCGCUART |= (1 << uart_num);
@@ -156,23 +131,12 @@ static int init_base(uart_t uart, uint32_t baudrate)
         u->cc2538_uart_ctl.CTLbits.CTSEN = 1;
     }
 
-    /* Enable clock for the UART while Running, in Sleep and Deep Sleep */
-    uart_num = ( (uintptr_t)u - (uintptr_t)uart_config[0].dev ) / 0x1000;
-    SYS_CTRL_RCGCUART |= (1 << uart_num);
-    SYS_CTRL_SCGCUART |= (1 << uart_num);
-    SYS_CTRL_DCGCUART |= (1 << uart_num);
-
     /*
-     * UART Interrupt Masks:
-     * Acknowledge RX and RX Timeout
-     * Acknowledge Framing, Overrun and Break Errors
+     * UART Interrupt Setup:
+     * Acknowledge Overrun, Break and Framing Errors
+     * Acknowledge RX Timeout and Rx
      */
-    u->cc2538_uart_im.IM = 0;
-    u->cc2538_uart_im.IMbits.RXIM = 1; /**< UART receive interrupt mask */
-    u->cc2538_uart_im.IMbits.RTIM = 1; /**< UART receive time-out interrupt mask */
-    u->cc2538_uart_im.IMbits.OEIM = 1; /**< UART overrun error interrupt mask */
-    u->cc2538_uart_im.IMbits.BEIM = 1; /**< UART break error interrupt mask */
-    u->cc2538_uart_im.IMbits.FEIM = 1; /**< UART framing error interrupt mask */
+    u->cc2538_uart_im.IM = OEMIS | BEMIS | FEMIS | RTMIS | RXMIS | 0x00;
 
     /* Set FIFO interrupt levels: */
     u->cc2538_uart_ifls.IFLSbits.RXIFLSEL = FIFO_LEVEL_4_8TH; /**< MCU default */
@@ -196,14 +160,21 @@ static int init_base(uart_t uart, uint32_t baudrate)
     /* UART Enable */
     u->cc2538_uart_ctl.CTLbits.UARTEN = 1;
 
+    /* register callbacks */
+    uart_ctx[uart].rx_cb = rx_cb;
+    uart_ctx[uart].arg = arg;
+
+    /* enable RX interrupt */
+    NVIC_EnableIRQ(UART_IRQ(uart_num));
+
     return UART_OK;
 }
 
 void uart_write(uart_t uart, const uint8_t *data, size_t len)
 {
-    cc2538_uart_t *u;
+    assert(uart < UART_NUMOF);
 
-    u = uart_config[uart].dev;
+    cc2538_uart_t *u = uart_config[uart].dev;
 
     /* Block if the TX FIFO is full */
     for (size_t i = 0; i < len; i++) {
@@ -214,17 +185,36 @@ void uart_write(uart_t uart, const uint8_t *data, size_t len)
 
 void uart_poweron(uart_t uart)
 {
-    (void) uart;
+    assert(uart < UART_NUMOF);
 
+    uart_config[uart].dev->cc2538_uart_ctl.CTLbits.UARTEN = 1;
 }
 
 void uart_poweroff(uart_t uart)
 {
-    (void) uart;
+    assert(uart < UART_NUMOF);
+
+    uart_config[uart].dev->cc2538_uart_ctl.CTLbits.UARTEN = 0;
+}
+
+void uart_clockon(uart_t uart)
+{
+    assert(uart < UART_NUMOF);
+
+    SYS_CTRL->cc2538_sys_ctrl_unnamed1.RCGCUART |= (1 << uart);
+}
+
+void uart_clockoff(uart_t uart)
+{
+    assert(uart < UART_NUMOF);
+
+    SYS_CTRL->cc2538_sys_ctrl_unnamed1.RCGCUART &= ~(1 << uart);
 }
 
 static inline void irq_handler(uart_t uart)
 {
+    assert(uart < UART_NUMOF);
+
     uint_fast16_t mis;
 
     /* Latch the Masked Interrupt Status and clear any active flags */
