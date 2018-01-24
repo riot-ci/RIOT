6c3c26c0a65d850f3e309e6d606ccc10256d711e
Adrian GHC <adrian.ghc@outlook.com>
2018-01-24 16:31:11 +0100

    tests/unittests: Add unit tests for pkg/keccak

--- tests/unittests/tests-hashes/tests-hashes.c
+++ tests/unittests/tests-hashes/tests-hashes.c
@@ -29,4 +29,5 @@ void tests_hashes(void)
     TESTS_RUN(tests_hashes_sha256_hmac_tests());
     TESTS_RUN(tests_hashes_sha256_chain_tests());
     TESTS_RUN(tests_hashes_sha3_tests());
+    TESTS_RUN(tests_hashes_keccak_tests());
 }
