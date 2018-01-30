aafde4f007bbb8ee81a1fec589fe5afcbd85b265
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-01-30 08:37:56 +0100

    boards/common/stm32-link: add common place for stlink
    - introduce common place for boards using stlink: same serial, all use openocd
    - apply this to nucleos

--- makefiles/tools/openocd-adapters/stlink.inc.mk
+++ makefiles/tools/openocd-adapters/stlink.inc.mk
@@ -1,4 +1,7 @@
 # ST-Link debug adapter
+# Use st-link v2-1 by default
+STLINK_VERSION ?= 2-1
+
 # Use STLINK_VERSION to select which stlink version is used
 OPENOCD_ADAPTER_INIT ?= \
   -c 'source [find interface/stlink-v$(STLINK_VERSION).cfg]' \
@@ -8,3 +11,15 @@ ifneq (,$(DEBUG_ADAPTER_ID))
   OPENOCD_ADAPTER_INIT += -c 'hla_serial $(DEBUG_ADAPTER_ID)'
 endif
 export OPENOCD_ADAPTER_INIT
+
+# select OpenOCD configuration if not already set by the board configuration
+ifeq (,$(OPENOCD_CONFIG))
+  OPENOCD_BOARD_CFG = $(RIOTBOARD)/$(BOARD)/dist/openocd.cfg
+  ifeq "$(OPENOCD_BOARD_CFG)" "$(wildcard $(OPENOCD_BOARD_CFG))"
+    # Use the openocd config file in the board directory if it exists
+    export OPENOCD_CONFIG := $(RIOTBOARD)/$(BOARD)/dist/openocd.cfg
+  else
+    # select OpenOCD configuration depending on CPU type
+    export OPENOCD_CONFIG := $(RIOTBASE)/boards/common/st/configs/$(CPU).cfg
+  endif
+endif
