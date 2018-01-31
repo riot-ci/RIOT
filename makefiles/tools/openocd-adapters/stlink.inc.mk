c0f5c451acd1adff8903164227d3a34bbc124bd1
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-01-31 10:06:10 +0100

    fixup! boards/common/stm32-link: add common place for stlink

--- makefiles/tools/openocd-adapters/stlink.inc.mk
+++ makefiles/tools/openocd-adapters/stlink.inc.mk
@@ -11,23 +11,5 @@ export OPENOCD_ADAPTER_INIT
 
 # select OpenOCD configuration depending on CPU type
 ifeq (,$(OPENOCD_CONFIG))
-  ifeq ($(CPU),stm32f0)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f0.cfg
-  else ifeq ($(CPU),stm32f1)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f1.cfg
-  else ifeq ($(CPU),stm32f2)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f2.cfg
-  else ifeq ($(CPU),stm32f3)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f3.cfg
-  else ifeq ($(CPU),stm32f4)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f4.cfg
-  else ifeq ($(CPU),stm32f7)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32f7.cfg
-  else ifeq ($(CPU),stm32l0)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32l0.cfg
-  else ifeq ($(CPU),stm32l1)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32l1.cfg
-  else ifeq ($(CPU),stm32l4)
-    export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/stm32l4.cfg
-  endif
+  export OPENOCD_CONFIG := $(RIOTMAKE)/tools/openocd-adapters/configs/$(CPU).cfg
 endif
