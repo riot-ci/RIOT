6c0c0c2d22478c5f238cb410cbd11e10e112e1f3
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-05 17:56:35 +0100

    uncrustify: fix file extensions in check.sh

--- dist/tools/uncrustify/check.sh
+++ dist/tools/uncrustify/check.sh
@@ -2,7 +2,7 @@
 RIOTBASE=$(git rev-parse --show-toplevel)
 CURDIR=$(cd $(dirname $0) && pwd)
 BLACKLIST=$CURDIR/blacklist.txt
-FILES=$(git ls-tree -r HEAD --name-only --full-tree | grep -E '.*\.(c|cpp|h|hpp)' | grep -vf $BLACKLIST)
+FILES=$(git ls-tree -r HEAD --name-only --full-tree | grep -E '.*\.(c$|cpp$|h$|hpp$)' | grep -vf $BLACKLIST)
 for F in $FILES
 do
 	OUT=$(diff $RIOTBASE/$F <(uncrustify -c $CURDIR/uncrustify-riot.cfg -f $RIOTBASE/$F 2>/dev/null))
