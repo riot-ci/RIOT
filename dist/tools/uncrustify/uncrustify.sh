dec90bef1145f856e4273b5c018137a5e2ed27f4
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-06 11:16:54 +0100

    uncrustify: merge check.sh into uncrustify.sh

--- dist/tools/uncrustify/uncrustify.sh
+++ dist/tools/uncrustify/uncrustify.sh
@@ -3,7 +3,28 @@ RIOTBASE=$(git rev-parse --show-toplevel)
 CURDIR=$(cd $(dirname $0) && pwd)
 BLACKLIST=$CURDIR/blacklist.txt
 FILES=$(git ls-tree -r HEAD --name-only --full-tree | grep -E '.*\.(c$|cpp$|h$|hpp$)' | grep -vf $BLACKLIST)
-for F in $FILES
-do
-    uncrustify -c $CURDIR/uncrustify-riot.cfg --no-backup $RIOTBASE/$F
-done
+
+check () {
+    for F in $FILES
+    do
+        OUT=$(diff $RIOTBASE/$F <(uncrustify -c $CURDIR/uncrustify-riot.cfg -f $RIOTBASE/$F 2>/dev/null))
+        if [ "$OUT" ] ; then
+            echo "Please run 'dist/tools/uncrustify/uncrustify.sh'"
+            exit 1
+        fi
+    done
+    echo "All files are uncrustified!"
+}
+
+exec_uncrustify () {
+    for F in $FILES
+    do
+        uncrustify -c $CURDIR/uncrustify-riot.cfg --no-backup $RIOTBASE/$F
+    done
+}
+
+if [ "$1" == "--check" ] ; then
+    check
+else
+    exec_uncrustify
+fi
