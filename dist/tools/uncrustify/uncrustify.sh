87d0944563d30224559cef90d59be18f0857f9d6
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-06 11:36:03 +0100

    uncrustify: reuse dist/tools/ci/changed_files.sh

--- dist/tools/uncrustify/uncrustify.sh
+++ dist/tools/uncrustify/uncrustify.sh
@@ -2,7 +2,9 @@
 RIOTBASE=$(git rev-parse --show-toplevel)
 CURDIR=$(cd $(dirname $0) && pwd)
 BLACKLIST=$CURDIR/blacklist.txt
-FILES=$(git ls-tree -r HEAD --name-only --full-tree | grep -E '.*\.(c$|cpp$|h$|hpp$)' | grep -vf $BLACKLIST)
+
+. $RIOTBASE/dist/tools/ci/changed_files.sh
+FILES=$(changed_files | grep -vf $BLACKLIST)
 
 check () {
     for F in $FILES
