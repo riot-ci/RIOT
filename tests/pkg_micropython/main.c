d75e5aeda38b1c29935dee2f107e471b891b512d
Kaspar Schleiser <kaspar@schleiser.de>
2018-02-16 08:22:28 +0100

    fixup! WIP: pkg: micropython: initial commit

--- tests/pkg_micropython/main.c
+++ tests/pkg_micropython/main.c
@@ -23,12 +23,7 @@
 #include "thread.h"
 
 #include "micropython.h"
-
-#include "py/nlr.h"
-#include "py/compile.h"
-#include "py/runtime.h"
-#include "py/repl.h"
-#include "py/gc.h"
+#include "py/stackctrl.h"
 #include "lib/utils/pyexec.h"
 
 #include "boot.py.h"
@@ -39,15 +34,22 @@ int main(void)
 {
     /* let micropython know the top of this thread's stack */
     uint32_t stack_dummy;
-    MP_STATE_THREAD(stack_top) = (char*)&stack_dummy;
+    mp_stack_set_top((char*)&stack_dummy);
+    // Make MicroPython's stack limit somewhat smaller than full stack available
+    mp_stack_set_limit(THREAD_STACKSIZE_MAIN - 512);
 
     mp_riot_init(mp_heap, sizeof(mp_heap));
 
     printf("-- Executing boot.py\n");
     mp_do_str((const char *)boot_py, boot_py_len);
 
-    printf("-- boot.py exited. Starting REPL...\n");
-    pyexec_friendly_repl();
+    printf("-- boot.py exited. Starting REPL..\n");
+
+    while (1) {
+        if (pyexec_friendly_repl() != 0) {
+            break;
+        }
+    }
 
     return 0;
 }
