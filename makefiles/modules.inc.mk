084ef9d1af9679bd03c7902861715ffa4a6f297b
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-02-28 23:06:13 +0100

    make: Use incremental link for modules

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -2,9 +2,9 @@ ED = $(addprefix MODULE_,$(sort $(USEMODULE) $(USEPKG)))
 EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
-NO_PSEUDOMODULES := $(filter $(NO_PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG)))
-REALMODULES = $(filter-out $(PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG))) $(NO_PSEUDOMODULES)
-export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.a)
+REALMODULES = $(filter-out $(PSEUDOMODULES), $(USEMODULE)) $(filter $(NO_PSEUDOMODULES), $(USEMODULE))
+REALPKGS = $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG))
+export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.o) $(REALPKGS:%=$(BINDIR)/%.a)
 
 CFLAGS += $(EXTDEFINES)
 
