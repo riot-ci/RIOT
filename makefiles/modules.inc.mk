61e6a476a9b0a28d26bf41fe733c3e9e73d82206
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-14 07:31:40 +0100

    squash modules.inc.mk duplicates

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -2,7 +2,9 @@ ED = $(addprefix MODULE_,$(sort $(USEMODULE) $(USEPKG)))
 EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
-REALMODULES = $(filter-out $(PSEUDOMODULES), $(USEMODULE)) $(filter $(NO_PSEUDOMODULES), $(USEMODULE))
+# filter out any duplicate module names
+REALMODULES = $(shell echo $(filter-out $(PSEUDOMODULES), $(USEMODULE)) \
+  $(filter $(NO_PSEUDOMODULES), $(USEMODULE)) | tr ' ' '\n' | awk '!a[$$0]++')
 REALPKGS = $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG))
 export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.o) $(REALPKGS:%=$(BINDIR)/%.a)
 
