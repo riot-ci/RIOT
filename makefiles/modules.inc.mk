307fd89bf4024a7557fcacbbcfb59a1141435bcc
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-14 09:18:54 +0100

    fixup! squash introduce USEARCHIVE

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -5,7 +5,7 @@ EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 # filter out any duplicate module names
 REALMODULES = $(filter-out $(USEARCHIVE), $(shell echo $(filter-out $(PSEUDOMODULES), $(USEMODULE)) \
   $(filter $(NO_PSEUDOMODULES), $(USEMODULE)) | tr ' ' '\n' | awk '!a[$$0]++'))
-REALPKGS = $(filter-out $(USEARCHIVE), $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG)))
+REALPKGS = $(filter-out $(USEARCHIVE), $(shell echo $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG)) | tr ' ' '\n' | awk '!a[$$0]++'))
 REALARCHIVES = $(filter $(USEARCHIVE),$(USEMODULE) $(USEPKG))
 export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.o) $(REALPKGS:%=$(BINDIR)/%.o) $(REALARCHIVES:%=$(BINDIR)/%.a)
 
