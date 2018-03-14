59a4f4ef2ca19c8474bab3c7f3ad5e114f3ec593
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-14 09:06:30 +0100

    fixup! squash introduce USEARCHIVE

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -3,9 +3,9 @@ EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
 # filter out any duplicate module names
-REALMODULES = $(shell echo $(filter-out $(PSEUDOMODULES) $(USEARCHIVE), $(USEMODULE)) \
-  $(filter $(NO_PSEUDOMODULES), $(USEMODULE)) | tr ' ' '\n' | awk '!a[$$0]++')
-REALPKGS = $(filter-out $(PSEUDOMODULES) $(USEARCHIVE), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG))
+REALMODULES = $(filter-out $(USEARCHIVE), $(shell echo $(filter-out $(PSEUDOMODULES), $(USEMODULE)) \
+  $(filter $(NO_PSEUDOMODULES), $(USEMODULE)) | tr ' ' '\n' | awk '!a[$$0]++'))
+REALPKGS = $(filter-out $(USEARCHIVE), $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG)))
 REALARCHIVES = $(filter $(USEARCHIVE),$(USEMODULE) $(USEPKG))
 export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.o) $(REALPKGS:%=$(BINDIR)/%.o) $(REALARCHIVES:%=$(BINDIR)/%.a)
 
