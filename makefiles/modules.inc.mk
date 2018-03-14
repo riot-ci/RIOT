b8317104424cadf996360397aec44cfe6561743b
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-14 07:56:01 +0100

    squash USEMODULE USEPKG pkgs use object files instead of static archives

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -5,7 +5,10 @@ EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 # filter out any duplicate module names
 REALMODULES = $(shell echo $(filter-out $(PSEUDOMODULES), $(USEMODULE)) \
   $(filter $(NO_PSEUDOMODULES), $(USEMODULE)) | tr ' ' '\n' | awk '!a[$$0]++')
-REALPKGS = $(filter-out $(PSEUDOMODULES), $(USEPKG)) $(filter $(NO_PSEUDOMODULES), $(USEPKG))
+# USEPKG packages are by default using static archives (.a) for linking, but
+# pkgs listed in USEMODULE are using object files (.o), so we filter out USEMODULE here
+REALPKGS = $(filter-out $(PSEUDOMODULES) $(USEMODULE), $(USEPKG)) \
+  $(filter $(NO_PSEUDOMODULES), $(filter-out $(USEMODULE), $(USEPKG)))
 export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.o) $(REALPKGS:%=$(BINDIR)/%.a)
 
 CFLAGS += $(EXTDEFINES)
