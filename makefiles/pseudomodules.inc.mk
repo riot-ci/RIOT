8af70e9bf52e0e13abc39d58f74181a5f0d3fbc5
Martine Lenders <mail@martine-lenders.eu>
2018-03-17 13:43:08 +0000

    dhcpv6: provide initial client implementation
    This implements a client for DHCPv6 IA_PD (Identity Association for
    Prefix Delegation). Goal was to have a IETF-compliant alternative to
    UHCP. The implementation was based on draft-ietf-dhc-rfc3315bis-12.
    
    We currently do not have a stack-independent API to access network
    interfaces, so every configuration of those is still GNRC-dependent,
    though the implementation is to be targeted to be stack independent one
    day.

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -1,3 +1,4 @@
+PSEUDOMODULES += auto_init_dhcpv6_client
 PSEUDOMODULES += auto_init_gnrc_rpl
 PSEUDOMODULES += can_mbox
 PSEUDOMODULES += can_pm
@@ -8,6 +9,7 @@ PSEUDOMODULES += cbor_float
 PSEUDOMODULES += cbor_semantic_tagging
 PSEUDOMODULES += conn_can_isotp_multi
 PSEUDOMODULES += core_%
+PSEUDOMODULES += dhcpv6_%
 PSEUDOMODULES += emb6_router
 PSEUDOMODULES += event_%
 PSEUDOMODULES += gnrc_ipv6_default
