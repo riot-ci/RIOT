53911e768bf7885d57720d7b311a542665894604
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-03-19 18:39:48 +0100

    fixup! posix/osx: fix missing AF_LINK on OSX native

--- sys/posix/include/sys/socket.h
+++ sys/posix/include/sys/socket.h
@@ -37,7 +37,7 @@
 
 #define __SOCKADDR_COMMON_SIZE  (sizeof (unsigned short int))
 #ifdef __MACH__
-#define AF_LINK 18  /* Link layer interface */
+#define AF_LINK (18)    /* Link layer interface */
 #endif
 #endif
 
