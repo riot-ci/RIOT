b453a5189a322a8f5ca7b84b06517d3ac530626d
Martine Lenders <mail@martine-lenders.eu>
2018-03-20 12:01:17 +0000

    fixup! tests: add dhcpv6_client test application
    Add some fuzzing

--- tests/gnrc_dhcpv6/tests/01-run.py
+++ tests/gnrc_dhcpv6/tests/01-run.py
@@ -10,7 +10,9 @@
 import os
 import scapy.all as scapy
 import sys
+import pexpect
 import queue
+import random
 import threading
 
 link_layer_addr = None
@@ -63,9 +65,21 @@ class MockServer(threading.Thread):
                         )
             # client is required to request SOL_MAX_RT option (82) from server
             assert 82 in p["DHCP6 Option Request Option"].reqopts
+            # Try to crash client
+            scapy.sendp(scapy.Ether(dst=p["Ethernet"].src) / \
+                        scapy.IPv6(dst=p["IPv6"].src) / \
+                        scapy.UDP(dport=p["UDP"].sport, sport=p["UDP"].dport) / \
+                        scapy.fuzz(
+                                scapy.DHCP6(
+                                        trid=p["DHCPv6 Solicit Message"].trid
+                                    ) / scapy.Raw()
+                            ),
+                        iface=os.environ["IFACE"],
+                        count=100)
             # TODO/XXX client doesn't listen for address leases yet, so no sense
             # in replying (since the client would interpret a ADVERTISE without
             # leases as invalid as per the RFC)
+
         except AssertionError as exc:
             self.exc_queue.put(exc)
 
@@ -76,6 +90,13 @@ def testfunc(child):
     child.expect(r"fe80::[0-9a-f:]+")
     link_layer_addr = child.match.group(0)
     ll_lock.release()
+    try:
+        child.expect("RIOT kernel panic")
+    except pexpect.TIMEOUT:
+        # we don't want a kernel panic ;-)
+        pass
+    else:
+        assert False, "Node crashed"
 
 if __name__ == "__main__":
     sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
