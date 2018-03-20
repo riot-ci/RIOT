fa4d224128c71dba2381bf8e5db9a226c599c939
Josarn <josuaarndt@live.de>
2018-03-20 23:32:08 +0100

    cpu/atmega_common: use __temp_reg__

--- cpu/atmega_common/irq_arch.c
+++ cpu/atmega_common/irq_arch.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 Freie Universität Berlin, Hinnerk van Bruinehsen
+ *               2018 RWTH Aachen, Josua Arndt <jarndt@ias.rwth-aachen.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,6 +16,7 @@
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
+ * @author      Josua Arndt <jarndt@ias.rwth-aachen.de>
  *
  * @}
  */
@@ -35,11 +37,11 @@ volatile uint8_t __in_isr = 0;
 __attribute__((always_inline)) static inline uint8_t  __get_interrupt_state(void)
 {
     uint8_t sreg;
-    __asm__ volatile("in r0, __SREG__; \n\t"
-                 "mov %0, r0       \n\t"
+    __asm__ volatile("in __tmp_reg__, __SREG__; \n\t"
+                 "mov %0, __tmp_reg__           \n\t"
                  : "=g"(sreg)
                  :
-                 : "r0");
+                 : "__tmp_reg__");
     return sreg & (1 << 7);
 }
 
