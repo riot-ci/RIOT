7d1a87844c2a180249b4abd13a130fdbf520c52d
Kaspar Schleiser <kaspar@schleiser.de>
2018-03-21 10:06:51 +0100

    fixup! murdock: add support for running tests on hardware

--- makefiles/murdock.inc.mk
+++ makefiles/murdock.inc.mk
@@ -22,7 +22,7 @@ test-murdock:
 # create $(BINDIR)/.test file only if BOARD is in $(TEST_WHITELIST)
 link: $(BINDIR)/.test
 $(BINDIR)/.test: $(filter clean, $(MAKECMDGOALS))
-ifneq (,$(filter $(BOARD), $(TEST_WHITELIST)))
+ifneq (,$(filter $(BOARD) all, $(TEST_WHITELIST)))
 	$(Q)touch $@
 else
 	$(Q)true
