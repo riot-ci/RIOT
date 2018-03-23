b2337bd00b2f93c5848b80d1a8312462f4720d88
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-23 16:05:23 +0100

    squash bench_periph_timer split print_results into separate file

---
+++ tests/bench_periph_timer/print_results.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Eistec AB
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Result printing function declarations
+ *
+ * @author      Joakim Nohlgård <joakim.nohlgard@eistec.se>
+ */
+
+#ifndef PRINT_RESULTS_H
+#define PRINT_RESULTS_H
+
+#include "matstat.h"
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * @brief   Present the results of the benchmark
+ *
+ * Depends on DETAILED_STATS, LOG2_STATS
+ * @param[in]   states      State vector from test run
+ * @param[in]   read_states State vector from test run
+ */
+void print_results(const matstat_state_t *states, const matstat_state_t *read_states);
+
+/**
+ * @brief   Calculate the limits for mean and variance for this test
+ */
+void set_limits(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PRINT_RESULTS_H */
