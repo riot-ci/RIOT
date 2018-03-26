b6e83592ca6cdc935957aebe1421ab0ceb61d7ae
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-03-26 17:02:35 +0200

    Makefile.include: Add FLASHFILE variable
    * Create 'FLASHFILE' dedicated variable to store the file used for flashing.
    * Remove overwriting 'HEXFILE' when a '.bin' is used.
    * Remove non required 'export' for FFLAGS
    * Use FLASHFILE in FFLAGS
    * Update mcuboot.mk

--- makefiles/tools/openocd.inc.mk
+++ makefiles/tools/openocd.inc.mk
@@ -3,7 +3,8 @@ export DEBUGGER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export DEBUGSERVER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export RESET ?= $(RIOTBASE)/dist/tools/openocd/openocd.sh
 
-export FFLAGS ?= flash $(ELFFILE)
+FLASHFILE = $(ELFFILE)
+FFLAGS ?= flash $(FLASHFILE)
 export DEBUGGER_FLAGS ?= debug $(ELFFILE)
 export DEBUGSERVER_FLAGS ?= debug-server
 export RESET_FLAGS ?= reset
