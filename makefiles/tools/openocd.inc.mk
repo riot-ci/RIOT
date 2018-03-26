5e4ded72f27ccf8520b10381c7f36ec4481f76c0
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-03-26 20:08:49 +0200

    openocd: get flash and debug files from cli
    Get FLASH_FILE and ELFFILE from command line instead of environment variable.

--- makefiles/tools/openocd.inc.mk
+++ makefiles/tools/openocd.inc.mk
@@ -3,8 +3,8 @@ export DEBUGGER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export DEBUGSERVER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export RESET ?= $(RIOTBASE)/dist/tools/openocd/openocd.sh
 
-export FFLAGS ?= flash
-export DEBUGGER_FLAGS ?= debug
+export FFLAGS ?= flash $(ELFFILE)
+export DEBUGGER_FLAGS ?= debug $(ELFFILE)
 export DEBUGSERVER_FLAGS ?= debug-server
 export RESET_FLAGS ?= reset
 
