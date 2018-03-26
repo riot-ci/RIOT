d8a3e9c9e4f50b1280e9aedf44b8460eb7a63660
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-03-26 17:02:35 +0200

    Makefile.include: Add FLASHFILE variable
    * Create 'FLASHFILE' dedicated variable to store the file used for flashing.
    * Remove overwriting 'HEXFILE' when a '.bin' is used.
    * Remove non required 'export' for FFLAGS
    * Use FLASHFILE in FFLAGS
    * Update mcuboot.mk

--- makefiles/mcuboot.mk
+++ makefiles/mcuboot.mk
@@ -40,11 +40,11 @@ $(MCUBOOT_BIN):
 
 .PHONY: mcuboot-flash-bootloader mcuboot-flash
 
-mcuboot-flash-bootloader: HEXFILE = $(MCUBOOT_BIN)
+mcuboot-flash-bootloader: FLASHFILE = $(MCUBOOT_BIN)
 mcuboot-flash-bootloader: $(MCUBOOT_BIN) $(FLASHDEPS)
 	FLASH_ADDR=0x0 $(FLASHER) $(FFLAGS)
 
-mcuboot-flash: HEXFILE = $(SIGN_BINFILE)
+mcuboot-flash: FLASHFILE = $(SIGN_BINFILE)
 mcuboot-flash: mcuboot $(FLASHDEPS) mcuboot-flash-bootloader
 	FLASH_ADDR=$(MCUBOOT_SLOT0_SIZE) $(FLASHER) $(FFLAGS)
 
