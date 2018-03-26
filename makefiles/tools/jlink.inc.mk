d7e8101fba16b65cb59ff51abd22698161613581
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-03-26 17:02:35 +0200

    Makefile.include: Add FLASHFILE variable
    * Create 'FLASHFILE' dedicated variable to store the file used for flashing.
    * Remove overwriting 'HEXFILE' when a '.bin' is used.
    * Remove non required 'export' for FFLAGS
    * Use FLASHFILE in FFLAGS
    * Update mcuboot.mk

--- makefiles/tools/jlink.inc.mk
+++ makefiles/tools/jlink.inc.mk
@@ -3,9 +3,9 @@ export DEBUGGER = $(RIOTBASE)/dist/tools/jlink/jlink.sh
 export DEBUGSERVER = $(RIOTBASE)/dist/tools/jlink/jlink.sh
 export RESET = $(RIOTBASE)/dist/tools/jlink/jlink.sh
 
-HEXFILE = $(BINFILE)
+FLASHFILE = $(BINFILE)
 
-export FFLAGS ?= flash $(HEXFILE)
+FFLAGS ?= flash $(FLASHFILE)
 export DEBUGGER_FLAGS ?= debug $(ELFFILE)
 export DEBUGSERVER_FLAGS ?= debug-server
 export RESET_FLAGS ?= reset
