61bf7fbf835e4c7ef52ac518c1c25feddd188009
Kaspar Schleiser <kaspar@schleiser.de>
2018-03-27 11:30:20 +0200

    fixup! cpu/atmega_common: provide common stdio initialization

--- cpu/atmega_common/atmega_stdio.c
+++ cpu/atmega_common/atmega_stdio.c
@@ -1,4 +1,21 @@
+/*
+ * Copyright (C) 2018 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
+/**
+ * @{
+ *
+ * @file
+ * @brief   Implements common atmega libc stdio initialization
+ *
+ * @author  Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
 #include <stdio.h>
 #include <avr/io.h>
 
