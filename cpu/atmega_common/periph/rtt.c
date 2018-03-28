f0c3d1d1cedca173ab850400b68fd0e75d071180
Matthew Blue <matthew.blue.neuro@gmail.com>
2018-03-28 00:03:49 -0400

    cpu/atmega_common: initial RTT support

---
+++ cpu/atmega_common/periph/rtt.c
@@ -0,0 +1,337 @@
+/*
+ * Copyright (C) 2016 Robert Hartung <hartung@ibr.cs.tu-bs.de>
+ *               2018 Matthew Blue <matthew.blue.neuro@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/*
+ * @ingroup cpu_atmega_common
+ * @ingroup drivers_periph_rtt
+ * @{
+ *
+ * @file
+ * @brief       Low-level ATmega RTT driver implementation
+ *
+ * @author      Robert Hartung <hartung@ibr.cs.tu-bs.de>
+ * @author      Matthew Blue <matthew.blue.neuro@gmail.com>
+ *
+ * @}
+ */
+
+#include <avr/interrupt.h>
+
+#include "cpu.h"
+#include "irq.h"
+#include "periph/rtt.h"
+#include "periph_conf.h"
+#include "thread.h"
+
+#ifdef MODULE_XTIMER
+#include "xtimer.h"
+#endif
+
+#define ENABLE_DEBUG    (0)
+#include "debug.h"
+
+/* guard file in case no RTT device is defined */
+#if RTT_NUMOF
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if RTC_NUMOF
+extern void atmega_rtc_incr(void);
+#endif
+
+typedef struct {
+    uint8_t ext_cnt[3];      /* Counter to make 8-bit timer 32-bit */
+    uint8_t ext_comp[3];     /* Extend compare to 32-bits */
+    rtt_cb_t alarm_cb;       /* callback called from RTT alarm */
+    void *alarm_arg;         /* argument passed to the callback */
+    rtt_cb_t overflow_cb;    /* callback called when RTT overflows */
+    void *overflow_arg;      /* argument passed to the callback */
+} rtt_state_t;
+
+static volatile rtt_state_t rtt_state;
+
+void rtt_init(void) {
+    DEBUG("Initializing RTT\n");
+
+    rtt_poweron();
+
+    /*
+     * From the datasheet section "Asynchronous Operation of Timer/Counter2"
+     * p148 for ATmega1284P.
+     * 1. Disable the Timer/Counter2 interrupts by clearing OCIE2x and TOIE2.
+     * 2. Select clock source by setting AS2 as appropriate.
+     * 3. Write new values to TCNT2, OCR2x, and TCCR2x.
+     * 4. To switch to asynchronous: Wait for TCN2UB, OCR2xUB, TCR2xUB.
+     * 5. Clear the Timer/Counter2 Interrupt Flags.
+     * 6. Enable interrupts, if needed
+    */
+
+    /* Disable all timer 2 interrupts */
+    TIMSK2 = 0;
+
+    /* Select asynchronous clock source */
+    ASSR = (1 << AS2);
+
+    /* Set counter to 0 */
+    TCNT2 = 0;
+    rtt_state.ext_cnt[2] = 0;
+    rtt_state.ext_cnt[1] = 0;
+    rtt_state.ext_cnt[0] = 0;
+
+    /* Reset compare values */
+    OCR2A = 0;
+    OCR2B = 0;
+    rtt_state.ext_comp[2] = 0;
+    rtt_state.ext_comp[1] = 0;
+    rtt_state.ext_comp[0] = 0;
+
+    /* Reset timer control */
+    TCCR2A = 0;
+
+    /* 32768Hz / 1024 -> 32 ticks per second! */
+    TCCR2B = (1 << CS22) | (1 << CS21) | (1 <<  CS20);
+
+    /* Wait until not busy anymore */
+    DEBUG("RTT waits until ASSR not busy\n");
+    while( ASSR & ((1 << TCN2UB) | (1 << OCR2AUB) | (1 << OCR2BUB)
+                 | (1 << TCR2AUB) | (1 << TCR2BUB)) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 2 RTC cycles */
+        xtimer_usleep(70 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+
+    /* Clear interrupt flags */
+    TIFR2 &= ~( (1 << OCF2B)  | (1 << OCF2A) | (1 << TOV2) );
+
+    /* Initialize callbacks */
+    rtt_state.alarm_cb = NULL;
+    rtt_state.alarm_arg = NULL;
+    rtt_state.overflow_cb = NULL;
+    rtt_state.overflow_arg = NULL;
+
+    /* Enable 8-bit overflow interrupt */
+    TIMSK2 |= (1 << TOIE2);
+
+    irq_enable();
+
+    DEBUG("RTT initialized\n");
+}
+
+void rtt_set_overflow_cb(rtt_cb_t cb, void *arg) {
+    /* Disable overflow interrupt */
+    TIMSK2 &= ~(1 << TOIE2);
+
+    rtt_state.overflow_cb = cb;
+    rtt_state.overflow_arg = arg;
+
+    /* Enable overflow interrupt */
+    TIMSK2 |= (1 << TOIE2);
+}
+
+void rtt_clear_overflow_cb(void) {
+    /* Disable overflow interrupt */
+    TIMSK2 &= ~(1 << TOIE2);
+
+    rtt_state.overflow_cb = NULL;
+    rtt_state.overflow_arg = NULL;
+}
+
+uint32_t rtt_get_counter(void) {
+    /* Make sure it is safe to read TCNT2, in case we just woke up */
+    DEBUG("RTT sleeps until safe to read TCNT2\n");
+    TCCR2A = TCCR2A;
+    while( ASSR & (1 << TCR2AUB) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 2 RTC cycles */
+        xtimer_usleep(70 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+
+    return (((uint32_t)rtt_state.ext_cnt[2] << 24)
+          | ((uint32_t)rtt_state.ext_cnt[1] << 16)
+          | ((uint32_t)rtt_state.ext_cnt[0] << 8)
+          |  (uint32_t)TCNT2);
+}
+
+void rtt_set_counter(uint32_t counter) {
+    /* Wait until not busy anymore (should be immediate) */
+    DEBUG("RTT sleeps until safe to write TCNT2\n");
+    while( ASSR & (1 << TCN2UB) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 1 RTC cycle */
+        xtimer_usleep(35 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+
+    rtt_state.ext_cnt[2] = (uint8_t)(counter >> 24);
+    rtt_state.ext_cnt[1] = (uint8_t)(counter >> 16);
+    rtt_state.ext_cnt[0] = (uint8_t)(counter >> 8);
+    TCNT2 = (uint8_t)counter;
+
+    /* Wait until it is safe to re-enter power-save */
+    DEBUG("RTT sleeps until safe power-save\n");
+    while( ASSR & (1 << TCN2UB) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 2 RTC cycles */
+        xtimer_usleep(70 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+}
+
+void rtt_set_alarm(uint32_t alarm, rtt_cb_t cb, void *arg) {
+    /* Disable alarm interrupt */
+    TIMSK2 &= ~(1 << OCIE2A);
+
+    /* Set callback */
+    rtt_state.alarm_cb = cb;
+    rtt_state.alarm_arg = arg;
+
+    /* Wait until not busy anymore (should be immediate) */
+    DEBUG("RTT sleeps until safe to write OCR2A\n");
+    while( ASSR & (1 << OCR2AUB) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 1 RTC cycle */
+        xtimer_usleep(35 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+
+    /* Set the alarm value */
+    rtt_state.ext_comp[2] = (uint8_t)(alarm >> 24);
+    rtt_state.ext_comp[1] = (uint8_t)(alarm >> 16);
+    rtt_state.ext_comp[0] = (uint8_t)(alarm >> 8);
+    OCR2A = (uint8_t)alarm;
+
+    /* Wait until alarm value takes effect */
+    DEBUG("RTT sleeps until safe power-save\n");
+    while( ASSR & (1 << OCR2AUB) ) {
+#ifdef MODULE_XTIMER
+        /* Sleep for 2 RTC cycles */
+        xtimer_usleep(70 * US_PER_MS);
+#else
+        thread_yield();
+#endif
+    }
+
+    /* Enable alarm interrupt only if it will trigger before overflow */
+    if ( rtt_state.ext_comp[2] == rtt_state.ext_cnt[2]
+      && rtt_state.ext_comp[1] == rtt_state.ext_cnt[1]
+      && rtt_state.ext_comp[0] == rtt_state.ext_cnt[0] ) {
+        TIMSK2 |= (1 << OCIE2A);
+    }
+}
+
+uint32_t rtt_get_alarm(void) {
+    return (((uint32_t)rtt_state.ext_comp[2] << 24)
+          | ((uint32_t)rtt_state.ext_comp[1] << 16)
+          | ((uint32_t)rtt_state.ext_comp[0] << 8)
+          |  (uint32_t)OCR2A);
+}
+
+void rtt_clear_alarm(void) {
+    /* Disable alarm interrupt */
+    TIMSK2 &= ~(1 << OCIE2A);
+
+    rtt_state.alarm_cb = NULL;
+    rtt_state.alarm_arg = NULL;
+}
+
+void rtt_poweron(void) {
+    power_timer2_enable();
+}
+
+void rtt_poweroff(void) {
+    power_timer2_disable();
+}
+
+ISR(TIMER2_OVF_vect) {
+    __enter_isr();
+    /* Enable RTT alarm if overflowed enough times */
+    if ((rtt_state.ext_comp[2] == rtt_state.ext_cnt[2])
+     && (rtt_state.ext_comp[1] == rtt_state.ext_cnt[1])
+     && (rtt_state.ext_comp[0] == rtt_state.ext_cnt[0])) {
+        TIMSK2 |= (1 << OCIE2A);
+    }
+
+#if RTC_NUMOF
+    /* Increment RTC by 8 seconds */
+    atmega_rtc_incr();
+#endif
+
+    /* Virtual 32-bit timer overflow */
+    if (rtt_state.ext_cnt[0] == 0xff) {
+        rtt_state.ext_cnt[0] = 0;
+
+        if (rtt_state.ext_cnt[1] == 0xff) {
+            rtt_state.ext_cnt[1] = 0;
+
+            if (rtt_state.ext_cnt[2] == 0xff) {
+                rtt_state.ext_cnt[2] = 0;
+
+                /* Execute callback */
+                if (rtt_state.overflow_cb != NULL) {
+                    rtt_state.overflow_cb(rtt_state.overflow_arg);
+                }
+            } else {
+                rtt_state.ext_cnt[2]++;
+            }
+        } else {
+            rtt_state.ext_cnt[1]++;
+        }
+    } else {
+        rtt_state.ext_cnt[0]++;
+    }
+
+    /* Wait until it is safe to re-enter power-save */
+    TCCR2A = TCCR2A;
+    while( ASSR & (1 << TCR2AUB) ) {
+        if (sched_context_switch_request) {
+            thread_yield();
+        }
+    }
+    __exit_isr();
+}
+
+ISR(TIMER2_COMPA_vect) {
+    __enter_isr();
+    /* Disable alarm until overflowed enough times */
+    TIMSK2 &= ~(1 << OCIE2A);
+
+    /* Execute callback */
+    if (rtt_state.alarm_cb != NULL) {
+        rtt_state.alarm_cb(rtt_state.alarm_arg);
+    }
+
+    /* Wait until it is safe to re-enter power-save */
+    TCCR2A = TCCR2A;
+    while( ASSR & (1 << TCR2AUB) ) {
+        if (sched_context_switch_request) {
+            thread_yield();
+        }
+    }
+    __exit_isr();
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RTT_NUMOF */
