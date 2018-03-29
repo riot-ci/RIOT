7382a19d3da36d47ff6673c597b96094607f30a1
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-03-29 08:54:16 +0200

    squash Refactor result presentation

--- tests/bench_periph_timer/print_results.h
+++ tests/bench_periph_timer/print_results.h
@@ -25,19 +25,36 @@
  extern "C" {
 #endif
 
+/* Test limits */
+typedef struct {
+    int32_t mean_low;
+    int32_t mean_high;
+    uint32_t variance_low;
+    uint32_t variance_high;
+} stat_limits_t;
+
+typedef struct  {
+    const char *label;
+    const char **sub_labels;
+    unsigned num_sub_labels;
+} result_group_t;
+
+typedef struct {
+    const result_group_t *groups;
+    unsigned num_groups;
+    const stat_limits_t *ref_limits; /* Reference stats limits */
+    const stat_limits_t *int_limits; /* Introspective stats limits */
+    const unsigned *offsets; /* Pointer to array of input offsets, for correctly printing the detailed stats */
+} result_presentation_t;
+
 /**
  * @brief   Present the results of the benchmark
  *
  * Depends on DETAILED_STATS, LOG2_STATS
- * @param[in]   states      State vector from test run
- * @param[in]   read_states State vector from test run
+ * @param[in]   ref_states  State vector from reference measurements
+ * @param[in]   int_states  State vector from introspective measurements
  */
-void print_results(const matstat_state_t *states, const matstat_state_t *read_states);
-
-/**
- * @brief   Calculate the limits for mean and variance for this test
- */
-void set_limits(void);
+void print_results(const result_presentation_t *pres, const matstat_state_t *ref_states, const matstat_state_t *int_states);
 
 #ifdef __cplusplus
 }
