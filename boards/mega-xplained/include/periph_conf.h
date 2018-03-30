f1bf841d01da7fa4ced4979189d7d633219862ec
Matthew Blue <matthew.blue.neuro@gmail.com>
2018-03-30 16:04:45 -0400

    fixup! boards/mega-xplained: RTT, RTC support

--- boards/mega-xplained/include/periph_conf.h
+++ boards/mega-xplained/include/periph_conf.h
@@ -78,7 +78,7 @@ extern "C" {
  * @{
  */
 #define RTT_NUMOF        (1U)
-#define RTT_MAX_VALUE    (0xffffffff)
+#define RTT_MAX_VALUE    (0x00FFFFFF)    /* 24-bit timer */
 #define RTT_FREQUENCY    (32U)
 /** @} */
 
