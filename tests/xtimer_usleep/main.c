3a65d23c149d9bff488116f65fc84ef6a849224e
Josarn <josuaarndt@live.de>
2018-04-03 19:53:45 +0200

    tests/xtimer_usleep: readme, test improved

--- tests/xtimer_usleep/main.c
+++ tests/xtimer_usleep/main.c
@@ -33,60 +33,57 @@
 
 static const uint32_t sleep_times[] = { 10000, 50000, 1234, 5678, 121212, 98765, 75000};
 
-#define ERROR_US 500
+#define ERROR_US 70
 
 /*
  * To use a pin to probe the sleep times enable the gpio module and define
  * respective SLEEP_GPIO_PIN and set SLEEP_PIN to (1)
  * */
 
-#define SLEEP_PIN (0)
-#if SLEEP_PIN
+#ifdef SLEEP_PIN
 #include "board.h"
 #include "periph/gpio.h"
-#define SLEEP_GPIO_PIN  GPIO_PIN(PORT_F, 7)
+#define SLEEP_GPIO_PIN GPIO_PIN(SLEEP_PORT, SLEEP_PIN)
 #endif
 int main(void)
 {
     uint32_t start_test, testtime;
-    uint32_t start_sleep;
-    uint32_t diff;
-    int32_t err;
 
-#if SLEEP_PIN
-    gpio_init( SLEEP_GPIO_PIN, GPIO_OUT );
+#ifdef SLEEP_PIN
+    gpio_init(SLEEP_GPIO_PIN, GPIO_OUT);
 #endif
 
     printf("Running test %u times with %u distinct sleep times\n", RUNS,
            (unsigned)SLEEP_TIMES_NUMOF);
-    puts("Please hit any key and then ENTER to continue\n");
-    // getchar();
+    puts("Please hit any key and then ENTER to continue");
+    getchar();
     start_test = xtimer_now_usec();
     for (unsigned m = 0; m < RUNS; m++) {
         for (unsigned n = 0;
              n < sizeof(sleep_times) / sizeof(sleep_times[0]);
              n++) {
 
+            uint32_t start_sleep, diff;
+
             start_sleep = xtimer_now_usec();
 
-#if SLEEP_PIN
+#ifdef SLEEP_PIN
             gpio_set(SLEEP_GPIO_PIN);
 #endif
             xtimer_usleep(sleep_times[n]);
-#if SLEEP_PIN
+#ifdef SLEEP_PIN
             gpio_clear(SLEEP_GPIO_PIN);
 #endif
 
             diff = xtimer_now_usec() - start_sleep;
 
-            err = (diff - sleep_times[n]);
-
-            if( err < 0 ){
-                err *= -1;
-            }
-          if(  err > ERROR_US ){
-                printf("\n\t\tSlept for %" PRIu32 " us (expected: %" PRIu32 " us)"
-                        "error: %" PRIi32 " us\n",diff, sleep_times[n], err);
+            if(sleep_times[n] < diff - ERROR_US
+              ||sleep_times[n] > diff + ERROR_US ){
+                int32_t err = (diff - sleep_times[n]);
+                printf("\nSlept for %" PRIu32 " us (expected: %" PRIu32 " us)",
+                        diff, sleep_times[n]);
+                printf("\nMCU max error\t%5" PRIi32 "\nerror\t\t%5" PRIi32 "",
+                        (uint32_t)ERROR_US, err);
             }else{
                 printf("Slept for %" PRIu32 " us (expected: %" PRIu32 " us)\n",
                                    diff, sleep_times[n]);
@@ -94,9 +91,7 @@ int main(void)
         }
     }
     testtime = xtimer_now_usec() - start_test;
-    printf("\nTest ran for %" PRIu32 " us\n", testtime);
+    printf("Test ran for %" PRIu32 " us\n", testtime);
 
-    /* atmega mcu will hang if main ends */
-    while(1){}
     return 0;
 }
