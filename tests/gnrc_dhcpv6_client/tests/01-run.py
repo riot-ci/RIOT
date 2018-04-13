6715b201797b648843924f70d7bb7f7d4e9e3046
Martine Lenders <m.lenders@fu-berlin.de>
2018-04-13 17:11:52 +0200

    fixup! tests: add dhcpv6_client test application
    Add also prefix lifetimes to test

--- tests/gnrc_dhcpv6_client/tests/01-run.py
+++ tests/gnrc_dhcpv6_client/tests/01-run.py
@@ -6,9 +6,11 @@
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-
+import json
 import os
 import sys
+
+
 
 
 def testfunc(child):
@@ -17,6 +19,11 @@ def testfunc(child):
     child.expect(r"Iface\s+\d\s+HWaddr: [0-9a-f:]+")
     child.expect(r"inet6 addr:\s+fe80:[0-9a-f:]+\s+scope: local")
     child.expect(r"inet6 addr:\s+[0-9a-f:]+\s+scope: global")
+    child.expect(r"[0-9a-f:]/64\s+dev #\d\s+"
+                 r"expires (?P<valid_ltime>\d+)sec\s+"
+                 r"deprecates (?P<pref_ltime>\d+)sec")
+    assert(int(child.match.group("valid_ltime")) < 4000)
+    assert(int(child.match.group("pref_ltime")) < 3000)
 
 
 if __name__ == "__main__":
