c4ac8cf6e7031fb0ed0de9160a779a0f93c7bc83
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-04-25 07:26:12 +0200

    squash matstat: Add workaround for negative variance in add

--- sys/matstat/matstat.c
+++ sys/matstat/matstat.c
@@ -34,7 +34,14 @@ void matstat_add(matstat_state_t *state, int32_t value)
     }
     else {
         int32_t new_mean = state->sum / state->count;
-        state->sum_sq += (value - state->mean) * (value - new_mean);
+        int64_t diff = (value - state->mean) * (value - new_mean);
+        if ((diff < 0) && ((uint64_t)(-diff) > state->sum_sq)) {
+            /* Handle corner cases where sum_sq becomes negative */
+            state->sum_sq = 0;
+        }
+        else {
+            state->sum_sq += diff;
+        }
         state->mean = new_mean;
     }
 }
