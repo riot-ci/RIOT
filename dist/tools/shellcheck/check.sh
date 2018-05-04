6f2449e3210ddd3a0b6662f1676c5edd35432f95
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-05-04 14:50:47 +0200

    fixup! fixup! tools/check: use RIOTTOOLS variable

---
+++ dist/tools/shellcheck/check.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2018 Freie Universität Berlin
+#                    Inria
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+#
+
+SHELLCHECK_CMD="$(which shellcheck)"
+export SHELLCHECK_OPTS="-e SC1090"
+
+if tput colors &> /dev/null && [ "$(tput colors)" -ge 8 ]; then
+    CERROR=$'\e[1;31m'
+    CRESET=$'\e[0m'
+else
+    CERROR=
+    CRESET=
+fi
+
+DIST_TOOLS=${RIOTBASE:-.}/dist/tools
+
+. "${DIST_TOOLS}/ci/changed_files.sh"
+
+FILES=$(FILEREGEX='(?=*.sh$)' changed_files)
+
+if [ -z "${FILES}" ]
+then
+    exit 0
+fi
+
+${SHELLCHECK_CMD} --version &> /dev/null || {
+    printf "%sError: \"%s\" is missing%s\n" "${CERROR}" "${SHELLCHECK_CMD}" "${CRESET}"
+    exit 1
+}
+
+ERRORS=$("${SHELLCHECK_CMD}" --format=gcc ${FILES})
+
+if [ -n "${ERRORS}" ]
+then
+    printf "%s There are issues in the following shell scripts %s\n" "${CERROR}" "${CRESET}"
+    printf "%s\n" "${ERRORS}"
+    exit 1
+else
+    exit 0
+fi
