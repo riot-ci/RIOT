ecc303b71fdc94e01bf0ce0bd1dcfa91425ad3d6
Juan Carrano <j.carrano@fu-berlin.de>
2018-05-07 11:56:42 +0200

    pkg/lua: Reduce stack usage and add repl.

--- examples/lua_REPL/main.c
+++ examples/lua_REPL/main.c
@@ -19,27 +19,30 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 #include "lua_run.h"
 #include "repl.lua.h"
 
-#define MAIN_LUA_MEM_SIZE (8192*3)
+#define MAIN_LUA_MEM_SIZE (20000)
 
-char lua_memory[MAIN_LUA_MEM_SIZE];
-static const size_t lua_memory_size = MAIN_LUA_MEM_SIZE;
+char lua_memory[MAIN_LUA_MEM_SIZE] __attribute__ ((aligned (__BIGGEST_ALIGNMENT__)));
 
 #define BARE_MINIMUM_MODS (LUAR_LOAD_BASE|LUAR_LOAD_IO|LUAR_LOAD_CORO)
 
 int main(void)
 {
+    printf("%p - %p %u\n", lua_memory, lua_memory+MAIN_LUA_MEM_SIZE, sizeof(void*));
+
     while (1) {
         int status, value;
         puts("This is Lua: starting interactive session\n");
 
-        status = luaR_do_buffer(repl_lua, repl_lua_len, lua_memory, lua_memory_size,
+        status = luaR_do_buffer(repl_lua, repl_lua_len, lua_memory, MAIN_LUA_MEM_SIZE,
                                 BARE_MINIMUM_MODS, &value);
 
         printf("Exited. status: %d, return code %d\n", status, value);
+        break;
     }
 
     return 0;
