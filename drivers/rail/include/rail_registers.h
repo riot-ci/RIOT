64ec02d09fc28794571d61b0ab374c3af7c755d8
Kai Beckmann <kai.beckmann@hs-rm.de>
2018-05-11 17:09:51 +0200

    squashed rail driver from older branches

---
+++ drivers/rail/include/rail_registers.h
@@ -0,0 +1,16 @@
+
+#ifndef  RAIL_REGISTERS_H_INC
+#define  RAIL_REGISTERS_H_INC
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif   /* ----- #ifndef RAIL_REGISTERS_H_INC  ----- */
