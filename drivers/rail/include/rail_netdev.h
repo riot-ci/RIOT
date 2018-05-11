465cca52bbe88622d544d5133ae3b46c77f4fbec
Kai Beckmann <kai.beckmann@hs-rm.de>
2018-05-11 17:09:51 +0200

    squashed rail driver from older branches

---
+++ drivers/rail/include/rail_netdev.h
@@ -0,0 +1,17 @@
+
+#ifndef  RAIL_NETDEV_H_INC
+#define  RAIL_NETDEV_H_INC
+
+#include "net/netdev.h"
+
+extern const netdev_driver_t rail_driver;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif   /* ----- #ifndef RAIL_NETDEV_H_INC  ----- */
