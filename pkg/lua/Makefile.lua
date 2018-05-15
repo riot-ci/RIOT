bdd99d7a38d3d049fc850521e1ce6f2bff5b2d95
Juan Carrano <j.carrano@fu-berlin.de>
2018-05-15 14:43:11 +0200

    pkg/lua: Support test module.
    Compile with -DLUA_DEBUG to enable the test module. Additionally
     -DDEBUG_OVERRIDE_SIZES changes some buffer sizes to make it more
    likely that the test catch a bug.

--- pkg/lua/Makefile.lua
+++ pkg/lua/Makefile.lua
@@ -1,6 +1,5 @@
 SRC := $(filter-out loadlib.c lua.c luac.c,$(wildcard *.c))
 
-
 # This is very important because lua defaults to 64 bit integers
 CFLAGS += -DLUA_32BITS -fstack-usage -Wstack-usage=128 \
           -Wno-error=stack-usage=128 -fconserve-stack \
