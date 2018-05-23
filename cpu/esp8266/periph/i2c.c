61d7721274fe5df71b3dac8193ff9bd309e87942
Gunar Schorcht <gunar@schorcht.net>
2018-05-23 08:39:39 +0200

    esp8266: add support for ESP8266

---
+++ cpu/esp8266/periph/i2c.c
@@ -0,0 +1,591 @@
+/*
+ * Copyright (C) 2018 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup cpu_esp8266
+ * @ingroup drivers_periph_i2c
+ * @{
+ *
+ * @file
+ * @brief       Low-level I2C driver implementation using ESP8266 SDK
+ *
+ * @note        This implementation only implements the 7-bit addressing mode.
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ *
+ * @}
+ */
+
+/*
+   PLEASE NOTE:
+
+   Some parts of the implementation bases on the bit-banging implementation as
+   described in [wikipedia](https://en.wikipedia.org/wiki/I%C2%B2C) as well as
+   its implementation in [esp-open-rtos](https://github.com/SuperHouse/esp-open-rtos.git).
+   These parts are under the copyright of their respective owners.
+*/
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+#include <stdbool.h>
+
+#include "cpu.h"
+#include "log.h"
+#include "mutex.h"
+#include "periph_conf.h"
+#include "periph/gpio.h"
+#include "periph/i2c.h"
+
+#include "common.h"
+#include "sdk/ets.h"
+#include "esp/gpio_regs.h"
+
+#if defined(I2C_NUMOF) && I2C_NUMOF > 0
+
+/* has to be declared as extern since it is not possible to include */
+/* user_interface.h due to conflicts with gpio_init */
+extern uint8_t system_get_cpu_freq(void);
+extern bool system_update_cpu_freq(uint8_t freq);
+
+/* max clock stretching counter */
+#define I2C_CLOCK_STRETCH 200
+
+typedef struct
+{
+    i2c_speed_t speed;
+
+    bool started;
+
+    gpio_t scl;
+    gpio_t sda;
+
+    uint32_t scl_bit;   /* gpio bit mask for faster access */
+    uint32_t sda_bit;   /* gpio bit mask for faster access */
+
+    uint32_t delay;
+
+} _i2c_bus_t;
+
+static _i2c_bus_t _i2c_bus[] =
+{
+  #if defined(I2C_SDA_0) && defined(I2C_SDA_0)
+  {
+    .sda = I2C_SDA_0,
+    .scl = I2C_SCL_0
+  },
+  #endif
+  #if defined(I2C_SDA_1) && defined(I2C_SDA_1)
+  {
+    .sda = I2C_SDA_1,
+    .scl = I2C_SCL_1
+  },
+  #endif
+  #if defined(I2C_SDA_2) && defined(I2C_SDA_2)
+  {
+    .sda = I2C_SDA_2,
+    .scl = I2C_SCL_2
+  },
+  #endif
+};
+
+static const uint32_t _i2c_delays[][2] =
+{
+    /* values specify one half-period and are only valid for -O2 option */
+    /* value = [period - 0.5us(160MHz) or 1.0us(80MHz)] * cycles per second / 2 */
+    /* cycles per us = ca. 20 (80 MHz) / ca. 40 (160 MHz) */
+    [I2C_SPEED_LOW]       = {1990, 990}, /*   10 kbps (period 100 us) */
+    [I2C_SPEED_NORMAL]    = { 190,  90}, /*  100 kbps (period 10 us) */
+    [I2C_SPEED_FAST]      = {  40,  17}, /*  400 kbps (period 2.5 us) */
+    [I2C_SPEED_FAST_PLUS] = {  13,   0}, /*    1 Mbps (period 1 us) */
+    [I2C_SPEED_HIGH]      = {   0,   0}  /*  3.4 Mbps (period 0.3 us) not working */
+};
+
+static mutex_t i2c_bus_lock[I2C_NUMOF] = { MUTEX_INIT };
+
+/* forward declaration of internal functions */
+
+static inline void _i2c_delay (_i2c_bus_t* bus);
+static inline bool _i2c_read_scl (_i2c_bus_t* bus);
+static inline bool _i2c_read_sda (_i2c_bus_t* bus);
+static inline void _i2c_set_scl (_i2c_bus_t* bus);
+static inline void _i2c_clear_scl (_i2c_bus_t* bus);
+static inline void _i2c_set_sda (_i2c_bus_t* bus);
+static inline void _i2c_clear_sda (_i2c_bus_t* bus);
+static void _i2c_start_cond (_i2c_bus_t* bus);
+static void _i2c_stop_cond (_i2c_bus_t* bus);
+static void _i2c_write_bit (_i2c_bus_t* bus, bool bit);
+static bool _i2c_read_bit (_i2c_bus_t* bus);
+static bool _i2c_write_byte (_i2c_bus_t* bus, uint8_t byte);
+static uint8_t _i2c_read_byte (_i2c_bus_t* bus, bool ack);
+
+/* implementation of i2c interface */
+
+int i2c_init_master(i2c_t dev, i2c_speed_t speed)
+{
+    if (I2C_NUMOF != sizeof(_i2c_bus)/sizeof(_i2c_bus_t)) {
+        LOG_INFO("I2C_NUMOF does not match number of the I2C_SDA_x/I2C_SCL_x definitions\n");
+        LOG_INFO("Please check your configuration in file board.h\n");
+        assert(I2C_NUMOF < sizeof(_i2c_bus)/sizeof(_i2c_bus_t));
+
+        return -1;
+    }
+
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1)
+
+    if (speed == I2C_SPEED_HIGH) {
+        LOG_INFO("%s I2C_SPEED_HIGH is not supported\n");
+        return -2;
+    }
+
+    _i2c_bus[dev].speed   = speed;
+    _i2c_bus[dev].delay   =_i2c_delays[speed][ets_get_cpu_frequency() == 80 ? 1 : 0];
+    _i2c_bus[dev].scl_bit = BIT(_i2c_bus[dev].scl); /* store bit mask for faster access */
+    _i2c_bus[dev].sda_bit = BIT(_i2c_bus[dev].sda); /* store bit mask for faster access */
+    _i2c_bus[dev].started = false; /* for handling of repeated start condition */
+
+    DEBUG ("%s scl=%d sda=%d speed=%d\n", __func__,
+           _i2c_bus[dev].scl, _i2c_bus[dev].sda, _i2c_bus[dev].speed);
+
+    /* configure SDA and SCL pin as GPIO in open-drain mode with enabled pull-ups */
+    gpio_init (_i2c_bus[dev].scl, GPIO_OD_PU);
+    gpio_init (_i2c_bus[dev].sda, GPIO_OD_PU);
+
+    /* set SDA and SCL to be floating and pulled-up to high */
+    _i2c_set_sda (&_i2c_bus[dev]);
+    _i2c_set_scl (&_i2c_bus[dev]);
+
+    return 0;
+}
+
+int i2c_acquire(i2c_t dev)
+{
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1)
+
+    mutex_lock(&i2c_bus_lock[dev]);
+    return 0;
+}
+
+int i2c_release(i2c_t dev)
+{
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1)
+
+    mutex_unlock(&i2c_bus_lock[dev]);
+    return 0;
+}
+
+int /* IRAM */ i2c_read_byte(i2c_t dev, uint8_t address, void *data)
+{
+    return i2c_read_bytes(dev, address, data, 1);
+}
+
+int /* IRAM */ i2c_read_bytes(i2c_t dev, uint8_t address, void *data, int length)
+{
+    DEBUG ("%s dev=%lu addr=%02x data=%p len=%d\n", __func__, dev, address, data, length);
+
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1);
+    CHECK_PARAM_RET (length > 0, 0);
+    CHECK_PARAM_RET (data != NULL, 0);
+
+    _i2c_bus_t* bus = &_i2c_bus[dev];
+    int i = 0;
+
+    /* send START condition */
+    _i2c_start_cond (bus);
+
+    /* send address byte with read flag */
+    if (_i2c_write_byte (bus, (address << 1) | I2C_FLAG_READ)) {
+        /* receive bytes if send address was successful */
+        for ( ; i < length; i++) {
+            ((uint8_t*)data)[i] = _i2c_read_byte (bus, i < length-1);
+        }
+    }
+    /* send STOP condition */
+    _i2c_stop_cond (bus);
+
+    return i;
+}
+
+int /* IRAM */ i2c_read_reg(i2c_t dev, uint8_t address, uint8_t reg, void *data)
+{
+    return i2c_read_regs(dev, address, reg, data, 1);
+}
+
+int /* IRAM */ i2c_read_regs(i2c_t dev, uint8_t address, uint8_t reg, void *data, int length)
+{
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1)
+    CHECK_PARAM_RET (length > 0, 0)
+    CHECK_PARAM_RET (data != NULL, 0);
+
+    _i2c_bus_t* bus = &_i2c_bus[dev];
+
+    /* send START condition */
+    _i2c_start_cond (bus);
+
+    /* send address with read flag and if successful */
+    /* send register address */
+    if (!_i2c_write_byte (bus, (address << 1) | I2C_FLAG_WRITE) ||
+        !_i2c_write_byte (bus, reg)) {
+        /* if not successful send STOP condition and return 0 */
+        _i2c_stop_cond (bus);
+        return 0;
+    }
+
+    /* receive data with repeated START condition if send address and register was successful */
+    return i2c_read_bytes(dev, address, data, length);
+}
+
+int /* IRAM */ i2c_write_byte(i2c_t dev, uint8_t address, uint8_t data)
+{
+    return i2c_write_bytes(dev, address, &data, 1);
+}
+
+int /* IRAM */ i2c_write_bytes(i2c_t dev, uint8_t address, const void *data, int length)
+{
+    DEBUG ("%s dev=%lu addr=%02x data=%p len=%d\n", __func__, dev, address, data, length);
+
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1);
+    CHECK_PARAM_RET (length > 0, 0);
+    CHECK_PARAM_RET (data != NULL, 0);
+
+    _i2c_bus_t* bus = &_i2c_bus[dev];
+    int i = 0;
+
+    /* send START condition */
+    _i2c_start_cond (bus);
+
+    /* send address with read flag */
+    if (!_i2c_write_byte (bus, (address << 1) | I2C_FLAG_WRITE)) {
+        length = 0;
+    }
+
+    /* send bytes if send address was successful */
+    for ( ; i < length; i++) {
+        if (!_i2c_write_byte (bus, ((uint8_t*)data)[i])) {
+            break;
+        }
+    }
+
+    /* send STOP condition */
+    _i2c_stop_cond (bus);
+
+    return i;
+}
+
+int /* IRAM */ i2c_write_reg(i2c_t dev, uint8_t address, uint8_t reg, uint8_t data)
+{
+    return i2c_write_regs(dev, address, reg, &data, 1);
+}
+
+int /* IRAM */ i2c_write_regs(i2c_t dev, uint8_t address, uint8_t reg, const void *data, int length)
+{
+    CHECK_PARAM_RET (dev < I2C_NUMOF, -1)
+    CHECK_PARAM_RET (length > 0, 0)
+    CHECK_PARAM_RET (data != NULL, 0);
+
+    _i2c_bus_t* bus = &_i2c_bus[dev];
+    int i = 0;
+
+    /* send START condition */
+    _i2c_start_cond (bus);
+
+    /* send address with read flag and if successful */
+    /* send register address */
+    if (!_i2c_write_byte (bus, (address << 1) | I2C_FLAG_WRITE) ||
+        !_i2c_write_byte (bus, reg)) {
+        length = 0;
+    }
+
+    /* send bytes if send address and register was successful */
+    for ( ; i < length; i++) {
+        if (!_i2c_write_byte (bus, ((uint8_t*)data)[i])) {
+            break;
+        }
+    }
+
+    /* send STOP condition */
+    _i2c_stop_cond (bus);
+
+    return i;
+}
+
+void i2c_poweron(i2c_t dev)
+{
+    /* since I2C is realized in software there is no device to power on */
+    /* just return */
+}
+
+void i2c_poweroff(i2c_t dev)
+{
+    /* since I2C is realized in software there is no device to power off */
+    /* just return */
+}
+
+/* --- internal functions --- */
+
+static inline void _i2c_delay (_i2c_bus_t* bus)
+{
+    /* produces a delay */
+    /* ca. 20 cycles = 1 us (80 MHz) or ca. 40 cycles = 1 us (160 MHz) */
+
+    uint32_t cycles = bus->delay;
+    if (cycles) {
+        __asm__ volatile ("1:     _addi.n  %0, %0, -1 \n"
+                          "       bnez     %0, 1b     \n" : "=r" (cycles) : "0" (cycles));
+    }
+}
+
+/*
+ * Please note: SDA and SDL pins are used in GPIO_OD_PU mode
+ *              (open-drain with pull-ups).
+ *
+ * Setting a pin which is in open-drain mode leaves the pin floating and
+ * the signal is pulled up to high. The signal can then be actively driven
+ * to low by a slave. A read operation returns the current signal at the pin.
+ *
+ * Clearing a pin which is in open-drain mode actively drives the signal to
+ * low.
+ */
+
+static inline bool _i2c_read_scl(_i2c_bus_t* bus)
+{
+    /* read SCL status (pin is in open-drain mode and set) */
+    return GPIO.IN & bus->scl_bit;
+}
+
+static inline bool _i2c_read_sda(_i2c_bus_t* bus)
+{
+    /* read SDA status (pin is in open-drain mode and set) */
+    return GPIO.IN & bus->sda_bit;
+}
+
+static inline void _i2c_set_scl(_i2c_bus_t* bus)
+{
+    /* set SCL signal high (pin is in open-drain mode and pulled-up) */
+    GPIO.OUT_SET = bus->scl_bit;
+}
+
+static inline void _i2c_clear_scl(_i2c_bus_t* bus)
+{
+    /* set SCL signal low (actively driven to low) */
+    GPIO.OUT_CLEAR = bus->scl_bit;
+}
+
+static inline void _i2c_set_sda(_i2c_bus_t* bus)
+{
+    /* set SDA signal high (pin is in open-drain mode and pulled-up) */
+    GPIO.OUT_SET = bus->sda_bit;
+}
+
+static inline void _i2c_clear_sda(_i2c_bus_t* bus)
+{
+    /* set SDA signal low (actively driven to low) */
+    GPIO.OUT_CLEAR = bus->sda_bit;
+}
+
+static /* IRAM */ void _i2c_start_cond(_i2c_bus_t* bus)
+{
+    /*
+     * send start condition
+     * on entry: SDA and SCL are set to be floating and pulled-up to high
+     * on exit : SDA and SCL are actively driven to low
+     */
+
+    if (bus->started) {
+        /* prepare the repeated start condition */
+
+        /* SDA = passive HIGH (floating and pulled-up) */
+        _i2c_set_sda (bus);
+
+        /* t_VD;DAT not neccessary */
+        /* _i2c_delay (bus); */
+
+        /* SCL = passive HIGH (floating and pulled-up) */
+        _i2c_set_scl (bus);
+
+        /* clock stretching wait as long as clock is driven to low by the slave */
+        uint32_t stretch = I2C_CLOCK_STRETCH;
+        while (!_i2c_read_scl (bus) && stretch--) {}
+
+        /* wait t_SU;STA - set-up time for a repeated START condition */
+        /* min. in us: 4.7 (SM), 0.6 (FM), 0.26 (FPM), 0.16 (HSM); no max. */
+        _i2c_delay (bus);
+    }
+
+    /* if SDA is low, arbitration is lost and someone else is driving the bus */
+    if (_i2c_read_sda (bus) == 0) {
+        DEBUG("%s arbitration lost dev\n", __func__);
+    }
+
+    /* begin the START condition: SDA = active LOW */
+    _i2c_clear_sda (bus);
+
+    /* wait t_HD;STA - hold time (repeated) START condition, */
+    /* max none */
+    /* min 4.0 us (SM), 0.6 us (FM), 0.26 us (FPM), 0.16 us (HSM) */
+    _i2c_delay (bus);
+
+    /* complete the START condition: SCL = active LOW */
+    _i2c_clear_scl (bus);
+
+    /* needed for repeated start condition */
+    bus->started = true;
+}
+
+static /* IRAM */ void _i2c_stop_cond(_i2c_bus_t* bus)
+{
+    /*
+     * send stop condition
+     * on entry: SCL is active low and SDA can be changed
+     * on exit : SCL and SDA are set to be floating and pulled-up to high
+     */
+
+    /* begin the STOP condition: SDA = active LOW */
+    _i2c_clear_sda (bus);
+
+    /* wait t_LOW - LOW period of SCL clock */
+    /* min. in us: 4.7 (SM), 1.3 (FM), 0.5 (FPM), 0.16 (HSM); no max. */
+    _i2c_delay (bus);
+
+    /* SCL = passive HIGH (floating and pulled up) while SDA = active LOW */
+    _i2c_set_scl (bus);
+
+    /* clock stretching wait as long as clock is driven to low by the slave */
+    uint32_t stretch = I2C_CLOCK_STRETCH;
+    while (!_i2c_read_scl (bus) && stretch--) {}
+
+    /* wait t_SU;STO - hold time (repeated) START condition, */
+    /* min. in us: 4.0 (SM), 0.6 (FM), 0.26 (FPM), 0.16 (HSM); no max. */
+    _i2c_delay (bus);
+
+    /* complete the STOP condition: SDA = passive HIGH (floating and pulled up) */
+    _i2c_set_sda (bus);
+
+    /* wait t_BUF - bus free time between a STOP and a START condition */
+    /* min. in us: 4.7 (SM), 1.3 (FM), 0.5 (FPM), 0.16 (HSM); no max. */
+    _i2c_delay (bus);
+
+    /* if SDA is low, arbitration is lost and someone else is driving the bus */
+    if (_i2c_read_sda (bus) == 0) {
+        DEBUG("%s arbitration lost dev\n", __func__);
+    }
+
+    bus->started = false;
+}
+
+static /* IRAM */ void _i2c_write_bit (_i2c_bus_t* bus, bool bit)
+{
+    /*
+     * send one bit
+     * on entry: SCL is active low, SDA can be changed
+     * on exit : SCL is active low, SDA can be changed
+     */
+
+    /* SDA = bit */
+    if (bit) {
+        _i2c_set_sda (bus);
+    }
+    else {
+        _i2c_clear_sda (bus);
+    }
+
+    /* wait t_VD;DAT - data valid time (time until data are valid) */
+    /* max. in us: 3.45 (SM), 0.9 (FM), 0.45 (FPM); no min */
+    _i2c_delay (bus);
+
+    /* SCL = passive HIGH (floating and pulled-up), SDA value is available */
+    _i2c_set_scl (bus);
+
+    /* wait t_HIGH - time for the slave to read SDA */
+    /* min. in us: 4 (SM), 0.6 (FM), 0.26 (FPM), 0.09 (HSM); no max. */
+    _i2c_delay (bus);
+
+    /* clock stretching wait as long as clock is driven low by the slave */
+    uint32_t stretch = I2C_CLOCK_STRETCH;
+    while (!_i2c_read_scl (bus) && stretch--) {}
+
+    /* if SCL is high, now data is valid */
+    /* if SDA is high, check that nobody else is driving SDA low */
+    if (bit && !_i2c_read_sda(bus)) {
+        DEBUG("%s arbitration lost dev\n", __func__);
+    }
+
+    /* SCL = active LOW to allow next SDA change */
+    _i2c_clear_scl(bus);
+}
+
+static /* IRAM */ bool _i2c_read_bit (_i2c_bus_t* bus)
+{
+    /* read one bit
+     * on entry: SCL is active low, SDA can be changed
+     * on exit : SCL is active low, SDA can be changed
+     */
+
+    bool bit;
+
+    /* SDA = passive HIGH (floating and pulled-up) to let the slave drive data */
+    _i2c_set_sda (bus);
+
+    /* wait t_VD;DAT - data valid time (time until data are valid) */
+    /* max. in us: 3.45 (SM), 0.9 (FM), 0.45 (FPM); no min */
+    _i2c_delay (bus);
+
+    /* SCL = passive HIGH (floating and pulled-up), SDA value is available */
+    _i2c_set_scl (bus);
+
+    /* clock stretching wait as long as clock is driven to low by the slave */
+    uint32_t stretch = I2C_CLOCK_STRETCH;
+    while (!_i2c_read_scl (bus) && stretch--) {}
+
+    /* wait t_HIGH - time for the slave to read SDA */
+    /* min. in us: 4 (SM), 0.6 (FM), 0.26 (FPM), 0.09 (HSM); no max. */
+    _i2c_delay (bus);
+
+    /* SCL is high, read out bit */
+    bit = _i2c_read_sda (bus);
+
+    /* SCL = active LOW to allow next SDA change */
+    _i2c_clear_scl(bus);
+
+    return bit;
+}
+
+static /* IRAM */ bool _i2c_write_byte (_i2c_bus_t* bus, uint8_t byte)
+{
+    /* send one byte and returns true in case of ACK from slave */
+
+    uint8_t bit;
+
+    /* send the byte */
+    for (bit = 0; bit < 8; ++bit) {
+        _i2c_write_bit(bus, (byte & 0x80) != 0);
+        byte <<= 1;
+    }
+
+    /* read acknowledge bit from slave */
+    return !_i2c_read_bit (bus);
+}
+
+
+static /* IRAM */ uint8_t _i2c_read_byte(_i2c_bus_t* bus, bool ack)
+{
+    uint8_t byte = 0;
+    uint8_t bit;
+
+    /* read the byte */
+    for (bit = 0; bit < 8; ++bit) {
+        byte = (byte << 1) | _i2c_read_bit (bus);
+    }
+
+    /* write acknowledgement flag */
+    _i2c_write_bit(bus, !ack);
+
+    return byte;
+}
+
+#endif
---
+++ cpu/esp8266/syscalls.c
@@ -0,0 +1,398 @@
+/*
+ * Copyright (C) 2018 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_esp8266
+ * @{
+ *
+ * @file
+ * @brief       Implementation of required system calls
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ *
+ * @}
+ */
+
+#define ENABLE_DEBUG    0
+#define MEMLEAK_DEBUG   0
+#include "debug.h"
+
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <sys/reent.h>
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "ets_sys.h"
+#include "c_types.h"
+
+#include "common.h"
+#include "cpu_conf.h"
+#include "irq.h"
+#include "kernel_defines.h"
+#include "log.h"
+#include "mutex.h"
+#include "rmutex.h"
+#include "sched.h"
+#include "syscalls.h"
+
+#include "esp/xtensa_ops.h"
+#include "esp/common_macros.h"
+
+#include "sdk/ets.h"
+
+int IRAM puts(const char * str)
+{
+    char c;
+    while ((c = *str) != 0) {
+        ets_putc(c);
+        ++str;
+    }
+    ets_putc('\n');
+    return true;
+}
+
+int IRAM putchar(int c)
+{
+    /* function is neccessary to avoid unproducable results */
+    ets_putc(c);
+    return true;
+}
+
+char _printf_buf[PRINTF_BUFSIZ];
+
+int /* IRAM */ printf(const char* format, ...)
+{
+    va_list arglist;
+    va_start(arglist, format);
+
+    int ret = vsnprintf(_printf_buf, PRINTF_BUFSIZ, format, arglist);
+
+    if (ret > 0) {
+        ets_printf (_printf_buf);
+    }
+
+    va_end(arglist);
+
+    return ret;
+}
+
+#ifdef SDK_HEAP_USED
+/**
+ * Map memory management functions to SDK memory management functions.
+ * This is necessary to use the same heap as the SDK internally does.
+ * Furthermore, these functions do at least avoid interrupts during the
+ * execution of memory management functions. Memory management function
+ * of ETS are not used and have not to considered therefore.
+ */
+extern void *pvPortMalloc (size_t size, const char *, unsigned);
+extern void vPortFree (void *ptr, const char *, unsigned);
+extern void *pvPortZalloc (size_t size, const char *, unsigned);
+extern void *pvPortCalloc (size_t nmemb, size_t size, const char *, unsigned);
+extern void *pvPortRealloc (void *ptr, size_t size, const char *, unsigned);
+extern unsigned int xPortGetFreeHeapSize(void);
+
+void* IRAM malloc(size_t size)
+{
+    #if MEMLEAK_DEBUG
+    static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;
+    return pvPortMalloc(size, mem_debug_file, __LINE__);
+    #else
+    return pvPortMalloc(size, "", 0);
+    #endif
+}
+
+void IRAM free(void *ptr)
+{
+    #if MEMLEAK_DEBUG
+    static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;
+    return vPortFree (ptr, mem_debug_file, __LINE__);
+    #else
+    return vPortFree (ptr, "", 0);
+    #endif
+}
+
+void* IRAM calloc(size_t nmemb, size_t size)
+{
+    #if MEMLEAK_DEBUG
+    static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;
+    return pvPortCalloc(nmemb, size, mem_debug_file, __LINE__);
+    #else
+    return pvPortCalloc(nmemb, size, "", 0);
+    #endif
+}
+
+void* IRAM realloc(void *ptr, size_t size)
+{
+    #if MEMLEAK_DEBUG
+    static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;
+    return pvPortRealloc(ptr, size, mem_debug_file, __LINE__);
+    #else
+    return pvPortRealloc(ptr, size, "", 0);
+    #endif
+}
+
+void* IRAM _malloc_r (struct _reent *r, size_t size)
+{
+    return malloc (size);
+}
+
+void IRAM _free_r (struct _reent *r, void *ptr)
+{
+    free (ptr);
+}
+
+unsigned int get_free_heap_size (void)
+{
+    return xPortGetFreeHeapSize();
+}
+
+void IRAM syscalls_init (void) {}
+
+#else   /* SDK_HEAP_USED */
+/*
+ * To use the same heap SDK memory management functions have to be replaced by
+ * newlib memory functions. In that case the _malloc_lock/_unlock functions
+ * have to be defined. Memory management functions of ETS are not used and
+ * have not to considered here.
+ */
+
+void* IRAM pvPortMalloc (size_t size, const char *file, unsigned line)
+{
+    (void)file;
+    (void)line;
+
+    return malloc (size);
+}
+
+void IRAM vPortFree (void *ptr, const char *file, unsigned line)
+{
+    (void)file;
+    (void)line;
+
+    free (ptr);
+}
+
+void* IRAM pvPortCalloc (size_t nmemb, size_t size, const char *file, unsigned line)
+{
+    (void)file;
+    (void)line;
+
+    void *ptr = malloc (nmemb*size);
+    if (ptr) {
+        memset (ptr, 0x0, nmemb*size);
+    }
+    return ptr;
+}
+
+void* IRAM pvPortZalloc (size_t size, const char *file, unsigned line)
+{
+    (void)file;
+    (void)line;
+
+    void *ptr = malloc (size);
+    if (ptr) {
+        memset (ptr, 0x0, size);
+    }
+    return ptr;
+}
+
+void* IRAM pvPortRealloc (void *ptr, size_t size, const char *file, unsigned line)
+{
+    (void)file;
+    (void)line;
+
+    return realloc (ptr, size);
+}
+
+size_t IRAM xPortWantedSizeAlign(size_t size)
+{
+    /* allign the size to a multiple of 8 */
+    return (size & 0x7) ? (size & ~0x7) + 8 : size;
+}
+
+size_t IRAM xPortGetFreeHeapSize (void)
+{
+    return get_free_heap_size ();
+}
+
+/*
+ * Following function implement the lock mechanism in newlib. The only static
+ * mutex defined here is the __malloc_recursive_mutex to avoid that memory
+ * management functions try to lock before RIOT's threads are running.
+ */
+
+extern _lock_t __malloc_recursive_mutex;
+
+static rmutex_t _malloc_rmtx = RMUTEX_INIT;
+
+void IRAM syscalls_init (void)
+{
+    __malloc_recursive_mutex = (_lock_t)&_malloc_rmtx;
+}
+
+void IRAM _lock_init(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL);
+    CHECK_PARAM (*lock != (_lock_t)&_malloc_rmtx);
+
+    mutex_t* mtx = malloc (sizeof(mutex_t));
+
+    if (mtx) {
+        memset (mtx, 0, sizeof(mutex_t));
+        *lock = (_lock_t)mtx;
+    }
+}
+
+void IRAM _lock_init_recursive(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL);
+    CHECK_PARAM (*lock != (_lock_t)&_malloc_rmtx);
+
+    rmutex_t* rmtx = malloc (sizeof(rmutex_t));
+
+    if (rmtx) {
+        memset (rmtx, 0, sizeof(rmutex_t));
+        *lock = (_lock_t)rmtx;
+    }
+}
+
+void IRAM _lock_close(_lock_t *lock)
+{
+    CHECK_PARAM (lock != NULL);
+    CHECK_PARAM (*lock != (_lock_t)&_malloc_rmtx);
+
+    free ((void*)*lock);
+    *lock = 0;
+}
+
+void IRAM _lock_close_recursive(_lock_t *lock)
+{
+    CHECK_PARAM (lock != NULL);
+    CHECK_PARAM (*lock != (_lock_t)&_malloc_rmtx);
+
+    free ((void*)*lock);
+    *lock = 0;
+}
+
+void IRAM _lock_acquire(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL && *lock != 0);
+
+    mutex_lock ((mutex_t*)*lock);
+}
+
+void IRAM _lock_acquire_recursive(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL && *lock != 0);
+
+    rmutex_lock ((rmutex_t*)*lock);
+}
+
+int IRAM _lock_try_acquire(_lock_t *lock)
+{
+    CHECK_PARAM_RET (sched_active_thread != 0, 0);
+    CHECK_PARAM_RET (lock != NULL && *lock != 0, 0);
+
+    return rmutex_trylock ((rmutex_t*)*lock);
+}
+
+int IRAM _lock_try_acquire_recursive(_lock_t *lock)
+{
+    CHECK_PARAM_RET (sched_active_thread != 0, 0);
+    CHECK_PARAM_RET (lock != NULL && *lock != 0, 0);
+
+    return mutex_trylock ((mutex_t*)*lock);
+}
+
+void IRAM _lock_release(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL && *lock != 0);
+
+    mutex_unlock ((mutex_t*)*lock);
+}
+
+void IRAM _lock_release_recursive(_lock_t *lock)
+{
+    CHECK_PARAM (sched_active_thread != 0);
+    CHECK_PARAM (lock != NULL && *lock != 0);
+
+    rmutex_unlock ((rmutex_t*)*lock);
+}
+
+
+#ifdef MODULE_NEWLIB_SYSCALLS_DEFAULT
+
+#define _cheap heap_top
+
+extern char *heap_top;
+extern char _eheap;     /* end of heap (defined in esp8266.riot-os.app.ld) */
+
+#else /* MODULE_NEWLIB_SYSCALLS_DEFAULT */
+
+static uint8_t* _cheap = 0; /* last allocated chunk of heap */
+extern uint8_t  _eheap;     /* end of heap (defined in esp8266.riot-os.app.ld) */
+extern uint8_t  _sheap;     /* start of heap (defined in esp8266.riot-os.app.ld) */
+
+void* IRAM _sbrk_r (struct _reent *r, ptrdiff_t incr)
+{
+    uint8_t* _cheap_old;
+
+    /* initial _cheap */
+    if (_cheap == NULL) {
+        _cheap = &_sheap;
+    }
+
+    /* save old _cheap */
+    _cheap_old = _cheap;
+
+    /* check whether _cheap + incr overflows the heap */
+    if (_cheap + incr >= &_eheap) {
+        r->_errno = ENOMEM;
+        return (caddr_t)-1;
+    }
+
+    /* set new _cheap */
+    _cheap += incr;
+
+    #if ENABLE_DEBUG
+    uint32_t remaining = &_eheap - _cheap;
+    printf ("%s %lu byte allocated in %p .. %p, remaining %u\n",
+            __func__, incr, _cheap_old, _cheap, remaining);
+    #endif
+
+    /* return allocated memory */
+    return (caddr_t) _cheap_old;
+}
+
+#endif  /* MODULE_NEWLIB_SYSCALLS_DEFAULT */
+
+unsigned int IRAM get_free_heap_size (void)
+{
+    return (_cheap) ? &_eheap - _cheap : 0;
+}
+
+#endif /* SDK_HEAP_USED */
+
+#if !defined(MODULE_NEWLIB_SYSCALLS_DEFAULT)
+
+NORETURN void _exit(int status)
+{
+    UNREACHABLE();
+}
+
+#endif /* MODULE_NEWLIB_SYSCALLS_DEFAULT */
