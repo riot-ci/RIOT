b1cb995a61a3f8954e9d31adef6bb7da0ca0cc93
danpetry <daniel.petry@fu-berlin.de>
2018-05-23 14:27:49 +0200

    fixup: removed dependency on absent syscalls
    This removes the need for _times and _link to be provided to
    Lua via newlib.

--- pkg/lua/Makefile.lua
+++ pkg/lua/Makefile.lua
@@ -1,9 +1,13 @@
 SRC := $(filter-out lua.c luac.c,$(wildcard *.c))
 
-# This builds using POSIX system calls and some extra libraries, and
+# This builds for native using POSIX system calls and some extra libraries, and
 # removes a compiler warning that warns against using tmpnam().
 ifeq ($(BOARD),native)
 CFLAGS += -DLUA_USE_LINUX
 endif
+
+# This redefines a lua prng function, avoiding the need for the _times system
+# call, which RIOT doesn't provide
+CFLAGS += -D'l_randomizePivot()=0'
 
 include $(RIOTBASE)/Makefile.base
