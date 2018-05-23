6d77b29274c4890e45074b7cd9a73d2aa6a6fa67
Gunar Schorcht <gunar@schorcht.net>
2018-05-23 08:39:56 +0200

    esp8266: add support for esp-12x boards

---
+++ boards/esp8266-esp-12x/include/board.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    boards_esp8266-esp-12x ESP-12x based modules
+ * @ingroup     boards
+ * @brief       Support for ESP-12x based modules.
+ *
+ * Compatible modules: Wemos D1 mini, NodeMCU, HUZZAH ESO8266 Breakout
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the ESP-12x based modules.
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdint.h>
+
+#include "board_common.h"
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * @brief   Pin of first LED
+ */
+#define LED0_PIN    GPIO2
+
+/**
+ * @brief   Number of ADC inputs
+ */
+#define ADC_NUMOF   1
+
+/**
+ * @name   I2C configuration
+ * @{
+ */
+#define I2C_NUMOF   1
+#define I2C_SDA_0   GPIO4
+#define I2C_SCL_0   GPIO5
+/** @} */
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
+
+#endif /* BOARD_H */
+/** @} */
