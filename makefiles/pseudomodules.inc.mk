4a4f4549ac788fdff6987777464804a1ef326b20
Hauke Petersen <hauke.petersen@fu-berlin.de>
2018-05-24 17:38:32 +0200

    fixup! driver: simplified makefile.dep

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -82,9 +82,9 @@ PSEUDOMODULES += at86rf23%
 PSEUDOMODULES += at86rf21%
 
 # include variants of the BMX280 drivers as pseudo modules
-PSEUDOMODULES += bmp280
+PSEUDOMODULES += bmp280_i2c
 PSEUDOMODULES += bmp280_spi
-PSEUDOMODULES += bme280
+PSEUDOMODULES += bme280_i2c
 PSEUDOMODULES += bme280_spi
 
 # variants of TI ADCXX1C
