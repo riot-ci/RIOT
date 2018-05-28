ae0b08e3c7e4bf9ef25f6bdb177bfc234ad92372
Josarn <josuaarndt@live.de>
2018-05-28 15:39:16 +0200

    xtimer: timer & target overflow, hang resolved.
    1. When the 32 bit target of the xtimer overflowed the timer was not placed in the right list.
    2. When the hardware timer overflowed the comparison was wrong for setting next target.

--- sys/xtimer/xtimer_core.c
+++ sys/xtimer/xtimer_core.c
@@ -195,6 +195,11 @@ int _xtimer_set_absolute(xtimer_t *timer, uint32_t target)
 
     timer->target = target;
     timer->long_target = _long_cnt;
+
+    /* Ensure timer is fired in right hardware timer period. */
+    target = target - XTIMER_OVERHEAD;
+
+    /* 32 bit target overflow */
     if (target < now) {
         timer->long_target++;
     }
@@ -214,7 +219,7 @@ int _xtimer_set_absolute(xtimer_t *timer, uint32_t target)
 
             if (timer_list_head == timer) {
                 DEBUG("timer_set_absolute(): timer is new list head. updating lltimer.\n");
-                _lltimer_set(target - XTIMER_OVERHEAD);
+                _lltimer_set(target);
             }
         }
     }
@@ -506,7 +511,7 @@ overflow:
         next_target = timer_list_head->target - XTIMER_OVERHEAD;
 
         /* make sure we're not setting a time in the past */
-        if (next_target < (_xtimer_lltimer_now() + XTIMER_ISR_BACKOFF)) {
+        if (next_target < (_xtimer_now() + XTIMER_ISR_BACKOFF)) {
             goto overflow;
         }
     }
