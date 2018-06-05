6e240b3b3dda81a333ce3d5b0a5b1c25977f5e4e
smlng <s@mlng.net>
2018-06-05 17:06:11 +0200

    tools: allow static checks to be run from any directory

--- dist/tools/pr_check/pr_check.sh
+++ dist/tools/pr_check/pr_check.sh
@@ -7,9 +7,13 @@
 # directory for more details.
 #
 
+: "${RIOTBASE:=$(cd $(dirname $0)/../../../; pwd)}"
+cd $RIOTBASE
+
+: "${RIOTTOOLS:=${RIOTBASE}/dist/tools}"
+. "${RIOTTOOLS}"/ci/changed_files.sh
+
 EXIT_CODE=0
-: "${RIOTTOOLS:=${PWD}/dist/tools}"
-. "${RIOTTOOLS}"/pr_check/check_labels.sh
 
 if tput colors &> /dev/null && [ $(tput colors) -ge 8 ]; then
     CERROR="\e[1;31m"
