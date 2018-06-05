53c72d5f52b3d1590b7d9f0a0f05549a5011bb39
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-06-05 18:28:27 +0200

    fixup! dist/tools/doccheck: add check for undefined groups

--- dist/tools/doccheck/check.sh
+++ dist/tools/doccheck/check.sh
@@ -23,22 +23,21 @@ fi
 
 # Check all groups are defined
 
-DEFINED_GROUPS="$(git grep defgroup | \
-                  grep -v vendor | \
+DEFINED_GROUPS="$(git grep defgroup | grep -v vendor | \
+                  grep -v examples | \
+                  grep -v tests | \
                   grep -oE '@defgroup[ ]+[^ ]+' | \
                   grep -oE '[^ ]+$' | sort -u)"
 
 UNDEFINED_GROUPS=$( \
-    for group in $(git grep '@ingroup' | \
-                   grep -v vendor | \
-                   grep -v examples | \
-                   grep -v tests | \
-                   grep -oE '[^ ]+$' | \
-                   sort -u); \
+    for group in $(git grep '@ingroup' | grep -v vendor | \
+                    grep -v examples | \
+                    grep -v tests | \
+                    grep -oE '[^ ]+$' | sort -u); \
     do \
         echo "$DEFINED_GROUPS" | grep -xq "$group" || echo "$group"; \
     done \
-)
+    )
 
 if [ -n "${UNDEFINED_GROUPS}" ]
 then
