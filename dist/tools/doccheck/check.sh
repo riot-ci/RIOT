9ef2d43ee779707a70ff9aba022ad681d267ba0b
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-06-06 09:53:50 +0200

    fixup! dist/tools/doccheck: add check for undefined groups

--- dist/tools/doccheck/check.sh
+++ dist/tools/doccheck/check.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Copyright 2014 Oliver Hahm <oliver.hahm@inria.fr>
 #           2018 Kaspar Schleiser <kaspar@schleiser.de>
@@ -10,27 +10,38 @@
 
 RIOTBASE=$(readlink -f "$(dirname $(realpath $0))/../../..")
 
+if tput colors &> /dev/null && [ $(tput colors) -ge 8 ]; then
+    CERROR="\e[1;31m"
+    CWARN="\033[1;33m"
+    CRESET="\e[0m"
+else
+    CERROR=
+    CWARN=
+    CRESET=
+fi
+
 ERRORS=$(make -C "${RIOTBASE}" doc 2>&1 | \
             grep '.*warning' | \
             sed "s#${PWD}/\([^:]*\)#\1#g")
 
 if [ -n "${ERRORS}" ]
 then
-    echo "ERROR: Doxygen generates the following warnings:"
+    echo -e "${CERROR}ERROR: Doxygen generates the following warnings:${CRESET}"
     echo "${ERRORS}"
     exit 2
 fi
 
 # Check all groups are defined
-
-DEFINED_GROUPS="$(git grep defgroup | grep -v vendor | \
+DEFINED_GROUPS=$(git grep @defgroup -- '*.h' '*.txt' | \
+                  grep -v vendor | \
                   grep -v examples | \
                   grep -v tests | \
                   grep -oE '@defgroup[ ]+[^ ]+' | \
-                  grep -oE '[^ ]+$' | sort -u)"
+                  grep -oE '[^ ]+$' | sort -u)
 
 UNDEFINED_GROUPS=$( \
-    for group in $(git grep '@ingroup' | grep -v vendor | \
+    for group in $(git grep '@ingroup' -- '*.h' '*.c' '*.txt' | \
+                    grep -v vendor | \
                     grep -v examples | \
                     grep -v tests | \
                     grep -oE '[^ ]+$' | sort -u); \
@@ -39,11 +50,25 @@ UNDEFINED_GROUPS=$( \
     done \
     )
 
+ALL_RAW_INGROUP=$(git grep '@ingroup' -- '*.h' '*.c' '*.txt' | \
+                grep -v vendor | \
+                grep -v examples | \
+                grep -v tests)
+
+UNDEFINED_GROUPS_PRINT=$( \
+    for group in $UNDEFINED_GROUPS; \
+    do \
+        echo -e "\n${CWARN}$group${CRESET} found in:"; \
+        echo "$ALL_RAW_INGROUP" | grep " $group$" | sort -u | \
+                awk '{ print "\t" substr($1, 1, length($1)-1) }'; \
+    done \
+    )
+
 if [ -n "${UNDEFINED_GROUPS}" ]
 then
     COUNT=$(echo "${UNDEFINED_GROUPS}" | wc -l)
-    echo "WARNING: There are ${COUNT} undefined Doxygen groups:"
-    echo "${UNDEFINED_GROUPS}"
+    echo -ne "${CWARN}WARNING${CRESET} "
+    echo -ne "There are ${CWARN}${COUNT}${CRESET} undefined Doxygen groups:"
+    echo "${UNDEFINED_GROUPS_PRINT}"
+    exit 2
 fi
-
-exit 0
