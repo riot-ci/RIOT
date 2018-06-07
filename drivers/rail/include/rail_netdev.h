ec7ac9b38f442841df1cb2aa56045adf67b04381
Kai Beckmann <kai.beckmann@hs-rm.de>
2018-06-07 14:05:51 +0200

    add licence headers

--- drivers/rail/include/rail_netdev.h
+++ drivers/rail/include/rail_netdev.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2018 Hochschule RheinMain
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
 #ifndef  RAIL_NETDEV_H
 #define  RAIL_NETDEV_H
