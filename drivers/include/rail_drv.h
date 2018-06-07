ec482de72bfc0a7a3e889160e6b46a3de927058a
Kai Beckmann <kai.beckmann@hs-rm.de>
2018-06-07 14:05:51 +0200

    add licence headers

--- drivers/include/rail_drv.h
+++ drivers/include/rail_drv.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2018 Hochschule RheinMain
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
 #ifndef  RAIL_DRV_H
 #define  RAIL_DRV_H
