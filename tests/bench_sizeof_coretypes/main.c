0f5539246176b53d14651ebdf07dadc778c10658
Hauke Petersen <hauke.petersen@fu-berlin.de>
2018-06-14 16:51:21 +0200

    fixup! joined the benchmark with sizeof_tcb

--- tests/bench_sizeof_coretypes/main.c
+++ tests/bench_sizeof_coretypes/main.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2018 Freie Universität Berlin
+ * Copyright (C) 2014 René Kijewski <rene.kijewski@fu-berlin.de>
+ *               2018 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -13,6 +14,7 @@
  * @file
  * @brief       Print size of core types
  *
+ * @author      René Kijewski <rene.kijewski@fu-berlin.de>
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  *
  * @}
@@ -37,32 +39,59 @@
 #include "thread.h"
 
 
+#define P(NAME) printf("    tcb->%-11s            %3u     %3u\n", #NAME, \
+                       (unsigned)sizeof(((thread_t *) 0)->NAME), \
+                       (unsigned)offsetof(thread_t, NAME));
+
 int main(void)
 {
     puts("Sizeof RIOT core types\n");
 
-    puts("Type\t\t\tSize [bytes]");
+    puts("                                size");
 
-    printf("sizeof(cib_t):                  %u\n", sizeof(cib_t));
-    printf("sizeof(clist_node_t):           %u\n", sizeof(clist_node_t));
-    printf("sizeof(core_panic_t):           %u\n", sizeof(core_panic_t));
-    printf("sizeof(kernel_pid_t):           %u\n", sizeof(kernel_pid_t));
-    printf("sizeof(list_node_t):            %u\n", sizeof(list_node_t));
-    printf("sizeof(mbox_t):                 %u\n", sizeof(mbox_t));
-    printf("sizeof(msg_t):                  %u\n", sizeof(msg_t));
-    printf("sizeof(mutex_t):                %u\n", sizeof(mutex_t));
-    printf("sizeof(priority_queue_node_t):  %u\n", sizeof(priority_queue_node_t));
-    printf("sizeof(priority_queue_t):       %u\n", sizeof(priority_queue_t));
-    printf("sizeof(ringbuffer_t):           %u\n", sizeof(ringbuffer_t));
-    printf("sizeof(rmutex_t):               %u\n", sizeof(rmutex_t));
-#ifdef MODULE_CORE_THREAD_FLAGS
-    printf("sizeof(thread_flags_t):         %u\n", sizeof(thread_flags_t));
+    printf("sizeof(cib_t):                  %3u\n", sizeof(cib_t));
+    printf("sizeof(clist_node_t):           %3u\n", sizeof(clist_node_t));
+    printf("sizeof(core_panic_t):           %3u\n", sizeof(core_panic_t));
+    printf("sizeof(kernel_pid_t):           %3u\n", sizeof(kernel_pid_t));
+    printf("sizeof(list_node_t):            %3u\n", sizeof(list_node_t));
+    printf("sizeof(mbox_t):                 %3u\n", sizeof(mbox_t));
+#ifdef MODULE_CORE_MSG
+    printf("sizeof(msg_t):                  %3u\n", sizeof(msg_t));
 #else
-    puts("sizeof(thread_flags_t):         0   (not enabled)");
+    puts("sizeof(msg_t):                    0   (not enabled)");
 #endif
-    printf("sizeof(thread_t):               %u"
-           "  (see test/sizeof_tcb for more details)\n",
-           sizeof(thread_t));
+    printf("sizeof(mutex_t):                %3u\n", sizeof(mutex_t));
+    printf("sizeof(priority_queue_node_t):  %3u\n", sizeof(priority_queue_node_t));
+    printf("sizeof(priority_queue_t):       %3u\n", sizeof(priority_queue_t));
+    printf("sizeof(ringbuffer_t):           %3u\n", sizeof(ringbuffer_t));
+    printf("sizeof(rmutex_t):               %3u\n", sizeof(rmutex_t));
+#ifdef MODULE_CORE_THREAD_FLAGS
+    printf("sizeof(thread_flags_t):         %3u\n", sizeof(thread_flags_t));
+#else
+    puts("sizeof(thread_flags_t):           0   (not enabled)");
+#endif
+    printf("\nTCB (thread_t) details:         size  offset\n");
+    printf("sizeof(thread_t):               %3u       -\n", sizeof(thread_t));
+    P(sp);
+    P(status);
+    P(priority);
+    P(pid);
+#ifdef MODULE_CORE_THREAD_FLAGS
+    P(flags);
+#endif
+    P(rq_entry);
+#ifdef MODULE_CORE_MSG
+    P(wait_data);
+    P(msg_waiters);
+    P(msg_queue);
+    P(msg_array);
+#endif
+#ifdef DEVELHELP
+    P(name);
+#endif
+#if defined(DEVELHELP) || defined(SCHED_TEST_STACK) || defined(MODULE_MPU_STACK_GUARD)
+    P(stack_start);
+#endif
 
     puts("\n[SUCCESS]");
     return 0;
