2f5b48f6ce0c51de9fc72ff684d4d23ec1eabd01
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-06-18 15:16:51 +0200

    serial: remove unneccesary exports
    There variables are only used directly in the main Makefile.include or in
    application makefiles. So there is no need to export the value.
    
    Not exporting will also prevent evaluating them for no reason when 'term' is not
    done.

--- makefiles/vars.inc.mk
+++ makefiles/vars.inc.mk
@@ -74,7 +74,7 @@ export FFLAGS                # The parameters to supply to FLASHER.
 export FLASH_ADDR            # Define an offset to flash code into ROM memory.
 # TERMPROG                   # The command to call on "make term".
 # TERMFLAGS                  # Additional parameters to supply to TERMPROG.
-export PORT                  # The port to connect the TERMPROG to.
+# PORT                       # The port to connect the TERMPROG to.
 export ELFFILE               # The unstripped result of the compilation.
 export HEXFILE               # The stripped result of the compilation.
 # FLASHFILE                  # The output file used for flashing (transition phase: only if defined)
