39ae2cd6ee978a6873e00a58819f2d517679e782
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-06-29 18:41:13 +0200

    newlib.mk: never add a empty directory variable to INCLUDES
    This does prevents adding '-isystem' with nothing after which makes the next
    item in the list `-I/path/RIOT/core/include` considered as an include directory
    with the '-I' in the path.

--- makefiles/libc/newlib.mk
+++ makefiles/libc/newlib.mk
@@ -87,7 +87,7 @@ ifeq ($(TOOLCHAIN),llvm)
   # for the system being built.
   # We also add -nostdinc to avoid including the host system headers by mistake
   # in case some header is missing from the cross tool chain
-  NEWLIB_INCLUDES := -isystem $(NEWLIB_INCLUDE_DIR) -nostdinc
+  NEWLIB_INCLUDES := $(addprefix -isystem ,$(NEWLIB_INCLUDE_DIR)) -nostdinc
   NEWLIB_INCLUDES += $(addprefix -isystem ,$(realpath $(wildcard $(dir $(NEWLIB_INCLUDE_DIR))/usr/include)))
 endif
 
@@ -100,7 +100,7 @@ ifeq (1,$(USE_NEWLIB_NANO))
 
   # newlib-nano overrides newlib.h and its include dir should therefore go before
   # the regular system include dirs.
-  INCLUDES := -isystem $(NEWLIB_NANO_INCLUDE_DIR) $(INCLUDES)
+  INCLUDES := $(addprefix -isystem ,$(NEWLIB_NANO_INCLUDE_DIR)) $(INCLUDES)
 endif
 
 # Newlib includes should go before GCC includes. This is especially important
