ecbac3da0bd4e3f36814c58caeac3930d02c10d5
Gunar Schorcht <gunar@schorcht.net>
2018-06-29 16:34:57 +0200

    esp32: support for ESP32 added

---
+++ cpu/esp32/esp-idf/include/driver/gpio.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2018 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/* This file is just a mapper for source code compatibility with ESP-IDF */
+
+#ifndef DRIVER_GPIO_H
+#define DRIVER_GPIO_H
+
+#ifndef DOXYGEN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* the order of these includes is important */
+#include "periph_conf.h"
+#include "gpio_arch.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DOXYGEN */
+#endif /* DRIVER_GPIO_H */
