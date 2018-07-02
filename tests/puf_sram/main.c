fa228e9316828a2e7ad6711507bdded2649899ff
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2018-07-02 19:15:09 +0200

    rename (global) variables

--- tests/puf_sram/main.c
+++ tests/puf_sram/main.c
@@ -24,7 +24,7 @@ int main(void)
 {
     puts("Start: Test random number generator");
 
-    printf("Success: Data for global_puf_seed: [0x%08lX]", global_puf_seed);
+    printf("Success: Data for puf_sram_seed: [0x%08lX]", puf_sram_seed);
 
     puts("End: Test finished");
     return 0;
