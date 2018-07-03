15f4cfb3083b3ebb33062ff91ba1b251c9f75cf4
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-07-03 12:17:46 +0200

    newlib.mk: llvm, fix newlib-nano header not used
    In the previous state, with llvm and arm for example, newlib-nano include dir
    NEWLIB_NANO_INCLUDE_DIR is placed after NEWLIB_INCLUDES and so the default
    'newlib.h' is used instead of the nano version.

--- makefiles/libc/newlib.mk
+++ makefiles/libc/newlib.mk
@@ -94,6 +94,13 @@ ifeq ($(TOOLCHAIN),llvm)
   # in case some header is missing from the cross tool chain
   NEWLIB_INCLUDES := $(addprefix -isystem ,$(NEWLIB_INCLUDE_DIR)) -nostdinc
   NEWLIB_INCLUDES += $(addprefix -isystem ,$(realpath $(wildcard $(dir $(NEWLIB_INCLUDE_DIR))/usr/include)))
+
+  # Newlib includes should go before GCC includes. This is especially important
+  # when using Clang, because Clang will yield compilation errors on some GCC-
+  # bundled headers. Clang compatible versions of those headers are already
+  # provided by Newlib, so placing this directory first will eliminate those problems.
+  # The above problem was observed with LLVM 3.9.1 when building against GCC 6.3.0 headers.
+  INCLUDES := $(NEWLIB_INCLUDES) $(INCLUDES)
 endif
 
 ifeq (1,$(USE_NEWLIB_NANO))
@@ -116,10 +123,3 @@ ifeq (1,$(USE_NEWLIB_NANO))
     CFLAGS += -include '$(RIOTMAKE)/libc/assert_newlib_nano_included.h'
   endif
 endif
-
-# Newlib includes should go before GCC includes. This is especially important
-# when using Clang, because Clang will yield compilation errors on some GCC-
-# bundled headers. Clang compatible versions of those headers are already
-# provided by Newlib, so placing this directory first will eliminate those problems.
-# The above problem was observed with LLVM 3.9.1 when building against GCC 6.3.0 headers.
-INCLUDES := $(NEWLIB_INCLUDES) $(INCLUDES)
