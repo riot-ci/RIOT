b67ecc5a825386968b3678986d7650410f43b5ee
Hauke Petersen <hauke.petersen@fu-berlin.de>
2018-07-05 15:00:32 +0200

    fixup! asymcute: visual fixes + is_used opt

--- sys/include/net/asymcute.h
+++ sys/include/net/asymcute.h
@@ -46,7 +46,6 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include <string.h>
 
 #include "assert.h"
 #include "event/timeout.h"
@@ -246,36 +245,37 @@ typedef unsigned(*asymcute_to_cb_t)(asymcute_con_t *con, asymcute_req_t *req);
  * @brief   Asymcute request context
  */
 struct asymcute_req {
-    struct asymcute_req *next;  /**< the requests list entry */
-    mutex_t lock;               /**< synchronization lock */
-    asymcute_con_t *con;        /**< connection the request is using */
-    asymcute_to_cb_t cb;        /**< internally used callback */
-    void *arg;                  /**< internally used additional state */
-    event_callback_t to_evt;    /**< timeout event */
-    event_timeout_t to_timer;   /**< timeout timer */
+    mutex_t lock;                   /**< synchronization lock */
+    struct asymcute_req *next;      /**< the requests list entry */
+    asymcute_con_t *con;            /**< connection the request is using */
+    asymcute_to_cb_t cb;            /**< internally used callback */
+    void *arg;                      /**< internally used additional state */
+    event_callback_t to_evt;        /**< timeout event */
+    event_timeout_t to_timer;       /**< timeout timer */
     uint8_t data[ASYMCUTE_BUFSIZE]; /**< buffer holding the request's data */
-    size_t data_len;            /**< length of the request packet in byte */
-    uint16_t msg_id;            /**< used message id for this request */
-    uint8_t retry_cnt;          /**< retransmission counter */
+    size_t data_len;                /**< length of the request packet in byte */
+    uint16_t msg_id;                /**< used message id for this request */
+    uint8_t retry_cnt;              /**< retransmission counter */
 };
 
 /**
  * @brief   Asymcute connection context
  */
 struct asymcute_con {
-    mutex_t lock;               /**< lock synchronizing access to this struct */
-    sock_udp_t sock;            /**< socket used by a connections */
-    sock_udp_ep_t server_ep;    /**< the gateway's UDP endpoint */
-    asymcute_req_t *pending;        /**< list holding pending requests */
-    asymcute_sub_t *subscriptions;  /**< list holding active subscriptions */
-    asymcute_evt_cb_t user_cb;  /**< event callback provided by user */
+    mutex_t lock;                       /**< synchronization lock */
+    sock_udp_t sock;                    /**< socket used by a connections */
+    sock_udp_ep_t server_ep;            /**< the gateway's UDP endpoint */
+    asymcute_req_t *pending;            /**< list holding pending requests */
+    asymcute_sub_t *subscriptions;      /**< list holding active subscriptions */
+    asymcute_evt_cb_t user_cb;          /**< event callback provided by user */
     event_callback_t keepalive_evt;     /**< keep alive event */
     event_timeout_t keepalive_timer;    /**< keep alive timer */
-    uint16_t last_id;           /**< last used message ID for this connection */
+    uint16_t last_id;                   /**< last used message ID for this
+                                         *   connection */
     uint8_t keepalive_retry_cnt;        /**< keep alive transmission counter */
-    uint8_t state;              /**< connection state */
+    uint8_t state;                      /**< connection state */
     uint8_t rxbuf[ASYMCUTE_BUFSIZE];    /**< connection specific receive buf */
-    char cli_id[ASYMCUTE_ID_MAXLEN + 1];    /**< buffer to store client ID */
+    char cli_id[ASYMCUTE_ID_MAXLEN + 1];/**< buffer to store client ID */
 };
 
 /**
@@ -338,7 +338,7 @@ static inline bool asymcute_sub_active(const asymcute_sub_t *sub)
 /**
  * @brief   Reset the given topic
  *
- * @note    Make sure that the given topic is not used by any subscription or
+ * @warning Make sure that the given topic is not used by any subscription or
  *          last will when calling this function
  *
  * @param[out] topic    topic to reset
@@ -374,7 +374,7 @@ static inline bool asymcute_topic_is_reg(const asymcute_topic_t *topic)
 static inline bool asymcute_topic_is_init(const asymcute_topic_t *topic)
 {
     assert(topic);
-    return (strlen(topic->name) > 0);
+    return (topic->name[0] != '\0');
 }
 
 /**
