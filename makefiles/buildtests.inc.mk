e385b5e160771e7094daa3b4b4c8585cdcdcd4cd
Martine Lenders <m.lenders@fu-berlin.de>
2018-08-07 19:19:07 +0200

    fixup! make: provide toolchain support for buildtests

--- makefiles/buildtests.inc.mk
+++ makefiles/buildtests.inc.mk
@@ -9,7 +9,7 @@ buildtest:
 	@ \
 	RESULT=true ; \
 	for board in $(BOARDS); do \
-		if $(MAKE) check-toolchain-supported; then
+		if BOARD=$${board} $(MAKE) check-toolchain-supported > /dev/null 2>&1; then \
 			$(COLOR_ECHO) -n "Building for $$board ... " ; \
 			BOARD=$${board} RIOT_CI_BUILD=1 RIOT_VERSION_OVERRIDE=buildtest \
 				$(MAKE) clean all -j $(NPROC) $(BUILDTEST_MAKE_REDIRECT); \
@@ -21,7 +21,7 @@ buildtest:
 				RESULT=false ; \
 			fi ; \
 			$(MAKE) clean-intermediates >/dev/null 2>&1 || true; \
-		fi
+		fi; \
 	done ; \
 	$${RESULT}
 endif # BUILD_IN_DOCKER
