2f1fbd754d55bc271ca87cf0f89d3b0fb9b5f3bd
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2018-08-20 14:15:45 +0200

    boards/lorabox: Switch to stm32loader

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,20 +20,13 @@
  */
 
 #include <stdio.h>
-#include "xtimer.h"
 
 int main(void)
 {
-    //__asm__("bkpt");
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-
-    while (1) {
-        printf("Loop...\n");
-        xtimer_usleep(100);
-    }
 
     return 0;
 }
