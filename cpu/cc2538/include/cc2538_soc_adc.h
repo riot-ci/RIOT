269d442b66eee4892a28cf269807bddc078ec62d
smlng <s@mlng.net>
2018-08-28 17:04:27 +0200

    fixup! cpu/cc2538: enhance periph ADC

--- cpu/cc2538/include/cc2538_soc_adc.h
+++ cpu/cc2538/include/cc2538_soc_adc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 Loci Controls Inc.
+ *               2018 HAW Hamburg
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,6 +16,7 @@
  * @brief           CC2538 SOC ADC interface
  *
  * @author          Ian Martin <ian@locicontrols.com>
+ * @author          Sebastian Meiling <s@mlng.net>
  */
 
 #ifndef CC2538_SOC_ADC_H
@@ -30,15 +32,15 @@ extern "C" {
  * @brief SOC ADC component registers
  */
 typedef struct {
-    cc2538_reg_t ADCCON1;            /**< ADC Control Register 1 */
-    cc2538_reg_t ADCCON2;                /**< ADC Control Register 2 */
-    cc2538_reg_t ADCCON3;                /**< ADC Control Register 3 */
-    cc2538_reg_t ADCL;                   /**< Least-significant part of ADC conversion result. */
-    cc2538_reg_t ADCH;                   /**< Most-significant part of ADC conversion result. */
-    cc2538_reg_t RNDL;                   /**< Random-number-generator data; low byte. */
-    cc2538_reg_t RNDH;                   /**< Random-number-generator data; high byte. */
-    cc2538_reg_t RESERVED[2];            /**< Reserved bytes */
-    cc2538_reg_t CMPCTL;                 /**< Analog comparator control and status register. */
+    cc2538_reg_t ADCCON1;       /**< ADC Control Register 1 */
+    cc2538_reg_t ADCCON2;       /**< ADC Control Register 2 */
+    cc2538_reg_t ADCCON3;       /**< ADC Control Register 3 */
+    cc2538_reg_t ADCL;          /**< Least-significant part of ADC conversion result. */
+    cc2538_reg_t ADCH;          /**< Most-significant part of ADC conversion result. */
+    cc2538_reg_t RNDL;          /**< Random-number-generator data; low byte. */
+    cc2538_reg_t RNDH;          /**< Random-number-generator data; high byte. */
+    cc2538_reg_t RESERVED[2];   /**< Reserved bytes */
+    cc2538_reg_t CMPCTL;        /**< Analog comparator control and status register. */
 } cc2538_soc_adc_t;
 
 #ifdef __cplusplus
