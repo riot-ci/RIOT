c6514c638dfa7bde2979709d047daebf7fc37198
Kaspar Schleiser <kaspar@schleiser.de>
2018-08-31 00:51:06 +0200

    fixup! fixup! fixup! WIP: add staging/

--- staging/staging.mk
+++ staging/staging.mk
@@ -1,21 +1,35 @@
 # staging.mk
 # this makefile is being included by staging/Makefile.include and staging/Makefile.dep.
 
-# list of staging modules
+#
+# list of staging modules and applications
+#
 STAGING_MODULES += test
+STAGING_APPS += test/hello-staging
+#BROKEN += test
 
+#
 # housekeeping
-BROKEN := $(sort $(BROKEN))
+#
 STAGING_MODULES := $(sort $(STAGING_MODULES))
 STAGING_MODULES_USED := $(filter $(USEMODULE),$(STAGING_MODULES))
 
+BROKEN := $(sort $(BROKEN))
+
 # error out on dependencies to broken staging module
 STAGING_MODULES_USED_BROKEN := \
-  $(filter $(BROKEN), $(STAGING_MODULES_USED))
+  $(sort $(filter $(BROKEN), $(STAGING_MODULES_USED)))
 
 ifneq (, $(STAGING_MODULES_USED_BROKEN))
-  $(error using broken staging modules: $(STAGING_MODULES_USED_BROKEN))
+  FEATURES_REQUIRED += $(patsubst %,broken-staging-module-%,$(STAGING_MODULES_USED_BROKEN))
 endif
 
 # list of folders with staging modules
 STAGING_DIRS := $(sort $(wildcard $(STAGING_MODULES_USED:%=$(RIOTBASE)/staging/%)))
+
+# create app make file list to be consumed by makefiles/app_dirs.inc.mk
+STAGING_APP_MAKEFILES := \
+    $(sort \
+        $(wildcard \
+            $(patsubst %,$(RIOTBASE)/staging/%/Makefile,$(STAGING_APPS)) \
+    ))
