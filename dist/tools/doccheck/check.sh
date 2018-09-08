3551fde07ab9dbef591b60971351455456bed35b
Kaspar Schleiser <kaspar@schleiser.de>
2018-09-08 22:54:32 +0200

    doc: disable doxygen for staging/

--- dist/tools/doccheck/check.sh
+++ dist/tools/doccheck/check.sh
@@ -32,7 +32,7 @@ then
 fi
 
 exclude_filter() {
-    grep -v -e vendor -e examples -e tests -e "\<dist/tools\>"
+    grep -v -e vendor -e examples -e tests -e "\<dist/tools\>" -e staging
 }
 
 # Check all groups are defined
