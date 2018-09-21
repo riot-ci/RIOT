9b76a5990e60c3f03c396224bb2f2f875f3a94b8
smlng <s@mlng.net>
2018-09-21 13:56:52 +0200

    fixup! dist/tools: fix tunslip6 strncat num param

--- dist/tools/tunslip/tunslip6.c
+++ dist/tools/tunslip/tunslip6.c
@@ -650,7 +650,9 @@ devopen(const char *dev, int flags)
 {
     char t[1024];
     strcpy(t, "/dev/");
-    /* need to consider `\0` terminator and strlen("dev") = 5, hence 6 */
+    /* need to consider `\0` terminator and strlen("dev") = 5, hence 6
+     * cppcheck-suppress bufferAccessOutOfBounds
+     * (reason: seems to be a bug in cppcheck 1.7x) */
     strncat(t, dev, sizeof(t) - 6);
     return open(t, flags);
 }
