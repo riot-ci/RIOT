76d0dc3a42e0155bdd590ee623627ec2f97fc706
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-09-27 13:21:31 +0200

    drivers/periph_eeprom: fix eeprom bound assert

--- drivers/periph_common/eeprom.c
+++ drivers/periph_common/eeprom.c
@@ -30,7 +30,7 @@
 
 size_t eeprom_read(uint32_t pos, uint8_t *data, size_t len)
 {
-    assert(pos + len < EEPROM_SIZE);
+    assert(pos + len <= EEPROM_SIZE);
 
     for (size_t i = 0; i < len; i++) {
         data[i] = eeprom_read_byte(pos++);
@@ -41,7 +41,7 @@ size_t eeprom_read(uint32_t pos, uint8_t *data, size_t len)
 
 size_t eeprom_write(uint32_t pos, const uint8_t *data, size_t len)
 {
-    assert(pos + len < EEPROM_SIZE);
+    assert(pos + len <= EEPROM_SIZE);
 
     for (size_t i = 0; i < len; i++) {
         eeprom_write_byte(pos++, data[i]);
