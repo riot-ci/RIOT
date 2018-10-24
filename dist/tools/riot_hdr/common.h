e77b481a671420e0c300267aacf4e2c7204a08b3
Francisco Acosta <fco.ja.ac@gmail.com>
2018-10-24 16:09:45 +0200

    fixup! fixup! dist/tools: add genhdr tool to generate RIOT image headers

--- dist/tools/riot_hdr/common.h
+++ dist/tools/riot_hdr/common.h
@@ -14,6 +14,14 @@
  extern "C" {
 #endif
 
+/**
+ * @brief  Write len bytes of a given buffer into a file
+ *
+ * @param[out] filename    name of the file to be written
+ * @param[in]  buf         a pointer to the buffer which needs to be written
+ * @param[in]  len         the number of bytes from buf to be written
+ *
+ */
 int to_file(const char *filename, void *buf, size_t len);
 
 #ifdef __cplusplus
