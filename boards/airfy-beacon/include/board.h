e836f704647c1cae5b9db0247575902bb6715ff0
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-10-24 22:00:44 +0200

    fixup! boards/airfy-beacon: factorize common code

--- boards/airfy-beacon/include/board.h
+++ boards/airfy-beacon/include/board.h
@@ -22,24 +22,7 @@
 #include "cpu.h"
 
 #ifdef __cplusplus
- extern "C" {
-#endif
-
-/**
- * @name    Xtimer configuration
- * @{
- */
-#define XTIMER_WIDTH                (24)
-#define XTIMER_BACKOFF              (40)
-/** @} */
-
-/**
- * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
- */
-void board_init(void);
-
-#ifdef __cplusplus
-} /* end extern "C" */
+ extern "C" {}
 #endif
 
 #endif /* BOARD_H */
