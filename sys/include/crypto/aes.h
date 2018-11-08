7542e0336efc5406f4cc0999c9b855c21f066f4c
Mathias Tausig <mathias.tausig@fh-campuswien.ac.at>
2018-11-08 10:46:58 +0100

    undo style change

--- sys/include/crypto/aes.h
+++ sys/include/crypto/aes.h
@@ -43,9 +43,9 @@ typedef uint8_t u8;
 # define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ \
                      ((u32)(pt)[2] <<  8) ^ ((u32)(pt)[3]))
 # define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); \
-                          (ct)[1] = (u8)((st) >> 16); \
-                          (ct)[2] = (u8)((st) >>  8); \
-                          (ct)[3] = (u8)(st); }
+                              (ct)[1] = (u8)((st) >> 16); \
+                              (ct)[2] = (u8)((st) >>  8); \
+                              (ct)[3] = (u8)(st); }
 
 #define AES_MAXNR         14
 #define AES_BLOCK_SIZE    16
