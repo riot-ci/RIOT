2154f0da9c5bc91eac6ab7f22043acbe0ffce9b6
Koen Zandberg <koen@bergzand.net>
2018-11-13 20:41:03 +0100

    netdev_ieee802154: Add PAN ID to reset function
    This commit adds the reset of the PAN ID to the generic ieee802154 reset
    function.

--- drivers/netdev_ieee802154/netdev_ieee802154.c
+++ drivers/netdev_ieee802154/netdev_ieee802154.c
@@ -63,6 +63,9 @@ void netdev_ieee802154_reset(netdev_ieee802154_t *dev)
 #elif MODULE_GNRC
     dev->proto = GNRC_NETTYPE_UNDEF;
 #endif
+
+    static const uint16_t nid = IEEE802154_DEFAULT_PANID;
+    dev->netdev.driver->set(&dev->netdev, NETOPT_NID, &nid, sizeof(nid));
 }
 
 int netdev_ieee802154_get(netdev_ieee802154_t *dev, netopt_t opt, void *value,
