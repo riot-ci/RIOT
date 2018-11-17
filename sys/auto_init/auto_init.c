e67d9fea48518e09f2a70d5e8a6414c7bd0c8241
Schorcht <gunar@schorcht.net>
2018-11-17 11:47:21 +0100

    sys/auto_init: add APDS99XX ALS and proximity

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -395,6 +395,10 @@ void auto_init(void)
     extern void auto_init_adxl345(void);
     auto_init_adxl345();
 #endif
+#ifdef MODULE_APDS99XX
+    extern void auto_init_apds99xx(void);
+    auto_init_apds99xx();
+#endif
 #ifdef MODULE_BMP180
     extern void auto_init_bmp180(void);
     auto_init_bmp180();
