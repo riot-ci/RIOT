2ff6492901460134580d11c052383f526078c1a7
Juan I Carrano <juan@carrano.com.ar>
2018-11-21 11:08:53 +0100

    fixup! comment

--- makefiles/util/checks.mk
+++ makefiles/util/checks.mk
@@ -7,5 +7,5 @@
 #	value: a string which should not be empty
 #	message: The error message to display.
 # Returns:
-#	the first argument, if it is not zero.
+#	the first argument, if it is not empty.
 ensure_value = $(if $(1),$(1),$(error $(2)))
