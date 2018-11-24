482491b794ded05f3cff0f85111881431745d9ad
Gilles DOFFE <g.doffe@gmail.com>
2018-11-24 19:03:33 +0100

    fix boards/native: simplify callback in board.c
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- boards/native/board.c
+++ boards/native/board.c
@@ -22,21 +22,17 @@
 
 #ifdef QDEC_NUMOF
 
-#define SIMU_ENC_BUFSIZE    3
 extern int32_t qdecs_value[QDEC_NUMOF];
 
 void native_motor_driver_qdec_simulation(
     const motor_driver_t motor_driver, uint8_t motor_id,
     motor_direction_t direction, uint16_t pwm_duty_cycle)
 {
-    static int16_t simu_motor_encoder[QDEC_NUMOF][SIMU_ENC_BUFSIZE] = {0,};
-
-    uint32_t i = 0, id = 0;
-    int32_t s = 0;
+    uint32_t id;
 
     int32_t pwm_value = direction ? -pwm_duty_cycle : pwm_duty_cycle;
 
-    for (i = 0; i < motor_driver; i++) {
+    for (uint32_t i = 0; i < motor_driver; i++) {
         const motor_driver_config_t motor_driver_conf =
             motor_driver_config[motor_driver];
         id += motor_driver_conf.nb_motors;
@@ -44,27 +40,19 @@ void native_motor_driver_qdec_simulation(
     id += motor_id;
 
     if (id < QDEC_NUMOF) {
-
-        for (i = 0; i < SIMU_ENC_BUFSIZE - 1; i++) {
-            s += simu_motor_encoder[id][i];
-            simu_motor_encoder[id][i] = simu_motor_encoder[id][i+1];
-        }
-
-        s = (s + simu_motor_encoder[id][i] + pwm_value) / (SIMU_ENC_BUFSIZE + 1);
-        simu_motor_encoder[id][i] = s;
-        qdecs_value[id] = s;
+        qdecs_value[id] = pwm_value;
 
         LOG_DEBUG("MOTOR-DRIVER=%u"             \
             "    MOTOR_ID = %u"                 \
             "    PWM_VALUE = %d"                \
             "    QDEC_ID = %"PRIu32""           \
-            "    QDEC_AVERAGE = %d\n",          \
-            motor_driver, motor_id, pwm_value, id, s);
+            "    QDEC_VALUE = %d\n",          \
+            motor_driver, motor_id, pwm_value, id, pwm_value);
     }
     else {
         LOG_ERROR("MOTOR-DRIVER=%u"             \
             "    MOTOR_ID = %u"                 \
-            "    no QDEC device associated",    \
+            "    no QDEC device associated\n",    \
             motor_driver, motor_id);
     }
 }
