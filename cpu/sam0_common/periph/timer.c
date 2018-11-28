27c073bad35dec5b92510313f7d0b9f19d5a5f67
Sebastian Meiling <s@mlng.net>
2018-11-28 10:13:16 +0100

    periph/timer: add timer_diff API call
    Introducing a generic helper function to calculate the time difference
    of two values with respect to the actual timer configuration, e.g. 16
    or 32Bit. This utilises the platform specific timer_max call.

--- cpu/sam0_common/periph/timer.c
+++ cpu/sam0_common/periph/timer.c
@@ -207,6 +207,18 @@ unsigned int timer_read(tim_t tim)
     return dev(tim)->COUNT.reg;
 }
 
+unsigned int timer_max(tim_t dev)
+{
+    switch (dev) {
+#if TIMER_0_EN
+    case TIMER_0:
+        return TIMER_0_MAX_VALUE;
+#endif
+    default:
+        return 0;
+    }
+}
+
 void timer_stop(tim_t tim)
 {
     dev(tim)->CTRLA.bit.ENABLE = 0;
