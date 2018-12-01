7e7aa8e53b99b99f0e882bd5017ab0dacefcbb10
Kaspar Schleiser <kaspar@schleiser.de>
2018-12-01 14:40:44 +0100

    fixup! tests: add riot_hdr test

--- tests/riotboot_hdr/main.c
+++ tests/riotboot_hdr/main.c
@@ -11,79 +11,79 @@
  * @{
  *
  * @file
- * @brief      Tests for module riot_hdr
+ * @brief      Tests for module riotboot_hdr
  *
  * @author     Francisco Acosta <francisco.acosta@inria.fr>
  */
 
 #include <stdio.h>
 
-#include "riot_hdr.h"
+#include "riotboot/hdr.h"
 #include "embUnit.h"
 
-const riot_hdr_t riot_hdr_good = {
+const riotboot_hdr_t riotboot_hdr_good = {
     .magic_number = RIOT_HDR_MAGIC,
     .version = 0x5bd19bff,
     .start_addr = 0x00001100,
     .chksum = 0x02eda672
 };
 
-const riot_hdr_t riot_hdr_bad_magic = {
+const riotboot_hdr_t riotboot_hdr_bad_magic = {
     .magic_number = 0x12345678,
     .version = 0x5bd19bff,
     .start_addr = 0x00001100,
     .chksum = 0x02eda672
 };
 
-const riot_hdr_t riot_hdr_bad_chksum = {
+const riotboot_hdr_t riotboot_hdr_bad_chksum = {
     .magic_number = RIOT_HDR_MAGIC,
     .version = 0x5bd19bff,
     .start_addr = 0x00001100,
     .chksum = 0x02000000
 };
 
-static void test_riot_hdr_01(void)
+static void test_riotboot_hdr_01(void)
 {
-    int ret = riot_hdr_validate(&riot_hdr_good);
+    int ret = riotboot_hdr_validate(&riotboot_hdr_good);
     TEST_ASSERT_EQUAL_INT(0, ret);
 }
 
-static void test_riot_hdr_02(void)
+static void test_riotboot_hdr_02(void)
 {
-    int ret = riot_hdr_validate(&riot_hdr_bad_magic);
+    int ret = riotboot_hdr_validate(&riotboot_hdr_bad_magic);
     TEST_ASSERT_EQUAL_INT(-1, ret);
 }
 
-static void test_riot_hdr_03(void)
+static void test_riotboot_hdr_03(void)
 {
-    int ret = riot_hdr_validate(&riot_hdr_bad_chksum);
+    int ret = riotboot_hdr_validate(&riotboot_hdr_bad_chksum);
     TEST_ASSERT_EQUAL_INT(-1, ret);
 }
 
-static void test_riot_hdr_04(void)
+static void test_riotboot_hdr_04(void)
 {
-    uint32_t chksum = riot_hdr_checksum(&riot_hdr_good);
+    uint32_t chksum = riotboot_hdr_checksum(&riotboot_hdr_good);
     TEST_ASSERT_EQUAL_INT(0x02eda672, chksum);
 }
 
-Test *tests_riot_hdr(void)
+Test *tests_riotboot_hdr(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
-        new_TestFixture(test_riot_hdr_01),
-        new_TestFixture(test_riot_hdr_02),
-        new_TestFixture(test_riot_hdr_03),
-        new_TestFixture(test_riot_hdr_04),
+        new_TestFixture(test_riotboot_hdr_01),
+        new_TestFixture(test_riotboot_hdr_02),
+        new_TestFixture(test_riotboot_hdr_03),
+        new_TestFixture(test_riotboot_hdr_04),
     };
 
-    EMB_UNIT_TESTCALLER(riot_hdr_tests, NULL, NULL, fixtures);
+    EMB_UNIT_TESTCALLER(riotboot_hdr_tests, NULL, NULL, fixtures);
 
-    return (Test *)&riot_hdr_tests;
+    return (Test *)&riotboot_hdr_tests;
 }
 
 int main(void)
 {
     TESTS_START();
-    TESTS_RUN(tests_riot_hdr());
+    TESTS_RUN(tests_riotboot_hdr());
     TESTS_END();
     return 0;
 }
