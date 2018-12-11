020b0f5a1cdd7177f92198c25c3f3a2975b7ec4b
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-12-11 15:28:32 +0100

    fixup! tests/bench_runtime_coreapis: improve pexpect autotest

--- tests/bench_runtime_coreapis/tests/01-run.py
+++ tests/bench_runtime_coreapis/tests/01-run.py
@@ -17,15 +17,15 @@ BENCHMARK_REGEXP = r"\s+{func}:\s+\d+us\s+---\s+\d*\.*\d+us per call\s+---\s+\d+
 
 def testfunc(child):
     child.expect_exact('Runtime of Selected Core API functions')
-    child.expect(BENCHMARK_REGEXP.format(func="nop loop"), timeout=TIMEOUT)
-    child.expect(BENCHMARK_REGEXP.format(func=r"mutex_init\(\)"), timeout=TIMEOUT)
-    child.expect(BENCHMARK_REGEXP.format(func="mutex lock/unlock"), timeout=TIMEOUT)
-    child.expect(BENCHMARK_REGEXP.format(func=r"thread_flags_set\(\)"), timeout=TIMEOUT)
+    child.expect(BENCHMARK_REGEXP.format(func="nop loop"))
+    child.expect(BENCHMARK_REGEXP.format(func=r"mutex_init\(\)"))
+    child.expect(BENCHMARK_REGEXP.format(func="mutex lock/unlock"))
+    child.expect(BENCHMARK_REGEXP.format(func=r"thread_flags_set\(\)"))
     child.expect(BENCHMARK_REGEXP.format(func="thread flags set/wait any"), timeout=TIMEOUT)
     child.expect(BENCHMARK_REGEXP.format(func="thread flags set/wait all"), timeout=TIMEOUT)
     child.expect(BENCHMARK_REGEXP.format(func="thread flags set/wait one"), timeout=TIMEOUT)
-    child.expect(BENCHMARK_REGEXP.format(func=r"msg_try_receive\(\)"), timeout=TIMEOUT)
-    child.expect(BENCHMARK_REGEXP.format(func=r"msg_avail\(\)"), timeout=TIMEOUT)
+    child.expect(BENCHMARK_REGEXP.format(func=r"msg_try_receive\(\)"))
+    child.expect(BENCHMARK_REGEXP.format(func=r"msg_avail\(\)"))
     child.expect_exact('[SUCCESS]')
 
 
