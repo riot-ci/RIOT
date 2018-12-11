6f587a4a5872bf8caea0df23939093ba3480bf33
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-12-11 14:55:50 +0100

    tests/bench_runtime_coreapis: improve pexpect autotest

--- tests/bench_runtime_coreapis/tests/01-run.py
+++ tests/bench_runtime_coreapis/tests/01-run.py
@@ -13,9 +13,26 @@ from testrunner import run
 # The default timeout is not enough for this test on some of the slower boards
 TIMEOUT = 30
 
+BENCHMARK_FUNCS = [
+    "nop loop",
+    r"mutex_init\(\)",
+    "mutex lock/unlock",
+    r"thread_flags_set\(\)",
+    r"thread_flags_clear\(\)",
+    "thread flags set/wait any",
+    "thread flags set/wait all",
+    "thread flags set/wait one",
+    r"msg_try_receive\(\)",
+    r"msg_avail\(\)"
+]
+
 
 def testfunc(child):
-    child.expect_exact('[SUCCESS]', timeout=TIMEOUT)
+    child.expect_exact('Runtime of Selected Core API functions')
+    for func in BENCHMARK_FUNCS:
+        child.expect(r"\s+{func}:\s+\d+us\s+---\s+\d*\.*\d+us per call\s+---\s+\d+ calls per sec".format(
+            func=func), timeout=TIMEOUT)
+    child.expect_exact('[SUCCESS]')
 
 
 if __name__ == "__main__":
