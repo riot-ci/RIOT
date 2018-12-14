6f906a705d85a97b4e767a98a8bb0a9ec89356a8
Sebastian Meiling <s@mlng.net>
2018-12-14 11:14:35 +0100

    fixup! fixup! periph_common: add as default module

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -1,4 +1,6 @@
 DEFAULT_MODULE += board cpu core core_msg sys
 
 DEFAULT_MODULE += auto_init
+# this adds periph_init by default, other periph drivers
+# (e.g. i2c or spi) are only added when used
 DEFAULT_MODULE += periph_common
