fbb0045e3ce8fe157ad73afff936da0eede55010
Carton <joel.carron@eeproperty.ch>
2018-12-18 11:08:37 +0100

    Add support for "External PA/LNA Control" in mrf24j40 driver (useful for MRF24J40MD/ME)

--- drivers/include/mrf24j40.h
+++ drivers/include/mrf24j40.h
@@ -115,6 +115,10 @@ extern "C" {
 
 #define MRF24J40_MAX_FRAME_RETRIES      (3U)        /**< Number of frame retries (fixed) */
 
+#ifndef MRF24J40_USE_EXT_PA_LNA
+#define MRF24J40_USE_EXT_PA_LNA         (0U)        /**< Enable external PA/LNA control (Section 4.2 of datasheet) */
+#endif
+
 /**
  * @brief   struct holding all params needed for device initialization
  */
