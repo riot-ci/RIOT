554ae3e07d740ce47125182d18bdac73c755bc15
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-12-19 18:22:10 +0100

    TODO TEST ME riotboot: remove compatibility for HEXFILE/ELFFILE as file to flash
    FLASHFILE must be defined by the board to use this.

--- makefiles/boot/riotboot.mk
+++ makefiles/boot/riotboot.mk
@@ -92,20 +92,12 @@ riotboot/combined-slot0: $(RIOTBOOT_COMBINED_BIN)
 $(RIOTBOOT_COMBINED_BIN): $(BOOTLOADER_BIN)/riotboot.extended.bin $(SLOT0_RIOT_BIN)
 	$(Q)cat $^ > $@
 
-# Flashing rule for edbg to flash combined binaries
-riotboot/flash-combined-slot0: HEXFILE=$(RIOTBOOT_COMBINED_BIN)
-# Flashing rule for openocd to flash combined binaries
-riotboot/flash-combined-slot0: ELFFILE=$(RIOTBOOT_COMBINED_BIN)
 riotboot/flash-combined-slot0: FLASHFILE=$(RIOTBOOT_COMBINED_BIN)
 riotboot/flash-combined-slot0: $(RIOTBOOT_COMBINED_BIN) $(FLASHDEPS)
 	$(FLASHER) $(FFLAGS)
 
 # Flashing rule for slot 0
 riotboot/flash-slot0: export IMAGE_OFFSET=$(SLOT0_OFFSET)
-# Flashing rule for edbg to flash only slot 0
-riotboot/flash-slot0: HEXFILE=$(SLOT0_RIOT_BIN)
-# openocd
-riotboot/flash-slot0: ELFFILE=$(SLOT0_RIOT_BIN)
 riotboot/flash-slot0: FLASHFILE=$(SLOT0_RIOT_BIN)
 riotboot/flash-slot0: $(SLOT0_RIOT_BIN) $(FLASHDEPS)
 	$(FLASHER) $(FFLAGS)
