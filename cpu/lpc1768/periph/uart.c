4d655f7caac662021ac8be2ef1f8407800e30099
José Alamos <jialamos@uc.cl>
2019-01-02 14:48:59 +0100

    fixup! cpu/lpc1768: fix uninitialized variable

--- cpu/lpc1768/periph/uart.c
+++ cpu/lpc1768/periph/uart.c
@@ -140,7 +140,7 @@ static int init_base(uart_t uart, uint32_t baudrate)
 
 void uart_write(uart_t uart, const uint8_t *data, size_t len)
 {
-    LPC_UART_TypeDef *dev;
+    LPC_UART_TypeDef *dev = NULL;
 
     switch (uart) {
 #if UART_0_EN
@@ -154,8 +154,6 @@ void uart_write(uart_t uart, const uint8_t *data, size_t len)
             break;
 #endif
         default:
-            /* prevent cppcheck uninitialized variable error */
-            dev = (LPC_UART_TypeDef *) 0;
             return;
     }
 
