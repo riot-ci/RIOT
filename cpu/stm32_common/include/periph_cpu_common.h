588345f9655718fabe89cedfca3e49356efb8f2d
Yegor Yefremov <yegorslists@googlemail.com>
2019-01-08 11:10:49 +0100

    cpu/stm32_common: add support for uart_mode routine
    Add support for specifying data bits, stop bits and parity at
    runtime.
    
    Introduce feature periph_uart_modecfg for uart_mode() till all
    other CPUs implement it.
    
    STM32 L1, F1, F2, F4 supports following modes:
    
    * 7E1, 7E2
    * 7O1, 7O2
    * 8N1, 8N2
    * 8E1, 8E2
    * 8O1, 8O2
    
    STM32 L0, L4, F0, F3, F7 supports following modes:
    
    * 6E1, 6E2
    * 6O1, 6O2
    * 7E1, 7E2
    * 7O1, 7O2
    * 7N1, 7N2
    * 8N1, 8N2
    * 8E1, 8E2
    * 8O1, 8O2
    
    Use USART_CR1_M1 macro to detect 7-bit support because
    even inside one family there could be devices that don't
    support 7-bit mode. So just using a family macro is not
    enough.
    
    As stated in the datasheets for L0, L4, F0, F3, F7 devices,
    data bits can only be changed when UART is disabled (UE=0).
    Introduce uart_stop() routine to satisfy this requirement.
    
    STM32 UART adds parity to the MSB of a byte to send. The same
    also applies to the received bytes. As a result this bit must
    be masked in order to get the pure data.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- cpu/stm32_common/include/periph_cpu_common.h
+++ cpu/stm32_common/include/periph_cpu_common.h
@@ -370,6 +370,52 @@ typedef enum {
     STM32_LPUART,           /**< STM32 Low-power UART (LPUART) module type */
 } uart_type_t;
 
+#ifdef MODULE_PERIPH_UART_MODECFG
+#define UART_INVALID_MODE 0xFF
+/**
+ * @brief   Override parity values
+ * @{
+ */
+#define HAVE_UART_PARITY_T
+typedef enum {
+   UART_PARITY_NONE = 0,                               /**< no parity */
+   UART_PARITY_EVEN = USART_CR1_PCE,                   /**< even parity */
+   UART_PARITY_ODD = (USART_CR1_PCE | USART_CR1_PS),   /**< odd parity */
+   UART_PARITY_MARK = UART_INVALID_MODE,               /**< not supported */
+   UART_PARITY_SPACE = UART_INVALID_MODE               /**< not supported */
+} uart_parity_t;
+/** @} */
+
+/**
+ * @brief   Override data bits length values
+ * @{
+ */
+#define HAVE_UART_DATA_BITS_T
+typedef enum {
+   UART_DATA_BITS_8 = 0,                    /**< 8 data bits */
+    UART_DATA_BITS_5 = UART_INVALID_MODE,   /**< not supported */
+    UART_DATA_BITS_6 = UART_INVALID_MODE,   /**< not supported unless parity is set */
+#if defined(USART_CR1_M1)
+    UART_DATA_BITS_7 = USART_CR1_M1,        /**< 7 data bits */
+#else
+    UART_DATA_BITS_7 = UART_INVALID_MODE,   /**< not supported unless parity is set */
+#endif
+    UART_DATA_BITS_9 = UART_INVALID_MODE    /**< not supported */
+} uart_data_bits_t;
+/** @} */
+
+/**
+ * @brief   Override stop bits length values
+ * @{
+ */
+#define HAVE_UART_STOP_BITS_T
+typedef enum {
+   UART_STOP_BITS_1 = 0,                  /**< 1 stop bit */
+   UART_STOP_BITS_2 = USART_CR2_STOP_1,   /**< 2 stop bits */
+} uart_stop_bits_t;
+/** @} */
+#endif /* MODULE_PERIPH_UART_MODECFG */
+
 /**
  * @brief   Structure for UART configuration data
  */
