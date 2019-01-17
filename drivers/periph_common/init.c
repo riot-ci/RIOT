e7440f9c350f73c7c8633a897211f4547318e736
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-01-17 17:28:24 +0100

    boot: Moved stdio_init() into periph_init()
    Prior to this commit stdio is initialized at different stages during boot
    depending on the board used. This commit moves the initialization of stdio
    to the beginning of periph_init(). This commit makes stdio available at a
    predictable and hardware independent stage during boot, which makes the use of
    stdio (e.g. via DEBUG(...)) during boot more robust.

--- drivers/periph_common/init.c
+++ drivers/periph_common/init.c
@@ -20,6 +20,15 @@
  * @}
  */
 
+#ifdef MODULE_NEWLIB_SYSCALLS_FE310
+#include <stdio.h>
+#endif
+
+#ifdef MODULE_ATMEGA_COMMON
+#include "cpu.h"
+#else
+#include "stdio_base.h"
+#endif
 #ifdef MODULE_PERIPH_I2C
 #include "periph/i2c.h"
 #endif
@@ -36,8 +45,20 @@
 #include "periph/hwrng.h"
 #endif
 
+
 void periph_init(void)
 {
+    /* initialize stdio first to allow DEBUG() during later stages */
+#ifdef CPU_NATIVE
+#elif defined(MODULE_ATMEGA_COMMON)
+    atmega_stdio_init();
+#else
+    stdio_init();
+#ifdef CPU_FE310
+    setvbuf(stdout, NULL, _IONBF, 0);
+#endif /* CPU_FE310 */
+#endif /* CPU_NATIVE */
+
     /* initialize configured I2C devices */
 #ifdef MODULE_PERIPH_I2C
     for (unsigned i = 0; i < I2C_NUMOF; i++) {
