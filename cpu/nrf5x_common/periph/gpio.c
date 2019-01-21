a3fb90da446e34c48dc4c6df46a3ca3ebb5b176d
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-01-21 21:29:54 +0100

    fixup! cpu/nrf5x: handle multiple exti pins in gpio driver

--- cpu/nrf5x_common/periph/gpio.c
+++ cpu/nrf5x_common/periph/gpio.c
@@ -48,7 +48,7 @@
  * The index is incremented at the end of each call to gpio_init_int.
  * The index cannot be greater or equal than GPIOTE_CHAN_NUMOF.
  */
-static uint8_t _gpiote_chan_index = 0;
+static uint8_t _gpiote_next_index = 0;
 
 /**
  * @brief   Array containing a mapping between GPIOTE channel and pin
@@ -145,39 +145,50 @@ void gpio_write(gpio_t pin, int value)
 int gpio_init_int(gpio_t pin, gpio_mode_t mode, gpio_flank_t flank,
                   gpio_cb_t cb, void *arg)
 {
-    assert(_gpiote_chan_index < GPIOTE_CHAN_NUMOF);
+    uint8_t _pin_index = 0xff;
+    /* Looking for already known pin in exti table */
+    for (unsigned int i = 0; i < _gpiote_next_index; i++) {
+        if (_exti_pins[i] == pin) {
+            _pin_index = i;
+            break;
+        }
+    }
 
-    /* disable external interrupt in case one is active */
-    NRF_GPIOTE->INTENSET &= ~(GPIOTE_INTENSET_IN0_Msk);
+    /* New pin */
+    if (_pin_index == 0xff) {
+        assert(_gpiote_next_index < GPIOTE_CHAN_NUMOF);
+        _pin_index = _gpiote_next_index;
+        /* associate the current pin with channel index */
+        _exti_pins[_pin_index] = pin;
+        /* increase next index for next pin initialization */
+        _gpiote_next_index++;
+    }
+
     /* save callback */
-    exti_chan[_gpiote_chan_index].cb = cb;
-    exti_chan[_gpiote_chan_index].arg = arg;
+    exti_chan[_pin_index].cb = cb;
+    exti_chan[_pin_index].arg = arg;
     /* configure pin as input */
     gpio_init(pin, mode);
     /* set interrupt priority and enable global GPIOTE interrupt */
     NVIC_EnableIRQ(GPIOTE_IRQn);
     /* configure the GPIOTE channel: set even mode, pin and active flank */
-    NRF_GPIOTE->CONFIG[_gpiote_chan_index] = (GPIOTE_CONFIG_MODE_Event |
+    NRF_GPIOTE->CONFIG[_pin_index] = (GPIOTE_CONFIG_MODE_Event |
                              (pin_num(pin) << GPIOTE_CONFIG_PSEL_Pos) |
 #ifdef CPU_MODEL_NRF52840XXAA
                              ((pin & PORT_BIT) << 8) |
 #endif
                              (flank << GPIOTE_CONFIG_POLARITY_Pos));
     /* enable external interrupt */
-    NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk << _gpiote_chan_index;
+    NRF_GPIOTE->INTENSET |= (GPIOTE_INTENSET_IN0_Msk << _pin_index);
 
-    /* associate the current pin with channel index */
-    _exti_pins[_gpiote_chan_index] = pin;
-    /* increment channel index for next pin */
-    _gpiote_chan_index++;
     return 0;
 }
 
 void gpio_irq_enable(gpio_t pin)
 {
-    for (unsigned int i = 0; i < _gpiote_chan_index; ++i) {
+    for (unsigned int i = 0; i < _gpiote_next_index; i++) {
         if (_exti_pins[i] == pin) {
-            NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk << i;
+            NRF_GPIOTE->INTENSET |= (GPIOTE_INTENSET_IN0_Msk << i);
             break;
         }
     }
@@ -185,9 +196,12 @@ void gpio_irq_enable(gpio_t pin)
 
 void gpio_irq_disable(gpio_t pin)
 {
-    for (unsigned int i = 0; i < _gpiote_chan_index; ++i) {
+    for (unsigned int i = 0; i < _gpiote_next_index; i++) {
         if (_exti_pins[i] == pin) {
-            NRF_GPIOTE->INTENCLR |= GPIOTE_INTENCLR_IN0_Msk << i;
+            uint32_t inten = NRF_GPIOTE->INTENCLR;
+            NRF_GPIOTE->INTENCLR = (GPIOTE_INTENCLR_IN0_Msk << i);
+            inten &= ~(GPIOTE_INTENSET_IN0_Msk << i);
+            NRF_GPIOTE->INTENSET = inten;
             break;
         }
     }
@@ -195,8 +209,9 @@ void gpio_irq_disable(gpio_t pin)
 
 void isr_gpiote(void)
 {
-    for (unsigned int i = 0; i < _gpiote_chan_index; ++i) {
-        if (NRF_GPIOTE->EVENTS_IN[i] == 1) {
+    for (unsigned int i = 0; i < _gpiote_next_index; ++i) {
+        if (NRF_GPIOTE->EVENTS_IN[i] == 1 &&
+                (NRF_GPIOTE->INTENSET & (GPIOTE_INTENSET_IN0_Msk << i))) {
             NRF_GPIOTE->EVENTS_IN[i] = 0;
             exti_chan[i].cb(exti_chan[i].arg);
             break;
