47992d685b2c7ad29b3c0acac85073f775f51b66
Martine Lenders <m.lenders@fu-berlin.de>
2019-01-22 10:30:55 +0100

    dist/pythonlibs: provide unittest TestCase wrapper for testrunner
    I had this idea when implementing #10382 and #10392 as I introduced a
    very similar structure to python's standard unittests in those and it
    could also reduce some code duplication between those two tests.

--- dist/pythonlibs/testrunner/spawn.py
+++ dist/pythonlibs/testrunner/spawn.py
@@ -37,7 +37,7 @@ def find_exc_origin(exc_info):
 
 def setup_child(timeout=10, spawnclass=pexpect.spawnu, env=None, logfile=None):
     child = spawnclass("make term", env=env, timeout=timeout,
-                       codec_errors='replace', echo=(logfile is not None))
+                       codec_errors='replace')
 
     # on many platforms, the termprog needs a short while to be ready...
     time.sleep(MAKE_TERM_STARTED_DELAY)
