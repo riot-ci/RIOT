7245d0cb458a45bba70f7e6e8f22182c5071d4f3
Kaspar Schleiser <kaspar@schleiser.de>
2019-01-25 12:03:17 +0100

    core: disable uncrustify for atomic_*.c

--- core/atomic_c11.c
+++ core/atomic_c11.c
@@ -41,6 +41,11 @@
 #include <string.h>
 #include "irq.h"
 
+/*
+ * uncrustify mis-formats the macros in this file, so disable it globally.
+ * *INDENT-OFF*
+ */
+
 /* GCC documentation refers to the types as I1, I2, I4, I8, I16 */
 typedef uint8_t  I1;
 typedef uint16_t I2;
