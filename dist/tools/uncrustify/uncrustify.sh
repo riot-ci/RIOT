7c99f858de1037d223880047d60ca137dffe6d68
Kaspar Schleiser <kaspar@schleiser.de>
2019-01-25 12:20:29 +0100

    dist/tools/uncrustify: switch back to uncrustify-riot.cfg

--- dist/tools/uncrustify/uncrustify.sh
+++ dist/tools/uncrustify/uncrustify.sh
@@ -2,6 +2,7 @@
 RIOTBASE=$(git rev-parse --show-toplevel)
 CURDIR=$(cd "$(dirname "$0")" && pwd)
 BLACKLIST=$CURDIR/blacklist.txt
+UNCRUSTIFY_CFG="$RIOTBASE"/uncrustify-riot.cfg
 
 . "$RIOTBASE"/dist/tools/ci/changed_files.sh
 FILES=$(changed_files | grep -vf "$BLACKLIST")
@@ -9,7 +10,7 @@ FILES=$(changed_files | grep -vf "$BLACKLIST")
 check () {
     for F in $FILES
     do
-        OUT="$(uncrustify -c "$CURDIR"/uncrustify-force.cfg -f "$RIOTBASE/$F" --check 2> /dev/null)"
+        OUT="$(uncrustify -c "$UNCRUSTIFY_CFG" -f "$RIOTBASE/$F" --check 2> /dev/null)"
         if [ "$OUT" ] ; then
             echo "Please run 'dist/tools/uncrustify/uncrustify.sh'"
             exit 1
@@ -25,7 +26,7 @@ exec_uncrustify () {
     fi
     for F in $FILES
     do
-        uncrustify -c "$CURDIR"/uncrustify-force.cfg --no-backup "$RIOTBASE/$F"
+        uncrustify -c "$UNCRUSTIFY_CFG" --no-backup "$RIOTBASE/$F"
     done
 }
 
