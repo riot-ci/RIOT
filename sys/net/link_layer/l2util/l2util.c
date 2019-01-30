ad3487e4c6b724a2e464b540501bb8563259ad43
Martine Lenders <m.lenders@fu-berlin.de>
2019-01-30 14:40:39 +0100

    fixup! l2util: initial import
    Add function for EUI-64 conversion

--- sys/net/link_layer/l2util/l2util.c
+++ sys/net/link_layer/l2util/l2util.c
@@ -24,37 +24,76 @@
 #include "net/l2util.h"
 
 #if defined(MODULE_CC110X) || defined(MODULE_NRFMIN)
-static void _create_iid_from_short(const uint8_t *addr, size_t addr_len,
-                                   eui64_t *iid)
+static void _create_eui64_from_short(const uint8_t *addr, size_t addr_len,
+                                     eui64_t *eui64)
 {
     const unsigned offset = sizeof(eui64_t) - addr_len;
 
-    memset(iid->uint8, 0, sizeof(iid->uint8));
-    iid->uint8[3] = 0xff;
-    iid->uint8[4] = 0xfe;
-    memcpy(&iid->uint8[offset], addr, addr_len);
+    memset(eui64->uint8, 0, sizeof(eui64->uint8));
+    eui64->uint8[3] = 0xff;
+    eui64->uint8[4] = 0xfe;
+    memcpy(&eui64->uint8[offset], addr, addr_len);
 }
 #endif /* defined(MODULE_CC110X) || defined(MODULE_NRFMIN) */
+
+int l2util_eui64_from_addr(int dev_type, const uint8_t *addr, size_t addr_len,
+                           eui64_t *eui64)
+{
+    switch (dev_type) {
+#if defined(MODULE_NETDEV_ETH) || defined(MODULE_ESP_NOW) || \
+defined(MODULE_NORDIC_SOFTDEVICE_BLE)
+        case NETDEV_TYPE_ETHERNET:
+        case NETDEV_TYPE_ESP_NOW:
+        case NETDEV_TYPE_BLE:
+            if (addr_len == sizeof(eui48_t)) {
+                eui48_to_eui64(eui64, (const eui48_t *)addr);
+                return sizeof(eui64_t);
+            }
+            else {
+                return -EINVAL;
+            }
+#endif  /* defined(MODULE_NETDEV_ETH) || defined(MODULE_ESP_NOW) */
+#if defined(MODULE_NETDEV_IEEE802154) || defined(MODULE_XBEE)
+        case NETDEV_TYPE_IEEE802154:
+            switch (addr_len) {
+                /* EUI-64 can *not* be generated from the short address */
+                case IEEE802154_LONG_ADDRESS_LEN:
+                    memcpy(eui64, addr, addr_len);
+                    return sizeof(eui64_t);
+                default:
+                    return -EINVAL;
+            }
+#endif  /* defined(MODULE_NETDEV_IEEE802154) || defined(MODULE_XBEE) */
+#if defined(MODULE_CC110X) || defined(MODULE_NRFMIN)
+        case NETDEV_TYPE_CC110X:
+        case NETDEV_TYPE_NRFMIN:
+            if (addr_len <= 3) {
+                _create_eui64_from_short(addr, addr_len, eui64);
+                return sizeof(eui64_t);
+            }
+            else {
+                return -EINVAL;
+            }
+#endif  /* defined(MODULE_CC110X) || defined(MODULE_NRFMIN) */
+        default:
+            (void)addr;
+            (void)addr_len;
+            (void)eui64;
+#ifdef DEVELHELP
+            LOG_ERROR("l2util: can't convert hardware address to EUI-64 "
+                      "for device type %d\n", dev_type);
+#endif  /* DEVELHELP */
+            assert(false);
+            break;
+    }
+    return -ENOTSUP;
+}
 
 int l2util_ipv6_iid_from_addr(int dev_type,
                               const uint8_t *addr, size_t addr_len,
                               eui64_t *iid)
 {
     switch (dev_type) {
-#if defined(MODULE_NETDEV_ETH) || defined(MODULE_ESP_NOW) || \
-    defined(MODULE_NORDIC_SOFTDEVICE_BLE)
-        case NETDEV_TYPE_ETHERNET:
-        case NETDEV_TYPE_ESP_NOW:
-        case NETDEV_TYPE_BLE:
-            if (addr_len == sizeof(eui48_t)) {
-                eui48_to_ipv6_iid(iid, (const eui48_t *)addr);
-                return sizeof(eui64_t);
-            }
-            else {
-                return -EINVAL;
-            }
-#endif  /* defined(MODULE_NETDEV_ETH) || defined(MODULE_ESP_NOW) || \
-         * defined(MODULE_NORDIC_SOFTDEVICE_BLE) */
 #if defined(MODULE_NETDEV_IEEE802154) || defined(MODULE_XBEE)
         case NETDEV_TYPE_IEEE802154:
             if (ieee802154_get_iid(iid, addr, addr_len) != NULL) {
@@ -68,23 +107,24 @@ int l2util_ipv6_iid_from_addr(int dev_type,
         case NETDEV_TYPE_CC110X:
         case NETDEV_TYPE_NRFMIN:
             if (addr_len <= 3) {
-                _create_iid_from_short(addr, addr_len, iid);
+                _create_eui64_from_short(addr, addr_len, iid);
+                /* since this address conversion is based on the IEEE
+                 * 802.15.4 address conversion for short addresses, the
+                 * U/L bit doesn't need to be flipped.
+                 * see https://tools.ietf.org/html/rfc6282#section-3.2.2 */
                 return sizeof(eui64_t);
             }
             else {
                 return -EINVAL;
             }
 #endif  /* defined(MODULE_CC110X) || defined(MODULE_NRFMIN) */
-        default:
-            (void)addr;
-            (void)addr_len;
-            (void)iid;
-#ifdef DEVELHELP
-            LOG_ERROR("gnrc_netif: can't convert hardware address to IID "
-                      "for device type %d\n", dev_type);
-#endif  /* DEVELHELP */
-            assert(false);
-            break;
+        default: {
+            int res = l2util_eui64_from_addr(dev_type, addr, addr_len, iid);
+            if (res == sizeof(eui64_t)) {
+                iid->uint8[0] ^= 0x02;
+            }
+            return res;
+        }
     }
     return -ENOTSUP;
 }
@@ -124,8 +164,8 @@ int l2util_ipv6_iid_to_addr(int dev_type, const eui64_t *iid, uint8_t *addr)
             (void)iid;
             (void)addr;
 #ifdef DEVELHELP
-            LOG_ERROR("gnrc_netif: can't convert IID to hardware address "
-                      "for device type %d\n", dev_type);
+            LOG_ERROR("l2util: can't convert IID to hardware address for "
+                      "device type %d\n", dev_type);
 #endif  /* DEVELHELP */
             assert(false);
             break;
@@ -173,7 +213,7 @@ int l2util_ndp_addr_len_from_l2ao(int dev_type,
         default:
             (void)opt;
 #ifdef DEVELHELP
-            LOG_ERROR("gnrc_netif: can't get address length from NDP link-layer "
+            LOG_ERROR("l2util: can't get address length from NDP link-layer "
                       "address option for device type %d\n", dev_type);
 #endif
             assert(false);
