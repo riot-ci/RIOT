e2961207a75060ea3ead0f89e120137abaef9393
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-03-14 17:40:27 +0100

    makefiles/pyocd.inc.mk: remove OFLAGS
    OFLAGS is set by the `.bin` and `.hex` rules no need to define it anymore.

--- makefiles/tools/pyocd.inc.mk
+++ makefiles/tools/pyocd.inc.mk
@@ -3,7 +3,6 @@ export DEBUGGER = $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 export DEBUGSERVER = $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 export RESET ?= $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 
-export OFLAGS ?= -O ihex
 export FFLAGS ?= flash $(HEXFILE)
 export DEBUGGER_FLAGS ?= debug $(ELFFILE)
 export DEBUGSERVER_FLAGS ?= debug-server
