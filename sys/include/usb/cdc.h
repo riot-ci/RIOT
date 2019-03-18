8630d1f2324a8d74dc2b88b8188fcbc3751ed977
Koen Zandberg <koen@bergzand.net>
2019-03-18 22:29:33 +0100

    fixup! usb_cdc: Add common defines and message structs

--- sys/include/usb/cdc.h
+++ sys/include/usb/cdc.h
@@ -16,6 +16,12 @@
 extern "c" {
 #endif
 
+#define USB_CDC_VERSION_BCD          0x0120 /**< USB CDC version in BCD */
+
+/**
+ * @name USB CDC subclass types
+ * @{
+ */
 #define USB_CDC_SUBCLASS_NONE          0x00 /**< No subclass */
 #define USB_CDC_SUBCLASS_DLCM          0x01 /**< Direct Line Control Model */
 #define USB_CDC_SUBCLASS_ACM           0x02 /**< Abstract Control Model */
@@ -30,7 +36,12 @@ extern "c" {
 #define USB_CDC_SUBCLASS_OBEX          0x0B /**< OBEX */
 #define USB_CDC_SUBCLASS_EEM           0x0C /**< Ethernet Emulation Model */
 #define USB_CDC_SUBCLASS_NCM           0x0D /**< Network Control Model */
+/** @} */
 
+/**
+ * @name USB CDC protocol types
+ * @{
+ */
 #define USB_CDC_PROTOCOL_NONE          0x00 /**< No protocol required */
 #define USB_CDC_PROTOCOL_ITU           0x01 /**< AT Commands: V.250 etc */
 #define USB_CDC_PROTOCOL_PCCA          0x02 /**< AT Commands defined by PCCA-101 */
@@ -41,7 +52,93 @@ extern "c" {
 #define USB_CDC_PROTOCOL_EEM           0x07 /**< Ethernet Emulation Model */
 #define USB_CDC_PROTOCOL_EXT           0xFE /**< External Protocol */
 #define USB_CDC_PROTOCOL_VENDOR        0xFF /**< Vendor-specific */
+/** @} */
+/**
+ * @name USB CDC descriptor subtypes
+ */
+#define USB_CDC_DESCR_SUBTYPE_FUNCTIONAL    0x00 /**< Header functional
+                                                   *  descriptor */
+#define USB_CDC_DESCR_SUBTYPE_UNION         0x06 /** Union descriptor */
+#define USB_CDC_DESCR_SUBTYPE_ETH_NET       0x0f /** Ethernet descriptor */
+/** @} */
 
+/**
+ * @name USB CDC management requests
+ * @{
+ */
+
+/**
+ * @brief Set ethernet multicast filter request
+ */
+#define USB_CDC_MGNT_REQUEST_SET_ETH_MULTICAST_FILTER   0x40
+
+/**
+ * @brief Set ethernet power management pattern filter
+ */
+#define USB_CDC_MGNT_REQUEST_SET_ETH_PM_PATTERN_FILTER  0x41
+
+/**
+ * @brief Get ethernet power management pattern filter
+ */
+#define USB_CDC_MGNT_REQUEST_GET_ETH_PM_PATTERN_FILTER  0x42
+
+/**
+ * @brief Set ethernet packet filter
+ */
+#define USB_CDC_MGNT_REQUEST_SET_ETH_PACKET_FILTER      0x43
+
+/**
+ * @brief Get ethernet statistics
+ */
+#define USB_CDC_MGNT_REQUEST_GET_ETH_STATISTICS         0x44
+/** @} */
+
+/**
+ * @name USB CDC management notifications
+ * @{
+ */
+
+/**
+ * @brief Network connection status notification
+ */
+#define USB_CDC_MGNT_NOTIF_NETWORK_CONNECTION   0x00
+
+/**
+ * @brief Response available notification
+ */
+#define USB_CDC_MGNT_NOTIF_RESPONSE_AVAILABLE   0x01
+
+/**
+ * @brief Hook on the auxiliary phone changed notification
+ */
+#define USB_CDC_MGNT_NOTIF_AUX_JACK_HOOK_STATE  0x08
+
+/**
+ * @brief Ring voltage on the POTS line interface notification
+ */
+#define USB_CDC_MGNT_NOTIF_RING_DETECT          0x09
+
+/**
+ * @brief Asynchronous UART status notification
+ */
+#define USB_CDC_MGNT_NOTIF_SERIAL_STATE         0x20
+
+/**
+ * @brief Call state change notification
+ */
+#define USB_CDC_MGNT_NOTIF_CALL_STATE_CHANGE    0x28
+
+/**
+ * @brief Line state change notification
+ */
+#define USB_CDC_MGNT_NOTIF_LINE_STATE_CHANGE    0x29
+
+/**
+ * @brief Throughput change notification
+ */
+#define USB_CDC_MGNT_NOTIF_CONN_SPEED_CHANGE    0x2A
+
+/** @} */
 
 #define USB_HID_COUNTRYCODE_NONE        0x00
 
