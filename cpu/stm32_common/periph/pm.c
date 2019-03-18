d6eeb2d176001e7cd56b053c60591cbb5c743c1a
Vincent Dupont <vincent@otakeys.com>
2019-03-18 11:44:06 +0100

    fixup! fixup! cpu/stm32f0: add periph_pm support

--- cpu/stm32_common/periph/pm.c
+++ cpu/stm32_common/periph/pm.c
@@ -27,11 +27,7 @@
 
 #include "irq.h"
 #include "periph/pm.h"
-#if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
-    defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4) || \
-    defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
 #include "stmclk.h"
-#endif
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
@@ -51,106 +47,57 @@
 #endif
 #endif
 
-#ifndef PM_STOP_CLEAR
+#ifndef PM_STANDBY_CONFIG
 /**
- * @brief Define flags cleared before configuring stop mode
+ * @brief Define config flags for standby mode
+ *
+ * Available values can be found in reference manual, PWR section, register CR.
  */
 #if defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
-#define PM_STOP_CLEAR   (PWR_CR_LPSDSR | PWR_CR_PDDS)
+#define PM_STANDBY_CONFIG   (PWR_CR_PDDS | PWR_CR_CWUF | PWR_CR_CSBF | PWR_CR_ULP)
 #else
-#define PM_STOP_CLEAR   (PWR_CR_LPDS | PWR_CR_PDDS)
+#define PM_STANDBY_CONFIG   (PWR_CR_PDDS | PWR_CR_CWUF | PWR_CR_CSBF)
 #endif
 #endif
-
-static inline uint32_t _ewup_config(void)
-{
-    uint32_t tmp = 0;
-#ifdef PM_EWUP_CONFIG
-    tmp |= PM_EWUP_CONFIG;
-#elif defined(PWR_CSR_EWUP)
-    tmp |= PWR_CSR_EWUP;
-#else
-#if defined(PWR_CSR_EWUP8)
-    tmp |= PWR_CSR_EWUP8;
-#endif
-#if defined(PWR_CSR_EWUP7)
-    tmp |= PWR_CSR_EWUP7;
-#endif
-#if defined(PWR_CSR_EWUP6)
-    tmp |= PWR_CSR_EWUP6;
-#endif
-#if defined(PWR_CSR_EWUP5)
-    tmp |= PWR_CSR_EWUP5;
-#endif
-#if defined(PWR_CSR_EWUP4)
-    tmp |= PWR_CSR_EWUP4;
-#endif
-#if defined(PWR_CSR_EWUP3)
-    tmp |= PWR_CSR_EWUP3;
-#endif
-#if defined(PWR_CSR_EWUP2)
-    tmp |= PWR_CSR_EWUP2;
-#endif
-#if defined(PWR_CSR_EWUP1)
-    tmp |= PWR_CSR_EWUP1;
-#endif
-#endif
-    return tmp;
-}
 
 void pm_set(unsigned mode)
 {
-    int deep = 0;
+    int deep;
 
-/* I just copied it from stm32f1/2/4, but I suppose it would work for the
- * others... /KS */
-#if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
-    defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4) || \
-    defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
     switch (mode) {
+#ifdef STM32_PM_STANDBY
         case STM32_PM_STANDBY:
-            /* Set PDDS to enter standby mode on deepsleep and clear flags */
-            PWR->CR |= (PWR_CR_PDDS | PWR_CR_CWUF | PWR_CR_CSBF);
+            PWR->CR &= ~(PM_STOP_CONFIG | PM_STANDBY_CONFIG);
+            PWR->CR |= PM_STANDBY_CONFIG;
             /* Enable WKUP pin to use for wakeup from standby mode */
-#if defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
-            /* Enable Ultra Low Power mode */
-            PWR->CR |= PWR_CR_ULP;
-#endif
-            PWR->CSR |= _ewup_config();
+            PWR->CSR |= PM_EWUP_CONFIG;
             /* Set SLEEPDEEP bit of system control block */
             deep = 1;
             break;
+#endif
+#ifdef STM32_PM_STOP
         case STM32_PM_STOP:
-            /* Clear PDDS and LPDS bits to enter stop mode on */
-            /* deepsleep with voltage regulator on */
-            PWR->CR &= ~(PM_STOP_CLEAR);
+            PWR->CR &= ~(PM_STOP_CONFIG | PM_STANDBY_CONFIG);
             PWR->CR |= PM_STOP_CONFIG;
             /* Set SLEEPDEEP bit of system control block */
             deep = 1;
             break;
-    }
-#else
-    (void) mode;
 #endif
+        default:
+            deep = 0;
+            break;
+    }
 
     cortexm_sleep(deep);
 
-#if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
-    defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4) || \
-    defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
     if (deep) {
         /* Re-init clock after STOP */
         stmclk_init_sysclk();
     }
-#endif
 }
 
-#if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
-    defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4) || \
-    defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
 void pm_off(void)
 {
     irq_disable();
     pm_set(0);
 }
-#endif
