f6764d7f9a90bd119291e7cfa5090a23eb8779b0
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-04-15 11:50:40 +0200

    drivers/saul: Improved stringification
    - Changed declaration of SAUL classes (while keeping the format)
        - Termed "class of SAUL classes" category of SAUL classes in the doc to
          avoid confusion. (E.g. a SAUL class will now be in category sensor or
          actuator.)
        - Separate enums for SAUL category and intra-category ID. Numbers will now
          but auto-assigned by the compiler
    - Use constant look-up tables for stringification (one table per SAUL category).
      ==> This saves 512B in .data section of AVR

--- drivers/saul/saul_str.c
+++ drivers/saul/saul_str.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Freie Universität Berlin
+ *               2019 Otto-von-Guericke-Universität Magdeburg
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -14,55 +15,82 @@
  * @brief       SAUL string functions
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Marian Buschsieweke <marian.buschsieweke@ovgu.de>
  *
  * @}
  */
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include "saul.h"
 
-/*
- * This is surely not the most beautiful implementation of a stringification
- * function, but works...
- */
+static const char *actuators[] = {
+    [_SAUL_ACT_ANY]             = "ACT_ANY",
+    [_SAUL_ACT_LED_RGB]         = "ACT_LED_RGB",
+    [_SAUL_ACT_SERVO]           = "ACT_SERVO",
+    [_SAUL_ACT_MOTOR]           = "ACT_MOTOR",
+    [_SAUL_ACT_SWITCH]          = "ACT_SWITCH",
+    [_SAUL_ACT_DIMMER]          = "ACT_DIMMER",
+};
+
+static const char *sensors[] = {
+    [_SAUL_SENSE_ANY]           = "SENSE_ANY",
+    [_SAUL_SENSE_BTN]           = "SENSE_BTN",
+    [_SAUL_SENSE_TEMP]          = "SENSE_TEMP",
+    [_SAUL_SENSE_HUM]           = "SENSE_HUM",
+    [_SAUL_SENSE_LIGHT]         = "SENSE_LIGHT",
+    [_SAUL_SENSE_ACCEL]         = "SENSE_ACCEL",
+    [_SAUL_SENSE_MAG]           = "SENSE_MAG",
+    [_SAUL_SENSE_GYRO]          = "SENSE_GYRO",
+    [_SAUL_SENSE_COLOR]         = "SENSE_COLOR",
+    [_SAUL_SENSE_PRESS]         = "SENSE_PRESS",
+    [_SAUL_SENSE_ANALOG]        = "SENSE_ANALOG",
+    [_SAUL_SENSE_UV]            = "SENSE_UV",
+    [_SAUL_SENSE_OBJTEMP]       = "SENSE_OBJTEMP",
+    [_SAUL_SENSE_COUNT]         = "SENSE_PULSE_COUNT",
+    [_SAUL_SENSE_DISTANCE]      = "SENSE_DISTANCE",
+    [_SAUL_SENSE_CO2]           = "SENSE_CO2",
+    [_SAUL_SENSE_TVOC]          = "SENSE_TVOC",
+    [_SAUL_SENSE_PROXIMITY]     = "SENSE_PROXIMITY",
+    [_SAUL_SENSE_RSSI]          = "SENSE_RSSI",
+    [_SAUL_SENSE_CHARGE]        = "SENSE_CHARGE",
+    [_SAUL_SENSE_CURRENT]       = "SENSE_CURRENT",
+    [_SAUL_SENSE_OCCUP]         = "SENSE_OCCUP",
+    [_SAUL_SENSE_PM]            = "SENSE_PM",
+    [_SAUL_SENSE_CAPACITANCE]   = "SENSE_CAPACITANCE",
+    [_SAUL_SENSE_VOLTAGE]       = "SENSE_VOLTAGE",
+    [_SAUL_SENSE_PH]            = "SENSE_PH",
+};
+
 const char *saul_class_to_str(const uint8_t class_id)
 {
-    switch (class_id) {
-        case SAUL_CLASS_UNDEF:       return "CLASS_UNDEF";
-        case SAUL_ACT_ANY:           return "ACT_ANY";
-        case SAUL_ACT_LED_RGB:       return "ACT_LED_RGB";
-        case SAUL_ACT_SERVO:         return "ACT_SERVO";
-        case SAUL_ACT_MOTOR:         return "ACT_MOTOR";
-        case SAUL_ACT_SWITCH:        return "ACT_SWITCH";
-        case SAUL_ACT_DIMMER:        return "ACT_DIMMER";
-        case SAUL_SENSE_ANY:         return "SENSE_ANY";
-        case SAUL_SENSE_BTN:         return "SENSE_BTN";
-        case SAUL_SENSE_TEMP:        return "SENSE_TEMP";
-        case SAUL_SENSE_HUM:         return "SENSE_HUM";
-        case SAUL_SENSE_LIGHT:       return "SENSE_LIGHT";
-        case SAUL_SENSE_ACCEL:       return "SENSE_ACCEL";
-        case SAUL_SENSE_MAG:         return "SENSE_MAG";
-        case SAUL_SENSE_GYRO:        return "SENSE_GYRO";
-        case SAUL_SENSE_COLOR:       return "SENSE_COLOR";
-        case SAUL_SENSE_PRESS:       return "SENSE_PRESS";
-        case SAUL_SENSE_ANALOG:      return "SENSE_ANALOG";
-        case SAUL_SENSE_UV:          return "SENSE_UV";
-        case SAUL_SENSE_OBJTEMP:     return "SENSE_OBJTEMP";
-        case SAUL_SENSE_COUNT:       return "SENSE_PULSE_COUNT";
-        case SAUL_SENSE_DISTANCE:    return "SENSE_DISTANCE";
-        case SAUL_SENSE_CO2:         return "SENSE_CO2";
-        case SAUL_SENSE_TVOC:        return "SENSE_TVOC";
-        case SAUL_SENSE_PROXIMITY:   return "SENSE_PROXIMITY";
-        case SAUL_SENSE_RSSI:        return "SENSE_RSSI";
-        case SAUL_SENSE_CHARGE:      return "SENSE_CHARGE";
-        case SAUL_SENSE_CURRENT:     return "SENSE_CURRENT";
-        case SAUL_SENSE_OCCUP:       return "SENSE_OCCUP";
-        case SAUL_SENSE_PM:          return "SENSE_PM";
-        case SAUL_SENSE_CAPACITANCE: return "SENSE_CAPACITANCE";
-        case SAUL_SENSE_VOLTAGE:     return "SENSE_VOLTAGE";
-        case SAUL_SENSE_PH:          return "SENSE_PH";
-        case SAUL_CLASS_ANY:         return "CLASS_ANY";
-        default:                     return "CLASS_UNKNOWN";
+    const char *result = NULL;
+    uint8_t id = class_id & SAUL_ID_MASK;
+    uint8_t cat = class_id & SAUL_CAT_MASK;
+    switch (cat) {
+        case SAUL_CAT_UNDEF:
+            return "CLASS_UNDEF";
+        case SAUL_CAT_ACT:
+            if (id < SAUL_ACT_NUMOF) {
+                result = actuators[id];
+            }
+            break;
+        case SAUL_CAT_SENSE:
+            if (id < SAUL_SENSE_NUMOF) {
+                result = sensors[id];
+            }
+            break;
+        default:
+            if (class_id == SAUL_CLASS_ANY) {
+                return "CLASS_ANY";
+            }
+            break;
     }
+
+    if (result == NULL) {
+        result = "CLASS_UNKNOWN";
+    }
+
+    return result;
 }
