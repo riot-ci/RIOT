496e7fcf85c1801103c7ed8984094ea4ce553c84
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-04-25 08:18:35 +0200

    fixup! boards/frdm-kw41z: use common configuration

--- boards/frdm-kw41z/include/periph_conf.h
+++ boards/frdm-kw41z/include/periph_conf.h
@@ -21,11 +21,51 @@
 
 #include "periph_cpu.h"
 #include "periph_conf_common.h"
+#include "cfg_i2c_default.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+/**
+ * @name SPI configuration
+ * @{
+ */
+static const spi_conf_t spi_config[] = {
+    {
+        .dev      = SPI0,
+        .pin_miso = GPIO_PIN(PORT_C, 18),
+        .pin_mosi = GPIO_PIN(PORT_C, 17),
+        .pin_clk  = GPIO_PIN(PORT_C, 16),
+        .pin_cs   = {
+            GPIO_PIN(PORT_C, 19),
+            GPIO_UNDEF,
+            GPIO_UNDEF,
+            GPIO_UNDEF,
+            GPIO_UNDEF
+        },
+        .pcr      = GPIO_AF_2,
+        .simmask  = SIM_SCGC6_SPI0_MASK
+    },
+    {
+        .dev      = SPI1,
+        .pin_miso = GPIO_PIN(PORT_A, 17),
+        .pin_mosi = GPIO_PIN(PORT_A, 16),
+        .pin_clk  = GPIO_PIN(PORT_A, 18),
+        .pin_cs   = {
+            GPIO_PIN(PORT_A, 19),
+            GPIO_UNDEF,
+            GPIO_UNDEF,
+            GPIO_UNDEF,
+            GPIO_UNDEF
+        },
+        .pcr      = GPIO_AF_2,
+        .simmask  = SIM_SCGC6_SPI1_MASK
+    }
+};
+#define SPI_NUMOF           (sizeof(spi_config) / sizeof(spi_config[0]))
+/** @} */
 
 /**
  * @name ADC configuration
