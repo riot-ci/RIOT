269adb3e3f66767c84e98a1068a7ed1611021c65
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-05-16 14:03:50 +0200

    fixup! drivers: sys: add SAUL support for ds75lx sensor

--- drivers/ds75lx/ds75lx_saul.c
+++ drivers/ds75lx/ds75lx_saul.c
@@ -25,13 +25,15 @@
 
 static int read_temperature(const void *dev, phydat_t *res)
 {
+    ds75lx_wakeup((const ds75lx_t *)dev);
     if (ds75lx_read_temperature((const ds75lx_t *)dev, &res->val[0]) != DS75LX_OK) {
         return -ECANCELED;
     }
+    ds75lx_shutdown((const ds75lx_t *)dev);
 
     res->unit = UNIT_TEMP_C;
     res->scale = -2;
-    return 0;
+    return 1;
 }
 
 const saul_driver_t ds75lx_temperature_saul_driver = {
