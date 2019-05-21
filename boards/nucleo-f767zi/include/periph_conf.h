caa02fe2d67739104067e3f1c75764aa3b3d09f7
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-05-21 15:47:17 +0200

    stm32_eth: Code Cleanup and some fixes by Alexandre Abadie
    cpu/stm32_common: cleanup periph eth
    boards/nucleo-f767zi: cleanup dependencies
    boards/nucleo-f767zi: fix dma configuration attribute for eth
    examples/default: add nucleo-767zi in boards with netif

--- boards/nucleo-f767zi/include/periph_conf.h
+++ boards/nucleo-f767zi/include/periph_conf.h
@@ -40,7 +40,7 @@ static const dma_conf_t dma_config[] = {
     { .stream = 4 },    /* DMA1 Stream 4 - USART3_TX */
     { .stream = 14 },   /* DMA2 Stream 6 - USART6_TX */
     { .stream = 6 },    /* DMA1 Stream 6 - USART2_TX */
-    { .stream = 8 },    /* DMA2 Stream 8 - ETH_TX    */
+    { .stream = 8 },    /* DMA2 Stream 0 - ETH_TX    */
 };
 
 #define DMA_0_ISR  isr_dma1_stream4
@@ -178,8 +178,8 @@ static const eth_conf_t eth_config = {
     .mode = RMII,
     .mac = { 0 },
     .speed = ETH_SPEED_100TX_FD,
-    .dma_chan = 0,
-    .dma_stream = 8,
+    .dma = 3,
+    .dma_chan = 8,
     .phy_addr = 0x01,
     .pins = {
         GPIO_PIN(PORT_G, 13),
