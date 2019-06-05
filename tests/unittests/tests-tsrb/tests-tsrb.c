e3762fcd67eb57ecba025272b93bb8bb9015a259
Martine Lenders <mail@martine-lenders.eu>
2019-06-05 11:13:14 +0200

    fixup! Merge pull request #11615 from cladmi/pr/cpu/atmega_common/ldscripts_compat/doc

--- tests/unittests/tests-tsrb/tests-tsrb.c
+++ tests/unittests/tests-tsrb/tests-tsrb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Cenk Gündoğan <cnkgndgn@gmail.com>
+ * Copyright (C) 2019 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
