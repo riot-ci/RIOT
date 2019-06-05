6b28d70c586def91979e5a6cf879e1302e775cd3
Martine Lenders <m.lenders@fu-berlin.de>
2019-06-05 15:31:37 +0200

    fixup! tsrb: change input type to `uint8_t`

--- sys/include/tsrb.h
+++ sys/include/tsrb.h
@@ -47,7 +47,7 @@ typedef struct tsrb {
 /**
  * @brief Static initializer
  */
-#define TSRB_INIT(BUF) { (BUF), sizeof (BUF), 0, 0 }
+#define TSRB_INIT(BUF) { (uint8_t *)(BUF), sizeof (BUF), 0, 0 }
 
 /**
  * @brief        Initialize a tsrb.
