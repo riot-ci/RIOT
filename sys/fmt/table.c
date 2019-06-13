51b89ea4fbc9dd7ae6e61afead8025f6afa55d8d
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-06-13 14:52:59 +0200

    sys/fmt: Added submodule fmt_table
    - print_pattern can be used to fill up a column with spaces or to draw a
      line
    - print_col_u32_dec() / print_col_s32_dec() can be used to print an
      uint32_t / int32_t as a column of the given width

---
+++ sys/fmt/table.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_fmt_table
+ * @{
+ *
+ * @file
+ * @brief       Implementation of the table extensions of the string formatting
+ *              library
+ *
+ * @author      Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ *
+ * @}
+ */
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "fmt.h"
+
+const char fmt_table_spaces[16] = "                ";
+
+void print_pattern(const char *pat, size_t pat_size, size_t fill_size)
+{
+    while (fill_size > pat_size) {
+        print(pat, pat_size);
+    }
+
+    print(pat, fill_size);
+}
+
+void print_col_u32_dec(uint32_t number, size_t width)
+{
+    char sbuf[10]; /* "4294967295" */
+    size_t slen;
+
+    slen = fmt_u32_dec(sbuf, number);
+    if (width > slen) {
+        print_pattern(fmt_table_spaces, sizeof(fmt_table_spaces), width - slen);
+    }
+    print(sbuf, slen);
+}
+
+void print_col_s32_dec(int32_t number, size_t width)
+{
+    char sbuf[11]; /* "-2147483648" */
+    size_t slen;
+
+    slen = fmt_s32_dec(sbuf, number);
+    if (width > slen) {
+        print_pattern(fmt_table_spaces, sizeof(fmt_table_spaces), width - slen);
+    }
+    print(sbuf, slen);
+}
