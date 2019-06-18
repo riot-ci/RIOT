d6f496763639a719576717348796596523d3419c
Kaspar Schleiser <kaspar@schleiser.de>
2019-06-18 11:04:03 +0200

    riotboot: always link both slot .elf when linking
    This enables "make test" and "make test-murdock" to rely on both images being
    present and thus execute without re-compiling.

--- makefiles/boot/riotboot.mk
+++ makefiles/boot/riotboot.mk
@@ -44,7 +44,11 @@ $(BINDIR_APP)-slot0.elf: ROM_OFFSET=$(SLOT0_IMAGE_OFFSET)
 $(BINDIR_APP)-slot1.elf: FW_ROM_LEN=$$((SLOT1_LEN - $(RIOTBOOT_HDR_LEN)))
 $(BINDIR_APP)-slot1.elf: ROM_OFFSET=$(SLOT1_IMAGE_OFFSET)
 
+# ensure both slot elf files are always linked
+# this ensures that both "make test" and "make test-murdock" can rely on them
+# being present without having to trigger re-compilation.
 SLOT_RIOT_ELFS = $(BINDIR_APP)-slot0.elf $(BINDIR_APP)-slot1.elf
+link: $(SLOT_RIOT_ELFS)
 
 # Create binary target with RIOT header
 $(SLOT_RIOT_BINS): %.riot.bin: %.hdr %.bin
