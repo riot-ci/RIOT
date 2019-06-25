69cc4e70b9fc9818e639acb4d3835debf0dd1bfe
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-06-25 11:36:09 +0200

    fixup! gnrc_lorawan: add initial support for LoRaWAN stack

--- sys/net/gnrc/link_layer/gnrc_lorawan/gnrc_lorawan.c
+++ sys/net/gnrc/link_layer/gnrc_lorawan/gnrc_lorawan.c
@@ -21,6 +21,7 @@
 #include "net/gnrc/pktbuf.h"
 
 #include "net/lorawan/hdr.h"
+#include "net/loramac.h"
 #include "net/gnrc/lorawan/region.h"
 
 #define ENABLE_DEBUG    (0)
@@ -232,9 +233,11 @@ void gnrc_lorawan_event_timeout(gnrc_lorawan_t *mac)
     _sleep_radio(mac);
 }
 
+/* This function uses a precomputed table to calculate time on air without
+ * using floating point arithmetics */
 static uint32_t lora_time_on_air(size_t payload_size, uint8_t dr, uint8_t cr)
 {
-    assert(dr <= 6);
+    assert(dr <= LORAMAC_DR_6);
     uint8_t _K[6][4] = {    { 0, 1, 5, 5 },
                             { 0, 1, 4, 5 },
                             { 1, 5, 5, 5 },
@@ -245,7 +248,7 @@ static uint32_t lora_time_on_air(size_t payload_size, uint8_t dr, uint8_t cr)
     uint32_t t_sym = 1 << (15 - dr);
     uint32_t t_preamble = (t_sym << 3) + (t_sym << 2) + (t_sym >> 2);
 
-    int index = dr < 6 ? dr : 5;
+    int index = dr < LORAMAC_DR_6 ? dr : LORAMAC_DR_5;
     uint8_t n0 = _K[index][0];
     int nb_symbols;
 
