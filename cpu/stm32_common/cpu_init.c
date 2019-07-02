569fc6228ac37cd2bc73b98fcc7740e4f6d09477
francisco <femolina@uc.cl>
2019-07-02 12:07:54 +0200

    fixup! cpu/stm32_common: minimize consumption for STM32F0/2/3/4/7

--- cpu/stm32_common/cpu_init.c
+++ cpu/stm32_common/cpu_init.c
@@ -26,6 +26,8 @@
  * @author      Víctor Ariño <victor.arino@zii.aero>
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
  * @author      Vincent Dupont <vincent@otakeys.com>
+ * @author      Oleg Artamonov <oleg@unwds.com>
+ * @author      Francisco Molina <francisco.molina@inria.cl>
  *
  * @}
  */
@@ -41,6 +43,95 @@
 #define BIT_APB_PWREN       RCC_APB1ENR_PWREN
 #endif
 
+#if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
+    defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F3) || \
+    defined(CPU_FAM_STM32F4) || defined(CPU_FAM_STM32F7)
+
+#define STM32_CPU_MAX_GPIOS    (12U)
+
+#if defined(CPU_FAM_STM32F3) || defined(CPU_FAM_STM32F0)
+#define GPIO_CLK              (AHB)
+#define GPIO_CLK_ENR          (RCC->AHBENR)
+#define GPIO_CLK_ENR_MASK     (0xFFFF0000)
+#elif defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4) || \
+      defined(CPU_FAM_STM32F7)
+#define GPIO_CLK              (AHB1)
+#define GPIO_CLK_ENR          (RCC->AHB1ENR)
+#define GPIO_CLK_ENR_MASK     (0x0000FFFF)
+#elif defined(CPU_FAM_STM32F1)
+#define GPIO_CLK              (APB2)
+#define GPIO_CLK_ENR          (RCC->APB2ENR)
+#define GPIO_CLK_ENR_MASK     (0x000001FC)
+#endif
+
+/**
+ * @brief   Initialize gpio to AIN
+ *
+ * stm32f need to have all there pins initialized to AIN so the consumption
+ * of the input Schmitt trigger is saved when running in STOP mode.
+ *
+ * @see https://comm.eefocus.com/media/download/index/id-1013834
+ */
+void _gpio_init_ain(void)
+{
+    uint32_t ahb_gpio_clocks;
+
+    /* enable GPIO clock and save GPIO clock configuration */
+    ahb_gpio_clocks = GPIO_CLK_ENR & GPIO_CLK_ENR_MASK;
+    periph_clk_en(GPIO_CLK, GPIO_CLK_ENR_MASK);
+
+    /* switch all GPIOs to AIN mode to minimize power consumption */
+    for (uint8_t i = 0; i < STM32_CPU_MAX_GPIOS; i++) {
+        GPIO_TypeDef *port;
+        port = (GPIO_TypeDef *)(GPIOA_BASE + i*(GPIOB_BASE - GPIOA_BASE));
+        if (IS_GPIO_ALL_INSTANCE(port)) {
+#if !defined (DISABLE_JTAG)
+            switch (i) {
+                /* preserve JTAG pins on PORTA and PORTB */
+                case 0:
+#if defined(CPU_FAM_STM32F1)
+                    port->CR[0] = GPIO_CRL_CNF;
+                    port->CR[1] = GPIO_CRH_CNF & 0x000FFFFF;
+#else
+                    port->MODER = 0xABFFFFFF;
+#endif
+                    break;
+                case 1:
+#if defined(CPU_FAM_STM32F1)
+                    port->CR[0] = GPIO_CRL_CNF & 0xFFF00FFF;
+                    port->CR[1] = GPIO_CRH_CNF;
+#else
+                    port->MODER = 0xFFFFFEBF;
+#endif
+                    break;
+                default:
+#if defined(CPU_FAM_STM32F1)
+                    port->CR[0] = GPIO_CRL_CNF;
+                    port->CR[1] = GPIO_CRH_CNF;
+#else
+                    port->MODER = 0xFFFFFFFF;
+#endif
+                    break;
+            }
+#else
+#if defined(CPU_FAM_STM32F1)
+                    port->CR[0] = GPIO_CRL_CNF;
+                    port->CR[1] = GPIO_CRH_CNF;
+#else
+                    port->MODER = 0xFFFFFFFF;
+#endif
+#endif
+        }
+        else {
+            break;
+        }
+    }
+
+    /* restore GPIO clocks */
+    periph_clk_en(GPIO_CLK, ahb_gpio_clocks);
+}
+#endif
+
 void cpu_init(void)
 {
     /* initialize the Cortex-M core */
@@ -52,7 +143,7 @@ void cpu_init(void)
 #if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F1) || \
     defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F3) || \
     defined(CPU_FAM_STM32F4) || defined(CPU_FAM_STM32F7)
-    gpio_pm_init_ain();
+    _gpio_init_ain();
 #endif
 #ifdef MODULE_PERIPH_DMA
     /*  initialize DMA streams */
