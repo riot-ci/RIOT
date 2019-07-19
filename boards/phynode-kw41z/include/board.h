bb716ef457486806d3c6bed541c8c296d2348f6e
Kaspar Schleiser <kaspar@schleiser.de>
2019-07-19 23:02:20 +0200

    boards/*-kw41z: add ztimer_msec config

--- boards/phynode-kw41z/include/board.h
+++ boards/phynode-kw41z/include/board.h
@@ -106,6 +106,16 @@ extern "C"
 /** @} */
 
 /**
+ * @name    ztimer configuration
+ * @{
+ */
+#define CONFIG_ZTIMER_USEC_TYPE     ZTIMER_TYPE_PERIPH
+#define CONFIG_ZTIMER_USEC_DEV      (TIMER_PIT_DEV(0))
+#define CONFIG_ZTIMER_USEC_FREQ     (1000000U)
+#define CONFIG_ZTIMER_USEC_WIDTH    (32)
+/** @} */
+
+/**
  * @name Define the interface for the CCS811 gas sensors
  * @{
  */
