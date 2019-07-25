e647be4938c122730344111c34eec780d880f434
Martine S. Lenders <m.lenders@fu-berlin.de>
2019-07-25 17:10:54 +0200

    fixup! tests: provide testcase for issue 11398
    Use nodes actual destination address for test

--- tests/gnrc_netif_ieee802154/tests/01-run.py
+++ tests/gnrc_netif_ieee802154/tests/01-run.py
@@ -21,18 +21,23 @@ ZEP_V2_TYPE_DATA = 1
 def testfunc(child):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind(("", 17754))
+        child.expect("l2_addr: ([0-9A-F:]+)")
+        dst = child.match.group(1).split(':')
+        dst = [int(b, base=16) for b in reversed(dst)]
+        dst = bytes(dst)
+
         # first send valid packet to check if communication is set up correctly
         # generated with udp send <link-local> 61616 0 in tests/gnrc_udp
-        payload = b"\x61\xdc\x85\x23\x00\x5a\x45\x00\x0a\x50\x45\x5a\x00\x2a\xab\xdc" \
-                  b"\x15\x54\x01\x64\x79\x7e\x33\xf3\x00\xa8\x80"
+        payload = b"\x61\xd8\x85\x23\x00" + dst + b"\x2a\xab\xdc" \
+                  b"\x15\x54\x01\x64\x79\x7e\x33\xf3\x00\x54\x2e"
         packet = struct.pack(ZEP_HDR, b"EX", ZEP_V2_VERSION,
                              ZEP_V2_TYPE_DATA, 26, 1, 1, 0xff,
                              0, 0, 1, len(payload)) + payload
         s.sendto(packet, ("localhost", 17755))
         child.expect("PKTDUMP: data received:")
         child.expect("00000000  7E  33  F3  00")
-        child.expect("~~ PKT    -  2 snips, total size:  28 byte")
-        payload = b"\x01\xdc\x00\x23\x00\x5a\x45\x00\x0a\x50\x45\x5a\x00"
+        child.expect("~~ PKT    -  2 snips, total size:  23 byte")
+        payload = b"\x61\xd8\x00\x23\x00" + dst
         # child.expect("PKTDUMP: data received:")
         packet = struct.pack(ZEP_HDR, b"EX", ZEP_V2_VERSION,
                              ZEP_V2_TYPE_DATA, 26, 1, 1, 0xff,
@@ -41,7 +46,7 @@ def testfunc(child):
         res = child.expect([TIMEOUT, EOF, "PKTDUMP: data received:"])
         # we actually want the timeout here. The application either
         # receives a bogus packet or crashes in an error case case
-        assert(res == 0)
+        assert res == 0
 
 
 if __name__ == "__main__":
