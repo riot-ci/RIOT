8d305914d8601637eb3767a4bd966adfc22889a1
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-08-01 13:36:11 +0200

    fixup! fixup! fixup! sys: make use of ARRAY_SIZE macro

--- sys/include/embUnit.h
+++ sys/include/embUnit.h
@@ -22,10 +22,7 @@
 #define EMBUNIT_H
 
 #include "embUnit/embUnit.h"
-
-#ifndef ARRAY_SIZE
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*a))
-#endif
+#include "kernel_defines.h"
 
 #ifdef OUTPUT
 #   define OUTPUT_XML       (1)
