19e9c5b1ab12fa1c7698f4e02c79f01e0441ca8e
Daniele Lacamera <dan@danielinux.net>
2019-08-06 17:06:57 +0200

    wolfSSL pkg fixes during review

--- pkg/wolfssl/include/user_settings.h
+++ pkg/wolfssl/include/user_settings.h
@@ -21,17 +21,22 @@ extern "C" {
 #define NO_MAIN_DRIVER
 #define NO_SIG_WRAPPER
 #define NO_OLD_RNGNAME
-#define WOLFSSL_SMALL_STACK
-//#define WOLFSSL_STATIC_MEMORY
-//#define USE_FAST_MATH
 
+/* Uncomment the next two lines to enable wolfSSL debug */
+// #define DEBUG_WOLFSSL
+// #define WOLFSSL_LOG_PRINTF
 
 /* Single precision math */
 #define WOLFSSL_SP_MATH
 #define WOLFSSL_SP_SMALL
 #define SP_WORD_SIZE 32
+#define WOLFSSL_SP
 
 
+
+/* GNRC support enabled if not
+ * using sockets
+ */
 #ifndef MODULE_WOLFSSL_SOCKET
 #   define WOLFSSL_GNRC
 #   define WOLFSSL_USER_IO
@@ -39,27 +44,43 @@ extern "C" {
 #   include <sys/socket.h>
 #endif
 
+/* Select wolfcrypt only / +wolfssl
+ * at compile time (via USEMODULE)
+ */
 #ifndef MODULE_WOLFSSL_TLS
 #   ifndef MODULE_WOLFSSL_TLS13
 #       define WOLFCRYPT_ONLY
 #   else
 #       define NO_OLD_TLS
+#       define HAVE_TLS_EXTENSIONS
+#       define HAVE_AES_DECRYPT
+#       define HAVE_AESGCM
+#       define GCM_SMALL
+#       define HAVE_AESCCM
+#       define WOLFSSL_AES_COUNTER
+#       define WOLFSSL_AES_DIRECT
 #   endif
+#else
+#   define HAVE_TLS_EXTENSIONS
 #endif
 
-#ifdef BOARD_NATIVE
-//#   define WOLFSSL_GENERAL_ALIGNMENT 8
-#else
+/* Align on 32-bit (exc. native,
+ * don't modify default alignment.)
+ */
+#ifndef BOARD_NATIVE
 #   define WOLFSSL_GENERAL_ALIGNMENT 4
-#   ifdef CPU_ARM
-#      define TFM_ARM
-#   endif
+#endif
+
+/* ARM-specific optimizations */
+#ifdef CPU_ARM
+#  define TFM_ARM
 #endif
 
 /* defined somewhere else */
-int strncasecmp(const char *s1, const char * s2, unsigned int sz);
+#ifndef __mips__
+    int strncasecmp(const char *s1, const char * s2, unsigned int sz);
+#endif
 
-/* Fixme: add mt */
 #define SINGLE_THREADED
 
 /* Global settings */
@@ -70,53 +91,45 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #define USE_CERT_BUFFERS_2048
 #define NO_RC4
 
-/* Random */
+/* Modules */
 #undef WC_NO_RNG
 #ifndef MODULE_WOLFCRYPT_RANDOM
 #   define WC_NO_RNG
 #endif
 
-
-/* DTLS */
 #undef WOLFSSL_DTLS
 #ifdef MODULE_WOLFSSL_DTLS
 #   define WOLFSSL_DTLS
 #endif
 
-/* FFDHE_2048 */
 #undef HAVE_FFDHE_2048
 #ifdef MODULE_WOLFCRYPT_FFDHE_2048
 #   define HAVE_FFDHE_2048
 #endif
 
-/* ChaCha20 */
 #undef HAVE_CHACHA
 #ifdef MODULE_WOLFCRYPT_CHACHA
 #   define HAVE_CHACHA
 #endif
 
-/* Poly 1305 */
 #undef HAVE_POLY1305
 #ifdef MODULE_WOLFCRYPT_POLY1305
 #   define HAVE_POLY1305
 #   define HAVE_ONE_TIME_AUTH
 #endif
 
-/* x25519 */
 #undef HAVE_CURVE25519
 #ifdef MODULE_WOLFCRYPT_CURVE25519
 #   define HAVE_CURVE25519
 #   define CURVE25519_SMALL
 #endif
 
-/* ed25519 */
 #undef HAVE_ED25519
 #ifdef MODULE_WOLFCRYPT_ED25519
 #   define HAVE_ED25519
 #   define ED25519_SMALL
 #endif
 
-/* AES */
 #undef NO_AES
 #undef NO_CODING
 #undef NO_CMAC
@@ -130,12 +143,10 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #	define NO_CODING
 #endif
 
-/* ASN */
 #ifndef MODULE_WOLFCRYPT_ASN
 #	define NO_ASN
 #endif
 
-/* HMAC */
 #ifndef MODULE_WOLFCRYPT_HMAC
 #	define NO_HMAC
 #endif
@@ -148,7 +159,6 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #   define USE_SLOW_SHA2
 #endif
 
-/* SHA-2 */
 #undef HAVE_SHA512
 #undef HAVE_SHA384
 #undef WOLFSSL_SHA384
@@ -161,19 +171,19 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #   define USE_SLOW_SHA512
 #endif
 
-/* SHA-3 Keccac */
 #undef WOLFSSL_SHA3
 #ifdef MODULE_WOLFCRYPT_SHA3
 #	define WOLFSSL_SHA3
 #endif
 
-/* ecc */
 #undef HAVE_ECC
 #ifdef MODULE_WOLFCRYPT_ECC
   #define HAVE_ECC
   #define FP_ECC
   #define WOLFSSL_HAVE_SP_ECC
+  #define WOLFSSL_HAVE_SP_ECC
   #define ECC_TIMING_RESISTANT
+  #define HAVE_SUPPORTED_CURVES
 #endif
 
 #undef HAVE_BLAKE2B
@@ -201,16 +211,19 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #	define HAVE_PKCS7
 #endif
 
-/* PKCS12 */
 #undef NO_PKCS12
 #ifndef MODULE_WOLFCRYPT_PKCS12
 #	define NO_PKCS12
 #endif
 
-/* PWDBASED */
 #undef NO_PWDBASED
 #ifndef MODULE_WOLFCRYPT_PWDBASED
 #	define NO_PWDBASED
+#endif
+
+#undef WOLFSSL_STATIC_PSK
+#ifdef MODULE_WOLFSSL_PSK
+#   define WOLFSSL_STATIC_PSK
 #endif
 
 #undef HAVE_LIBZ
@@ -218,76 +231,67 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #	define HAVE_LIBZ
 #endif
 
-/* rsa */
 #ifdef MODULE_WOLFCRYPT_RSA
 #   define HAVE_RSA
 #   define RSA_LOW_MEM
 #   define WC_RSA_BLINDING
+#   define WOLFSSL_STATIC_RSA
+#   define WOLFSSL_HAVE_SP_DH
+#   define WOLFSSL_HAVE_SP_RSA
 #else
 #	define NO_RSA
 #endif
 
-/* 3-DES */
 #undef NO_DES3
 #ifndef MODULE_WOLFCRYPT_DES3
 #	define NO_DES3
 #endif
 
-/* DH */
 #undef NO_DH
 #ifndef MODULE_WOLFCRYPT_DH
 #	define NO_DH
 #endif
 
-/* DSA */
 #undef NO_DSA
 #ifndef MODULE_WOLFCRYPT_DSA
 #	define NO_DSA
 #endif
 
-/* MD2 */
 #undef WOLFSSL_MD2
 #ifdef MODULE_WOLFSSL_MD2
   #define WOLFSSL_MD2
 #endif
 
-/* MD4 */
 #undef NO_MD4
 #ifndef MODULE_WOLFCRYPT_MD4
 #	define NO_MD4
 #endif
 
-/* RABBIT */
 #undef NO_RABBIT
 #ifndef MODULE_WOLFCRYPT_RABBIT
 #	define NO_RABBIT
 #endif
 
-/* MD5 */
 #undef NO_MD5
 #ifndef MODULE_WOLFCRYPT_MD5
 #	define NO_MD5
 #endif
 
-/* ripe-md */
 #undef WOLFSSL_RIPEMD
 #ifdef MODULE_WOLFCRYPT_RIPEMD
 #   define WOLFSSL_RIPEMD
 #endif
 
-/* signature wrapper */
 #undef NO_SIG_WRAPPER
 #ifndef MODULE_WOLFCRYPT_SIGNATURE
 #	define NO_SIG_WRAPPER
 #endif
 
-/* SRP */
 #undef HAVE_SRP
 #ifdef MODULE_WOLFCRYPT_SRP
   #define HAVE_SRP
 #endif
 
-/** TLS Options **/
 #undef HAVE_OCSP
 #ifdef MODULE_WOLFSSL_OCSP
   #define HAVE_OCSP
@@ -302,12 +306,17 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz);
 #ifdef MODULE_WOLFSSL_TLS13
     #define HAVE_TLS13
     #define WOLFSSL_TLS13
-    #define HAVE_TLS_EXTENSIONS
     #define BUILD_TLS_AES_128_GCM_SHA256
 #endif
 
 #ifdef __cplusplus
 }
 #endif
+
+/* The following defines should prevent declaration of name-colliding "Aes"
+ * structure on SAML21, SAME54 platform code
+ */
+#define _SAML21_AES_COMPONENT_
+#define _SAME54_AES_COMPONENT_
 
 #endif /* USER_SETTINGS_H */
