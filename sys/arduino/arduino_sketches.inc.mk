871674454103af66e7933b0e92abc91c5c8de822
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-08-12 15:22:18 +0200

    fixup! fixup! sys/arduino/sketches: force rebuild on sketch delete

--- sys/arduino/arduino_sketches.inc.mk
+++ sys/arduino/arduino_sketches.inc.mk
@@ -30,5 +30,5 @@ _ARDUINO_SKETCHES_MAKEFILE := $(lastword $(MAKEFILE_LIST))
 $(SKETCH_MODULE_DIR)/$(SKETCH_CPP): $(_ARDUINO_SKETCHES_MAKEFILE)
 $(SKETCH_MODULE_DIR)/Makefile: $(_ARDUINO_SKETCHES_MAKEFILE)
 
-# Rebuild cpp files everytime in case one of the `SKETCHES` is deleted
+# HACK Rebuild cpp files everytime in case one of the `SKETCHES` is deleted
 $(SKETCH_MODULE_DIR)/$(SKETCH_CPP): FORCE
