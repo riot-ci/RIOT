acc6c132af9b8e644fde3d0c8e872fc5d72c4e51
Antonio Galea <antonio.galea@gmail.com>
2019-08-29 12:29:43 +0200

    enable usbdev support for boards based on SAML21 CPU

--- boards/samr30-xpro/include/periph_conf.h
+++ boards/samr30-xpro/include/periph_conf.h
@@ -160,6 +160,19 @@ static const adc_conf_chan_t adc_channels[] = {
 #define ADC_NUMOF                               ARRAY_SIZE(adc_channels)
 /** @} */
 
+/**
+ * @name USB peripheral configuration
+ * @{
+ */
+static const sam0_common_usb_config_t sam_usbdev_config[] = {
+    {
+        .dm     = GPIO_PIN(PA, 24),
+        .dp     = GPIO_PIN(PA, 25),
+        .d_mux  = GPIO_MUX_G,
+        .device = &USB->DEVICE,
+    }
+};
+
 #ifdef __cplusplus
 }
 #endif
