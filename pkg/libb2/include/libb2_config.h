e6c8e9b95600706358361aac8cce1a2959e5fbe1
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-09-02 12:12:22 +0200

    fixup! pkg/libb2: define HAVE_ALIGNED_ACCESS_REQUIRED based on CPU

--- pkg/libb2/include/libb2_config.h
+++ pkg/libb2/include/libb2_config.h
@@ -14,7 +14,7 @@ extern "C" {
 #endif
 
 /* CPUs that don't support unaligned access, taken from GStreamer */
-#if defined(__alpha__) || defined(__arc__) || defined(__arm__) || defined(__aarch64__) || defined(__bfin) || defined(__hppa__) || defined(__nios2__) || defined(__MICROBLAZE__) || defined(__mips__) || defined(__or1k__) || defined(__sh__) || defined(__SH4__) || defined(__sparc__) || defined(__sparc) || defined(__ia64__) || defined(_M_ALPHA) || defined(_M_ARM) || defined(_M_ARM64) || defined(_M_IA64) || defined(__xtensa__) || defined(__e2k__) || defined(__riscv)
+#if defined(__alpha__) || defined(__arc__) || defined(__arm__) || defined(__aarch64__) || defined(__AVR_MEGA__) || defined(__AVR_ATtiny85__) || defined(__bfin) || defined(__hppa__) || defined(__nios2__) || defined(__MICROBLAZE__) || defined(__mips__) || defined(__or1k__) || defined(__sh__) || defined(__SH4__) || defined(__sparc__) || defined(__sparc) || defined(__ia64__) || defined(_M_ALPHA) || defined(_M_ARM) || defined(_M_ARM64) || defined(_M_IA64) || defined(__MSP430__) || defined(__xtensa__) || defined(__e2k__) || defined(__riscv)
 #define HAVE_ALIGNED_ACCESS_REQUIRED
 #endif
 
