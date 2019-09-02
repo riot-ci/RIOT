d6c0c2cbd3e24e45ee7d5e794bbcc89c9c2767ce
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-09-02 18:24:54 +0200

    fixup! tests/driver_bmp180: fix display with negative temperature

--- tests/driver_bmp180/main.c
+++ tests/driver_bmp180/main.c
@@ -18,6 +18,7 @@
  * @}
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
 
@@ -63,10 +64,6 @@ int main(void)
     while (1) {
         /* Get temperature in deci degrees celsius */
         int16_t temperature = bmp180_read_temperature(&dev);
-        bool negative = (temperature < 0);
-        if (negative) {
-            temperature = -temperature;
-        }
 
         /* Get pressure in Pa */
         uint32_t pressure = bmp180_read_pressure(&dev);
@@ -77,13 +74,12 @@ int main(void)
         /* Get altitude in meters */
         int16_t altitude = bmp180_altitude(&dev, pressure_0);
 
-        printf("Temperature [°C]: %c%d.%d\n"
+        printf("Temperature [°C]: %d.%d\n"
                "Pressure [hPa]: %lu.%d\n"
                "Pressure at see level [hPa]: %lu.%d\n"
               "Altitude [m]: %i\n"
                "\n+-------------------------------------+\n",
-               (negative ? '-' : ' '),
-               (int)(temperature / 10), (int)(temperature % 10),
+               (int)(temperature / 10), abs(temperature % 10),
                (unsigned long)pressure / 100, (int)(pressure % 100),
                (unsigned long)pressure_0 / 100, (int)(pressure_0 % 100),
                (int)altitude);
