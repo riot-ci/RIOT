277714beb0f3362f87a865c62eed5790859d165d
Daniele Lacamera <dan@danielinux.net>
2019-09-09 16:07:32 +0200

    wolfSSL: more build fixes

--- pkg/wolfssl/sock_tls/sock_tls.c
+++ pkg/wolfssl/sock_tls/sock_tls.c
@@ -87,6 +87,7 @@ void sock_dtls_session_destroy(sock_tls_t *sk)
 
 
 
+#ifndef __mips__
 #include <ctype.h>
 int strncasecmp(const char *s1, const char * s2, unsigned int sz)
 {
@@ -101,3 +102,4 @@ int strncasecmp(const char *s1, const char * s2, unsigned int sz)
     }
     return 0;
 }
+#endif
