fae8db8cce687129cc7442568682699e99c70d2b
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-09-09 17:00:59 +0200

    makefiles: use 'BOARDSDIR' for the boards directory in compilation
    Replace uses of 'RIOTBOARD' by 'BOARDSDIR' during the compilation.

--- makefiles/application.inc.mk
+++ makefiles/application.inc.mk
@@ -1,6 +1,6 @@
 MODULE = $(APPLICATION_MODULE)
 
-DIRS += $(RIOTCPU)/$(CPU) $(RIOTBOARD)/$(BOARD)
+DIRS += $(RIOTCPU)/$(CPU) $(BOARDSDIR)/$(BOARD)
 DIRS += $(RIOTBASE)/core $(RIOTBASE)/drivers $(RIOTBASE)/sys
 
 include $(RIOTBASE)/Makefile.base
