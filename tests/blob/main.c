7a23ce67f3338d1563603826f80a9fae28c2f1ee
Kaspar Schleiser <kaspar@schleiser.de>
2019-09-13 12:40:05 +0200

    squashme: create module using blobs, move binary test there

--- tests/blob/main.c
+++ tests/blob/main.c
@@ -21,7 +21,8 @@
 #include "fmt.h"
 #include "blob/blobtest.txt.h"
 #include "blob/blob_subdir/blobtest_subdir.txt.h"
-#include "blob/blobtest.bin.h"
+
+void blobtest_bin_print(void);
 
 int main(void)
 {
@@ -31,11 +32,7 @@ int main(void)
     print((char *)blobtest_subdir_txt, blobtest_subdir_txt_len);
     print("\n", 1);
 
-    for (size_t n = 0; n < blobtest_bin_len; n++) {
-        print("0x", 2);
-        print_byte_hex(blobtest_bin[n]);
-        print("\n", 1);
-    }
+    blobtest_bin_print();
 
     return 0;
 }
