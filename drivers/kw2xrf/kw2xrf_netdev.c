37bf9cdf6ff8583f47daba123815a8c0f884732f
Adem-Can Agdas <adem-can.agdas@haw-hamburg.de>
2019-09-17 15:01:56 +0200

    drivers/kw2xrf: Fixed IRQ handling order

--- drivers/kw2xrf/kw2xrf_netdev.c
+++ drivers/kw2xrf/kw2xrf_netdev.c
@@ -666,7 +666,6 @@ static void _isr_event_seq_tr(netdev_t *netdev, uint8_t *dregs)
         irqsts1 |= MKW2XDM_IRQSTS1_TXIRQ;
         if (dregs[MKW2XDM_PHY_CTRL1] & MKW2XDM_PHY_CTRL1_RXACKRQD) {
             DEBUG("[kw2xrf] wait for RX ACK\n");
-            //kw2xrf_seq_timeout_on(dev, _MACACKWAITDURATION);
             kw2xrf_timer3_seq_abort_on(dev);
             kw2xrf_abort_rx_ops_enable(dev, _MACACKWAITDURATION);
         }
@@ -696,25 +695,26 @@ static void _isr_event_seq_tr(netdev_t *netdev, uint8_t *dregs)
             }
         }
 
-        DEBUG("[kw2xrf] SEQIRQ\n");
-        irqsts1 |= MKW2XDM_IRQSTS1_SEQIRQ;
-        assert(dev->pending_tx != 0);
-        dev->pending_tx--;
-        netdev->event_callback(netdev, NETDEV_EVENT_TX_COMPLETE);
-        //kw2xrf_seq_timeout_off(dev);
-        kw2xrf_timer3_seq_abort_off(dev);
-        kw2xrf_abort_rx_ops_disable(dev);
-        kw2xrf_set_idle_sequence(dev);
-    }
-    else if (dregs[MKW2XDM_IRQSTS3] & MKW2XDM_IRQSTS3_TMR3IRQ) {
-        DEBUG("[kw2xrf] TC3TMOUT, no SEQIRQ, TX failed\n");
-        assert(dev->pending_tx != 0);
-        dev->pending_tx--;
-        netdev->event_callback(netdev, NETDEV_EVENT_TX_NOACK);
-        //kw2xrf_seq_timeout_off(dev);
-        kw2xrf_timer3_seq_abort_off(dev);
-        kw2xrf_abort_rx_ops_disable(dev);
-        kw2xrf_set_sequence(dev, dev->idle_state);
+        if (dregs[MKW2XDM_IRQSTS3] & MKW2XDM_IRQSTS3_TMR3IRQ) {
+            /* if the sequence was aborted by timer 3, ACK timed out */
+            DEBUG("[kw2xrf] TC3TMOUT, SEQIRQ, TX failed\n");
+            irqsts1 |= MKW2XDM_IRQSTS1_SEQIRQ;
+            assert(dev->pending_tx != 0);
+            dev->pending_tx--;
+            netdev->event_callback(netdev, NETDEV_EVENT_TX_NOACK);
+            kw2xrf_timer3_seq_abort_off(dev);
+            kw2xrf_abort_rx_ops_disable(dev);
+            kw2xrf_set_sequence(dev, dev->idle_state);
+        } else {
+            DEBUG("[kw2xrf] SEQIRQ\n");
+            irqsts1 |= MKW2XDM_IRQSTS1_SEQIRQ;
+            assert(dev->pending_tx != 0);
+            dev->pending_tx--;
+            netdev->event_callback(netdev, NETDEV_EVENT_TX_COMPLETE);
+            kw2xrf_timer3_seq_abort_off(dev);
+            kw2xrf_abort_rx_ops_disable(dev);
+            kw2xrf_set_idle_sequence(dev);
+        }
     }
 
     kw2xrf_write_dreg(dev, MKW2XDM_IRQSTS1, irqsts1);
