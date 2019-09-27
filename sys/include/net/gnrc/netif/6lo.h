679e39eb35d4e6abfc93bb77838aabf87da9aedd
Martine S. Lenders <m.lenders@fu-berlin.de>
2019-09-27 12:10:47 +0200

    gnrc_netif: 6lo.h: add flags to indicate SRF capability

--- sys/include/net/gnrc/netif/6lo.h
+++ sys/include/net/gnrc/netif/6lo.h
@@ -18,11 +18,25 @@
 #ifndef NET_GNRC_NETIF_6LO_H
 #define NET_GNRC_NETIF_6LO_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @name    Local 6LoWPAN capability flags
+ * @anchor  net_gnrc_netif_6lo_local_flags
+ * @see     gnrc_netif_6lo_t::local_flags
+ * @{
+ */
+/**
+ * @brief   Selective Fragment Recovery
+ * @see     [RFC 8931](https://tools.ietf.org/html/rfc8931)
+ */
+#define GNRC_NETIF_6LO_LOCAL_FLAGS_SFR  (0x01)
+/** @} */
 
 /**
  * @brief   6Lo component of @ref gnrc_netif_t
@@ -35,6 +49,15 @@ typedef struct {
      *          @ref net_gnrc_sixlowpan_frag "gnrc_sixlowpan_frag".
      */
     uint16_t max_frag_size;
+    /**
+     * @brief   6LoWPAN capability flags beyond the ones advertised in
+     *          [6LoWPAN Capability Indication Option
+     *          (6CIO)](https://tools.ietf.org/html/rfc7400#section-3.3)
+     *
+     * @see     [Local 6LoWPAN capability flags](@ref
+     *          net_gnrc_netif_6lo_local_flags)
+     */
+    uint8_t local_flags;
 } gnrc_netif_6lo_t;
 
 #ifdef __cplusplus
