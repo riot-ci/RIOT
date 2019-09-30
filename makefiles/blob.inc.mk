a4127a9380e451597bf26cc469d2b8bb1bd2e0c7
Kaspar Schleiser <kaspar@schleiser.de>
2019-09-30 13:41:18 +0200

    fixup! makefiles/blob.inc.mk: introduce blob -> hdr utility makefile

--- makefiles/blob.inc.mk
+++ makefiles/blob.inc.mk
@@ -1,7 +1,7 @@
 #
 # makes any file in BLOBS available via '#include "blob/<filename>.h"
 #
-# Usage:
+# # Usage:
 #
 # Add this to an application or module Makefile:
 #
@@ -10,6 +10,19 @@
 # Then include in C file or header:
 #
 #    #include "blob/foo.ext.h"
+#
+# The blob can then be accessed using the symbols "foo_ext" and "foo_ext_len".
+#
+# # Subfolders
+#
+# It is possible to add files from subfolders to BLOBS:
+#
+#     BLOBS += subfolder/my_file.foo
+#
+# The subfolder will be part of the generated header's path, but *not* of the
+# generated symbols. E.g., above blob would be made available by including
+# "blobs/subfolder/my_file.ext.h", which would define the symbols "my_file_ext"
+# and "my_file_ext_len". Beware possible symbol name clashes.
 #
 
 # use "blobs/blob" so the headers can be included as "blob/foo.h", but
