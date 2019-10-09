6d32aa7276a9b1e65792c7d956a87a6091c4e894
Francisco Molina <femolina@uc.cl>
2019-10-10 00:48:08 +0200

    sys/shell/commands: add interactive_sync commands

---
+++ sys/shell/commands/sc_interactive_sync.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_shell_commands
+ * @{
+ *
+ * @file
+ * @brief       Shell commands interactive sync util
+ *
+ * @author      Francisco Molina <francois-xavier.molina@inria.fr>
+ * @}
+ */
+
+#include <stdio.h>
+#include "periph/pm.h"
+#include "test_utils/interactive_sync.h"
+
+int _s(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    puts("START");
+
+    return 0;
+}
+
+
+int _r(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    puts("READY");
+    return 0;
+}
