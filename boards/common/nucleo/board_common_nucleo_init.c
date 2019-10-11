6a2ce1121e648640aa55f9a7765aec1aa85a3146
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-10-11 13:06:41 +0200

    fixup! boards/common/nucleo: Marked board_init as weak

--- boards/common/nucleo/board_common_nucleo_init.c
+++ boards/common/nucleo/board_common_nucleo_init.c
@@ -29,14 +29,10 @@
  */
 
 #include "board.h"
+#include "board_nucleo.h"
 #include "periph/gpio.h"
 
-/*
- * Allow overwriting board_init if common implementation doesn't work.
- * If at link time another implementation of board_init() not marked as weak
- * ((a.k.a. a strong symbol) is present, it will be linked in instead.
- */
-void __attribute__((weak)) board_init(void)
+void board_common_nucleo_init(void)
 {
     /* initialize the CPU */
     cpu_init();
@@ -52,4 +48,14 @@ void __attribute__((weak)) board_init(void)
 #ifdef LED2_PIN
     gpio_init(LED2_PIN, GPIO_OUT);
 #endif
+}
+
+/*
+ * Allow overwriting board_init if common implementation doesn't work.
+ * If at link time another implementation of board_init() not marked as weak
+ * ((a.k.a. a strong symbol) is present, it will be linked in instead.
+ */
+void __attribute__((weak)) board_init(void)
+{
+    board_common_nucleo_init();
 }
