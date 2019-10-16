e3e6966f681b36702867863c1f8cce820f0c4ec0
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-10-16 14:44:31 +0200

    fixup! test/gnrc: typo fix and cleanup

--- tests/nimble_autoconn_gnrc/main.c
+++ tests/nimble_autoconn_gnrc/main.c
@@ -38,6 +38,6 @@ int main(void)
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-    /* should be never reached */
+    /* should never be reached */
     return 0;
 }
