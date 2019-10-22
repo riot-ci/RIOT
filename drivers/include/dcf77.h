821475ee7e68acffd4a9beb136cf805af8246dd5
Michel Gerlach <michel.gerlach@haw-hamburg.de>
2019-10-22 17:42:44 +0200

    description, whitespaces

--- drivers/include/dcf77.h
+++ drivers/include/dcf77.h
@@ -60,31 +60,31 @@ typedef struct {
 
 typedef union {
     struct values {
-        uint64_t start : 1,             /**< Number of Bits for start value*/
-                 wheater : 14,          /**< Number of Bits for weather value */
-                 calling : 1,           /**< Number of Bits for calling value */
-                 mez_mesz_shift : 1,    /**< Number of Bits for shift value */
-                 mesz : 2,              /**< Number of Bits for mesz value */
-                 shift_sec : 1,         /**< Number of Bits for leap-second value */
-                 start_time : 1,        /**< Number of Bits for start_Bit value */
-                 minute_l : 4,          /**< Number of Bits for lower minute value */
-                 minute_h : 3,          /**< Number of Bits for higher minute value */
-                 minute_par : 1,        /**< Number of Bits for minuteparity value */
-                 hour_l : 4,            /**< Number of Bits for lower hour value */
-                 hour_h : 2,            /**< Number of Bits for higher hour value */
-                 hour_par : 1,          /**< Number of Bits for hourparity value */
-                 day_l : 4,             /**< Number of Bits for lower calenderday value */
-                 day_h : 2,             /**< Number of Bits for higher calenderday value */
-                 wday : 3,              /**< Number of Bits for weekday value */
-                 month_l : 4,           /**< Number of Bits for lower month value */
-                 month_h : 1,           /**< Number of Bits for higher month value */
-                 year_l : 4,            /**< Number of Bits for lower year value */
-                 year_h : 4,            /**< Number of Bits for higher year value */
-                 date_par : 1,          /**< Number of Bits for dateparity value */
-                 buff : 5;              /**< Number of Bits for experimental buffer value */
-    } val;                              /**< struct with Bitfields of timeinformation*/
-    uint64_t bits;                      /**< Value of Bits in a received cycle */
-} dcf77_bits_t;                       
+        uint64_t start : 1,    /**< Number of Bits for start value*/
+        wheater : 14,          /**< Number of Bits for weather value */
+        calling : 1,           /**< Number of Bits for calling value */
+        mez_mesz_shift : 1,    /**< Number of Bits for shift value */
+        mesz : 2,              /**< Number of Bits for mesz value */
+        shift_sec : 1,         /**< Number of Bits for leap-second value */
+        start_time : 1,        /**< Number of Bits for start_Bit value */
+        minute_l : 4,          /**< Number of Bits for lower minute value */
+        minute_h : 3,          /**< Number of Bits for higher minute value */
+        minute_par : 1,        /**< Number of Bits for minuteparity value */
+        hour_l : 4,            /**< Number of Bits for lower hour value */
+        hour_h : 2,            /**< Number of Bits for higher hour value */
+        hour_par : 1,          /**< Number of Bits for hourparity value */
+        day_l : 4,             /**< Number of Bits for lower calenderday value */
+        day_h : 2,             /**< Number of Bits for higher calenderday value */
+        wday : 3,              /**< Number of Bits for weekday value */
+        month_l : 4,           /**< Number of Bits for lower month value */
+        month_h : 1,           /**< Number of Bits for higher month value */
+        year_l : 4,            /**< Number of Bits for lower year value */
+        year_h : 4,            /**< Number of Bits for higher year value */
+        date_par : 1,          /**< Number of Bits for dateparity value */
+        buff : 5;              /**< Number of Bits for experimental buffer value */
+    } val;                     /**< struct with Bitfields of timeinformation*/
+    uint64_t bits;             /**< Value of Bits in a received cycle */
+} dcf77_bits_t;                /**< Union which contains the Bitfields struct */
 
 /**
  * @brief   Device descriptor for DCF77 sensor devices
