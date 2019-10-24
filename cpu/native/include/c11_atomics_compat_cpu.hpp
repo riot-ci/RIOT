e29d9900935b1f19c2b11af2ec7fb17941213565
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-10-24 10:43:44 +0200

    fixup! cpu/native: Added C11 atomics compat

--- cpu/native/include/c11_atomics_compat_cpu.hpp
+++ cpu/native/include/c11_atomics_compat_cpu.hpp
@@ -17,14 +17,14 @@
 #define ATOMIC_INT_SAME_SIZED_TYPE                      uint32_t
 #define ATOMIC_UINT_SIZE                                (4U)
 #define ATOMIC_UINT_SAME_SIZED_TYPE                     uint32_t
-#define ATOMIC_LONG_SIZE                                (4U)
-#define ATOMIC_LONG_SAME_SIZED_TYPE                     uint32_t
-#define ATOMIC_ULONG_SIZE                               (4U)
-#define ATOMIC_ULONG_SAME_SIZED_TYPE                    uint32_t
-#define ATOMIC_LLONG_SIZE                               (Failed to determine size of atomic_llongU)
-#define ATOMIC_LLONG_SAME_SIZED_TYPE                    Failed to determine same sized type for atomic_llong
-#define ATOMIC_ULLONG_SIZE                              (Failed to determine size of atomic_ullongU)
-#define ATOMIC_ULLONG_SAME_SIZED_TYPE                   Failed to determine same sized type for atomic_ullong
+#define ATOMIC_LONG_SIZE                                (8U)
+#define ATOMIC_LONG_SAME_SIZED_TYPE                     uint64_t
+#define ATOMIC_ULONG_SIZE                               (8U)
+#define ATOMIC_ULONG_SAME_SIZED_TYPE                    uint64_t
+#define ATOMIC_LLONG_SIZE                               (8U)
+#define ATOMIC_LLONG_SAME_SIZED_TYPE                    uint64_t
+#define ATOMIC_ULLONG_SIZE                              (8U)
+#define ATOMIC_ULLONG_SAME_SIZED_TYPE                   uint64_t
 #define ATOMIC_INT_LEAST8_T_SIZE                        (1U)
 #define ATOMIC_INT_LEAST8_T_SAME_SIZED_TYPE             uint8_t
 #define ATOMIC_UINT_LEAST8_T_SIZE                       (1U)
@@ -37,35 +37,35 @@
 #define ATOMIC_INT_LEAST32_T_SAME_SIZED_TYPE            uint32_t
 #define ATOMIC_UINT_LEAST32_T_SIZE                      (4U)
 #define ATOMIC_UINT_LEAST32_T_SAME_SIZED_TYPE           uint32_t
-#define ATOMIC_INT_LEAST64_T_SIZE                       (Failed to determine size of atomic_int_least64_tU)
-#define ATOMIC_INT_LEAST64_T_SAME_SIZED_TYPE            Failed to determine same sized type for atomic_int_least64_t
-#define ATOMIC_UINT_LEAST64_T_SIZE                      (Failed to determine size of atomic_uint_least64_tU)
-#define ATOMIC_UINT_LEAST64_T_SAME_SIZED_TYPE           Failed to determine same sized type for atomic_uint_least64_t
+#define ATOMIC_INT_LEAST64_T_SIZE                       (8U)
+#define ATOMIC_INT_LEAST64_T_SAME_SIZED_TYPE            uint64_t
+#define ATOMIC_UINT_LEAST64_T_SIZE                      (8U)
+#define ATOMIC_UINT_LEAST64_T_SAME_SIZED_TYPE           uint64_t
 #define ATOMIC_INT_FAST8_T_SIZE                         (1U)
 #define ATOMIC_INT_FAST8_T_SAME_SIZED_TYPE              uint8_t
 #define ATOMIC_UINT_FAST8_T_SIZE                        (1U)
 #define ATOMIC_UINT_FAST8_T_SAME_SIZED_TYPE             uint8_t
-#define ATOMIC_INT_FAST16_T_SIZE                        (4U)
-#define ATOMIC_INT_FAST16_T_SAME_SIZED_TYPE             uint32_t
-#define ATOMIC_UINT_FAST16_T_SIZE                       (4U)
-#define ATOMIC_UINT_FAST16_T_SAME_SIZED_TYPE            uint32_t
-#define ATOMIC_INT_FAST32_T_SIZE                        (4U)
-#define ATOMIC_INT_FAST32_T_SAME_SIZED_TYPE             uint32_t
-#define ATOMIC_UINT_FAST32_T_SIZE                       (4U)
-#define ATOMIC_UINT_FAST32_T_SAME_SIZED_TYPE            uint32_t
-#define ATOMIC_INT_FAST64_T_SIZE                        (Failed to determine size of atomic_int_fast64_tU)
-#define ATOMIC_INT_FAST64_T_SAME_SIZED_TYPE             Failed to determine same sized type for atomic_int_fast64_t
-#define ATOMIC_UINT_FAST64_T_SIZE                       (Failed to determine size of atomic_uint_fast64_tU)
-#define ATOMIC_UINT_FAST64_T_SAME_SIZED_TYPE            Failed to determine same sized type for atomic_uint_fast64_t
-#define ATOMIC_INTPTR_T_SIZE                            (4U)
-#define ATOMIC_INTPTR_T_SAME_SIZED_TYPE                 uint32_t
-#define ATOMIC_UINTPTR_T_SIZE                           (4U)
-#define ATOMIC_UINTPTR_T_SAME_SIZED_TYPE                uint32_t
-#define ATOMIC_SIZE_T_SIZE                              (4U)
-#define ATOMIC_SIZE_T_SAME_SIZED_TYPE                   uint32_t
-#define ATOMIC_PTRDIFF_T_SIZE                           (4U)
-#define ATOMIC_PTRDIFF_T_SAME_SIZED_TYPE                uint32_t
-#define ATOMIC_INTMAX_T_SIZE                            (Failed to determine size of atomic_intmax_tU)
-#define ATOMIC_INTMAX_T_SAME_SIZED_TYPE                 Failed to determine same sized type for atomic_intmax_t
-#define ATOMIC_UINTMAX_T_SIZE                           (Failed to determine size of atomic_uintmax_tU)
-#define ATOMIC_UINTMAX_T_SAME_SIZED_TYPE                Failed to determine same sized type for atomic_uintmax_t
+#define ATOMIC_INT_FAST16_T_SIZE                        (8U)
+#define ATOMIC_INT_FAST16_T_SAME_SIZED_TYPE             uint64_t
+#define ATOMIC_UINT_FAST16_T_SIZE                       (8U)
+#define ATOMIC_UINT_FAST16_T_SAME_SIZED_TYPE            uint64_t
+#define ATOMIC_INT_FAST32_T_SIZE                        (8U)
+#define ATOMIC_INT_FAST32_T_SAME_SIZED_TYPE             uint64_t
+#define ATOMIC_UINT_FAST32_T_SIZE                       (8U)
+#define ATOMIC_UINT_FAST32_T_SAME_SIZED_TYPE            uint64_t
+#define ATOMIC_INT_FAST64_T_SIZE                        (8U)
+#define ATOMIC_INT_FAST64_T_SAME_SIZED_TYPE             uint64_t
+#define ATOMIC_UINT_FAST64_T_SIZE                       (8U)
+#define ATOMIC_UINT_FAST64_T_SAME_SIZED_TYPE            uint64_t
+#define ATOMIC_INTPTR_T_SIZE                            (8U)
+#define ATOMIC_INTPTR_T_SAME_SIZED_TYPE                 uint64_t
+#define ATOMIC_UINTPTR_T_SIZE                           (8U)
+#define ATOMIC_UINTPTR_T_SAME_SIZED_TYPE                uint64_t
+#define ATOMIC_SIZE_T_SIZE                              (8U)
+#define ATOMIC_SIZE_T_SAME_SIZED_TYPE                   uint64_t
+#define ATOMIC_PTRDIFF_T_SIZE                           (8U)
+#define ATOMIC_PTRDIFF_T_SAME_SIZED_TYPE                uint64_t
+#define ATOMIC_INTMAX_T_SIZE                            (8U)
+#define ATOMIC_INTMAX_T_SAME_SIZED_TYPE                 uint64_t
+#define ATOMIC_UINTMAX_T_SIZE                           (8U)
+#define ATOMIC_UINTMAX_T_SAME_SIZED_TYPE                uint64_t
