94eae1fb8ac5f4e91b50f703b350888225211dd8
Benjamin Valentin <benpicco@beuth-hochschule.de>
2019-11-04 16:16:07 +0100

    periph_common/rtc: add rtc_tm_compare()
    Add an easy way to compare two points in time.

--- drivers/include/periph/rtc.h
+++ drivers/include/periph/rtc.h
@@ -131,6 +131,18 @@ void rtc_poweroff(void);
  */
 void rtc_tm_normalize(struct tm *time);
 
+/**
+ * @brief Compare two time structs.
+ *
+ * @param[in] a       The first time struct.
+ * @param[in] b       The second time struct.
+ *
+ * @return an integer < 0 if a is earlier than b
+ * @return an integer > 0 if a is later than b
+ * @return              0 if a and b are equal
+ */
+int rtc_tm_compare(const struct tm *a, const struct tm *b);
+
 #ifdef __cplusplus
 }
 #endif
