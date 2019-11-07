44c4f12d4d851af4a2e2ddfb838d45117dbf3ce0
Robert Olsson <robert@herjulf.se>
2019-11-07 21:15:10 +0100

    Adding: boards/avr-rss2 and AtMega256RFR2 based board         See doc.txt for details.

---
+++ boards/avr-rss2/board.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Robert Olsson <roolss@kth.se>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_rss2-mega256rfr2
+ * @{
+ *
+ * @file
+ * @brief       Board specific LED initialization
+ * @author      Robert Olsson <roolss@kth.se>
+ *
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "cpu.h"
+
+#include "net/eui64.h"
+#include <string.h>
+#include <inttypes.h>
+
+void led_init(void)
+{
+    /* Ports Pins as Output */
+    LED_PORT_DDR |= LED1_MASK | LED0_MASK;
+    /* All LEDs OFF */
+    LED_PORT |= (LED1_MASK | LED0_MASK);
+}
