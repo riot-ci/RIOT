231cb1004217fcabc5fbf5d8c33949114b24d083
Jue <me@jue.yt>
2019-11-12 21:25:30 +0100

    fixup! cpu/stm32_common: add RAMMOD for STM32F1 family

--- cpu/stm32f1/stm32_line.mk
+++ cpu/stm32f1/stm32_line.mk
@@ -1,5 +1,5 @@
 # Compute CPU_LINE
-LINE := $(shell echo $(CPU_MODEL) | tr 'a-z-' 'A-Z_' | sed -E -e 's/^STM32F([0-9][0-9][0-9])(.)(.)(_A)?/\1 \2 \3/')
+LINE := $(shell echo $(CPU_MODEL) | tr 'a-z-' 'A-Z_' | sed -E -e 's/^STM32F([0-9][0-9][0-9])(.)(.)/\1 \2 \3/')
 TYPE := $(word 1, $(LINE))
 MODEL1 := $(word 2, $(LINE))
 MODEL2 := $(word 3, $(LINE))
