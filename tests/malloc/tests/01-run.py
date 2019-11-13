d0ded37e1ec395956cd1dce33f5e7cff04f9ca38
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-11-13 19:52:58 +0100

    fixup! tests/malloc: add Python script for automatic test

--- tests/malloc/tests/01-run.py
+++ tests/malloc/tests/01-run.py
@@ -11,19 +11,16 @@ from testrunner import run
 
 
 def testfunc(child):
-    child.expect_exact('Test application for malloc')
     child.expect(r'CHUNK_SIZE: (\d+)')
     chunk_size = int(child.match.group(1))
     child.expect(r'NUMBER_OF_TESTS: (\d+)')
     number_of_tests = int(child.match.group(1))
-    child.expect(r'MAX_NUMBER_BLOCKS: (\d+)')
-    max_number_blocks = int(child.match.group(1))
-    for i in range(number_of_tests):
-        child.expect_exact("TEST #{}".format(i + 1))
-        for _ in range(max_number_blocks):
-            child.expect(r"Allocated {} Bytes at 0x[a-z0-9]+, total [a-z0-9]+"
-                         .format(chunk_size))
-        for _ in range(max_number_blocks):
+    for _ in range(number_of_tests):
+        child.expect(r"Allocated {} Bytes at 0x[a-z0-9]+, total [a-z0-9]+"
+                     .format(chunk_size))
+        child.expect(r'Allocations count: (\d+)')
+        allocations = int(child.match.group(1))
+        for _ in range(allocations):
             child.expect(r"Free {} Bytes at 0x[a-z0-9]+, total [a-z0-9]+"
                          .format(chunk_size))
     child.expect_exact("[SUCCESS]")
