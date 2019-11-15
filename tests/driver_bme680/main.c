529aaf25aa0bdf9774b0793b5e9a9072c91061e4
Dylan Laduranty <dylan.laduranty@mesotic.com>
2019-11-15 16:57:16 +0100

    tests: add test application for BME680 driver

---
+++ tests/driver_bme680/main.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2018 Mesotic SAS
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Test application for the bme680_driver package.
+ *
+ * @author      Dylan Laduranty <dylan.laduranty@mesotic.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include "board.h"
+#include "bme680.h"
+#include "bme680_params.h"
+#include "bme680_defs.h"
+#include "bme680_hal.h"
+
+#ifdef MODULE_PERIPH_SPI
+#include "periph/spi.h"
+#endif
+
+#ifdef MODULE_PERIPH_I2C
+#include "periph/i2c.h"
+#endif
+
+#include "xtimer.h"
+
+#define BME680_DEV dev.dev
+
+
+int main(void)
+{
+    bme680_t dev;
+
+    /* You may assign a chip select identifier to be handled later */
+    dev.dev.dev_id = 0;
+    xtimer_sleep(5);
+    /* amb_temp can be set to 25 prior to configuring the gas sensor 
+     * or by performing a few temperature readings without operating the gas sensor.
+     */
+    BME680_DEV.amb_temp = 25;
+    BME680_DEV.delay_ms = ms_sleep;
+
+    printf("Intialize BME680 sensor...");
+    if(bme680_init(&dev, &bme680_params[0]) != BME680_OK) {
+        puts("failed");
+    }
+    else {
+        puts("OK");
+    }
+
+    struct bme680_field_data data;
+    uint8_t ret;
+    while(1)
+    {
+        xtimer_sleep(5);
+
+        ret = bme680_get_sensor_data(&data, &(dev.dev));
+        if(!ret) {
+#ifndef BME680_FLOAT_POINT_COMPENSATION
+        printf("[bme680]: T %d degC, P %ld hPa, H %ld ", data.temperature,
+            data.pressure, data.humidity);
+        /* Avoid using measurements from an unstable heating setup */
+        if(data.status & BME680_GASM_VALID_MSK)
+            printf(", G %ld ohms", data.gas_resistance);
+#else
+        printf("[bme680]: T %.2f degC, P %.2f hPa, H %2.f ", data.temperature,
+            data.pressure, data.humidity);
+        /* Avoid using measurements from an unstable heating setup */
+        if(data.status & BME680_GASM_VALID_MSK)
+            printf(", G %f ohms", data.gas_resistance);
+#endif
+        printf("\r\n");
+        }
+        else {
+            puts("[bme680]: measurement failed");
+        }
+        /* Trigger the next measurement if you would like to read data out
+           continuously */
+        if (BME680_DEV.power_mode == BME680_FORCED_MODE) {
+            bme680_set_sensor_mode(&dev.dev);
+        }
+    }
+    /* Should never reach here */
+    return 0;
+}
