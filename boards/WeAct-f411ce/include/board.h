8eef9253c21e957b1dc8c87bc41cc4b18970f2bc
Benjamin Valentin <benpicco@googlemail.com>
2019-11-17 01:55:29 +0100

    boards: add WeAct-f411ce board
    The WeAct-F411CE is a blackpill-like board with a STM32F411CE.
    It can be flashed using the vendor-provided bootloader.

---
+++ boards/WeAct-f411ce/include/board.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 Benjamin Valentin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_weact-f411ce
+ *
+ * @brief       Support for the WeAct-F411CE Board
+ * @{
+ *
+ * @file
+ * @brief       Pin definitions and board configuration options
+ *
+ * @author      Benjamin Valentin <benpicco@googlemail.com>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "periph_cpu.h"
+
+/**
+ * @name    Xtimer configuration
+ * @{
+ */
+#define XTIMER_BACKOFF              (8)
+#define XTIMER_OVERHEAD             (6)
+/** @} */
+
+/**
+ * @brief Product & Vendor ID taken from example firmware
+ *        that the board was shipped with.
+ */
+#define RIOT_INTERNALPERIPHERAL_VID (0x0483)
+#define RIOT_INTERNALPERIPHERAL_PID (0x5740)
+/** @} */
+
+/**
+ * @name    LED pin definition and handlers
+ * @{
+ */
+#define LED0_PORT           GPIOC
+#define LED0_PIN            GPIO_PIN(PORT_C, 13)
+#define LED0_MASK           (1 << 13)
+
+#define LED0_ON             (LED0_PORT->BSRR = (LED0_MASK << 16))
+#define LED0_OFF            (LED0_PORT->BSRR = (LED0_MASK <<  0))
+#define LED0_TOGGLE         (LED0_PORT->ODR  ^= LED0_MASK)
+/** @} */
+
+/**
+ * @name    User button pin definition
+ * @{
+ */
+#define BTN0_PIN            GPIO_PIN(PORT_A, 0)
+#define BTN0_MODE           GPIO_IN_PU
+/** @} */
+
+/**
+ * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
