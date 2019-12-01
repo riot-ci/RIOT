28dabde9e9976a3cefb8547a9eab73bcdf42ce60
fabian18 <fabian.huessler@ovgu.de>
2019-12-01 21:33:14 +0100

    drivers/nrf24l01p: Rewrite for netif and gnrc

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -64,6 +64,7 @@ PSEUDOMODULES += nimble_autoconn_%
 PSEUDOMODULES += newlib
 PSEUDOMODULES += newlib_gnu_source
 PSEUDOMODULES += newlib_nano
+PSEUDOMODULES += nrf24l01p_advanced
 PSEUDOMODULES += openthread
 PSEUDOMODULES += pktqueue
 PSEUDOMODULES += posix_headers
