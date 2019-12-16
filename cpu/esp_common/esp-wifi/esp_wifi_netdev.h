bf29f7370b994ba0e27c9a6d53aec17320571b1e
Gunar Schorcht <gunar@schorcht.net>
2019-12-16 01:47:44 +0100

    cpu/esp8266: move esp_wifi to cpu/esp_common

--- cpu/esp_common/esp-wifi/esp_wifi_netdev.h
+++ cpu/esp_common/esp-wifi/esp_wifi_netdev.h
@@ -7,11 +7,11 @@
  */
 
 /**
- * @ingroup     cpu_esp8266_esp_wifi
+ * @ingroup     cpu_esp_common_esp_wifi
  * @{
  *
  * @file
- * @brief       Network device driver for the ESP32 WiFi interface
+ * @brief       Network device driver for the ESP SoCs WiFi interface
  *
  * @author      Gunar Schorcht <gunar@schorcht.net>
  */
@@ -56,8 +56,6 @@ typedef struct
     uint8_t event_disc;                /**< number of pending disc events */
 
     bool connected;                    /**< indicates whether connected to AP */
-
-    gnrc_netif_t* netif;               /**< reference to the corresponding netif */
 
     mutex_t dev_lock;                  /**< device is already in use */
 
