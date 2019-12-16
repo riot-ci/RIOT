6ec66655bad2f0edbc39a3f80a5e972d9c21daa9
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-12-16 16:09:33 +0100

    cpu/sam0: use GCLK ID instead of bitmask
    To simplify board definitions and for unification between samd2x and
    newer models, don't use the GCLK bitmask in board definitions.
    Instead use the GCLK index and generate the bitmask when needed.

--- boards/samr30-xpro/include/periph_conf.h
+++ boards/samr30-xpro/include/periph_conf.h
@@ -41,7 +41,7 @@ static const tc32_conf_t timer_config[] = {
         .mclk           = &MCLK->APBCMASK.reg,
         .mclk_mask      = MCLK_APBCMASK_TC0 | MCLK_APBCMASK_TC1,
         .gclk_id        = TC0_GCLK_ID,
-        .gclk_src       = GCLK_PCHCTRL_GEN(0),
+        .gclk_src       = 0,
         .prescaler      = TC_CTRLA_PRESCALER(4),
         .flags          = TC_CTRLA_MODE_COUNT32,
     }
@@ -70,7 +70,7 @@ static const uart_conf_t uart_config[] = {
         .rx_pad   = UART_PAD_RX_1,
         .tx_pad   = UART_PAD_TX_0,
         .flags    = UART_FLAG_NONE,
-        .gclk_src = GCLK_PCHCTRL_GEN_GCLK0
+        .gclk_src = 0
     }
 };
 
@@ -112,7 +112,7 @@ static const i2c_conf_t i2c_config[] = {
         .scl_pin  = GPIO_PIN(PA, 17),
         .sda_pin  = GPIO_PIN(PA, 16),
         .mux      = GPIO_MUX_C,
-        .gclk_src = GCLK_PCHCTRL_GEN_GCLK0,
+        .gclk_src = 0,
         .flags    = I2C_FLAG_NONE
      }
 };
