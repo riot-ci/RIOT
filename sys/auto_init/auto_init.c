96e0ccf8fc1e61e302caca7e6a1d4d9cee9e7472
Kaspar Schleiser <kaspar@schleiser.de>
2019-12-18 16:43:54 +0100

    fixup! sys/ztimer: introduce xtimer wrapper

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -111,7 +111,7 @@ void auto_init(void)
     void auto_init_random(void);
     auto_init_random();
 #endif
-#ifdef MODULE_ZTIMER
+#ifdef MODULE_ZTIMER_AUTO_INIT
     DEBUG("Auto init ztimer module.\n");
     void ztimer_init(void);
     ztimer_init();
