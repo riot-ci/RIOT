0f1599254adbfdd67da77e267b8a63067c0717be
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-12-20 12:17:26 +0100

    fixup! vera++: add check.sh script

--- dist/tools/vera++/check.sh
+++ dist/tools/vera++/check.sh
@@ -28,7 +28,9 @@ fi
 VERA_CMD="vera++ --root $CURDIR --exclusions $CURDIR/exclude $_QUIET"
 
 if [ $WARNING -ne 0 ]; then
-    echo "$FILES" | $VERA_CMD --profile riot | sed 's/^/warning: /g'
+    echo "$FILES" | $VERA_CMD --profile riot \
+        --parameters $CURDIR/profiles/riot_params.txt | sed 's/^/warning: /g'
 fi
 
-echo "$FILES" | $VERA_CMD --profile riot_force --error
+echo "$FILES" | $VERA_CMD --profile riot_force \
+    --parameters $CURDIR/profiles/riot_force_params.txt --error
