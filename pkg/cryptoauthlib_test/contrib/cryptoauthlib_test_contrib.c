da2fc415e69699102153e68b152d6e2c54116388
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2019-12-23 09:34:01 +0100

    delete folder for cryptoauthlib test package

--- pkg/cryptoauthlib_test/contrib/cryptoauthlib_test_contrib.c
+++
@@ -1,9 +0,0 @@
-#include "cryptoauthlib.h"
-#include "atca.h"
-#include "atca_params.h"
-
-/* Function switches the default cfg in cryptoauthlib test to Riot cfg */
-void riot_switch_cfg(ATCAIfaceCfg *cfg)
-{
-    *cfg = atca_params[0];
-}
