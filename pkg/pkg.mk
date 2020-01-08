e29370d7a417227d016ea5c73679ead9d3f36145
Bas Stottelaar <basstottelaar@gmail.com>
2020-01-08 20:16:46 +0100

    squash! pkg/pkg.mk: fix add/remove patches.

--- pkg/pkg.mk
+++ pkg/pkg.mk
@@ -64,6 +64,11 @@ $(PKG_PREPARED): $(PKG_PATCHED)
 # directory is not a git repository for any reason (clean -xdff or others)
 GIT_IN_PKG = git -C $(PKG_BUILDDIR) --git-dir=.git --work-tree=.
 
+# When $(PKG_PATCHED).d is included $(PKG_PATCHED) pre-requisites will include
+# the old pre-requisites forcing a rebuild on pre-requisite removal, but we do
+# not want to generate $(PKG_PATCHED).d with the old pre-requisites
+PKG_PATCHED_PRE_REQUISITES = $(PKG_PATCHES) $(PKG_DOWNLOADED) $(MAKEFILE_LIST)
+
 # Generate dependency file. Force rebuilding on dependency deletion
 # Warning: It will be evaluated before target execution, so use as first step
 #   $1: output file name
@@ -74,18 +79,21 @@ gen_dependency_files = $(file >$1,$@: $2)$(foreach f,$2,$(file >>$1,$(f):))
 # * clean, without removing the 'state' files
 # * checkout the wanted base commit
 # * apply patches if there are any. (If none, it does nothing)
-$(PKG_PATCHED): $(PKG_PATCHES) $(PKG_DOWNLOADED) $(MAKEFILE_LIST)
-	$(call gen_dependency_files,$@.d,$^)
+$(PKG_PATCHED): $(PKG_PATCHED_PRE_REQUISITES)
+	$(info [INFO] patch $(PKG_NAME))
+	$(call gen_dependency_files,$@.d,$(PKG_PATCHED_PRE_REQUISITES))
 	$(Q)$(GIT_IN_PKG) clean $(GIT_QUIET) -xdff '**' $(PKG_STATE:$(PKG_BUILDDIR)/%=':!%*')
 	$(Q)$(GIT_IN_PKG) checkout $(GIT_QUIET) -f $(PKG_VERSION)
 	$(Q)$(GIT_IN_PKG) $(GITFLAGS) am $(GITAMFLAGS) $(PKG_PATCHES) </dev/null
 	@touch $@
 
 $(PKG_DOWNLOADED): $(MAKEFILE_LIST) | $(PKG_BUILDDIR)/.git
+	$(info [INFO] updating $(PKG_NAME) $(PKG_DOWNLOADED))
 	$(Q)$(GIT_IN_PKG) fetch $(GIT_QUIET) $(PKG_URL) $(PKG_VERSION)
 	echo $(PKG_VERSION) > $@
 
 $(PKG_BUILDDIR)/.git:
+	$(info [INFO] cloning $(PKG_NAME))
 	$(Q)rm -Rf $(PKG_BUILDDIR)
 	$(Q)mkdir -p $(PKG_BUILDDIR)
 	$(Q)$(GITCACHE) clone $(PKG_URL) $(PKG_VERSION) $(PKG_BUILDDIR)
