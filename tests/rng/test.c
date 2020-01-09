13d77b728a8672640cad6b09ec1eabb2897b5f76
Hyungsin <hyungkim@google.com>
2020-01-08 16:18:07 -0800

    squash: make murdock happy 2

--- tests/rng/test.c
+++ tests/rng/test.c
@@ -413,6 +413,7 @@ void test_speed(uint32_t duration)
     unsigned running = 1;
     xtimer_t xt = {
         .start_time = 0,
+        .long_start_time = 0,
         .offset = 0,
         .long_offset = 0,
         .callback = cb_speed_timeout,
@@ -446,6 +447,7 @@ void test_speed_range(uint32_t duration, uint32_t low_thresh, uint32_t high_thre
     unsigned running = 1;
     xtimer_t xt = {
         .start_time = 0,
+        .long_start_time = 0,
         .offset = 0,
         .long_offset = 0,
         .callback = cb_speed_timeout,
