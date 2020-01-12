bd2c61d869ba0572b9e48ea2ef3f7096e33bc6ae
Bas Stottelaar <basstottelaar@gmail.com>
2020-01-12 21:50:50 +0100

    boards/ikea-tradfri: expose SPI NOR Flash.

--- boards/ikea-tradfri/board.c
+++ boards/ikea-tradfri/board.c
@@ -21,6 +21,25 @@
 #include "board.h"
 #include "cpu.h"
 
+#include "mtd_spi_nor.h"
+
+static mtd_spi_nor_t board_nor_dev = {
+    .base = {
+        .driver = &mtd_spi_nor_driver,
+        .page_size = 256,
+        .pages_per_sector = 16,
+        .sector_count = 64,
+    },
+    .opcode = &mtd_spi_nor_opcode_default,
+    .spi = TRADFRI_SPI_FLASH_DEV,
+    .cs = TRADFRI_SPI_FLASH_CS,
+    .addr_width = 3,
+    .mode = SPI_MODE_0,
+    .clk = SPI_CLK_5MHZ,
+};
+
+mtd_dev_t *mtd0 = (mtd_dev_t *)&board_nor_dev;
+
 void board_init(void)
 {
     /* initialize the CPU */
