a3bbdd87390c04adcea6f1da06a0c5fa5050b832
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-01-14 19:08:47 +0100

    fixup! tests/pkg_tensorflow-lite: add mnist_mlp complete example

--- tests/pkg_tensorflow-lite/tests/01-run.py
+++ tests/pkg_tensorflow-lite/tests/01-run.py
@@ -5,6 +5,8 @@ from testrunner import run
 
 
 def testfunc(child):
+    # The default image of the test application contains a 7 (e.g. it's the
+    # first image in the MNIST test dataset)
     child.expect_exact("Digit prediction: 7")
 
 
