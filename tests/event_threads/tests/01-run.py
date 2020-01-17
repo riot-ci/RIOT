ba7bb07c835c9f46b461077dc549b5c749799c7b
Kaspar Schleiser <kaspar@schleiser.de>
2020-01-17 10:29:20 +0100

    fixup! tests/event_threads: initial event threads test application

--- tests/event_threads/tests/01-run.py
+++ tests/event_threads/tests/01-run.py
@@ -6,6 +6,7 @@ from testrunner import run
 
 def testfunc(child):
     child.expect_exact('high\r\n')
+    child.expect_exact('medium\r\n')
     child.expect_exact('main done\r\n')
     child.expect_exact('low\r\n')
 
