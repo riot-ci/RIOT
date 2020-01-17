147e2f649e8f09ea51d1f13622c34d8e8cc058b0
Kaspar Schleiser <kaspar@schleiser.de>
2020-01-17 10:29:21 +0100

    fixup! tests/event_threads: initial event threads test application

--- tests/event_threads/main.c
+++ tests/event_threads/main.c
@@ -25,13 +25,6 @@
 #include "thread.h"
 #include "event/thread.h"
 
-static void _handler_low(event_t *event) {
-    (void)event;
-    puts("low");
-}
-
-static event_t event_low = { .handler=_handler_low };
-
 static void _handler_high(event_t *event) {
     (void)event;
     puts("high");
@@ -39,10 +32,26 @@ static void _handler_high(event_t *event) {
 
 static event_t event_high = { .handler=_handler_high };
 
+static void _handler_medium(event_t *event) {
+    (void)event;
+    puts("medium");
+}
+
+static event_t event_medium = { .handler=_handler_medium };
+
+static void _handler_low(event_t *event) {
+    (void)event;
+    puts("low");
+}
+
+static event_t event_low = { .handler=_handler_low };
+
 int main(void)
 {
     event_post(EVENT_PRIO_LOWEST, &event_low);
+    event_post(EVENT_PRIO_MEDIUM, &event_medium);
     event_post(EVENT_PRIO_HIGHEST, &event_high);
+
     puts("main done");
 
     return 0;
