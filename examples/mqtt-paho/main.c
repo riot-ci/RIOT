893f31699fd12d7efab88d3a17b6470e2646d04f
Javier FILEIV <javier.fileiv@gmail.com>
2020-01-21 18:45:28 +0100

    mqtt: lest review changes, added README.md

--- examples/mqtt-paho/main.c
+++ examples/mqtt-paho/main.c
@@ -21,20 +21,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include "thread.h"
-#include "log.h"
-#include "mqtt.h"
 #include "msg.h"
-#include "lwip.h"
-#include "lwip/netif.h"
 #include "shell.h"
+#include "lwip/netif.h"
+#include "lwip.h"
+#include "mqtt.h"
 
 #include "MQTTClient.h"
 
 #define BUF_SIZE                250
-#define KEEP_ALIVE_INTERVAL     3600
 #define MQTT_VERSION            3
-#define MQTT_DEFAULT_QOS        0
 #define COMMAND_TIMEOUT_MS      1000
 #define IS_CLEAN_SESSION        1
 #define NOT_RETAINED_MSG        0
@@ -66,7 +62,7 @@ static void _on_msg_received(MessageData *data)
            (char *)data->message->payload);
 }
 
-static int cmd_con(int argc, char **argv)
+static int _cmd_con(int argc, char **argv)
 {
     if (argc < 7) {
         printf(
@@ -79,12 +75,12 @@ static int cmd_con(int argc, char **argv)
     int port = atoi(argv[2]);
 
     /* ensure client isn't connected in case of a new connection */
-    if(client.isconnected) {
+    if (client.isconnected) {
        printf("client already connected, disconnecting it\n");
        MQTTDisconnect(&client);
     }
 
-    if(!is_initialized) {
+    if (!is_initialized) {
         MQTTNetworkInit(&network);
         printf("Launching MQTT Task\n");
         MQTTStartTask(&client);
@@ -114,13 +110,14 @@ static int cmd_con(int argc, char **argv)
     int ret = MQTTConnect(&client, &data);
     if (ret) {
         printf("error: Unable to connect client %d\n", ret);
-    } else {
+    }
+    else {
         printf("Connection successfully\n");
     }
     return ret;
 }
 
-static int cmd_discon(int argc, char **argv)
+static int _cmd_discon(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
@@ -137,7 +134,7 @@ static int cmd_discon(int argc, char **argv)
     return res;
 }
 
-static int cmd_pub(int argc, char **argv)
+static int _cmd_pub(int argc, char **argv)
 {
     enum QoS qos = QOS0;
 
@@ -167,7 +164,7 @@ static int cmd_pub(int argc, char **argv)
     return rc;
 }
 
-static int cmd_sub(int argc, char **argv)
+static int _cmd_sub(int argc, char **argv)
 {
     enum QoS qos = QOS0;
 
@@ -194,7 +191,7 @@ static int cmd_sub(int argc, char **argv)
     return ret;
 }
 
-static int cmd_unsub(int argc, char **argv)
+static int _cmd_unsub(int argc, char **argv)
 {
     if (argc < 2) {
         printf("usage %s <topic name>\n", argv[0]);
@@ -215,11 +212,11 @@ static int cmd_unsub(int argc, char **argv)
 
 static const shell_command_t shell_commands[] =
 {
-    { "con", "connect to MQTT broker", cmd_con },
-    { "discon", "disconnect from the current broker", cmd_discon },
-    { "pub", "publish something", cmd_pub },
-    { "sub", "subscribe topic", cmd_sub },
-    { "unsub", "unsubscribe from topic", cmd_unsub },
+    { "con", "connect to MQTT broker", _cmd_con },
+    { "discon", "disconnect from the current broker", _cmd_discon },
+    { "pub", "publish something", _cmd_pub },
+    { "sub", "subscribe topic", _cmd_sub },
+    { "unsub", "unsubscribe from topic", _cmd_unsub },
     { NULL, NULL, NULL }
 };
 
