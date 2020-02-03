d897a90433126f4a1213c89b9c1d0e61e341b0fc
Karl Fessel <karl.fessel@ovgu.de>
2020-02-03 18:10:14 +0100

    bitarithm: Fixed missing bitarithm includes
    frac.c and nrf52/usbdev.c use bitarithm.h but where missing the include
    
    sam0/rtt.c defined a bit using mask from bitarithm,
    changed that to the soulution used in sam0/rtc.c

--- cpu/sam0_common/periph/rtt.c
+++ cpu/sam0_common/periph/rtt.c
@@ -31,8 +31,9 @@
  * Bit introduced by SAML21xxxB, setting it on SAML21xxxxA too has no ill
  * effects, but simplifies the code. (This bit is always set on SAML21xxxxA)
  */
-#ifndef RTC_MODE0_CTRLA_COUNTSYNC
-#define RTC_MODE0_CTRLA_COUNTSYNC   BIT15
+#ifndef RTC_MODE2_CTRLA_CLOCKSYNC
+#define RTC_MODE2_CTRLA_CLOCKSYNC_Pos   15
+#define RTC_MODE2_CTRLA_CLOCKSYNC       (0x1ul << RTC_MODE2_CTRLA_CLOCKSYNC_Pos)
 #endif
 
 static rtt_cb_t _overflow_cb;
