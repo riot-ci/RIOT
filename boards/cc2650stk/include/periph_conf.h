6a90cf9dc84a56b98993b7641f30258860b25bb3
dylad <dylan.laduranty@mesotic.com>
2020-02-04 20:13:40 +0100

    yet another fixup

--- boards/cc2650stk/include/periph_conf.h
+++ boards/cc2650stk/include/periph_conf.h
@@ -80,8 +80,8 @@ static const uart_conf_t uart_config[] = {
         .tx_pin = 29,
         .rx_pin = 28,
 #ifdef MODULE_PERIPH_UART_HW_FC
-        .rts_pin = GPIO_UNDEF,      /* ignored when flow_control is 0 */
-        .cts_pin = GPIO_UNDEF,      /* ignored when flow_control is 0 */
+        .rts_pin = GPIO_UNDEF,
+        .cts_pin = GPIO_UNDEF,
 #endif
         .intn = UART0_IRQN
     }
