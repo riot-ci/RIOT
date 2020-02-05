7a6f5d16099224db941c20780a9b7567ab886d06
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-02-05 13:46:20 -0500

    cc1312-launchpad: use radio when gnrc_netdev_default is present
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- cpu/cc26x2_cc13x2/prop_radio/rf_internal.c
+++ cpu/cc26x2_cc13x2/prop_radio/rf_internal.c
@@ -488,7 +488,7 @@ static uint_fast16_t cc13x2_prop_rf_send_enable_cmd(void)
 
     /* initialize radio setup command */
     _cmd_radio_setup = cRadioSetupCmd;
-    /* initally set the radio tx power to the max */
+    /* initially set the radio tx power to the max */
     _cmd_radio_setup.txPower             = _current_tx_power->value;
     _cmd_radio_setup.pRegOverride        = sPropOverrides;
     _cmd_radio_setup.config.frontEndMode = 0; /* differential */
@@ -917,7 +917,9 @@ void cc13x2_prop_rf_set_chan(uint16_t channel)
     uint16_t frac;
     cc13x2_prop_rf_freq_parts(new_freq, &freq, &frac);
 
-    assert(cc13x2_prop_rf_send_fs_cmd(freq, frac) == CMDSTA_Done);
+    if (cc13x2_prop_rf_send_fs_cmd(freq, frac) != CMDSTA_Done) {
+        /* TODO: error */
+    }
 
     _channel = channel;
 
@@ -1000,7 +1002,7 @@ int cc13x2_prop_rf_recv(void *buf, size_t len, netdev_ieee802154_rx_info_t *rx_i
     }
 
     /* wait for entry to become finished */
-    /* TOOD: timeout */
+    /* TODO: timeout */
     while (cur_entry->status == DATA_ENTRY_BUSY) {}
 
     /*
@@ -1051,7 +1053,7 @@ int cc13x2_prop_rf_recv(void *buf, size_t len, netdev_ieee802154_rx_info_t *rx_i
         return -ENOSPC;
     }
 
-    /* Substract checksum bytes. */
+    /* Subtract checksum bytes. */
     payload_len -= 2;
 
     /* Copy packet data. */
