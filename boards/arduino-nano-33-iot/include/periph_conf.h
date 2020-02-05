0d5ed36d51c70b408ce762bca4f5731ff1677d1b
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-02-05 17:06:37 +0100

    Update boards/arduino-nano-33-iot/include/periph_conf.h
    Co-Authored-By: benpicco <benpicco@googlemail.com>

--- boards/arduino-nano-33-iot/include/periph_conf.h
+++ boards/arduino-nano-33-iot/include/periph_conf.h
@@ -133,7 +133,7 @@ static const uart_conf_t uart_config[] = {
         .rx_pad   = UART_PAD_RX_3,
         .tx_pad   = UART_PAD_TX_2,
         .flags    = UART_FLAG_NONE,
-        .gclk_src = GCLK_CLKCTRL_GEN_GCLK0
+        .gclk_src = SAM0_GCLK_MAIN,
     }
 };
 
