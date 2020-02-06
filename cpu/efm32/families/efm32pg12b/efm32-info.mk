9c5d9299b826e9b57474a10851e563cd35dc9c72
Bas Stottelaar <basstottelaar@gmail.com>
2020-02-06 22:59:36 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! cpu/efm32: move cpus.txt info to efm32-vars.mk

--- cpu/efm32/families/efm32pg12b/efm32-info.mk
+++ cpu/efm32/families/efm32pg12b/efm32-info.mk
@@ -2,8 +2,8 @@
 # probably little reason to edit this file anyway, since it should already
 # contain all information for the EFM32PG12B family of CPUs.
 
-# CPU - Family - Series - Architecture - Flash base - Flash size - SRAM base - SRAM size - Crypto? - TRNG? - Radio?
-EFM32_INFO_efm32pg12b500f1024im48  = efm32pg12b 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
-EFM32_INFO_efm32pg12b500f1024il125 = efm32pg12b 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
-EFM32_INFO_efm32pg12b500f1024gl125 = efm32pg12b 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
-EFM32_INFO_efm32pg12b500f1024gm48  = efm32pg12b 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
+# Series - Architecture - Flash base - Flash size - SRAM base - SRAM size - Crypto? - TRNG? - Radio?
+EFM32_INFO_efm32pg12b500f1024im48  = 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
+EFM32_INFO_efm32pg12b500f1024il125 = 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
+EFM32_INFO_efm32pg12b500f1024gl125 = 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
+EFM32_INFO_efm32pg12b500f1024gm48  = 1 cortex-m4f 0x00000000 0x00100000 0x20000000 0x00040000 1 1 0
