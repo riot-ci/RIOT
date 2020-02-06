9ec3de78a0c1d82712125af02a3b39ced6f66da2
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2020-02-06 13:29:41 +0100

    fixup! tests: add test application for Sensirion SPS30 sensor driver

--- tests/driver_sps30/main.c
+++ tests/driver_sps30/main.c
@@ -22,77 +22,112 @@
 #include "sps30.h"
 #include "sps30_params.h"
 
-#define POLL_FOR_READY_US (100000U)
+#define TEST_START_DELAY_US          (2 * US_PER_SEC)
+#define SENSOR_RESET_DELAY_US        (10 * US_PER_SEC)
+#define SENSOR_STARTUP_DELAY_US      (10 * US_PER_SEC)
+#define POLL_FOR_READY_US            (1000000U)
+#define NUM_OF_MEASUREMENTS          (10)
+
+#define TYPE_MC_STR  "MC PM"
+#define TYPE_NC_STR  "NC PM"
+#define TYPE_TPS_STR "TPS"
 #define MC_UNIT_STR  "[µg/m³]"
 #define NC_UNIT_STR  "[#/cm³]"
 #define TPS_UNIT_STR "[µm]"
 
-static void _print_error(const char *msg, sps30_error_code_t ec)
+/* prints the result of an operation and returns true if an error occured */
+static bool _print_error(const char *msg, sps30_error_code_t ec)
 {
-    printf("%s_ERROR: %s\n", ec == SPS30_CRC_ERROR ? "CRC" : "I2C", msg);
+    printf("sps30_%s: [%s]\n", msg, (ec == SPS30_OK) ? "OK" :
+                              (ec == SPS30_CRC_ERROR ? "CRC_ERROR"
+                                                     : "I2C_ERROR"));
+    return ec != SPS30_OK;
+}
+
+static void _print_val_row(char *typ1, char *typ2, char *unit, float val)
+{
+    printf("| %-5s %4s:%3"PRIu32".%03"PRIu32" %-8s |\n", typ1, typ2,
+           (uint32_t)val, ((uint32_t)((val+0.0005)*1000))%1000, unit);
 }
 
 int main(void)
 {
-    char str[SPS30_SER_ART_LEN];
     sps30_t dev;
+    sps30_data_t data;
     sps30_error_code_t ec;
+    char str[SPS30_SER_ART_LEN];
+    uint32_t ci = 0;   /* clean interval */
+    uint32_t nci = 0;  /* new clean interval */
+    bool error = false;
+    unsigned cnt = NUM_OF_MEASUREMENTS;
+
+    xtimer_usleep(TEST_START_DELAY_US);
 
     puts("SPS30 test application\n");
 
     ec = sps30_init(&dev, &sps30_params[0]);
-    if (ec != SPS30_OK) {
-        _print_error("init failed", ec);
-        return 1;
-    }
-    else {
-        puts("Initialization successful\n");
-    }
+    error |= _print_error("init", ec);
 
     ec = sps30_read_article_code(&dev, str, sizeof(str));
     if (ec == SPS30_OK) {
         printf("Article code: %s\n", str);
     } else {
-        _print_error("while reading article code", ec);
+        error |= _print_error("read_article_code", ec);
     }
 
     ec = sps30_read_serial_number(&dev, str, sizeof(str));
     if (ec == SPS30_OK) {
         printf("Serial: %s\n", str);
     } else {
-        _print_error("while reading serial number", ec);
+        error |= _print_error("read_serial_number", ec);
     }
 
-    uint32_t ci = 0;
+    ec = sps30_start_fan_clean(&dev);
+    error |= _print_error("start_fan_clean", ec);
+
+    /* wait long enough for the fan clean to be done and the fan to settle */
+    xtimer_usleep(2* SPS30_FAN_CLEAN_S * US_PER_SEC);
+
+    /* read the currently set value from the sensor */
     ec = sps30_read_ac_interval(&dev, &ci);
+    error |= _print_error("read_ac_interval", ec);
 
-    if (ec == SPS30_OK) {
-        printf("Current auto-clean interval: %"PRIu32" seconds\n", ci);
-    } else {
-        _print_error("while reading article code", ec);
-    }
+    nci = ci + 1;
+    ec = sps30_write_ac_interval(&dev, nci);
+    error |= _print_error("write_ac_interval", ec);
 
-    printf("Starting a cleaning cycle...\n");
-    sps30_start_fan_clean(&dev);
-    xtimer_usleep(SPS30_FAN_CLEAN_S * US_PER_SEC);
+    /* resetting the sensor so the updated value can be read */
+    sps30_reset(&dev);
 
+    xtimer_usleep(SENSOR_RESET_DELAY_US);
+
+    /* start the sensor again again... */
     ec = sps30_start_measurement(&dev);
-    if (ec == SPS30_OK) {
-        puts("started measurement: OK\n");
-    } else {
-        puts("started measurement: ERROR\n");
+    error |= _print_error("start_measurement", ec);
+
+    xtimer_usleep(SENSOR_STARTUP_DELAY_US);
+
+    ec = sps30_read_ac_interval(&dev, &ci);
+    error |= _print_error("read_ac_interval", ec);
+
+    if (ci != nci) {
+        printf("ERROR: the auto-clean interval was not updated properly! (%"
+               PRIu32" != %"PRIu32")\n", ci, nci);
     }
 
-    sps30_data_t data;
-    while (1) {
+    /* restore the default auto-clean cycle */
+    ec = sps30_write_ac_interval(&dev, SPS30_DEFAULT_ACI_S);
+    error |= _print_error("write_ac_interval", ec);
+
+    while (cnt) {
         int err_code;
         bool ready = sps30_data_ready(&dev, &err_code);
 
-        if (err_code != SPS30_OK) {
-            _print_error("while reading ready flag", err_code);
-        }
-
         if (!ready) {
+            if (err_code != SPS30_OK) {
+                error |= _print_error("data_ready", err_code);
+                cnt--; /* if errors happen, stop after NUM_OF_MEASUREMENTS */
+            }
             /* try again after some time */
             xtimer_usleep(POLL_FOR_READY_US);
             continue;
@@ -101,25 +136,35 @@ int main(void)
         ec = sps30_read_measurement(&dev, &data);
 
         if (ec == SPS30_OK) {
-            puts("v==== SPS30 measurements ====v");
-            printf("| MC PM 1.0:   %2.3f "MC_UNIT_STR" |\n", data.mc_pm1);
-            printf("| MC PM 2.5:   %2.3f "MC_UNIT_STR" |\n", data.mc_pm2_5);
-            printf("| MC PM 4.0:   %2.3f "MC_UNIT_STR" |\n", data.mc_pm4);
-            printf("| MC PM 10.0:  %2.3f "MC_UNIT_STR" |\n", data.mc_pm10);
-            printf("| NC PM 0.5:   %2.3f "NC_UNIT_STR" |\n", data.nc_pm0_5);
-            printf("| NC PM 1.0:   %2.3f "NC_UNIT_STR" |\n", data.nc_pm1);
-            printf("| NC PM 2.5:   %2.3f "NC_UNIT_STR" |\n", data.nc_pm2_5);
-            printf("| NC PM 4.0:   %2.3f "NC_UNIT_STR" |\n", data.nc_pm4);
-            printf("| NC PM 10.0:  %2.3f "NC_UNIT_STR" |\n", data.nc_pm10);
-            printf("| TPS:         %2.3f "TPS_UNIT_STR"    |\n", data.ps);
+            puts("\nv==== SPS30 measurements ====v");
+            _print_val_row(TYPE_MC_STR,"1.0", MC_UNIT_STR, data.mc_pm1);
+            _print_val_row(TYPE_MC_STR,"2.5", MC_UNIT_STR, data.mc_pm2_5);
+            _print_val_row(TYPE_MC_STR,"4.0", MC_UNIT_STR, data.mc_pm4);
+            _print_val_row(TYPE_MC_STR,"10.0", MC_UNIT_STR, data.mc_pm10);
+            _print_val_row(TYPE_NC_STR,"0.5", NC_UNIT_STR, data.nc_pm0_5);
+            _print_val_row(TYPE_NC_STR,"1.0", NC_UNIT_STR, data.nc_pm1);
+            _print_val_row(TYPE_NC_STR,"2.5", NC_UNIT_STR, data.nc_pm2_5);
+            _print_val_row(TYPE_NC_STR,"4.0", NC_UNIT_STR, data.nc_pm4);
+            _print_val_row(TYPE_NC_STR,"10.0", NC_UNIT_STR, data.nc_pm10);
+            _print_val_row(TYPE_TPS_STR, "", TPS_UNIT_STR, data.ps);
             puts("+----------------------------+");
             puts("| MC:  Mass Concentration    |");
             puts("| NC:  Number Concentration  |");
             puts("| TPS: Typical Particle Size |");
-            puts("^============================^\n\n\n");
+            printf("^========= %2u / %2u ==========^\n\n",
+                   NUM_OF_MEASUREMENTS - cnt + 1, NUM_OF_MEASUREMENTS);
         } else {
-            _print_error("while reading measurements", ec);
+            error |= _print_error("read_measurement", ec);
         }
+
+        cnt--;
+    }
+
+    if (error) {
+        puts("SPS30 test: [FAILED]");
+    }
+    else {
+        puts("SPS30 test: [SUCCESS]");
     }
 
     return 0;
