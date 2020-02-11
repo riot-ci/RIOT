6f184070c40f4c910d35085ea3ea80fdc2e3f17d
Francisco Molina <femolina@uc.cl>
2020-02-11 16:34:43 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! tests: remove uneeded DISABLE_MODULE+=auto_init

--- tests/thread_flood/main.c
+++ tests/thread_flood/main.c
@@ -27,8 +27,6 @@
 #include "thread.h"
 #include "kernel_types.h"
 
-#include "test_utils/interactive_sync.h"
-
 /* One stack for all threads. DON'T TRY THIS AT HOME!! */
 static char dummy_stack[THREAD_STACKSIZE_IDLE];
 
