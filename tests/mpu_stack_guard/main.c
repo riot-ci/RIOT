ee7e645c9f115dedde5845501fd2e85538b1da86
Kaspar Schleiser <kaspar@schleiser.de>
2020-02-17 13:54:00 +0100

    tests/mpu_stack_guard: check for __MPU_PRESENT, error if not

--- tests/mpu_stack_guard/main.c
+++ tests/mpu_stack_guard/main.c
@@ -22,6 +22,14 @@
 
 #include "cpu.h"
 #include "thread.h"
+#include "mpu.h"
+
+/* RIOT's MPU headers gracefully fail when no MPU is present.
+ * Use this to catch if RIOT's features are correctly gateing MPU use.
+ */
+#ifndef __MPU_PRESENT
+#error "__MPU_PRESENT not defined!"
+#endif
 
 #define CANARY_VALUE 0xdeadbeef
 
