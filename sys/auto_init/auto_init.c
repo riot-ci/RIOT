2ff8063d961eb5c5c1b14d1c42c5ed30e730d4d2
Kaspar Schleiser <kaspar@schleiser.de>
2020-02-20 12:09:59 +0100

    sys/ztimer: adapt to DEFAULT_MODULE based auto_init scheme

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -116,13 +116,13 @@ void auto_init(void)
     void auto_init_random(void);
     auto_init_random();
 #endif
-#ifdef MODULE_ZTIMER_AUTO_INIT
-    DEBUG("Auto init ztimer.\n");
+#ifdef MODULE_AUTO_INIT_ZTIMER
+    LOG_DEBUG("Auto init ztimer.\n");
     void ztimer_init(void);
     ztimer_init();
 #endif
 #if defined(MODULE_AUTO_INIT_XTIMER) && (!defined(MODULE_ZTIMER_XTIMER_COMPAT))
-    DEBUG("Auto init xtimer.\n");
+    LOG_DEBUG("Auto init xtimer.\n");
     xtimer_init();
 #endif
 #ifdef MODULE_SCHEDSTATISTICS
