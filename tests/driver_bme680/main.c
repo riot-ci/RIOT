19471815517d7273990f590b186b75202f7cfca2
Gunar Schorcht <gunar@schorcht.net>
2020-02-24 14:48:29 +0100

    tests/driver_bme680: update for easy to use API

--- tests/driver_bme680/main.c
+++ tests/driver_bme680/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 Mesotic SAS
+ *               2020 Gunar Schorcht
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -14,85 +15,101 @@
  * @brief       Test application for the bme680_driver package.
  *
  * @author      Dylan Laduranty <dylan.laduranty@mesotic.com>
- *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
  * @}
  */
 
 #include <stdio.h>
+
 #include "board.h"
 #include "bme680.h"
-#include "bme680_params.h"
-#include "bme680_defs.h"
 #include "bme680_hal.h"
-
-#ifdef MODULE_PERIPH_SPI
-#include "periph/spi.h"
-#endif
-
-#ifdef MODULE_PERIPH_I2C
-#include "periph/i2c.h"
-#endif
-
+#include "bme680_params.h"
+#include "mutex.h"
 #include "xtimer.h"
 
-#define BME680_DEV dev.dev
+#define BME680_TEST_PERIOD_US   (5 * US_PER_SEC)
 
+xtimer_t timer;
+
+static void _timer_cb(void *arg)
+{
+    xtimer_set(&timer, BME680_TEST_PERIOD_US);
+    mutex_unlock(arg);
+}
 
 int main(void)
 {
-    bme680_t dev;
-    xtimer_sleep(2);
-    /* You may assign a chip select identifier to be handled later */
-    dev.dev.dev_id = 0;
+    mutex_t timer_mtx = MUTEX_INIT_LOCKED;
 
-    /* amb_temp can be set to 25 prior to configuring the gas sensor
-     * or by performing a few temperature readings without operating the
-     * gas sensor */
-    BME680_DEV.amb_temp = 25;
-    BME680_DEV.delay_ms = ms_sleep;
+    bme680_t dev[BME680_NUMOF]; // [BME680_NUMOF];
 
-    printf("Intialize BME680 sensor...");
-    if (bme680_init(&dev, &bme680_params[0]) != BME680_OK) {
-        puts("failed");
+    for (unsigned i = 0; i < BME680_NUMOF; i++) {
+        /*
+         * We use a fix temperature here. The ambient temperature could be
+         * determined by performing a few temperature readings without
+         * operating the gas sensor or by another temperature sensor. Function
+         * bme680_set_ambient_temp can be used at any time to change it.
+         */
+        BME680_SENSOR(&dev[i]).amb_temp = 25;
+
+        printf("Intialize BME680 sensor %d...", i);
+        if (bme680_init(&dev[i], &bme680_params[i]) != BME680_OK) {
+            puts("failed");
+        }
+        else {
+            puts("OK");
+        }
     }
-    else {
-        puts("OK");
-    }
+
+    timer.callback = _timer_cb;
+    timer.arg = &timer_mtx;
+    xtimer_set(&timer, BME680_TEST_PERIOD_US);
 
     while(1)
     {
         struct bme680_field_data data;
-        uint8_t ret;
 
-        xtimer_sleep(5);
-
-        ret = bme680_get_sensor_data(&data, &(dev.dev));
-        if (!ret) {
-#ifndef BME680_FLOAT_POINT_COMPENSATION
-        printf("[bme680]: T %02" PRIi16 ".%02" PRIu16 " degC, P %" PRIu32 " Pa," \
-               " H %02" PRIu32 ".%03" PRIu32 "",
-               data.temperature / 100, data.temperature % 100,
-            data.pressure, data.humidity / 1000, data.humidity % 1000);
-        /* Avoid using measurements from an unstable heating setup */
-        if (data.status & BME680_GASM_VALID_MSK)
-            printf(", G %" PRIu32 " ohms", data.gas_resistance);
+        for (unsigned i = 0; i < BME680_NUMOF; i++) {
+            /* trigger one measuerment */
+            bme680_force_measurement(&dev[i]);
+            /* get the duration for the measurement */
+            int duration = bme680_get_duration(&dev[i]);
+            /* wait for the duration */
+            xtimer_usleep(duration * US_PER_MS);
+            /* read the data */
+            int res = bme680_get_data(&dev[i], &data);
+            
+            if (res == 0 && dev[i].sensor.new_fields) {
+#ifndef MODULE_BME680_FP
+                printf("[bme680]: dev=%d, T = %02" PRIi16 ".%02" PRIu16 " degC, "
+                       "P = %" PRIu32 " Pa, H = %02" PRIu32 ".%03" PRIu32 " %%",
+                       i, data.temperature / 100, data.temperature % 100,
+                       data.pressure,
+                       data.humidity / 1000, data.humidity % 1000);
+                /* Avoid using measurements from an unstable heating setup */
+                if (data.status & BME680_GASM_VALID_MSK) {
+                    printf(", G = %" PRIu32 " ohms", data.gas_resistance);
+                }
 #else
-        printf("[bme680]: T %.2f degC, P %.2f Pa, H %2.f ", data.temperature,
-            data.pressure, data.humidity);
-        /* Avoid using measurements from an unstable heating setup */
-        if (data.status & BME680_GASM_VALID_MSK)
-            printf(", G %f ohms", data.gas_resistance);
+                printf("[bme680]: dev=%d T = %.2f degC, P = %.2f Pa, H %.3f %%",
+                       i, data.temperature, data.pressure, data.humidity);
+                /* Avoid using measurements from an unstable heating setup */
+                if (data.status & BME680_GASM_VALID_MSK) {
+                    printf(", G = %.0f ohms", data.gas_resistance);
+                }
 #endif
-        printf("\r\n");
+                printf("\n");
+            }
+            else if (res == 0) {
+                printf("[bme680]: no new data\n");
+            }
+            else {
+                printf("[bme680]: read data failed with reason %d\n", res);
+            }
         }
-        else {
-            puts("[bme680]: measurement failed");
-        }
-        /* Trigger the next measurement if you would like to read data out
-           continuously */
-        if (BME680_DEV.power_mode == BME680_FORCED_MODE) {
-            bme680_set_sensor_mode(&dev.dev);
-        }
+        printf("+-----------------------------------------+\n");
+        mutex_lock(&timer_mtx);
     }
     /* Should never reach here */
     return 0;
