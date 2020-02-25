b815f7b5460cab3039c0b5294c279978554c8605
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-02-25 15:03:06 +0100

    fixup! drivers/ili9341: provide adaption to disp_dev interface

--- drivers/ili9341/ili9341_disp_dev.c
+++ drivers/ili9341/ili9341_disp_dev.c
@@ -19,10 +19,17 @@
 
 #include <assert.h>
 #include <stdint.h>
-#include <errno.h>
 
 #include "ili9341.h"
 #include "ili9341_disp_dev.h"
+
+#ifndef ILI9341_DISP_DEV_HEIGHT
+#define ILI9341_DISP_DEV_HEIGHT     (240U)
+#endif
+
+#ifndef ILI9341_DISP_COLOR_DEPTH
+#define ILI9341_DISP_COLOR_DEPTH    (16U)
+#endif
 
 static void _ili9341_map(disp_dev_t *dev, uint16_t x1, uint16_t x2,
                   uint16_t y1, uint16_t y2, const uint16_t *color)
@@ -31,71 +38,44 @@ static void _ili9341_map(disp_dev_t *dev, uint16_t x1, uint16_t x2,
     ili9341_pixmap(ili9341, x1, x2, y1, y2, color);
 }
 
-static int _ili9341_get(disp_dev_t *disp_dev, disp_opt_t opt, void *value, size_t max_len)
+static uint16_t _ili9341_height(disp_dev_t *disp_dev)
 {
-    const ili9341_t *dev = (ili9341_t *)disp_dev;
-
-    if (disp_dev == NULL) {
-        return -ENODEV;
-    }
-
-    int res = -ENOTSUP;
-    switch (opt) {
-        case DISP_OPT_MAX_WIDTH:
-            assert(max_len == sizeof(uint16_t));
-            *(uint16_t *)value = dev->params->lines;
-            res = sizeof(uint16_t);
-            break;
-
-        case DISP_OPT_MAX_HEIGHT:
-            assert(max_len == sizeof(uint16_t));
-            *(uint16_t *)value = 240;
-            res = sizeof(uint16_t);
-            break;
-
-        case DISP_OPT_COLOR_DEPTH:
-            assert(max_len == sizeof(uint8_t));
-            *(uint8_t *)value = 16;
-            res = sizeof(uint8_t);
-            break;
-
-        default:
-            break;
-    }
-
-    return res;
+    (void)disp_dev;
+    return ILI9341_DISP_DEV_HEIGHT;
 }
 
-static int _ili9341_set(disp_dev_t *disp_dev, disp_opt_t opt, const void *value, size_t max_len)
+static uint16_t _ili9341_width(disp_dev_t *disp_dev)
+{
+    const ili9341_t *dev = (ili9341_t *)disp_dev;
+    assert(dev);
+
+    return dev->params->lines;
+}
+
+static uint8_t _ili9341_color_depth(disp_dev_t *disp_dev)
+{
+    (void)disp_dev;
+    return ILI9341_DISP_COLOR_DEPTH;
+}
+
+static void _ili9341_set_invert(disp_dev_t *disp_dev, bool invert)
 {
     const ili9341_t *dev = (ili9341_t *)disp_dev;
 
-    if (disp_dev == NULL) {
-        return -ENODEV;
+    assert(dev);
+
+    if (invert) {
+        ili9341_invert_on(dev);
     }
-
-    int res = -ENOTSUP;
-    switch (opt) {
-        case DISP_OPT_COLOR_INVERT:
-            assert(max_len == sizeof(bool));
-            if (*(bool *)value) {
-                ili9341_invert_on(dev);
-            }
-            else {
-                ili9341_invert_off(dev);
-            }
-            res = sizeof(bool);
-            break;
-
-        default:
-            break;
+    else {
+        ili9341_invert_off(dev);
     }
-
-    return res;
 }
 
 const disp_dev_driver_t ili9341_disp_dev_driver = {
-    .map = _ili9341_map,
-    .get = _ili9341_get,
-    .set = _ili9341_set,
+    .map            = _ili9341_map,
+    .height         = _ili9341_height,
+    .width          = _ili9341_width,
+    .color_depth    = _ili9341_color_depth,
+    .set_invert     = _ili9341_set_invert,
 };
