cb6f8e8ad9f436eab550a2f07855abf02a102924
Koen Zandberg <koen@bergzand.net>
2020-02-25 15:35:05 +0100

    fixup! mulle: add erase timings to mtd_spi_nor instance

--- boards/mulle/board.c
+++ boards/mulle/board.c
@@ -55,10 +55,10 @@ static const mtd_spi_nor_params_t mulle_nor_params = {
     .addr_width = 3,
     .mode = SPI_MODE_3,
     .clk = SPI_CLK_10MHZ,
-    .wait_chip_erase = 16 * US_PER_SEC,
-    .wait_sector_erase = 40 * US_PER_MS,
-    .wait_32k_erase = 20 *US_PER_MS,
-    .wait_4k_erase = 10 * US_PER_MS,
+    .wait_chip_erase = 16LU * US_PER_SEC,
+    .wait_sector_erase = 40LU * US_PER_MS,
+    .wait_32k_erase = 20LU *US_PER_MS,
+    .wait_4k_erase = 10LU * US_PER_MS,
 };
 
 static mtd_spi_nor_t mulle_nor_dev = {
