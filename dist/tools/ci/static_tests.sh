fca1389265e926f042ee1bf2d7063f3d636124d7
Kaspar Schleiser <kaspar@schleiser.de>
2020-02-26 12:11:10 +0100

    fixup! fixup! dist: split out static tests from build_and_test.sh

--- dist/tools/ci/static_tests.sh
+++ dist/tools/ci/static_tests.sh
@@ -51,19 +51,24 @@ function run {
 
 RESULT=0
 
+if -n "${CI_BASE_COMMIT}"; then
+    echo "-- tagging master HEAD commit"
+    git tag master "${CI_BASE_COMMIT}"
+fi
+
 CI_BASE_BRANCH=${CI_BASE_BRANCH:-master}
 
 export BASE_BRANCH="${CI_BASE_BRANCH}"
 
-run ./dist/tools/commit-msg/check.sh "${CI_BASE_BRANCH}"
-run ./dist/tools/whitespacecheck/check.sh "${CI_BASE_BRANCH}"
+run ./dist/tools/commit-msg/check.sh "${BASE_BRANCH}"
+run ./dist/tools/whitespacecheck/check.sh "${BASE_BRANCH}"
 DIFFFILTER="MR" ERROR_EXIT_CODE=0 run ./dist/tools/licenses/check.sh
 DIFFFILTER="AC" run ./dist/tools/licenses/check.sh
 run ./dist/tools/doccheck/check.sh
 run ./dist/tools/externc/check.sh
 run ./dist/tools/cppcheck/check.sh
 run ./dist/tools/vera++/check.sh
-run ./dist/tools/pr_check/pr_check.sh "${CI_BASE_BRANCH}"
+run ./dist/tools/pr_check/pr_check.sh "${BASE_BRANCH}"
 run ./dist/tools/coccinelle/check.sh
 run ./dist/tools/flake8/check.sh
 run ./dist/tools/headerguards/check.sh
