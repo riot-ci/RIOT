f673d15538423343b04959a6cf0e3c8d555dba4e
Kaspar Schleiser <kaspar@schleiser.de>
2020-03-02 17:09:17 +0100

    fixup! boards/*-kw41z: add ztimer_msec config

--- boards/usb-kw41z/include/board.h
+++ boards/usb-kw41z/include/board.h
@@ -104,7 +104,7 @@ extern "C"
  * @name    ztimer configuration
  * @{
  */
-#define CONFIG_ZTIMER_USEC_TYPE     ZTIMER_TYPE_PERIPH
+#define CONFIG_ZTIMER_USEC_TYPE     ZTIMER_TYPE_PERIPH_TIMER
 #define CONFIG_ZTIMER_USEC_DEV      (TIMER_PIT_DEV(0))
 #define CONFIG_ZTIMER_USEC_FREQ     (1000000LU)
 #define CONFIG_ZTIMER_USEC_WIDTH    (32)
