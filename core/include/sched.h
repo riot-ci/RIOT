b8267078f9f4cb92c556629bdab97c0bc8f7e25d
Kaspar Schleiser <kaspar@schleiser.de>
2020-03-10 13:00:27 +0100

    core: uncrustify

--- core/include/sched.h
+++ core/include/sched.h
@@ -87,7 +87,7 @@
 #include "clist.h"
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /**
@@ -123,7 +123,7 @@ typedef enum {
  */
 #define STATUS_ON_RUNQUEUE      STATUS_RUNNING  /**< to check if on run queue:
                                                      `st >= STATUS_ON_RUNQUEUE`
-                                                     */
+                                                 */
 #define STATUS_NOT_FOUND ((thread_status_t)-1)  /**< Describes an illegal thread status */
 /** @} */
 /**
