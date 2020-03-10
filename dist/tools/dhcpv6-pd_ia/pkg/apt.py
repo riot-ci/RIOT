4a3de56d503d1d54bcd6e74dca5247bb46aa21f8
Martine S. Lenders <m.lenders@fu-berlin.de>
2020-03-10 13:23:03 +0100

    dhcpv6-pd_ia: initial import of a DHCPv6 server bootstrapper

---
+++ dist/tools/dhcpv6-pd_ia/pkg/apt.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright (C) 2020 Freie Universität Berlin
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
+import subprocess
+
+from .base import Installer
+
+__author__ = "Martine S. Lenders"
+__copyright__ = "Copyright (C) 2020 Freie Universität Berlin"
+__credits__ = ["Martine S. Lenders"]
+__license__ = "LGPLv2.1"
+__maintainer__ = "Martine S. Lenders"
+__email__ = "m.lenders@fu-berlin.de"
+
+
+class Apt(Installer):
+    def install(self, package):
+        if self._ask(package):
+            subprocess.run(["apt-get", "-y", "install",
+                            package[self.os]["name"]])
