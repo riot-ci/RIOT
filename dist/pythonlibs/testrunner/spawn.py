53f1ae260966e29ed5fdfc6345765e954def19cc
Francisco Molina <femolina@uc.cl>
2020-03-16 11:44:53 +0100

    fixup! fixup! fixup! fixup! tests: add interactive_sync adapted to shell

--- dist/pythonlibs/testrunner/spawn.py
+++ dist/pythonlibs/testrunner/spawn.py
@@ -99,7 +99,7 @@ def sync_child(child, env):
     # Do a child synchronization if used by a module
     modules = modules_list()
     if 'test_utils_interactive_sync' in modules:
-        if 'shell' in modules:
+        if 'test_utils_interactive_sync_shell' in modules:
             utils.test_utils_interactive_sync_shell(child,
                                                     TEST_INTERACTIVE_RETRIES,
                                                     TEST_INTERACTIVE_DELAY)
