21b303bbe4624bc3c2263a8b8d1e058079e89263
Koen Zandberg <koen@bergzand.net>
2020-03-17 09:12:54 +0100

    fixup! sys/suit: Add SUIT draft ietf-v3 firmware upgrade module

--- makefiles/boot/riotboot.mk
+++ makefiles/boot/riotboot.mk
@@ -146,8 +146,8 @@ riotboot/flash: riotboot/flash-slot0 riotboot/flash-bootloader
 FLASHFILE = $(RIOTBOOT_EXTENDED_BIN)
 
 # include suit targets
-ifneq (,$(filter suit_v3, $(USEMODULE)))
-  include $(RIOTMAKE)/suit.v3.inc.mk
+ifneq (,$(filter suit, $(USEMODULE)))
+  include $(RIOTMAKE)/suit.inc.mk
 endif
 
 else
