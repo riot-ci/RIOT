300d7aef65043a44624d9ac0e11948659a4b3462
Benjamin Valentin <benpicco@googlemail.com>
2020-03-18 23:48:08 +0100

    cpu/samd5x: work around errata when (re-)initializing DFLL
    When a previously disabled DFLL gets enabled again, the frequency will
    be incorrect. Follow the procedure outlined in the errata sheet, section 2.8.3
    to work around the issue.

--- cpu/samd5x/cpu.c
+++ cpu/samd5x/cpu.c
@@ -147,9 +147,14 @@ static void dfll_init(void)
 #endif
     ;
 
-    OSCCTRL->DFLLCTRLB.reg = reg;
-    OSCCTRL->DFLLCTRLA.reg = OSCCTRL_DFLLCTRLA_ENABLE;
+    /* workaround for Errata 2.8.3 DFLLVAL.FINE Value When DFLL48M Re-enabled */
+    OSCCTRL->DFLLMUL.reg   = 0;   /* Write new DFLLMULL configuration */
+    OSCCTRL->DFLLCTRLB.reg = 0;   /* Select Open loop configuration */
+    OSCCTRL->DFLLCTRLA.bit.ENABLE = 1; /* Enable DFLL */
+    OSCCTRL->DFLLVAL.reg   = OSCCTRL->DFLLVAL.reg; /* Reload DFLLVAL register */
+    OSCCTRL->DFLLCTRLB.reg = reg; /* Write final DFLL configuration */
 
+    OSCCTRL->DFLLCTRLA.reg = OSCCTRL_DFLLCTRLA_ENABLE;
     while (!OSCCTRL->STATUS.bit.DFLLRDY) {}
 }
 
