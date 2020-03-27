49855514bf37b1bd36e6ae68bb65f80e6663360c
Martine Lenders <m.lenders@fu-berlin.de>
2020-03-27 10:49:58 +0100

    fixup! tests: add test to test RDNSS option handling

--- tests/gnrc_ipv6_nib_dns/main.c
+++ tests/gnrc_ipv6_nib_dns/main.c
@@ -27,8 +27,6 @@
 #include "shell.h"
 
 #define MAIN_QUEUE_SIZE     (8)
-static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-
 static int _dns(int argc, char **argv);
 
 static const shell_command_t _shell_commands[] = {
@@ -39,22 +37,14 @@ static char _shell_buffer[SHELL_DEFAULT_BUFSIZE];
 
 static void _usage(char *cmd)
 {
-    printf("usage: %s server <DNS server addr> <DNS server port>\n", cmd);
-    printf("       %s request <name>\n", cmd);
+    printf("usage: %s server\n", cmd);
 }
 
 static int _dns_server(int argc, char **argv)
 {
-    if (((argc > 2) && !inet_pton(AF_INET6, argv[2],
-                                  sock_dns_server.addr.ipv6)) ||
-        ((argc > 3) && ((sock_dns_server.port = atoi(argv[3])) == 0))) {
-        _usage(argv[1]);
-        return 1;
-    }
-    else if (argc == 3) {
-        sock_dns_server.port = SOCK_DNS_PORT;
-    }
-    else if (argc == 2) {
+    int res = (argc < 2);
+
+    if (!res) {
         if (sock_dns_server.port > 0) {
             char addrstr[INET6_ADDRSTRLEN];
 
@@ -66,36 +56,16 @@ static int _dns_server(int argc, char **argv)
             puts("DNS server: -");
         }
     }
-    sock_dns_server.family = AF_INET6;
-    return 0;
-}
-
-static int _dns_request(char **argv)
-{
-    uint8_t addr[16] = {0};
-    int res = sock_dns_query(argv[2], addr, AF_UNSPEC);
-
-    if (res > 0) {
-        char addrstr[INET6_ADDRSTRLEN];
-
-        inet_ntop(res == 4 ? AF_INET : AF_INET6, addr, addrstr,
-                  sizeof(addrstr));
-        printf("%s resolves to %s\n", argv[2], addrstr);
-    }
     else {
-        printf("error resolving %s\n", argv[2]);
-        return 1;
+        _usage(argv[0]);
     }
-    return 0;
+    return res;
 }
 
 static int _dns(int argc, char **argv)
 {
     if ((argc > 1) && (strcmp(argv[1], "server") == 0)) {
         return _dns_server(argc, argv);
-    }
-    else if ((argc > 2) && (strcmp(argv[1], "request") == 0)) {
-        return _dns_request(argv);
     }
     else {
         _usage(argv[0]);
@@ -105,10 +75,6 @@ static int _dns(int argc, char **argv)
 
 int main(void)
 {
-    /* we need a message queue for the thread running the shell in order to
-     * receive potentially fast incoming networking packets */
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-
     /* start shell */
     shell_run(_shell_commands, _shell_buffer, sizeof(_shell_buffer));
     return 0;
