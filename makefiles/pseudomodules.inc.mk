2ec96f9cc308728d0eba29cf75e01bf334e5d8e7
fabian18 <fabian.huessler@ovgu.de>
2020-04-01 00:04:23 +0200

    drivers/nrf24l01p_ng: netdev driver for nrf24l01+
    The driver uses the netdev interface. Due to the limited
    capabilities of the transceiver (32 byte FIFO and no source address in the layer2 frame),
    it is not ment to be used with IPv6 over 6LowPAN, yet. This will be
    tackled in another commit.

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -91,6 +91,7 @@ PSEUDOMODULES += nimble_autoconn_%
 PSEUDOMODULES += newlib
 PSEUDOMODULES += newlib_gnu_source
 PSEUDOMODULES += newlib_nano
+PSEUDOMODULES += nrf24l01p_ng_diagnostics
 PSEUDOMODULES += openthread
 PSEUDOMODULES += picolibc
 PSEUDOMODULES += picolibc_stdout_buffered
