db858fa468df7ef26deb1be1293583471c4a43e3
Francisco Molina <femolina@uc.cl>
2020-04-01 14:24:59 +0200

    makefiles/defaultmodules.inc.mk: add BOARDs, CPUs defaultmodules.inc.mk
    Allow for BOARDs and CPUs to declare DEFAULT_MODULEs that might have
    dependencies and therefore require being handled before parsing
    Makefile.dep.
    
    DEFAULT_MODULEs declared in %/defaultmodules.inc.mk can only be disabled
    at application level. In other cases the behaviour is undefined.

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -1,4 +1,7 @@
 DEFAULT_MODULE += board cpu core core_init core_msg core_panic sys
 
+-include $(BOARDSDIR)/$(BOARD)/defaultmodules.inc.mk
+-include $(RIOTCPU)/$(CPU)/defaultmodules.inc.mk
+
 # Handle inclusion of DEFAULT_MODULEs once
 USEMODULE := $(USEMODULE) $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
