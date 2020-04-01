d2d50eba71ef35a0f6b69590b6588252c5d00cbc
Francisco Molina <femolina@uc.cl>
2020-04-01 14:19:49 +0200

    makefiles/defaultmodules: include DEFAULT_MODULEs only once
    Before DEFAULT_MODULEs where recursiverly added to USEMODULE. This
    led to undefined behaviour where DEFAULT_MODULEs added in the first
    iteration through MAKEFILE.dep would end up in USEMODULE but if
    conditionally added during the recursion they would not be converted
    to USEMODULE.
    
    They are now added through simple expansion so that they are only added
    once before the parsing of Makefile.dep.

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -1,1 +1,4 @@
 DEFAULT_MODULE += board cpu core core_init core_msg core_panic sys
+
+# Handle inclusion of DEFAULT_MODULEs once
+USEMODULE := $(USEMODULE) $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
