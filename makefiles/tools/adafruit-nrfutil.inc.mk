cb61d78031eafcb4d0b419609814886ffea422b8
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-04-07 18:06:26 +0200

    fixup! makefiles/tools: add support for nrfutil

--- makefiles/tools/adafruit-nrfutil.inc.mk
+++ makefiles/tools/adafruit-nrfutil.inc.mk
@@ -1,7 +1,7 @@
 FLASHFILE = $(HEXFILE)
 FLASHDEPS += $(HEXFILE).zip
 FLASHER = adafruit-nrfutil
-FFLAGS = dfu serial --port=${PORT} --baudrate=115200 --touch=1200 --package=$(HEXFILE).zip --singlebank
+FFLAGS = dfu serial --port=${PORT} --baudrate=${BAUD} --touch=1200 --package=$(HEXFILE).zip --singlebank
 
 %.hex.zip: %.hex
 	$(call check_cmd,$(FLASHER),Flash program and preparation tool)
