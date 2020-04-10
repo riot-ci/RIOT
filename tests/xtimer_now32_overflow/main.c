e1ecae47b9fb76b1a8161acb22c667f8876694dc
Martine Lenders <m.lenders@fu-berlin.de>
2020-04-10 12:28:48 +0200

    fixup! tests/xtimer: add regression test for long_start_time update bug

--- tests/xtimer_now32_overflow/main.c
+++ tests/xtimer_now32_overflow/main.c
@@ -16,36 +16,56 @@
 #include <stdio.h>
 
 #include "fmt.h"
+#include "kernel_defines.h"
 #include "msg.h"
 #include "test_utils/expect.h"
 #include "thread.h"
 #include "xtimer.h"
 
 #define MAIN_MSG_QUEUE_SIZE     (4U)
+#define TIMERS_NUMOF            (3U)
 
 msg_t _main_msg_queue[MAIN_MSG_QUEUE_SIZE];
+static const uint64_t _timers_offsets[TIMERS_NUMOF] = {
+    /* MUST ASCEND */
+    1 * US_PER_SEC,
+    2 * US_PER_SEC,
+    3 * US_PER_SEC,
+};
 
 int main(void)
 {
-    xtimer_t timer1, timer2;
-    msg_t msg1 = { .content = { .value = 1U } };
-    msg_t msg2 = { .content = { .value = 2U } };
+    xtimer_t timers[TIMERS_NUMOF];
+    msg_t msgs[TIMERS_NUMOF];
+    uint64_t start;
 
+    expect(ARRAY_SIZE(_timers_offsets) == TIMERS_NUMOF);
     msg_init_queue(_main_msg_queue, MAIN_MSG_QUEUE_SIZE);
     /* ensure that xtimer_now64() is greater than UINT32_MAX */
-    _xtimer_current_time = (1LLU << 32U);
+    _xtimer_current_time = (2LLU << 32U);
     xtimer_init();
-    expect(xtimer_now64().ticks64 > UINT32_MAX);
-    print_str("Setting 2 timers:\n");
-    print_str(" #1 in 1 sec\n");
-    print_str(" #2 in 3 sec\n\n");
-    xtimer_set_msg64(&timer1, 1 * US_PER_SEC, &msg1, thread_getpid());
-    xtimer_set_msg64(&timer2, 3 * US_PER_SEC, &msg2, thread_getpid());
+    print_str("Setting ");
+    print_u32_dec(TIMERS_NUMOF);
+    print_str(" timers:\n");
+    for (unsigned i = 0; i < TIMERS_NUMOF; i++) {
+        msgs[i].content.value = i;
+        print_str(" #");
+        print_u32_dec(i);
+        print_str(" in ");
+        print_u64_dec(_timers_offsets[i]);
+        print_str(" usec\n");
+    }
     print_str("now=");
-    print_u64_dec(xtimer_now64().ticks64);
+    start = xtimer_now64().ticks64;
+    print_u64_dec(start);
     print_str("\n");
-    expect(timer1.long_start_time > 0);
-    expect(timer2.long_start_time > 0);
+    expect(start > UINT32_MAX);
+    /* set timers after all were printed for better timing */
+    for (unsigned i = 0; i < TIMERS_NUMOF; i++) {
+        xtimer_set_msg64(&timers[i], _timers_offsets[i], &msgs[i],
+                         thread_getpid());
+        expect(timers[i].long_start_time > 0);
+    }
     while (1) {
         msg_t msg;
 
@@ -55,21 +75,14 @@ int main(void)
         print_str(":now=");
         print_u64_dec((uint64_t)xtimer_now64().ticks64);
         print_str("\n");
-        switch (msg.content.value) {
-            case 1U:
-                /* timer1 expired */
-                expect(timer1.long_start_time == 0);
-                /* upper half of timer2's start_time stays above 1 as it is
-                 * based on xtimer_now64() during timer1's callback execution */
-                expect(timer2.long_start_time > 0);
-                break;
-            case 2U:
-                /* both timers expired */
-                expect(timer1.long_start_time == 0);
-                expect(timer2.long_start_time == 0);
-                break;
-            default:
-                break;
+        for (unsigned i = 0; i <= msg.content.value; i++) {
+            /* all fired timers expired */
+            expect(timers[i].long_start_time == 0);
+        }
+        for (unsigned i = (msg.content.value + 1); i <= TIMERS_NUMOF; i++) {
+            /* upper half of remaing_timers' start_time stays above 0 as it is
+             * based on xtimer_now64() during the timer's callback execution */
+            expect(timers[i].long_start_time > 0);
         }
     }
     return 0;
