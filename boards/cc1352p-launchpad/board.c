6b307180464ad592e64952585db55eaa8cc19d5c
Luis Ruiz <luisan00@MBP-de-luisan00.home>
2020-04-27 19:53:07 +0200

    Add support for cc1352p-launchpad
    - Fix some typos
    - Add attribution to related files
    - delete Makefile.dep

---
+++ boards/cc1352p-launchpad/board.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C)    2016 Nicholas Jackson
+ *                  2017 HAW Hamburg
+ *                  2020 Locha Inc
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         boards_cc1352p_launchpad
+ * @{
+ *
+ * @file
+ * @brief           Board specific implementations for TI CC1352P LaunchPad
+ *
+ * @author          Nicholas Jackson <nicholas.jackson@griffithuni.edu.au>
+ * @author          Sebastian Meiling <s@mlng.net>
+ * @author          Luis A. Ruiz <luisan00@hotmail.com>
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+/**
+ * @brief           Initialise the board.
+ */
+
+void board_init(void)
+{
+    cpu_init();
+
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+}
