83ab4b3e411cfb712b68a28b254a7518a90a883f
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-03 22:22:10 +0200

    cpu/stm32: add stm32g0 support

--- cpu/stm32/stm32_info.mk
+++ cpu/stm32/stm32_info.mk
@@ -22,7 +22,7 @@ else ifneq (,$(filter $(CPU_FAM),g4 wb))
   CPU_ARCH = cortex-m4
 else ifeq (f7,$(CPU_FAM))
   CPU_ARCH = cortex-m7
-else ifeq (l0,$(CPU_FAM))
+else ifneq (,$(CPU_FAM),g0 l0)
   CPU_ARCH = cortex-m0plus
 else
   $(error Not supported CPU family: 'stm32$(CPU_FAM)')
