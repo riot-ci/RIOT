62aa601b46c194602e9910357766a8fac70fc62d
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-30 11:04:02 +0200

    fixup! boards/nucleo-g474re: add support for nucleo-g474re

--- boards/nucleo-g474re/include/periph_conf.h
+++ boards/nucleo-g474re/include/periph_conf.h
@@ -52,9 +52,9 @@ static const uart_conf_t uart_config[] = {
         .tx_pin     = GPIO_PIN(PORT_C, 4),
         .rx_af      = GPIO_AF7,
         .tx_af      = GPIO_AF7,
-        .bus        = APB1,
+        .bus        = APB2,
         .irqn       = USART1_IRQn,
-        .type       = STM32_LPUART,
+        .type       = STM32_USART,
         .clk_src    = 0, /* Use APB clock */
     },
 };
