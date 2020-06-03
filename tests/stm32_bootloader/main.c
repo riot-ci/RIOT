0c50d7fda347a73c65d38996df0941b770e6f1cc
Benjamin Valentin <benpicco@googlemail.com>
2020-06-03 20:23:32 +0200

    fixup! tests/stm32_bootloader: add test for STM32 bootloader mode

--- tests/stm32_bootloader/main.c
+++ tests/stm32_bootloader/main.c
@@ -24,29 +24,14 @@
 #include "shell.h"
 #include "shell_commands.h"
 
-extern void usb_board_reset_in_bootloader(void);
-
-static int _bootloader_reset(int argc, char** argv)
-{
-    (void) argc;
-    (void) argv;
-
-    usb_board_reset_in_bootloader();
-
-    return 0;
-}
-
-static const shell_command_t shell_commands[] = {
-    { "bootloader", "jump to STM32 bootloader", _bootloader_reset},
-    { NULL, NULL, NULL }
-};
-
 int main(void)
 {
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+
     puts("STM32 bootloader test application.");
 
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    /* bootloader command is provided by default */
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
