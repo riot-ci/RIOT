d26c92859f4d6480644ffbb069d569a91b5420d4
Benjamin Valentin <benpicco@googlemail.com>
2020-06-05 16:18:58 +0200

    fixup! makefiles/bossa: move USB reset logic to common file

--- makefiles/tools/usb_board_reset.mk
+++ makefiles/tools/usb_board_reset.mk
@@ -1,4 +1,11 @@
-PREFLASH_DELAY ?= 1
+ifneq (,$(filter reset flash flash-only, $(MAKECMDGOALS)))
+  # By default, add 2 seconds delay before opening terminal: this is required
+  # when opening the terminal right after flashing. In this case, the stdio
+  # over USB needs some time after reset before being ready.
+  TERM_DELAY ?= 2
+  TERMDEPS += term-delay
+  TERMDELAYDEPS = $(filter reset flash flash-only, $(MAKECMDGOALS))
+endif
 
 ifeq ($(OS),Linux)
   STTY_FLAG = -F
@@ -8,14 +15,17 @@ endif
 
 PREFLASHER ?= stty
 PREFFLAGS  ?= $(STTY_FLAG) $(PROG_DEV) raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
-ifneq (,$(PREFLASH_DELAY))
-  FLASHDEPS += preflash-delay
-else
-  FLASHDEPS += preflash
-endif
+PREFLASH_DELAY ?= 1
+FLASHDEPS += preflash-delay
 
 RESETFFLASG ?= $(STTY_FLAG) $(PORT) raw ispeed 600 ospeed 600 cs8 -cstopb ignpar eol 255 eof 255
 RESET ?= $(PREFLASHER) $(RESETFFLASG)
 
+TESTRUNNER_CONNECT_DELAY ?= $(TERM_DELAY)
+$(call target-export-variables,test,TESTRUNNER_CONNECT_DELAY)
+
 preflash-delay: preflash
 	sleep $(PREFLASH_DELAY)
+
+term-delay: $(TERMDELAYDEPS)
+	sleep $(TERM_DELAY)
