2af5c4ca122c1894eb7aaf9bb34a7d9c79865a9c
Juergen Fitschen <me@jue.yt>
2020-06-12 12:51:33 +0200

    fixup! boards/samr30-xpro: adjust RTT_MIN_VALUE

--- boards/samr30-xpro/include/periph_conf.h
+++ boards/samr30-xpro/include/periph_conf.h
@@ -155,6 +155,7 @@ static const i2c_conf_t i2c_config[] = {
  */
 #define RTT_FREQUENCY                           (32768U)
 #define RTT_MAX_VALUE                           (0xffffffffU)
+/* determined by using tests/ztimer_underflow */
 #define RTT_MIN_VALUE                           (8U)
 /** @} */
 
