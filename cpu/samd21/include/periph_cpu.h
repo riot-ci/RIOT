adda3a46d26635168aa867af867e9e133a85d7b5
Francisco Molina <femolina@uc.cl>
2020-06-17 12:27:12 +0200

    cpu/samd21: add unified rt% configuration

--- cpu/samd21/include/periph_cpu.h
+++ cpu/samd21/include/periph_cpu.h
@@ -127,6 +127,29 @@ typedef enum {
  */
 #define DAC_NUMOF           (1)
 
+/**
+ * @name    RTC configuration
+ * @{
+ */
+#define RTC_DEV             RTC->MODE2
+/** @} */
+
+/**
+ * @name    Real time counter configuration
+ * @{
+ */
+#define RTT_DEV             RTC->MODE0
+#define RTT_IRQ             RTC_IRQn
+#define RTT_ISR             isr_rtc
+
+#define RTT_RUNSTDBY        (1)         /* Keep RTT running in sleep states */
+
+#define RTT_MAX_VALUE       (0xffffffff)
+#define RTT_CLOCK_FREQUENCY (32768U)                      /* in Hz */
+#define RTT_MIN_FREQUENCY   (RTT_CLOCK_FREQUENCY / 1024U) /* in Hz */
+#define RTT_MAX_FREQUENCY   (RTT_CLOCK_FREQUENCY)         /* in Hz */
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
