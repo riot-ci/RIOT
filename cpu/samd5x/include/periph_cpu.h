00805f780518ce5915b253b561c022eb760b0355
Francisco Molina <femolina@uc.cl>
2020-06-17 12:42:39 +0200

    cpu/samd5x: add unified rtt configuration

--- cpu/samd5x/include/periph_cpu.h
+++ cpu/samd5x/include/periph_cpu.h
@@ -79,6 +79,20 @@ enum {
  */
 #define DAC_NUMOF           (2)
 
+/**
+ * @name    Real time counter configuration
+ * @{
+ */
+#define RTT_DEV             RTC->MODE0
+#define RTT_IRQ             RTC_IRQn
+#define RTT_ISR             isr_rtc
+
+#define RTT_MAX_VALUE       (0xffffffff)
+#define RTT_CLOCK_FREQUENCY (32768U)                      /* in Hz */
+#define RTT_MIN_FREQUENCY   (RTT_CLOCK_FREQUENCY / 1024U) /* in Hz */
+#define RTT_MAX_FREQUENCY   (RTT_CLOCK_FREQUENCY)         /* in Hz */
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
