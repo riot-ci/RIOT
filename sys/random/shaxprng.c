49cb508d61f12d745c35356d5bbef354cb35209f
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2020-07-09 11:46:12 +0200

    sys/random: make sha1prng sha256 compatible

--- sys/random/shaxprng.c
+++ sys/random/shaxprng.c
@@ -14,7 +14,9 @@
  * @{
  * @file
  *
- * @brief   SHA1PRNG random number generator implementation
+ * @brief   SHA based PRNG random number generator implementation
+ *
+ * BLABLA
  *
  * @author Peter Kietzmann <peter.kietzmann@haw-hamburg.de>
  * @}
@@ -24,10 +26,52 @@
 #include <string.h>
 
 #include "hashes/sha1.h"
+#include "hashes/sha256.h"
+#include "kernel_defines.h"
 
-#define STATE_SIZE           (SHA1_DIGEST_LENGTH)
+#if IS_ACTIVE(MODULE_PRNG_SHA1PRNG)
+/* state size is digset length of SHA-1 */
+#define STATE_SIZE                      (SHA1_DIGEST_LENGTH)
+typedef sha1_context shax_context_t;
+#elif IS_ACTIVE(MODULE_PRNG_SHA256PRNG)
+/* state size is digest length of SHA-256 */
+#define STATE_SIZE                      (SHA256_DIGEST_LENGTH)
+typedef sha256_context_t shax_context_t;
+#endif
 
-static sha1_context ctx;
+static inline void _shax_init(shax_context_t *ctx)
+{
+    if (IS_ACTIVE(MODULE_PRNG_SHA1PRNG)) {
+        sha1_init((sha1_context *)ctx);
+    }
+    else if (IS_ACTIVE(MODULE_PRNG_SHA256PRNG)) {
+        sha256_init((sha256_context_t *)ctx);
+    }
+}
+
+static inline void _shax_update(shax_context_t *ctx, const void *data, size_t len)
+{
+    if (IS_ACTIVE(MODULE_PRNG_SHA1PRNG)) {
+        sha1_update((sha1_context *)ctx, data, len);
+    }
+    else if (IS_ACTIVE(MODULE_PRNG_SHA256PRNG)) {
+        sha256_update((sha256_context_t *)ctx, data, len);
+    }
+}
+
+static inline void _shax_final(shax_context_t *ctx, void *digest)
+{
+    if (IS_ACTIVE(MODULE_PRNG_SHA1PRNG)) {
+        sha1_final((sha1_context *)ctx, digest);
+    }
+    else if (IS_ACTIVE(MODULE_PRNG_SHA256PRNG)) {
+        sha256_final((sha256_context_t *)ctx, digest);
+    }
+}
+
+/* allocate SHA context */
+static shax_context_t ctx;
+
 static uint32_t datapos = STATE_SIZE;
 static int8_t digestdata[STATE_SIZE];
 static int8_t prng_state[STATE_SIZE];
@@ -88,14 +132,14 @@ void _random_bytes(uint8_t *bytes, size_t size) /* TODO: use with global API */
         /* no out data ready, (re)fill internal buffer */
         else
         {
-            /* reset SHA1 internal state */
-            sha1_init(&ctx);
+            /* reset SHA internal state */
+            _shax_init(&ctx);
 
-            /* update SHA1 internal state with PRNG state */
-            sha1_update(&ctx, (void *)prng_state, sizeof(prng_state));
+            /* update SHA internal state with PRNG state */
+            _shax_update(&ctx, (void *)prng_state, sizeof(prng_state));
 
             /* get the digest */
-            sha1_final(&ctx, digestdata);
+            _shax_final(&ctx, digestdata);
 
             /* update PRNG state for next round */
             _updatestate(prng_state);
@@ -108,9 +152,9 @@ void _random_bytes(uint8_t *bytes, size_t size) /* TODO: use with global API */
 
 void random_init_by_array(uint32_t init_key[], int key_length)
 {
-    sha1_init(&ctx);
-    sha1_update(&ctx, (void *)init_key, key_length);
-    sha1_final(&ctx, digestdata);
+    _shax_init(&ctx);
+    _shax_update(&ctx, (void *)init_key, key_length);
+    _shax_final(&ctx, digestdata);
 
     /* copy seeded SHA1 state to PRNG state */
     memcpy(prng_state, &ctx.state, STATE_SIZE);
