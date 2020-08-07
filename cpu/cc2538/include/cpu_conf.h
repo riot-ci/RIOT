674a31de48697472760bf40ba31db71ceae161ee
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-08-07 17:42:25 +0200

    cpu/cc2538: set CPU_HAS_BITBAND

--- cpu/cc2538/include/cpu_conf.h
+++ cpu/cc2538/include/cpu_conf.h
@@ -42,6 +42,15 @@ extern "C" {
 #define CPU_FLASH_BASE                  FLASH_BASE
 /** @} */
 
+/**
+￼ * @brief   Bit-Band configuration
+￼ * @{
+￼ */
+#ifdef SRAM_BB_BASE
+#define CPU_HAS_BITBAND 1
+#endif
+/** @} */
+
 #ifdef __cplusplus
 } /* end extern "C" */
 #endif
