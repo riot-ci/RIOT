2ee0ef84b4c18a3a31a5bd4fd144d28efb17feec
János Brodbeck <janos.brodbeck@posteo.de>
2020-08-07 19:37:12 +0200

    sam0/adc: add support for samd5x/same5x
    Add samd5x/same5x support  through introducing ADC_DEV as alias for ADC0/ADC1/ADC. ADC (respectively ADC0) is the default if no device is set.

--- cpu/sam0_common/periph/adc.c
+++ cpu/sam0_common/periph/adc.c
@@ -28,9 +28,25 @@
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
+/* The SAMD5x/SAME5x family has two ADCs: ADC0 and ADC1.
+ * Introducing ADC_DEV as alias for the respective device (ADC/ADC0/ADC1). */
+#ifndef ADC_DEV
+    #ifdef ADC0
+        #define ADC_DEV ADC0
+    #else
+        #define ADC_DEV ADC
+    #endif
+#endif
+
+#ifndef ADC_GCLK_SRC
+    #define ADC_GCLK_SRC SAM0_GCLK_MAIN
+#endif
+
 /* Prototypes */
 static bool _adc_syncing(void);
 static void _adc_poweroff(void);
+static void _setup_clock(adc_res_t res);
+static void _setup_calibration(void);
 static int _adc_configure(adc_res_t res);
 
 static mutex_t _lock = MUTEX_INIT;
@@ -48,11 +64,11 @@ static inline void _done(void)
 static bool _adc_syncing(void)
 {
 #ifdef CPU_SAMD21
-    if (ADC->STATUS.reg & ADC_STATUS_SYNCBUSY) {
+    if (ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY) {
         return true;
     }
 #else /* CPU_SAML21 */
-    if (ADC->SYNCBUSY.reg) {
+    if (ADC_DEV->SYNCBUSY.reg) {
         return true;
     }
 #endif
@@ -63,7 +79,7 @@ static void _adc_poweroff(void)
 {
     while (_adc_syncing()) {}
     /* Disable */
-    ADC->CTRLA.reg &= ~ADC_CTRLA_ENABLE;
+    ADC_DEV->CTRLA.reg &= ~ADC_CTRLA_ENABLE;
     while (_adc_syncing()) {}
     /* Disable bandgap */
 #ifdef CPU_SAMD21
@@ -77,6 +93,75 @@ static void _adc_poweroff(void)
 #endif
 }
 
+static void _setup_clock(adc_res_t res)
+{
+#ifdef CPU_SAMD21
+ /* GCLK Setup */
+    GCLK->CLKCTRL.reg = (uint32_t)(GCLK_CLKCTRL_CLKEN
+            | GCLK_CLKCTRL_GEN(ADC_GCLK_SRC) | (GCLK_CLKCTRL_ID(ADC_GCLK_ID)));
+    /* Configure CTRLB Register HERE IS THE RESOLUTION SET! */
+    ADC_DEV->CTRLB.reg = ADC_PRESCALER | res;
+#else
+#ifdef ADC0_GCLK_ID
+    /* GCLK Setup */
+    if (ADC_DEV == ADC0) {
+        GCLK->PCHCTRL[ADC0_GCLK_ID].reg = GCLK_PCHCTRL_CHEN
+                | GCLK_PCHCTRL_GEN(ADC_GCLK_SRC);
+    }
+    else {
+        GCLK->PCHCTRL[ADC1_GCLK_ID].reg = GCLK_PCHCTRL_CHEN
+                | GCLK_PCHCTRL_GEN(ADC_GCLK_SRC);
+    }
+    /* Configure CTRLA & CTRLB Register */
+    ADC_DEV->CTRLA.reg = ADC_PRESCALER;
+    ADC_DEV->CTRLB.reg |= res;
+#else
+    /* GCLK Setup */
+    GCLK->PCHCTRL[ADC_GCLK_ID].reg = GCLK_PCHCTRL_CHEN
+            | GCLK_PCHCTRL_GEN(ADC_GCLK_SRC);
+    /* Configure CTRLB & CTRLC Register */
+    ADC_DEV->CTRLB.reg = ADC_PRESCALER;
+    ADC_DEV->CTRLC.reg |= res;
+#endif
+#endif
+}
+
+static void _setup_calibration(void)
+{
+#ifdef CPU_SAMD21
+    /* Load the fixed device calibration constants */
+    ADC_DEV->CALIB.reg =
+        ADC_CALIB_BIAS_CAL((*(uint32_t*)ADC_FUSES_BIASCAL_ADDR >>
+                            ADC_FUSES_BIASCAL_Pos)) |
+        ADC_CALIB_LINEARITY_CAL((*(uint64_t*)ADC_FUSES_LINEARITY_0_ADDR >>
+                                ADC_FUSES_LINEARITY_0_Pos));
+#else
+    /* Set default calibration from NVM */
+#ifdef ADC0_FUSES_BIASCOMP_ADDR
+    if (ADC_DEV == ADC0) {
+        ADC_DEV->CALIB.reg =
+            ADC0_FUSES_BIASCOMP((*(uint32_t*)ADC0_FUSES_BIASCOMP_ADDR)) >>
+            ADC_CALIB_BIASCOMP_Pos |
+            ADC0_FUSES_BIASREFBUF((*(uint32_t*)ADC0_FUSES_BIASREFBUF_ADDR) >>
+            ADC0_FUSES_BIASREFBUF_Pos);
+    }
+    else {
+        ADC_DEV->CALIB.reg =
+            ADC1_FUSES_BIASCOMP((*(uint32_t*)ADC1_FUSES_BIASCOMP_ADDR)) >>
+            ADC_CALIB_BIASCOMP_Pos |
+            ADC1_FUSES_BIASREFBUF((*(uint32_t*)ADC1_FUSES_BIASREFBUF_ADDR) >>
+            ADC1_FUSES_BIASREFBUF_Pos);
+    }
+#else
+    ADC_DEV->CALIB.reg =
+            ADC_FUSES_BIASCOMP((*(uint32_t*)ADC_FUSES_BIASCOMP_ADDR)) >>
+            ADC_CALIB_BIASCOMP_Pos |
+            ADC_FUSES_BIASREFBUF((*(uint32_t*)ADC_FUSES_BIASREFBUF_ADDR) >>
+            ADC_FUSES_BIASREFBUF_Pos);
+#endif
+#endif
+}
+
 static int _adc_configure(adc_res_t res)
 {
     /* Individual comparison necessary because ADC Resolution Bits are not
@@ -87,29 +172,20 @@ static int _adc_configure(adc_res_t res)
         return -1;
     }
     _adc_poweroff();
-    if (ADC->CTRLA.reg & ADC_CTRLA_SWRST ||
-        ADC->CTRLA.reg & ADC_CTRLA_ENABLE ) {
+    if (ADC_DEV->CTRLA.reg & ADC_CTRLA_SWRST ||
+        ADC_DEV->CTRLA.reg & ADC_CTRLA_ENABLE ) {
         DEBUG("adc: not ready\n");
         return -1;
     }
 #ifdef CPU_SAMD21
     /* Power On */
     PM->APBCMASK.reg |= PM_APBCMASK_ADC;
-    /* GCLK Setup */
-    GCLK->CLKCTRL.reg = (uint32_t)(GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 |
-                        (GCLK_CLKCTRL_ID(ADC_GCLK_ID)));
-    /* Configure CTRLB Register HERE IS THE RESOLUTION SET! */
-    ADC->CTRLB.reg = ADC_PRESCALER | res;
-    /* Load the fixed device calibration constants */
-    ADC->CALIB.reg =
-        ADC_CALIB_BIAS_CAL((*(uint32_t*)ADC_FUSES_BIASCAL_ADDR >>
-                            ADC_FUSES_BIASCAL_Pos)) |
-        ADC_CALIB_LINEARITY_CAL((*(uint64_t*)ADC_FUSES_LINEARITY_0_ADDR >>
-                                ADC_FUSES_LINEARITY_0_Pos));
+    _setup_clock(res);
+    _setup_calibration();
     /* Set Voltage Reference */
-    ADC->REFCTRL.reg = ADC_REF_DEFAULT;
+    ADC_DEV->REFCTRL.reg = ADC_REF_DEFAULT;
     /* Disable all interrupts */
-    ADC->INTENCLR.reg = (ADC_INTENCLR_SYNCRDY) | (ADC_INTENCLR_WINMON) |
+    ADC_DEV->INTENCLR.reg = (ADC_INTENCLR_SYNCRDY) | (ADC_INTENCLR_WINMON) |
                               (ADC_INTENCLR_OVERRUN) | (ADC_INTENCLR_RESRDY);
     while (_adc_syncing()) {}
     /* Enable bandgap if VREF is internal 1V */
@@ -121,24 +197,23 @@ static int _adc_configure(adc_res_t res)
 #ifdef CPU_SAML1X
     MCLK->APBCMASK.reg |= MCLK_APBCMASK_ADC;
 #else
+#ifdef MCLK_APBDMASK_ADC0
+    if (ADC_DEV == ADC0) {
+        MCLK->APBDMASK.reg |= MCLK_APBDMASK_ADC0;
+    } else {
+        MCLK->APBDMASK.reg |= MCLK_APBDMASK_ADC1;
+    }
+#else
     MCLK->APBDMASK.reg |= MCLK_APBDMASK_ADC;
 #endif
-    /* GCLK Setup */
-    GCLK->PCHCTRL[ADC_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK0;
+#endif
+    _setup_clock(res);
+    _setup_calibration();
     /* Set Voltage Reference */
-    ADC->REFCTRL.reg = ADC_REF_DEFAULT;
-    /* Configure CTRLB & CTRLC Register */
-    ADC->CTRLB.reg = ADC_PRESCALER;
-    ADC->CTRLC.reg |= res;
+    ADC_DEV->REFCTRL.reg = ADC_REF_DEFAULT;
     /* Disable all interrupts */
-    ADC->INTENCLR.reg = ADC_INTENCLR_WINMON | ADC_INTENCLR_OVERRUN |
+    ADC_DEV->INTENCLR.reg = ADC_INTENCLR_WINMON | ADC_INTENCLR_OVERRUN |
                               ADC_INTENCLR_RESRDY;
-    /* Set default calibration from NVM */
-    ADC->CALIB.reg =
-            ADC_FUSES_BIASCOMP((*(uint32_t*)ADC_FUSES_BIASCOMP_ADDR)) >>
-            ADC_CALIB_BIASCOMP_Pos |
-            ADC_FUSES_BIASREFBUF((*(uint32_t*)ADC_FUSES_BIASREFBUF_ADDR) >>
-            ADC_FUSES_BIASREFBUF_Pos);
     while (_adc_syncing()) {}
     /* Enable bandgap if necessary */
     if (ADC_REF_DEFAULT == ADC_REFCTRL_REFSEL_INTREF) {
@@ -146,7 +221,7 @@ static int _adc_configure(adc_res_t res)
     }
 #endif
     /*  Enable ADC Module */
-    ADC->CTRLA.reg |= ADC_CTRLA_ENABLE;
+    ADC_DEV->CTRLA.reg |= ADC_CTRLA_ENABLE;
     while (_adc_syncing()) {}
     return 0;
 }
@@ -177,17 +252,17 @@ int32_t adc_sample(adc_t line, adc_res_t res)
         return -1;
     }
 #ifdef CPU_SAMD21
-    ADC->INPUTCTRL.reg = ADC_GAIN_FACTOR_DEFAULT |
+    ADC_DEV->INPUTCTRL.reg = ADC_GAIN_FACTOR_DEFAULT |
                          adc_channels[line].muxpos | ADC_NEG_INPUT;
 #else /* CPU_SAML21 */
-    ADC->INPUTCTRL.reg = adc_channels[line].muxpos | ADC_NEG_INPUT;
+    ADC_DEV->INPUTCTRL.reg = adc_channels[line].muxpos | ADC_NEG_INPUT;
 #endif
     while (_adc_syncing()) {}
     /* Start the conversion */
-    ADC->SWTRIG.reg = ADC_SWTRIG_START;
+    ADC_DEV->SWTRIG.reg = ADC_SWTRIG_START;
     /* Wait for the result */
-    while (!(ADC->INTFLAG.reg & ADC_INTFLAG_RESRDY)) {}
-    int result = ADC->RESULT.reg;
+    while (!(ADC_DEV->INTFLAG.reg & ADC_INTFLAG_RESRDY)) {}
+    int result = ADC_DEV->RESULT.reg;
     _adc_poweroff();
     _done();
     return result;
