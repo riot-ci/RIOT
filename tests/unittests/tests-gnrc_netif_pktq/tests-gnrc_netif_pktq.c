9898a5e2fd08a08dce068844fc1db5e73bb23744
Martine S. Lenders <m.lenders@fu-berlin.de>
2020-08-26 13:23:39 +0200

    fixup! tests: provide unittests for gnrc_netif_pktq

--- tests/unittests/tests-gnrc_netif_pktq/tests-gnrc_netif_pktq.c
+++ tests/unittests/tests-gnrc_netif_pktq/tests-gnrc_netif_pktq.c
@@ -36,7 +36,7 @@ static void test_pktq_put__full(void)
 {
     gnrc_pktsnip_t pkt;
 
-    for (unsigned i = 0; i < GNRC_NETIF_PKTQ_POOL_SIZE; i++) {
+    for (unsigned i = 0; i < CONFIG_GNRC_NETIF_PKTQ_POOL_SIZE; i++) {
         TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_put(&_netif, &pkt));
     }
     TEST_ASSERT_EQUAL_INT(-1, gnrc_netif_pktq_put(&_netif, &pkt));
@@ -66,6 +66,55 @@ static void test_pktq_put_get3(void)
     }
 }
 
+static void test_pktq_push_back__full(void)
+{
+    gnrc_pktsnip_t pkt;
+
+    for (unsigned i = 0; i < CONFIG_GNRC_NETIF_PKTQ_POOL_SIZE; i++) {
+        TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_put(&_netif, &pkt));
+    }
+    TEST_ASSERT_EQUAL_INT(-1, gnrc_netif_pktq_push_back(&_netif, &pkt));
+}
+
+static void test_pktq_push_back_get1(void)
+{
+    gnrc_pktsnip_t pkt_in, *pkt_out;
+
+    TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_push_back(&_netif, &pkt_in));
+    TEST_ASSERT_NOT_NULL((pkt_out = gnrc_netif_pktq_get(&_netif)));
+    TEST_ASSERT(&pkt_in == pkt_out);
+}
+
+static void test_pktq_push_back_get3(void)
+{
+    gnrc_pktsnip_t pkt_in[3];
+
+    for (unsigned i = 0; i < 3; i++) {
+        TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_push_back(&_netif, &pkt_in[i]));
+    }
+    for (unsigned i = 0; i < 3; i++) {
+        gnrc_pktsnip_t *pkt_out;
+
+        TEST_ASSERT_NOT_NULL((pkt_out = gnrc_netif_pktq_get(&_netif)));
+        TEST_ASSERT(&pkt_in[3 - i - 1] == pkt_out);
+    }
+}
+
+static void test_pktq_empty(void)
+{
+    gnrc_pktsnip_t pkt_in;
+
+    TEST_ASSERT(gnrc_netif_pktq_empty(&_netif));
+    TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_put(&_netif, &pkt_in));
+    TEST_ASSERT(!gnrc_netif_pktq_empty(&_netif));
+    TEST_ASSERT_NOT_NULL(gnrc_netif_pktq_get(&_netif));
+    TEST_ASSERT(gnrc_netif_pktq_empty(&_netif));
+    TEST_ASSERT_EQUAL_INT(0, gnrc_netif_pktq_push_back(&_netif, &pkt_in));
+    TEST_ASSERT(!gnrc_netif_pktq_empty(&_netif));
+    TEST_ASSERT_NOT_NULL(gnrc_netif_pktq_get(&_netif));
+    TEST_ASSERT(gnrc_netif_pktq_empty(&_netif));
+}
+
 static Test *test_gnrc_netif_pktq(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
@@ -73,6 +122,10 @@ static Test *test_gnrc_netif_pktq(void)
         new_TestFixture(test_pktq_put__full),
         new_TestFixture(test_pktq_put_get1),
         new_TestFixture(test_pktq_put_get3),
+        new_TestFixture(test_pktq_push_back__full),
+        new_TestFixture(test_pktq_push_back_get1),
+        new_TestFixture(test_pktq_push_back_get3),
+        new_TestFixture(test_pktq_empty),
     };
 
     EMB_UNIT_TESTCALLER(pktq_tests, set_up, NULL, fixtures);
