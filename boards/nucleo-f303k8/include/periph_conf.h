bb097d0be26452a6114705003521e53de9034105
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-08-30 12:30:50 +0200

    boards/nucleo-f3xx: use shared default clock configuration

--- boards/nucleo-f303k8/include/periph_conf.h
+++ boards/nucleo-f303k8/include/periph_conf.h
@@ -19,7 +19,15 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
+/* No HSE available for this board */
+#define CLOCK_HSE           (0U)
+
+/* Adjust PLL prescalers to reach 72MHz sysclock */
+#define CLOCK_PLL_PREDIV    (2)
+#define CLOCK_PLL_MUL       (16)
+
 #include "periph_cpu.h"
+#include "f1f3/cfg_clock_default.h"
 #include "cfg_timer_tim2.h"
 
 #ifdef __cplusplus
