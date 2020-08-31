b3a60a0f88c5d598ef0e061f1d67dc5e59c69a86
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-08-31 10:47:09 +0200

    boards: cpu: stm32f1/f3: rework clock configuration and init

--- cpu/stm32/stmclk/stmclk_f0f1f3.c
+++ cpu/stm32/stmclk/stmclk_f0f1f3.c
@@ -13,7 +13,7 @@
  * @{
  *
  * @file
- * @brief       Implementation of STM32 clock configuration for STM32 F0
+ * @brief       Implementation of STM32 clock configuration for STM32 F0/F1/F3
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      Vincent Dupont <vincent@otakeys.com>
@@ -26,17 +26,31 @@
 #include "periph_conf.h"
 
 /* PLL configuration */
+#if defined(CPU_FAM_STM32F1)
 #if IS_ACTIVE(CONFIG_BOARD_HAS_HSE)
+#define PLL_SRC                 (RCC_CFGR_PLLSRC) /* HSE */
+#else
+#define PLL_SRC                 (0) /* HSI / 2 */
+#endif
+
+/* RCC_CR_HSITRIM_4 is not defined on stm32f1 and corresponds to a value of 16.
+   Use the same definition used for stm32f0/stm32f3 */
+#define RCC_CR_HSITRIM_4        (0x10UL << RCC_CR_HSITRIM_Pos)
+#define RCC_CFGR_PLLMUL         RCC_CFGR_PLLMULL
+#else
+#if CONFIG_BOARD_HAS_HSE
 #define PLL_SRC                 (RCC_CFGR_PLLSRC_HSE_PREDIV | RCC_CFGR_PLLXTPRE_HSE_PREDIV_DIV1)
 #else
 #define PLL_SRC                 (RCC_CFGR_PLLSRC_HSI_DIV2)
 #endif
+#endif /* CPU_FAM_STM32F1 */
 
-#define PLL_MUL                 ((CONFIG_CLOCK_PLL_MUL - 2) << RCC_CFGR_PLLMUL_Pos)
+#define PLL_MUL                 ((CONFIG_CLOCK_PLL_MUL - 2) << RCC_CFGR_PLLMULL_Pos)
 #define PLL_PREDIV              (CONFIG_CLOCK_PLL_PREDIV - 1)
 
 #define CLOCK_AHB_DIV           (RCC_CFGR_HPRE_DIV1)
 
+#if defined(CPU_FAM_STM32F0)
 #if CONFIG_CLOCK_APB1_DIV == 1
 #define CLOCK_APB1_DIV          (RCC_CFGR_PPRE_DIV1)
 #elif CONFIG_CLOCK_APB1_DIV == 2
@@ -49,6 +63,35 @@
 #define CLOCK_APB1_DIV          (RCC_CFGR_PPRE_DIV16)
 #else
 #error "Invalid APB prescaler value (only 1, 2, 4, 8 and 16 allowed)"
+#endif
+#else
+#if CONFIG_CLOCK_APB1_DIV == 1
+#define CLOCK_APB1_DIV          (RCC_CFGR_PPRE1_DIV1)
+#elif CONFIG_CLOCK_APB1_DIV == 2
+#define CLOCK_APB1_DIV          (RCC_CFGR_PPRE1_DIV2)
+#elif CONFIG_CLOCK_APB1_DIV == 4
+#define CLOCK_APB1_DIV          (RCC_CFGR_PPRE1_DIV4)
+#elif CONFIG_CLOCK_APB1_DIV == 8
+#define CLOCK_APB1_DIV          (RCC_CFGR_PPRE1_DIV8)
+#elif CONFIG_CLOCK_APB1_DIV == 16
+#define CLOCK_APB1_DIV          (RCC_CFGR_PPRE1_DIV16)
+#else
+#error "Invalid APB1 prescaler value (only 1, 2, 4, 8 and 16 allowed)"
+#endif
+
+#if CONFIG_CLOCK_APB2_DIV == 1
+#define CLOCK_APB2_DIV          (RCC_CFGR_PPRE2_DIV1)
+#elif CONFIG_CLOCK_APB2_DIV == 2
+#define CLOCK_APB2_DIV          (RCC_CFGR_PPRE2_DIV2)
+#elif CONFIG_CLOCK_APB2_DIV == 4
+#define CLOCK_APB2_DIV          (RCC_CFGR_PPRE2_DIV4)
+#elif CONFIG_CLOCK_APB2_DIV == 8
+#define CLOCK_APB2_DIV          (RCC_CFGR_PPRE2_DIV8)
+#elif CONFIG_CLOCK_APB2_DIV == 16
+#define CLOCK_APB2_DIV          (RCC_CFGR_PPRE2_DIV16)
+#else
+#error "Invalid APB2 prescaler value (only 1, 2, 4, 8 and 16 allowed)"
+#endif
 #endif
 
 
@@ -71,7 +114,11 @@ void stmclk_init_sysclk(void)
 
     /* use HSI as system clock while we do any further configuration and
      * configure the AHB and APB clock dividers as configure by the board */
+#if defined(CPU_FAM_STM32F0)
     RCC->CFGR = (RCC_CFGR_SW_HSI | CLOCK_AHB_DIV | CLOCK_APB1_DIV);
+#else /* CPU_FAM_STM32F1 && CPU_FAM_STM32F3 */
+    RCC->CFGR = (RCC_CFGR_SW_HSI | CLOCK_AHB_DIV | CLOCK_APB1_DIV | CLOCK_APB2_DIV);
+#endif /* CPU_FAM_STM32F0*/
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI) {}
 
     /* Flash config */
@@ -100,9 +147,11 @@ void stmclk_init_sysclk(void)
         if (CONFIG_CLOCK_PLL_PREDIV == 2) {
             RCC->CFGR |= RCC_CFGR_PLLXTPRE; /* PREDIV == 2 */
         }
+#if !defined(CPU_FAM_STM32F1)
         else if (CONFIG_CLOCK_PLL_PREDIV > 2) {
             RCC->CFGR2 = PLL_PREDIV;        /* PREDIV > 2 */
         }
+#endif
         RCC->CR |= (RCC_CR_PLLON);
         while (!(RCC->CR & RCC_CR_PLLRDY)) {}
 
