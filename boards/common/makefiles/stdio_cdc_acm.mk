01f023d36942624a8e8fc392f4372026332cde7d
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-09-04 16:13:22 +0200

    boards/common/nrf52: add common config for stdio via CDC ACM

---
+++ boards/common/makefiles/stdio_cdc_acm.mk
@@ -0,0 +1,4 @@
+ifeq (,$(filter-out stdio_cdc_acm,$(filter stdio_% slipdev_stdio,$(USEMODULE))))
+  # Use stdio_cdc_acm only if no other stdio is requested explicitly.
+  USEMODULE += stdio_cdc_acm
+endif
---
+++ boards/common/nrf52/bootloader_nrfutil.dep.mk
@@ -0,0 +1,7 @@
+include $(RIOTBOARD)/common/makefiles/stdio_cdc_acm.mk
+
+# enable bootloader reset over USB, requires USB bootloader to be used
+ifneq (,$(filter stdio_cdc_acm,$(USEMODULE)))
+  FEATURES_REQUIRED += $(BOOTLOADER_TYPE)
+  USEMODULE += usb_board_reset
+endif
