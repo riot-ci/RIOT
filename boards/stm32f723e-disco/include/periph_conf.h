da1376ba3988d3f16390da7262bbdee39fb104b0
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-09-04 08:12:34 +0200

    boards/stm32f7*: use new clock configuration scheme

--- boards/stm32f723e-disco/include/periph_conf.h
+++ boards/stm32f723e-disco/include/periph_conf.h
@@ -19,7 +19,21 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
+/* This board provides an LSE */
+#ifndef CONFIG_BOARD_HAS_LSE
+#define CONFIG_BOARD_HAS_LSE    1
+#endif
+
+/* This board provides an HSE */
+#ifndef CONFIG_BOARD_HAS_HSE
+#define CONFIG_BOARD_HAS_HSE    1
+#endif
+
+/* The HSE provides a 25MHz clock */
+#define CLOCK_HSE               MHZ(25)
+
 #include "periph_cpu.h"
+#include "f2f4f7/cfg_clock_default_216.h"
 #include "cfg_rtt_default.h"
 #include "cfg_usb_otg_fs.h"
 
