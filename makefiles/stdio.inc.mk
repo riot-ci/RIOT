0ca571b76e0c7752961ab06bfc7689ed0caecfa9
Koen Zandberg <koen@bergzand.net>
2020-09-09 11:32:30 +0200

    stdio_semihosting: Extend with RISC-V support
    RISC-V support semihosting in very similar way as the cortex-m
    microcontrollers. The code calls a breakpoint instruction and the
    attached debugger reads/writes registers and memory for stdio.
    
    The RISC-V architecture doesn't support a call number with the EBREAK
    instruction, to allow the debugger to detect a semihosting break point,
    the EBREAK instruction is wrapped in a SLLI and SRAI instruction. These
    use x0 as output register, making them NOP instructions.
    
    One caveat when using this is that the RISC-V core traps the EBREAK
    instruction with trap code 3 when no debugger is attached. Restarting
    the application with the debugger attached avoids this.

--- makefiles/stdio.inc.mk
+++ makefiles/stdio.inc.mk
@@ -46,7 +46,7 @@ endif
 
 ifneq (,$(filter stdio_semihosting,$(USEMODULE)))
   USEMODULE += xtimer
-  FEATURES_REQUIRED += cpu_core_cortexm
+  FEATURES_REQUIRED_ANY += arch_arm|arch_riscv
 endif
 
 # enable stdout buffering for modules that benefit from sending out buffers in larger chunks
