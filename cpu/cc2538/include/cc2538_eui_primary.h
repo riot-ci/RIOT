c58ecad1bb8686ad7d78808c05969953556d9181
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-09-11 13:25:51 +0200

    fixup! cc2538_rf: add EUI provoder for primary EUI-64

--- cpu/cc2538/include/cc2538_eui_primary.h
+++ cpu/cc2538/include/cc2538_eui_primary.h
@@ -44,14 +44,14 @@ static inline int cc2538_get_eui64_primary(const void *arg, eui64_t *addr)
      * The primary EUI-64 seems to be written to memory in a non-sequential
      * byte order, with both 4-byte halves of the address flipped.
      */
-    addr->uint8[0] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[4];
-    addr->uint8[1] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[5];
-    addr->uint8[2] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[6];
-    addr->uint8[3] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[7];
-    addr->uint8[4] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[0];
-    addr->uint8[5] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[1];
-    addr->uint8[6] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[2];
-    addr->uint8[7] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[3];
+    addr->uint8[7] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[4];
+    addr->uint8[6] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[5];
+    addr->uint8[5] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[6];
+    addr->uint8[4] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[7];
+    addr->uint8[3] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[0];
+    addr->uint8[2] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[1];
+    addr->uint8[1] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[2];
+    addr->uint8[0] = ((uint8_t*)CC2538_EUI64_LOCATION_PRI)[3];
 
     return 0;
 }
