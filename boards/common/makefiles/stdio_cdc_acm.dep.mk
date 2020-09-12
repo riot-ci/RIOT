95601edb22dbed8c90c5626278c5c3b550b33ddd
Benjamin Valentin <benpicco@googlemail.com>
2020-09-13 00:24:45 +0200

    boards: replace bootloader_% feature with stdio_cdc_acm

--- boards/common/makefiles/stdio_cdc_acm.dep.mk
+++ boards/common/makefiles/stdio_cdc_acm.dep.mk
@@ -1,4 +1,5 @@
 ifeq (,$(filter-out stdio_cdc_acm,$(filter stdio_% slipdev_stdio,$(USEMODULE))))
   # Use stdio_cdc_acm only if no other stdio is requested explicitly.
+  FEATURES_REQUIRED += stdio_cdc_acm
   USEMODULE += stdio_cdc_acm
 endif
--- boards/common/nrf52/bootloader_nrfutil.dep.mk
+++ boards/common/nrf52/bootloader_nrfutil.dep.mk
@@ -2,6 +2,5 @@ include $(RIOTBOARD)/common/makefiles/stdio_cdc_acm.dep.mk
 
 # enable bootloader reset over USB, requires USB bootloader to be used
 ifneq (,$(filter stdio_cdc_acm,$(USEMODULE)))
-  FEATURES_REQUIRED += $(BOOTLOADER_TYPE)
   USEMODULE += usb_board_reset
 endif
