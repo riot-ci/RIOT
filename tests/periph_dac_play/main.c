60aa106aff0aab8f68756317d318a2d7834f6d3f
Benjamin Valentin <benpicco@beuth-hochschule.de>
2020-09-22 23:54:05 +0200

    fixup! fixup! fixup! fixup! 	tests/periph_dac_play: add test for dac_play()

--- tests/periph_dac_play/main.c
+++ tests/periph_dac_play/main.c
@@ -49,6 +49,7 @@
 #define ISIN_PERIOD     (0x7FFF)
 #define ISIN_MAX        (0x1000)
 
+static bool res_16b = 0;
 static unsigned sample_rate = 8000;
 
 /**
@@ -85,20 +86,35 @@ static void _unlock(void *arg)
     mutex_unlock(arg);
 }
 
-static size_t _fill_sine_samples(uint8_t *buf, uint16_t period,
-                               uint16_t *x, size_t len)
-{
-    len = min(len, DAC_BUF_SIZE);
+typedef uint16_t (*sample_gen_t)(uint8_t *dst, size_t len,
+                                 uint16_t period, uint16_t x);
 
+static uint16_t _fill_sine_samples_8(uint8_t *buf, size_t len,
+                                     uint16_t period, uint16_t x)
+{
     for (uint16_t i = 0; i < len; ++i) {
-        *x += ISIN_PERIOD / period;
-        buf[i] = isin(*x & ISIN_PERIOD) >> 5;
+        x += ISIN_PERIOD / period;
+        buf[i] = isin(x & ISIN_PERIOD) >> 5;
     }
 
-    return len;
+    return x;
 }
 
-static void play_sine(uint16_t period, uint32_t samples)
+static uint16_t _fill_sine_samples_16(uint8_t *buf, size_t len,
+                                      uint16_t period, uint16_t x)
+{
+    for (uint16_t i = 0; i < len; ++i) {
+        x += ISIN_PERIOD / period;
+
+        uint16_t y = isin(x & ISIN_PERIOD);
+        buf[i]   = y;
+        buf[++i] = y >> 8;
+    }
+
+    return x;
+}
+
+static void play_function(uint16_t period, uint32_t samples, sample_gen_t fun)
 {
     static uint8_t buf[2][DAC_BUF_SIZE];
 
@@ -109,9 +125,11 @@ static void play_sine(uint16_t period, uint32_t samples)
     dac_play_set_cb(DAC_CHAN, _unlock, &lock);
 
     while (samples) {
-        samples -= _fill_sine_samples(buf[cur_buf], period, &x, samples);
+        size_t len = min(samples, DAC_BUF_SIZE);
+        samples -= len;
+        x = fun(buf[cur_buf], len, period, x);
 
-        dac_play(DAC_CHAN, buf[cur_buf], DAC_BUF_SIZE);
+        dac_play(DAC_CHAN, buf[cur_buf], len);
 
         mutex_lock(&lock);
         cur_buf = !cur_buf;
@@ -124,6 +142,10 @@ static int cmd_greeting(int argc, char **argv)
     (void) argc;
     (void) argv;
 
+    if (sample_rate != 8000 || res_16b) {
+        puts("Warning: audio clip was recoded with 8bit/8000 Hz");
+    }
+
     puts("Play Greeting…");
     dac_play(DAC_CHAN, hello_raw, hello_raw_len);
 
@@ -131,9 +153,43 @@ static int cmd_greeting(int argc, char **argv)
 }
 #endif
 
+static void _dac_init(void)
+{
+    printf("init DAC with %u bit, %u Hz\n", res_16b ? 16 : 8, sample_rate);
+    dac_play_init(DAC_CHAN, sample_rate,
+                  res_16b ? DAC_FLAG_16BIT : DAC_FLAG_8BIT, NULL, NULL);
+}
+
+static int cmd_init(int argc, char **argv)
+{
+    printf("argc: %d\n", argc);
+
+    if (argc < 2) {
+        printf("usage: %s <freq> <bit>\n", argv[0]);
+        return 1;
+    }
+
+    if (argc > 2) {
+        unsigned bit = atoi(argv[2]);
+
+        if (bit != 8 && bit != 16) {
+            printf("Only 8 and 16 bit samples supported.\n");
+            return 1;
+        }
+
+        res_16b = bit == 16;
+    }
+
+    sample_rate = atoi(argv[1]);
+
+    _dac_init();
+
+    return 0;
+}
+
 static int cmd_sine(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         printf("usage: %s <freq> <secs>\n", argv[0]);
         return 1;
     }
@@ -141,7 +197,8 @@ static int cmd_sine(int argc, char **argv)
     unsigned freq = atoi(argv[1]);
     unsigned secs = atoi(argv[2]);
 
-    play_sine((sample_rate + freq/2) / freq, secs * sample_rate);
+    play_function((sample_rate + freq/2) / freq, secs * sample_rate,
+                  res_16b ? _fill_sine_samples_16 : _fill_sine_samples_8);
 
     return 0;
 }
@@ -150,16 +207,15 @@ static const shell_command_t shell_commands[] = {
 #if ENABLE_GREETING
     { "hello", "Play Greeting", cmd_greeting },
 #endif
+    { "init", "Initialize DAC", cmd_init },
     { "sine", "Play Sine wave", cmd_sine },
     { NULL, NULL, NULL }
 };
 
 int main(void)
 {
-    printf("init DAC with 8 bit, %u samples / s\n", sample_rate);
-
     dac_init(DAC_CHAN);
-    dac_play_init(DAC_CHAN, sample_rate, DAC_FLAG_8BIT, NULL, NULL);
+    _dac_init();
 
     /* start the shell */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
