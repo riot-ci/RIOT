3f74b72d520dee81253c050f93bdabe85041ca73
Benjamin Valentin <benpicco@googlemail.com>
2020-09-26 14:01:58 +0200

    dist/tools/suit: convert to package
    Fetch the suit-manifest-generator from upstream instead of keeping
    a copy in the RIOT repository.

--- makefiles/suit.base.inc.mk
+++ makefiles/suit.base.inc.mk
@@ -24,7 +24,7 @@ SUIT_PUB_HDR_DIR = $(dir $(SUIT_PUB_HDR))
 CFLAGS += -I$(SUIT_PUB_HDR_DIR)
 BUILDDEPS += $(SUIT_PUB_HDR)
 
-$(SUIT_SEC): $(CLEAN)
+$(SUIT_SEC): $(CLEAN) $(SUIT_TOOL)
 	@echo suit: generating key in $(SUIT_KEY_DIR)
 	@mkdir -p $(SUIT_KEY_DIR)
 	@$(RIOTBASE)/dist/tools/suit/gen_key.py $(SUIT_SEC)
