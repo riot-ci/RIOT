21264a23560fd1ed0c4daa5949bd5cf5f2c4f4ae
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2020-09-29 10:03:54 +0200

    fixup! cpu/atmega_common: Add atomic_utils_arch.h

--- cpu/atmega_common/include/atomic_utils_arch.h
+++ cpu/atmega_common/include/atomic_utils_arch.h
@@ -25,6 +25,9 @@
 extern "C" {
 #endif
 
+/* clang provides no built-in atomic access to regular variables */
+#ifndef __clang__
+
 #define HAS_ATOMIC_LOAD_U8
 static inline uint8_t atomic_load_u8(const uint8_t *var)
 {
@@ -36,6 +39,8 @@ static inline void atomic_store_u8(uint8_t *dest, uint8_t val)
 {
     __atomic_store_1(dest, val, __ATOMIC_SEQ_CST);
 }
+
+#endif /* __clang__ */
 
 #ifdef __cplusplus
 }
