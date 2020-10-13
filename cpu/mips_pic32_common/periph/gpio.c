a7144cc34098c8b46766dba02a667f2f1f023af1
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-10-13 16:56:18 +0200

    fixup! cpu/mips: fix build error raised by a fallthrough warning

--- cpu/mips_pic32_common/periph/gpio.c
+++ cpu/mips_pic32_common/periph/gpio.c
@@ -201,13 +201,9 @@ int gpio_init(gpio_t pin, gpio_mode_t mode)
             pu = 1;
             break;
         case GPIO_OD_PU:
-            pu = 1;
-            output = 1;
-            break;
+            pu = 1; /* fall-through */
         case GPIO_OD:
-            od = 1;
-            output = 1;
-            break;
+            od = 1; /* fall-through */
         case GPIO_OUT:
             output = 1;
             break;
