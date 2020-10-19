8ac70f7d6b9d912ad7603c26180ffdce2c338e10
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-10-19 18:55:53 +0200

    tools/codespell: make it fail when typos are found

--- dist/tools/codespell/check.sh
+++ dist/tools/codespell/check.sh
@@ -56,8 +56,7 @@ if [ -n "${ERRORS}" ]
 then
     printf "%sThere are typos in the following files:%s\n\n" "${CERROR}" "${CRESET}"
     printf "%s\n" "${ERRORS}"
-    # TODO: return 1 when all typos are fixed
-    exit 0
+    exit 1
 else
     exit 0
 fi
