094c0dd51644a2975484dca9eaa3f70106dcce1f
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2020-11-22 23:50:51 +0100

    sys/net/gnrc/netif/init_devs: IEEE802154_STACKSIZE_DEFAULT

---
+++ sys/net/gnrc/netif/init_devs/include/init_devs.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_auto_init_gnrc_netif
+ * @{
+ *
+ * @file
+ * @brief       common netif device initialization definitions
+ *
+ * @author      Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef INIT_DEVS_H
+#define INIT_DEVS_H
+
+#include "thread.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   extra stack size if ieee802154 security is enables
+ */
+#define IEEE802154_SECURITY_EXTRA_STACKSIZE (32)
+
+#ifndef IEEE802154_STACKSIZE_DEFAULT
+#ifdef MODULE_IEEE802154_SECURITY
+#define IEEE802154_STACKSIZE_DEFAULT    (THREAD_STACKSIZE_DEFAULT + \
+                                        IEEE802154_SECURITY_EXTRA_STACKSIZE)
+#else
+/**
+ * @brief   stack size of an ieee802154 device
+ */
+#define IEEE802154_STACKSIZE_DEFAULT    (THREAD_STACKSIZE_DEFAULT)
+#endif
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INIT_DEVS_H */
+/** @} */
