66c11658819349d9211cf8f377aaad9508fd09dd
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-12-01 17:34:03 +0200

    cpu/nrf52: Fix issues that have been commented on

--- cpu/nrf52/periph/i2c.c
+++ cpu/nrf52/periph/i2c.c
@@ -99,23 +99,19 @@ void i2c_init(i2c_t dev)
     /* disable device during initialization, will be enabled when acquire is
      * called */
     bus(dev)->ENABLE = TWIM_ENABLE_ENABLE_Disabled;
-    /* configure pins */
-    gpio_init(i2c_config[dev].scl, GPIO_IN_OD_PU);
-    gpio_init(i2c_config[dev].sda, GPIO_IN_OD_PU);
-    bus(dev)->PSEL.SCL = i2c_config[dev].scl;
-    bus(dev)->PSEL.SDA = i2c_config[dev].sda;
+
     /* configure dev clock speed */
     bus(dev)->FREQUENCY = i2c_config[dev].speed;
 
     spi_twi_irq_register_i2c(bus(dev), i2c_isr_handler, (void *)dev);
 
-    /* re-enable the device. We expect that the device was being acquired before
+    /* configure pins */
+    /* also re-enables the device internally. We expect that the device was being acquired before
      * the i2c_init_master() function is called, so it should be enabled when
      * exiting this function. */
-    bus(dev)->ENABLE = TWIM_ENABLE_ENABLE_Enabled;
+    i2c_init_pins(dev);
 }
 
-#ifdef MODULE_PERIPH_I2C_RECONFIGURE
 void i2c_init_pins(i2c_t dev)
 {
     assert(dev < I2C_NUMOF);
@@ -136,9 +132,7 @@ void i2c_deinit_pins(i2c_t dev)
 
     mutex_lock(&locks[dev]);
     bus(dev)->ENABLE = TWIM_ENABLE_ENABLE_Disabled;
-
 }
-#endif
 
 int i2c_acquire(i2c_t dev)
 {
