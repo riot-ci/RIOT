3d64ee674e9137895fffbf7ce287875e9e5a33cc
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-12-18 12:49:55 +0100

    boards/remote-revb: add arduino feature

---
+++ boards/remote-revb/include/arduino_board.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2020 J. David Ibáñez <jdavid.ibp@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup    boards_remote-revb
+ * @{
+ *
+ * @file
+ * @brief       Configuration of the Arduino API for Remote revision B board
+ *
+ * You can use the defines in this file for simplified interaction with the
+ * Arduino specific pin numbers.
+ *
+ * @author      J. David Ibáñez <jdavid.ibp@gmail.com>
+ */
+
+#ifndef ARDUINO_BOARD_H
+#define ARDUINO_BOARD_H
+
+#include "arduino_pinmap.h"
+#include "periph/pwm.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The on-board LED is connected to pin 13 on this board
+ */
+#define ARDUINO_LED         (22)
+
+/**
+ * @brief   Look-up table for the Arduino's digital pins
+ */
+static const gpio_t arduino_pinmap[] = {
+    ARDUINO_PIN_0,
+    ARDUINO_PIN_1,
+    ARDUINO_PIN_2,
+    ARDUINO_PIN_3,
+    ARDUINO_PIN_4,
+    ARDUINO_PIN_5,
+    ARDUINO_PIN_6,
+    ARDUINO_PIN_7,
+    ARDUINO_PIN_8,
+    ARDUINO_PIN_9,
+    ARDUINO_PIN_10,
+    ARDUINO_PIN_11,
+    ARDUINO_PIN_12,
+    ARDUINO_PIN_13,
+    ARDUINO_PIN_14,
+    ARDUINO_PIN_15,
+    ARDUINO_PIN_16,
+    ARDUINO_PIN_17,
+    ARDUINO_PIN_18,
+    ARDUINO_PIN_19,
+    ARDUINO_PIN_20,
+    ARDUINO_PIN_21,
+    ARDUINO_PIN_22,
+    ARDUINO_PIN_23,
+    ARDUINO_PIN_24,
+};
+
+/**
+ * @brief   Look-up table for the Arduino's analog pins
+ */
+static const adc_t arduino_analog_map[] = {
+    ADC_UNDEF,
+    ARDUINO_A1,
+    ARDUINO_A2,
+    ARDUINO_A3,
+    ADC_UNDEF,
+    ARDUINO_A5,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARDUINO_BOARD_H */
+/** @} */
---
+++ boards/remote-revb/include/arduino_pinmap.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2020 J. David Ibáñez <jdavid.ibp@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_remote-revb
+ * @{
+ *
+ * @file
+ * @brief       Mapping from MCU pins to Arduino pins for Remote revision B board
+ *
+ * You can use the defines in this file for simplified interaction with the
+ * Arduino specific pin numbers.
+ *
+ * @author      J. David Ibáñez <jdavid.ibp@gmail.com>
+ */
+
+#ifndef ARDUINO_PINMAP_H
+#define ARDUINO_PINMAP_H
+
+#include "periph/gpio.h"
+#include "periph/adc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name    Mapping of MCU pins to Arduino pins
+ *
+ * @{
+ */
+
+#define ARDUINO_PIN_0           GPIO_PIN(PORT_A, 0) // UART0.RX
+#define ARDUINO_PIN_1           GPIO_PIN(PORT_A, 1) // UART0.TX
+#define ARDUINO_PIN_2           GPIO_PIN(PORT_D, 0) // I2C.INT
+#define ARDUINO_PIN_3           GPIO_PIN(PORT_C, 2) // I2C.SDA
+#define ARDUINO_PIN_4           GPIO_PIN(PORT_C, 3) // I2C.SDL
+#define ARDUINO_PIN_5           GPIO_PIN(PORT_B, 4) // CC1200.GPIO0
+#define ARDUINO_PIN_6           GPIO_PIN(PORT_B, 0) // CC1200.GPIO2
+#define ARDUINO_PIN_7           GPIO_PIN(PORT_C, 1) // UART1.RX
+#define ARDUINO_PIN_8           GPIO_PIN(PORT_C, 0) // UART1.TX
+#define ARDUINO_PIN_9           GPIO_PIN(PORT_B, 5) // CC1200.CSN
+#define ARDUINO_PIN_10          GPIO_PIN(PORT_A, 3) // USER.BUTTON
+#define ARDUINO_PIN_11          GPIO_PIN(PORT_B, 2) // CC1200.CLK
+#define ARDUINO_PIN_12          GPIO_PIN(PORT_B, 1) // CC1200.MOSI
+#define ARDUINO_PIN_13          GPIO_PIN(PORT_B, 3) // CC1200.MISO
+#define ARDUINO_PIN_14          GPIO_PIN(PORT_A, 7) // ADC5/AIN7
+#define ARDUINO_PIN_15          GPIO_PIN(PORT_A, 5) // ADC1/AIN5
+#define ARDUINO_PIN_16          GPIO_PIN(PORT_A, 4) // ADC2/AIN4
+#define ARDUINO_PIN_17          GPIO_PIN(PORT_A, 2) // ADC3/AIN2
+#define ARDUINO_PIN_18          GPIO_PIN(PORT_A, 6) // USD.SEL/ADC4
+#define ARDUINO_PIN_19          GPIO_PIN(PORT_C, 6) // USD.MISO
+#define ARDUINO_PIN_20          GPIO_PIN(PORT_C, 5) // USD.MOSI
+#define ARDUINO_PIN_21          GPIO_PIN(PORT_C, 4) // USD.SCLK
+#define ARDUINO_PIN_22          GPIO_PIN(PORT_D, 4) // LED1.R
+#define ARDUINO_PIN_23          GPIO_PIN(PORT_B, 7) // LED2.G/JTAG.TDO
+#define ARDUINO_PIN_24          GPIO_PIN(PORT_B, 6) // LED3.B/JTAG.TDI
+#define ARDUINO_PIN_A1          ARDUINO_PIN_15
+#define ARDUINO_PIN_A2          ARDUINO_PIN_16
+#define ARDUINO_PIN_A3          ARDUINO_PIN_17
+#define ARDUINO_PIN_A5          ARDUINO_PIN_14
+
+#define ARDUINO_A1              ADC_LINE(1)
+#define ARDUINO_A2              ADC_LINE(2)
+#define ARDUINO_A3              ADC_LINE(3)
+#define ARDUINO_A5              ADC_LINE(5)
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARDUINO_PINMAP_H */
+/** @} */
