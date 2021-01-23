649d64125427dea461ecc226d3170b89d91c41d7
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2021-01-23 10:09:59 +0100

    fixup! makefiles: allow boards to define default modules

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -7,3 +7,6 @@ DEFAULT_MODULE += periph_init
 
 # Include potentially added default modules by the board
 -include $(BOARDDIR)/Makefile.default
+
+# Include potentially added default modules by the CPU
+-include $(RIOTCPU)/$(CPU)/Makefile.default
