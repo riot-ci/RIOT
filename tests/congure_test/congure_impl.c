224346cbd3ac5f2b8612739cf2c6643a94311735
Martine Lenders <m.lenders@fu-berlin.de>
2021-02-12 11:04:00 +0100

    fixup! fixup! tests: initial import of tests for CongURE and its test framework

--- tests/congure_test/congure_impl.c
+++ tests/congure_test/congure_impl.c
@@ -13,113 +13,17 @@
  * @author  Martine Lenders <m.lenders@fu-berlin.de>
  */
 
+#include "congure/mock.h"
+
 #include "congure_impl.h"
-
-static void _snd_init(congure_snd_t *cong, void *ctx);
-static int32_t _snd_inter_msg_interval(congure_snd_t *cong, unsigned msg_size);
-static void _snd_report_msg_sent(congure_snd_t *cong, unsigned msg_size);
-static void _snd_report_msg_discarded(congure_snd_t *cong, unsigned msg_size);
-static void _snd_report_msgs_lost(congure_snd_t *cong, congure_snd_msg_t *msgs);
-static void _snd_report_msgs_timeout(congure_snd_t *cong,
-                                     congure_snd_msg_t *msgs);
-static void _snd_report_msg_acked(congure_snd_t *cong, congure_snd_msg_t *msg,
-                                  congure_snd_ack_t *ack);
-static void _snd_report_ecn_ce(congure_snd_t *cong, ztimer_now_t time);
-
-static const congure_snd_driver_t _driver = {
-    .init = _snd_init,
-    .inter_msg_interval = _snd_inter_msg_interval,
-    .report_msg_sent = _snd_report_msg_sent,
-    .report_msg_discarded = _snd_report_msg_discarded,
-    .report_msgs_timeout = _snd_report_msgs_timeout,
-    .report_msgs_lost = _snd_report_msgs_lost,
-    .report_msg_acked = _snd_report_msg_acked,
-    .report_ecn_ce = _snd_report_ecn_ce,
-};
 
 int congure_test_snd_setup(congure_test_snd_t *c, unsigned id)
 {
     if (id > 0) {
         return -1;
     }
-    c->super.driver = &_driver;
+    congure_mock_snd_setup(c);
     return 0;
-}
-
-static void _snd_init(congure_snd_t *cong, void *ctx)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->init_calls++;
-    c->init_args.c = &c->super;
-    c->init_args.ctx = ctx;
-}
-
-static int32_t _snd_inter_msg_interval(congure_snd_t *cong, unsigned msg_size)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->inter_msg_interval_calls++;
-    c->inter_msg_interval_args.c = &c->super;
-    c->inter_msg_interval_args.msg_size = msg_size;
-    return -1;
-}
-
-static void _snd_report_msg_sent(congure_snd_t *cong, unsigned msg_size)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_msg_sent_calls++;
-    c->report_msg_sent_args.c = &c->super;
-    c->report_msg_sent_args.msg_size = msg_size;
-}
-
-static void _snd_report_msg_discarded(congure_snd_t *cong, unsigned msg_size)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_msg_discarded_calls++;
-    c->report_msg_discarded_args.c = &c->super;
-    c->report_msg_discarded_args.msg_size = msg_size;
-}
-
-static void _snd_report_msgs_lost(congure_snd_t *cong, congure_snd_msg_t *msgs)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_msgs_lost_calls++;
-    c->report_msgs_lost_args.c = &c->super;
-    c->report_msgs_lost_args.msgs = msgs;
-}
-
-static void _snd_report_msgs_timeout(congure_snd_t *cong,
-                                     congure_snd_msg_t *msgs)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_msgs_timeout_calls++;
-    c->report_msgs_timeout_args.c = &c->super;
-    c->report_msgs_timeout_args.msgs = msgs;
-}
-
-static void _snd_report_msg_acked(congure_snd_t *cong, congure_snd_msg_t *msg,
-                                  congure_snd_ack_t *ack)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_msg_acked_calls++;
-    c->report_msg_acked_args.c = &c->super;
-    c->report_msg_acked_args.msg = msg;
-    c->report_msg_acked_args.ack = ack;
-}
-
-static void _snd_report_ecn_ce(congure_snd_t *cong, ztimer_now_t time)
-{
-    congure_test_snd_t *c = (congure_test_snd_t *)cong;
-
-    c->report_ecn_ce_calls++;
-    c->report_ecn_ce_args.c = &c->super;
-    c->report_ecn_ce_args.time = time;
 }
 
 /** @} */
--- tests/congure_test/congure_impl.h
+++ tests/congure_test/congure_impl.h
@@ -16,56 +16,13 @@
 #ifndef CONGURE_IMPL_H
 #define CONGURE_IMPL_H
 
-#include "congure.h"
+#include "congure/mock.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
-    congure_snd_t super;
-    uint8_t init_calls;
-    uint8_t inter_msg_interval_calls;
-    uint8_t report_msg_sent_calls;
-    uint8_t report_msg_discarded_calls;
-    uint8_t report_msgs_timeout_calls;
-    uint8_t report_msgs_lost_calls;
-    uint8_t report_msg_acked_calls;
-    uint8_t report_ecn_ce_calls;
-    struct {
-        congure_snd_t *c;
-        void *ctx;
-    } init_args;
-    struct {
-        congure_snd_t *c;
-        unsigned msg_size;
-    } inter_msg_interval_args;
-    struct {
-        congure_snd_t *c;
-        unsigned msg_size;
-    } report_msg_sent_args;
-    struct {
-        congure_snd_t *c;
-        unsigned msg_size;
-    } report_msg_discarded_args;
-    struct {
-        congure_snd_t *c;
-        congure_snd_msg_t *msgs;
-    } report_msgs_timeout_args;
-    struct {
-        congure_snd_t *c;
-        congure_snd_msg_t *msgs;
-    } report_msgs_lost_args;
-    struct {
-        congure_snd_t *c;
-        congure_snd_msg_t *msg;
-        congure_snd_ack_t *ack;
-    } report_msg_acked_args;
-    struct {
-        congure_snd_t *c;
-        ztimer_now_t time;
-    } report_ecn_ce_args;
-} congure_test_snd_t;
+typedef congure_mock_snd_t congure_test_snd_t;
 
 int congure_test_snd_setup(congure_test_snd_t *c, unsigned id);
 
