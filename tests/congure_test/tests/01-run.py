d2ab1776bad60961cda354fc617197de83966315
Martine Lenders <m.lenders@fu-berlin.de>
2021-02-13 12:26:47 +0100

    REMOVE ME! Spam some newlines on start then help

--- tests/congure_test/tests/01-run.py
+++ tests/congure_test/tests/01-run.py
@@ -26,6 +26,12 @@ class TestCongUREBase(unittest.TestCase):
         if cls.DEBUG:
             cls.ctrl.term.logfile = sys.stdout
         cls.ctrl.reset()
+        cls.ctrl.term.sendline('')
+        cls.ctrl.term.sendline('')
+        cls.ctrl.term.sendline('')
+        cls.ctrl.term.sendline('')
+        cls.ctrl.term.sendline('help')
+        cls.ctrl.term.sendline('')
         cls.shell = ShellInteraction(cls.ctrl)
         cls.json_parser = RapidJSONShellInteractionParser()
         cls.json_parser.set_parser_args(
