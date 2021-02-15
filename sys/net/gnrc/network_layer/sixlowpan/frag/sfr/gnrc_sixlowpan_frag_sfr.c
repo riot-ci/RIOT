a9f7f8d04f0312f64bf8e934b6ab9f265ec396bf
Martine Lenders <m.lenders@fu-berlin.de>
2021-02-15 22:20:27 +0100

    gnrc_sixlowpan_frag_sfr: make ARQ timer mockable

--- sys/net/gnrc/network_layer/sixlowpan/frag/sfr/gnrc_sixlowpan_frag_sfr.c
+++ sys/net/gnrc/network_layer/sixlowpan/frag/sfr/gnrc_sixlowpan_frag_sfr.c
@@ -475,6 +475,10 @@ void gnrc_sixlowpan_frag_sfr_arq_timeout(gnrc_sixlowpan_frag_fb_t *fbuf)
 
     DEBUG("6lo sfr: ARQ timeout for datagram %u\n", fbuf->tag);
     fbuf->sfr.arq_timeout_event.msg.content.ptr = NULL;
+    if (IS_ACTIVE(CONFIG_GNRC_SIXLOWPAN_SFR_MOCK_ARQ_TIMER)) {
+        /* time goes by ;-) */
+        now -= (fbuf->sfr.arq_timeout * US_PER_MS) + 1;
+    }
     /* copying clist_foreach because we can't work just in function context */
     if (frag_desc) {
         gnrc_sixlowpan_frag_sfr_congure_snd_report_frags_timeout(fbuf);
@@ -1572,18 +1576,20 @@ static void _sched_next_frame(gnrc_sixlowpan_frag_fb_t *fbuf)
 
 static void _sched_arq_timeout(gnrc_sixlowpan_frag_fb_t *fbuf, uint32_t offset)
 {
-    if (fbuf->sfr.arq_timeout_event.msg.content.ptr != NULL) {
-        DEBUG("6lo sfr: ARQ timeout for datagram %u already scheduled\n",
-              (uint8_t)fbuf->tag);
-        return;
+    if (!IS_ACTIVE(CONFIG_GNRC_SIXLOWPAN_SFR_MOCK_ARQ_TIMER)) {
+        if (fbuf->sfr.arq_timeout_event.msg.content.ptr != NULL) {
+            DEBUG("6lo sfr: ARQ timeout for datagram %u already scheduled\n",
+                  (uint8_t)fbuf->tag);
+            return;
+        }
+        DEBUG("6lo sfr: arming ACK timeout in %lums for datagram %u\n",
+              (long unsigned)offset, fbuf->tag);
+        fbuf->sfr.arq_timeout_event.event.offset = offset;
+        fbuf->sfr.arq_timeout_event.msg.content.ptr = fbuf;
+        fbuf->sfr.arq_timeout_event.msg.type = GNRC_SIXLOWPAN_FRAG_SFR_ARQ_TIMEOUT_MSG;
+        evtimer_add_msg(&_arq_timer, &fbuf->sfr.arq_timeout_event,
+                        _getpid());
     }
-    DEBUG("6lo sfr: arming ACK timeout in %lums for datagram %u\n",
-          (long unsigned)offset, fbuf->tag);
-    fbuf->sfr.arq_timeout_event.event.offset = offset;
-    fbuf->sfr.arq_timeout_event.msg.content.ptr = fbuf;
-    fbuf->sfr.arq_timeout_event.msg.type = GNRC_SIXLOWPAN_FRAG_SFR_ARQ_TIMEOUT_MSG;
-    evtimer_add_msg(&_arq_timer, &fbuf->sfr.arq_timeout_event,
-                    _getpid());
 }
 
 static void _sched_abort_timeout(gnrc_sixlowpan_frag_fb_t *fbuf)
