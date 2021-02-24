9e03bacb4a50fd260ceed1433ef3130e5ae17b20
Jan Mohr <jan.mohr@ml-pa.com>
2021-02-24 12:48:00 +0100

    tests/driver_lis2dh12: added test for new functionality

--- tests/driver_lis2dh12/main.c
+++ tests/driver_lis2dh12/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Freie Universität Berlin
+ * Copyright (C) 2020 ML!PA Consulting GmbH
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -11,171 +11,42 @@
  * @{
  *
  * @file
- * @brief       Test application for the LIS2DH12 accelerometer driver
+ * @brief       Test application for LIS2DH12 accelerometer driver
  *
- * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Jan Mohr <jan.mohr@ml-pa.com>
  *
  * @}
  */
 
-#include <assert.h>
-#include <stdio.h>
+#include "shell.h"
 
-#include "fmt.h"
-#include "xtimer.h"
-#include "mutex.h"
-#include "lis2dh12.h"
-#include "lis2dh12_params.h"
-#include "lis2dh12_registers.h"
+#include "thread.h"
 
- /* delay between sensor data reads */
-#define DELAY       (100UL * US_PER_MS)
+#include "include/lis2dh12_test.h"
 
-/* allocate some memory to hold one formatted string for each sensor output, so
- * one string for the X, Y, and Z reading, respectively */
-static char str_out[3][8];
+kernel_pid_t lis2dh12_process;
 
-/* allocate device descriptor */
-static lis2dh12_t dev;
-
-#ifdef MODULE_LIS2DH12_INT
-/* control interrupt */
-typedef struct {
-    uint8_t line;
-    mutex_t *lock;
-    uint8_t *flags;
-} lis_ctx;
-
-/* timer lock */
-static uint8_t isr_flags;
-static mutex_t isr_mtx = MUTEX_INIT_LOCKED;
-static lis_ctx ctx[2] = {
-    {
-        .line  = 1,
-        .lock  = &isr_mtx,
-        .flags = &isr_flags,
-    }, {
-        .line  = 2,
-        .lock  = &isr_mtx,
-        .flags = &isr_flags,
-    }
+static const shell_command_t shell_commands[] = {
+                { "lis", "Command with multiple subcommands.", shell_lis2dh12_cmd },
+                { NULL, NULL, NULL },
 };
 
-/* interrupt callback function. */
-static void lis2dh12_int_cb(void* _ctx) {
-    lis_ctx *control = _ctx;
-
-    *control->flags |= control->line;
-
-    mutex_unlock(control->lock);
-}
-
-/* print interrupt register */
-static void lis2dh12_int_reg_content(lis2dh12_t *dev, uint8_t pin){
-
-    assert(pin == LIS2DH12_INT1 || pin == LIS2DH12_INT2);
-
-    uint8_t buffer;
-    lis2dh12_read_int_src(dev, &buffer, pin);
-
-    printf("content SRC_Reg_%d: 0x%02x\n", pin, buffer);
-    printf("\t XL %d\n", !!(buffer & LIS2DH12_INT_SRC_XL));
-    printf("\t XH %d\n", !!(buffer & LIS2DH12_INT_SRC_XH));
-    printf("\t YL %d\n", !!(buffer & LIS2DH12_INT_SRC_YL));
-    printf("\t YH %d\n", !!(buffer & LIS2DH12_INT_SRC_YH));
-    printf("\t ZL %d\n", !!(buffer & LIS2DH12_INT_SRC_ZL));
-    printf("\t ZH %d\n", !!(buffer & LIS2DH12_INT_SRC_ZH));
-    printf("\t IA %d\n", !!(buffer & LIS2DH12_INT_SRC_IA));
-}
-#endif
+char lis2dh12_process_stack[THREAD_STACKSIZE_MAIN];
 
 int main(void)
 {
+    /* processing lis2dh12 acceleration data */
+    lis2dh12_process = thread_create(lis2dh12_process_stack, sizeof(lis2dh12_process_stack),
+                  THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
+                  lis2dh12_test_process, NULL, "lis2dh12_process");
 
-#ifdef MODULE_LIS2DH12_INT
-    uint8_t flags = 0;
-#endif
+    /* init lis */
+    lis2dh12_test_init();
 
-    puts("LIS2DH12 accelerometer driver test application\n");
+    /* running shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-    puts("Initializing LIS2DH12 sensor... ");
-    if (lis2dh12_init(&dev, &lis2dh12_params[0]) == LIS2DH12_OK) {
-        puts("[OK]");
-    }
-    else {
-        puts("[Failed]\n");
-        return 1;
-    }
-
-#ifdef MODULE_LIS2DH12_INT
-    /* enable interrupt Pins */
-    if (gpio_is_valid(lis2dh12_params[0].int1_pin)) {
-        /* create and set the interrupt params */
-        lis2dh12_int_params_t params_int1 = {
-            .int_type = LIS2DH12_INT_TYPE_I1_IA1,
-            .int_config = LIS2DH12_INT_CFG_XLIE,
-            .int_threshold = 31,
-            .int_duration = 1,
-            .cb = lis2dh12_int_cb,
-            .arg = &ctx[0],
-        };
-        lis2dh12_set_int(&dev, &params_int1, LIS2DH12_INT1);
-    }
-
-    /* create and set the interrupt params */
-    if (gpio_is_valid(lis2dh12_params[0].int2_pin)) {
-        lis2dh12_int_params_t params_int2 = {
-            .int_type = LIS2DH12_INT_TYPE_I2_IA2,
-            .int_config = LIS2DH12_INT_CFG_YLIE,
-            .int_threshold = 31,
-            .int_duration = 1,
-            .cb = lis2dh12_int_cb,
-            .arg = &ctx[1],
-        };
-        lis2dh12_set_int(&dev, &params_int2, LIS2DH12_INT2);
-    }
-#endif
-
-    while (1) {
-
-#ifdef MODULE_LIS2DH12_INT
-        if (xtimer_mutex_lock_timeout(&isr_mtx, DELAY) == 0) {
-            flags = isr_flags;
-            isr_flags = 0;
-        }
-
-        /* check interrupt 1 and read register */
-        if (flags & 0x1) {
-            printf("reads interrupt %d\n", LIS2DH12_INT1);
-            lis2dh12_int_reg_content(&dev, LIS2DH12_INT1);
-            flags &= ~(0x1);
-        }
-        /* check interrupt 2 and read register */
-        if (flags & 0x2) {
-            printf("reads interrupt %d\n", LIS2DH12_INT2);
-            lis2dh12_int_reg_content(&dev, LIS2DH12_INT2);
-            flags &= ~(0x2);
-        }
-#else
-        xtimer_usleep(DELAY);
-#endif
-
-        /* read sensor data */
-        int16_t data[3];
-        if (lis2dh12_read(&dev, data) != LIS2DH12_OK) {
-            puts("error: unable to retrieve data from sensor");
-            continue;
-        }
-
-        /* format data */
-        for (int i = 0; i < 3; i++) {
-            size_t len = fmt_s16_dfp(str_out[i], data[i], -3);
-            str_out[i][len] = '\0';
-        }
-
-        /* print data to STDIO */
-        printf("X: %8s Y: %8s Z: %8s\n", str_out[0], str_out[1], str_out[2]);
-    }
-
+    printf("THIS SHOULD NEVER BE REACHED!\n");
     return 0;
 }
