dfeb94a9ca6152506fded1af7fbc3a73fecf6019
Ollrogge <nils-ollrogge@outlook.de>
2021-04-14 22:18:59 +0200

    sys/crypto: Enable support for AES-192, AES-256

--- tests/sys_crypto/tests-crypto-cipher.c
+++ tests/sys_crypto/tests-crypto-cipher.c
@@ -65,32 +65,13 @@ static void test_crypto_cipher_init_aes_key_length(void)
     cipher_t cipher;
     int err;
 
-    /* A keylength of 192 bit is not supported by the current implementation */
-    uint8_t unsupported_key_1[24];
-
-    memset(unsupported_key_1, 0, sizeof(unsupported_key_1));
-
-    /* A keylength of 256 bit is not supported by the current implementation */
-    uint8_t unsupported_key_2[32];
-    memset(unsupported_key_2, 0, sizeof(unsupported_key_2));
-
     /* A keylength of 64 bit is not supported by AES */
-    uint8_t unsupported_key_3[8];
-    memset(unsupported_key_3, 0, sizeof(unsupported_key_3));
+    uint8_t unsupported_key_1[8];
+    memset(unsupported_key_1, 0, sizeof(unsupported_key_1));
 
     err =
         cipher_init(&cipher, CIPHER_AES_128, unsupported_key_1,
                     sizeof(unsupported_key_1));
-    TEST_ASSERT_EQUAL_INT(CIPHER_ERR_INVALID_KEY_SIZE, err);
-
-    err =
-        cipher_init(&cipher, CIPHER_AES_128, unsupported_key_2,
-                    sizeof(unsupported_key_2));
-    TEST_ASSERT_EQUAL_INT(CIPHER_ERR_INVALID_KEY_SIZE, err);
-
-    err =
-        cipher_init(&cipher, CIPHER_AES_128, unsupported_key_3,
-                    sizeof(unsupported_key_3));
     TEST_ASSERT_EQUAL_INT(CIPHER_ERR_INVALID_KEY_SIZE, err);
 }
 
