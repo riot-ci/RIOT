db1533d55db95f4f494e3631fb1dd787213fef4a
Martine Lenders <m.lenders@fu-berlin.de>
2021-04-14 15:01:21 +0200

    squash! tests/periph_rtt_min: increase pexpect timeout
    tests/periph_rtt_min: adapt for slower RTTs

--- tests/periph_rtt_min/tests/01-run.py
+++ tests/periph_rtt_min/tests/01-run.py
@@ -15,11 +15,12 @@ def testfunc(child):
     child.expect(r"Evaluate RTT_MIN_OFFSET over (\d+) samples")
 
     exp_samples = int(child.match.group(1))
-    child.expect(
-        r'RTT_MIN_OFFSET for [a-zA-Z\-\_0-9]+ over {samples} samples: \d+'
-        .format(samples=exp_samples)
-    )
+    test_end = r'RTT_MIN_OFFSET for [a-zA-Z\-\_0-9]+ over {samples} ' \
+               r'samples: \d+'.format(samples=exp_samples)
+    test_ongoing = r'Sample \d+'
+    while child.expect([test_end, test_ongoing]):
+        pass
 
 
 if __name__ == "__main__":
-    sys.exit(run(testfunc, timeout=30))
+    sys.exit(run(testfunc))
