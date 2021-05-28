4b556b71bd479cd90d27b880bce061fe358a910f
Francisco Molina <femolina@uc.cl>
2021-05-28 15:25:53 +0200

    fixup! fixup! pkg/nimble: use mynewt-core pkg

--- pkg/mynewt-core/include/syscfg/syscfg.h
+++ pkg/mynewt-core/include/syscfg/syscfg.h
@@ -1,9 +1,29 @@
-/*
- * Copyright (C) 2020 Inria
+/**
+ * Apache Mynewt
+ * Copyright 2015-2021 The Apache Software Foundation
  *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * This product includes software developed at
+ * The Apache Software Foundation (http://www.apache.org/).
+ *
+ * Portions of this software were developed at
+ * Runtime Inc, copyright 2015.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 /**
@@ -12,8 +32,7 @@
  *
  * @file
  * @brief       mynewt-core system configurations
-  *
- * @author      Francisco Molina <francois-xavier.molina@inria.fr>
+ *
  * @}
  */
 
@@ -26,6 +45,10 @@
  * @name    MyNewt header inclusion macro definitions
  * @{
  *
+ * PLEASE NOTE: Following macro definitions where copied directly from
+ *              apache/mynewt-core and are under the copyright specified in
+ *              the header.
+ *
  * This macro exists to ensure code includes this header when needed.  If code
  * checks the existence of a setting directly via ifdef without including this
  * header, the setting macro will silently evaluate to 0.  In contrast, an
@@ -36,9 +59,25 @@
 #define MYNEWT_VAL_CHOICE(_name, _val)          MYNEWT_VAL_ ## _name ## __ ## _val
 /** @} */
 
+/**
+ * @brief TIMER 5 (RTC_DEV0) will be mynewt-core OS_CPUTIME timer
+ */
+#ifndef MYNEWT_VAL_OS_CPUTIME_TIMER_NUM
+#define MYNEWT_VAL_OS_CPUTIME_TIMER_NUM (5)
+#endif
+
+/**
+ * @brief Enable TIMER 5 (RTC_DEV0)
+ */
+#ifndef MYNEWT_VAL_TIMER_5
+#define MYNEWT_VAL_TIMER_5 (1)
+#endif
+
 #if IS_USED(MODULE_NIMBLE)
 /*** @mynewt-nimble */
-#include "syscfg_nimble.h"
+#undef MYNEWT_VAL
+#undef MYNEWT_VAL_CHOICE
+#include "npl_syscfg.h"
 #endif
 
 #if IS_USED(MODULE_UWB_CORE)
@@ -70,20 +109,6 @@
 #if IS_USED(MODULE_UWB_DW1000)
 /*** @decawave-uwb-dw1000/hw/drivers/uwb/uwb_dw1000 */
 #include "syscfg_uwb_dw1000.h"
-#endif
-
-/**
- * @brief TIMER 5 (RTC_DEV0) will be mynewt-core OS_CPUTIME timer
- */
-#ifndef MYNEWT_VAL_OS_CPUTIME_TIMER_NUM
-#define MYNEWT_VAL_OS_CPUTIME_TIMER_NUM (5)
-#endif
-
-/**
- * @brief Enable TIMER 5 (RTC_DEV0)
- */
-#ifndef MYNEWT_VAL_TIMER_5
-#define MYNEWT_VAL_TIMER_5 (1)
 #endif
 
 #ifdef __cplusplus
