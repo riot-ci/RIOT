38f6d04de4d6e9c99b4d9955f75040af9878d908
Francisco Molina <femolina@uc.cl>
2021-06-02 09:30:43 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! pkg/edhoc-c: initial commit

--- pkg/edhoc-c/include/edhoc_config.h
+++ pkg/edhoc-c/include/edhoc_config.h
@@ -21,8 +21,12 @@
 #ifndef EDHOC_CONFIG_H
 #define EDHOC_CONFIG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
- * @def EDHOC_CIPHER_SUITE_X_ENABLED
+ * @brief EDHOC_CIPHER_SUITE_X_ENABLED
  *
  * Enables support for a specific EDHOC cipher suite
  *
@@ -30,7 +34,8 @@
 #define EDHOC_CIPHER_SUITE_0_ENABLED
 
 /**
- * @def EDHOC_AUTH_METHOD_X_ENABLED
+ * @brief EDHOC_AUTH_METHOD_X_ENABLED
+ * @{
  *
  * Enables support for a specific EDHOC authentication method
  *
@@ -39,9 +44,10 @@
 #define EDHOC_AUTH_METHOD_1_ENABLED
 #define EDHOC_AUTH_METHOD_2_ENABLED
 #define EDHOC_AUTH_METHOD_3_ENABLED
+/** @} */
 
 /**
- * @def EDHOC_ASYNC_API_ENABLED
+ * @brief EDHOC_ASYNC_API_ENABLED
  *
  * Exposes the individual EDHOC message functions for asynchronous usage.
  *
@@ -49,7 +55,7 @@
 #define EDHOC_ASYNC_API_ENABLED
 
 /**
- * @def EDHOC_DEBUG_ENABLE
+ * @brief EDHOC_DEBUG_ENABLE
  *
  * Enables some extra methods that allow for easier testing and debugging
  *
@@ -57,7 +63,8 @@
 #define EDHOC_DEBUG_ENABLED
 
 /**
- * @def EDHOC_AUTH_CERT_ENABLED
+ * @brief EDHOC_AUTH_CERT_ENABLED
+ * @{
  *
  * Enables CBOR certificates as the EDHOC local credential
  *
@@ -66,9 +73,10 @@
 #if defined(EDHOC_AUTH_CERT_ENABLED)
 #define EDHOC_AUTH_CBOR_CERT
 #endif
+/** @} */
 
 /**
- * @def EDHOC_AUTH_RPK_ENABLED
+ * @brief EDHOC_AUTH_RPK_ENABLED
  *
  * Enables COSE raw public keys as the EDHOC local credential
  *
@@ -76,14 +84,14 @@
 #define EDHOC_AUTH_RPK_ENABLED
 
 /**
- * @def EDHOC_COSE_HEADER_SIZE
+ * @brief EDHOC_COSE_HEADER_SIZE
  *
  * Sets the maximum number of COSE header elements
  */
 #define EDHOC_COSE_HEADER_SIZE          (5)
 
 /**
- * @def EDHOC_CREDENTIAL_MAX_SIZE
+ * @brief EDHOC_CREDENTIAL_MAX_SIZE
  *
  * Sets the maximum buffer size for credentials (raw keys or certificates)
  *
@@ -91,7 +99,7 @@
 #define EDHOC_CRED_SIZE           (256)
 
 /**
- * @def EDHOC_CREDENTIAL_ID_MAX_SIZE
+ * @brief EDHOC_CREDENTIAL_ID_MAX_SIZE
  *
  * Sets the maximum buffer size for credential identifiers
  *
@@ -99,11 +107,15 @@
 #define EDHOC_CRED_ID_SIZE        (256)
 
 /**
- * @def EDHOC_ADD_DATA_MAX_SIZE
+ * @brief EDHOC_ADD_DATA_MAX_SIZE
  *
  * Maximum number of additional data bytes to piggy-back on the EDHOC exchange
  *
  */
 #define EDHOC_ADDITIONAL_DATA_SIZE             (64)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* EDHOC_CONFIG_H */
