c3d0b1394adc89ab937a8782514decad1fe04658
János Brodbeck <janos.brodbeck@posteo.de>
2021-06-04 18:59:25 +0200

    fixup! fixup! fixup! net/dtls/dsm: add DTLS session management module

--- sys/net/dsm/dsm.c
+++ sys/net/dsm/dsm.c
@@ -75,7 +75,7 @@ dsm_state_t dsm_store(sock_dtls_t *sock, sock_dtls_session_t *session,
             DEBUG("dsm: existing session found, restoring\n")
             memcpy(session, &session_slot->session, sizeof(sock_dtls_session_t));
         }
-        session_slot->last_used_sec = (uint32_t) (xtimer_now_usec64() / US_PER_SEC);
+        session_slot->last_used_sec = (uint32_t)(xtimer_now_usec64() / US_PER_SEC);
     } else {
         DEBUG("dsm: no space for session to store\n")
     }
