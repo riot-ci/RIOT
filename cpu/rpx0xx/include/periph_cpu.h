859c2ca59aba4377c0baacc475d53e3d2de9e7a5
nickw96 <nick.weiler@st.ovgu.de>
2021-06-22 18:41:02 +0200

    cpu/rpx0xx: implement periph_uart
    Co-authored-by: nickw96 <nick.weiler@st.ovgu.de>
    Co-authored-by: MaestroOnICe <justus.krebs@st.ovgu.de>
    Co-authored-by: Franz2000 <franz.freitag@st.ovgu.de>

--- cpu/rpx0xx/include/periph_cpu.h
+++ cpu/rpx0xx/include/periph_cpu.h
@@ -262,6 +262,19 @@ enum {
 };
 
 /**
+ * @brief   Overwrite the default gpio_t type definition
+ * @{
+ */
+#define HAVE_GPIO_T
+typedef uint32_t gpio_t;
+/** @} */
+
+/**
+ * @brief   Definition of a fitting UNDEF value
+ */
+#define GPIO_UNDEF          UINT32_MAX
+
+/**
  * @brief   Override GPIO active flank values
  * @{
  */
@@ -290,6 +303,13 @@ typedef struct {
     uint32_t irq_override           : 2;    /**< interrupt inversion override */
     uint32_t                        : 2;
 } gpio_io_ctrl_t;
+
+typedef struct {
+    UART0_Type *dev;
+    gpio_t rx_pin;
+    gpio_t tx_pin;
+    IRQn_Type irqn;
+} uart_conf_t;
 
 /**
  * @brief   Get the PAD control register for the given GPIO pin as word
