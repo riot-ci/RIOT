2e4b77d2c1276bd756d2c6804eb32d66357d61cf
Akshai M <akshai.m@fu-berlin.de>
2021-07-02 11:28:56 +0200

    fixup! boards/nucleo-wl55jc: Update board.h

--- boards/nucleo-wl55jc/include/periph_conf.h
+++ boards/nucleo-wl55jc/include/periph_conf.h
@@ -84,6 +84,11 @@ static const uart_conf_t uart_config[] = {
  */
 static const spi_conf_t spi_config[] = {
     {
+        .dev      = SUBGHZSPI, /* Internally connected to Sub-GHz radio Modem  */
+        .rccmask  = RCC_APB3ENR_SUBGHZSPIEN,
+        .apbbus   = APB3,
+    },
+    {
         .dev      = SPI1,
         .mosi_pin = GPIO_PIN(PORT_A, 7),
         .miso_pin = GPIO_PIN(PORT_A, 6),
@@ -95,12 +100,8 @@ static const spi_conf_t spi_config[] = {
         .cs_af    = GPIO_AF5,
         .rccmask  = RCC_APB2ENR_SPI1EN,
         .apbbus   = APB2,
-    },
-    {
-        .dev      = SUBGHZSPI, /* Internally connected to Sub-GHz radio Modem  */
-        .rccmask  = RCC_APB3ENR_SUBGHZSPIEN,
-        .apbbus   = APB3,
     }
+
 };
 
 #define SPI_NUMOF           ARRAY_SIZE(spi_config)
